1222e231022f090d7bb2c930c14f53fbd3436e2f
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2016-07-28 13:10:15 +0700

    cpu: conv: implement fwd ref conv

--- src/cpu/reference_convolution.cpp
+++ src/cpu/reference_convolution.cpp
@@ -2,6 +2,7 @@
 
 #include "c_types_map.hpp"
 #include "reference_convolution.hpp"
+#include "type_helpers.hpp"
 
 #define CHECK(f) do { \
     status_t status = f; \
@@ -16,8 +17,76 @@ using namespace mkl_dnn::impl::alg_kind;
 using namespace mkl_dnn::impl::memory_format;
 using namespace mkl_dnn::impl::primitive_kind;
 
+typedef float real_t;
+
 status_t reference_convolution::execute_forward() {
-    printf("conv does forward :)\n");
+    auto obtain_ptr = [this](uint32_t idx) {
+        const size_t oi = this->input()[idx].output_index;
+        return reinterpret_cast<const real_t*>(
+                this->input()[idx].primitive->output()[oi]->memory_const());
+    };
+    const real_t *src = obtain_ptr(0);
+    const real_t *weights = obtain_ptr(1);
+    const real_t *bias = obtain_ptr(2);
+    real_t *dst = reinterpret_cast<real_t*>(this->output()[0]->memory());
+
+    const types::memory_desc_wrapper
+        src_d(this->_cpd.input_primitive_desc.memory_desc),
+        weights_d(this->_cpd.weights_primitive_desc.memory_desc),
+        bias_d(this->_cpd.bias_primitive_desc.memory_desc),
+        dst_d(this->_cpd.output_primitive_desc.memory_desc);
+
+    const uint32_t MB = src_d.dims()[0];
+    const uint32_t IC = src_d.dims()[1];
+    const uint32_t OC = dst_d.dims()[1];
+    const uint32_t IH = src_d.dims()[2];
+    const uint32_t IW = src_d.dims()[3];
+    const uint32_t OH = dst_d.dims()[2];
+    const uint32_t OW = dst_d.dims()[3];
+    const uint32_t KH = weights_d.dims()[2];
+    const uint32_t KW = weights_d.dims()[3];
+
+    const uint32_t KSH = this->_cpd.convolution_desc.strides[0];
+    const uint32_t KSW = this->_cpd.convolution_desc.strides[1];
+
+    const uint32_t padH = this->_cpd.convolution_desc.padding[0];
+    const uint32_t padW = this->_cpd.convolution_desc.padding[1];
+
+    auto ker = [=](real_t *d, uint32_t mb, uint32_t oc, uint32_t oh,
+            uint32_t ow)
+    {
+        for (uint32_t ic = 0; ic < IC; ++ic) {
+            for (uint32_t kh = 0; kh < KH; ++kh) {
+                for (uint32_t kw = 0; kw < KW; ++kw) {
+                    if (oh*KSH + kh < padH) continue;
+                    if (ow*KSW + kw < padW) continue;
+
+                    if (oh*KSH + kh >= IH + padH) continue;
+                    if (oh*KSW + kw >= IW + padW) continue;
+
+                    const uint32_t ih = oh * KSH - padH + kh;
+                    const uint32_t iw = ow * KSW - padW + kw;
+
+                    *d += src[src_d.off(mb, ic, ih, iw)] *
+                        weights[weights_d.off(oc, ic, kh, kw)];
+                }
+            }
+        }
+    };
+
+#   pragma omp parallel for collapse(4)
+    for (uint32_t mb = 0; mb < MB; ++mb) {
+        for (uint32_t oc = 0; oc < OC; ++oc) {
+            for (uint32_t oh = 0; oh < OH; ++oh) {
+                for (uint32_t ow = 0; ow < OW; ++ow) {
+                    real_t *d = &dst[dst_d.off(mb, oc, oh, ow)];
+                    *d = bias[bias_d.off(oc)];
+                    ker(d, mb, oc, oh, ow);
+                }
+            }
+        }
+    }
+
     return success;
 }
 
