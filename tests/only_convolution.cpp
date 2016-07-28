408e56245a8206778c1cd4b3c778f6d8b3b11ea7
Roman Dubtsov <roman.s.dubtsov@intel.com>
2016-07-28 08:24:26 +0300

    c++ api: update conv example so that it works :)

--- tests/only_convolution.cpp
+++ tests/only_convolution.cpp
@@ -23,18 +23,39 @@ int doit() {
 
     // TODO: make tensor desc optional and default to N C X1 .. XN
 
-    auto c3_input_tensor_desc = tensor::desc(1, 1, 2, {256, 256, 13, 13});
-    auto c3_input_memory_desc = memory::desc({1, 1, 2, {256, 256, 13, 13}}, memory::format::nchw_f32);
-    auto c3_input_primitive_desc = memory::primitive_desc({{1, 1, 2, {256, 256, 13, 13}}, memory::format::nchw_f32}, cpu_engine);
+    // XXX: descs for memory should be not necessary!
 
+    auto c3_input_desc = memory::desc({1, 1, 2, {256, 256, 13, 13}}, memory::format::nchw_f32);
+    auto c3_input = memory({c3_input_desc, cpu_engine});
+    auto c3_weights_desc = memory::desc({0, 2, 2, {384, 256, 3, 3}}, memory::format::oihw_f32);
+    auto c3_weights = memory({c3_weights_desc, cpu_engine});
+    auto c3_bias_desc = memory::desc({0, 0, 1, {384}}, memory::format::n_f32);
+    auto c3_bias = memory({c3_bias_desc, cpu_engine});
+    auto c3_output_desc = memory::desc({1, 1, 2, {256, 384, 13, 13}}, memory::format::nchw_f32);
+    auto c3_output = memory({c3_output_desc, cpu_engine});
+
+#if 0
     auto c3_input = memory({{{1, 1, 2, {256, 256, 13, 13}}, memory::format::nchw_f32}, cpu_engine});
     auto c3_weights = memory({{{0, 2, 2, {384, 256, 3, 3}}, memory::format::oihw_f32}, cpu_engine});
     auto c3_bias = memory({{{0, 0, 1, {384}}, memory::format::n_f32}, cpu_engine});
     auto c3_output = memory({{{1, 1, 2, {256, 384, 13, 13}}, memory::format::nchw_f32}, cpu_engine});
 
-    // auto c3 = convolution::create({propagation::forward, algorithm::direct, {0, 0, 1, 1}, {0, 0, 1, 1}, padding::zero}, {input, weights, bias}, {output});
+    auto c3_desc = convolution::desc(prop_kind::forward, convolution::direct,
+            c3_input, c3_weights, c3_bias, c3_output,
+            {0, 0, 1, 1}, {0, 0, 1, 1}, padding_kind::zero);
+    auto c3_primitive_desc = convolution::primitive_desc(c3_desc, cpu_engine);
+    auto c3 = convolution(c3_primitive_desc,
+            c3_input, c3_weights, c3_bias, c3_output);
+#endif
 
-    // stream::create().submit({c3}).wait();
+    auto c3_desc = convolution::desc(prop_kind::forward, convolution::direct,
+            c3_input_desc, c3_weights_desc, c3_bias_desc, c3_output_desc,
+            {0, 0, 1, 1}, {0, 0, 1, 1}, padding_kind::zero);
+    auto c3_primitive_desc = convolution::primitive_desc(c3_desc, cpu_engine);
+    auto c3 = convolution(c3_primitive_desc,
+            c3_input, c3_weights, c3_bias, c3_output);
+
+    stream().submit({c3}).wait();
 
     return 0;
 }
