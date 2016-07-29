a27e093647a1929b106e6c44b13c3f399a076d5f
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2016-07-30 05:25:40 +0700

    cpu: ref conv: declare only neccesary things

--- src/cpu/reference_convolution.cpp
+++ src/cpu/reference_convolution.cpp
@@ -37,24 +37,23 @@ status_t reference_convolution<prec>::execute_forward() {
         dst_d(this->_cpd.output_primitive_desc.memory_desc);
 
     const uint32_t MB = src_d.dims()[0];
-    const uint32_t IC = src_d.dims()[1];
     const uint32_t OC = dst_d.dims()[1];
-    const uint32_t IH = src_d.dims()[2];
-    const uint32_t IW = src_d.dims()[3];
     const uint32_t OH = dst_d.dims()[2];
     const uint32_t OW = dst_d.dims()[3];
-    const uint32_t KH = weights_d.dims()[2];
-    const uint32_t KW = weights_d.dims()[3];
-
-    const uint32_t KSH = this->_cpd.convolution_desc.strides[0];
-    const uint32_t KSW = this->_cpd.convolution_desc.strides[1];
-
-    const uint32_t padH = this->_cpd.convolution_desc.padding[0];
-    const uint32_t padW = this->_cpd.convolution_desc.padding[1];
 
     auto ker = [=](data_t *d, uint32_t mb, uint32_t oc, uint32_t oh,
             uint32_t ow)
     {
+        const uint32_t IC = src_d.dims()[1];
+        const uint32_t IH = src_d.dims()[2];
+        const uint32_t IW = src_d.dims()[3];
+        const uint32_t KH = weights_d.dims()[2];
+        const uint32_t KW = weights_d.dims()[3];
+        const uint32_t KSH = this->_cpd.convolution_desc.strides[0];
+        const uint32_t KSW = this->_cpd.convolution_desc.strides[1];
+        const uint32_t padH = this->_cpd.convolution_desc.padding[0];
+        const uint32_t padW = this->_cpd.convolution_desc.padding[1];
+
         for (uint32_t ic = 0; ic < IC; ++ic) {
             for (uint32_t kh = 0; kh < KH; ++kh) {
                 for (uint32_t kw = 0; kw < KW; ++kw) {
