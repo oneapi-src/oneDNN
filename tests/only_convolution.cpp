63d19e7cccb055c6070736123316b66bc133ea56
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2016-08-04 10:41:44 +0700

    tests: conv: aling c++ test with c one

--- tests/only_convolution.cpp
+++ tests/only_convolution.cpp
@@ -9,6 +9,13 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-but-set-variable"
 
+uint32_t tensor_volume(const mkl_dnn::tensor::dims &t)
+{
+    uint32_t x = 1;
+    for (uint32_t i = 0; i < t.size(); ++i) x *= t[i];
+    return x;
+}
+
 int doit(bool lazy) {
     using namespace mkl_dnn;
 
@@ -25,29 +32,38 @@ int doit(bool lazy) {
 
     // XXX: descs for memory should be not necessary!
 
-    auto c3_src_desc = memory::desc({1, 1, 2, {2, 256, 13, 13}}, memory::precision::f32, memory::format::nchw);
-    auto c3_weights_desc = memory::desc({0, 2, 2, {384, 256, 3, 3}}, memory::precision::f32, memory::format::oihw);
-    auto c3_bias_desc = memory::desc({0, 0, 1, {384}}, memory::precision::f32, memory::format::n);
-    auto c3_dst_desc = memory::desc({1, 1, 2, {2, 384, 13, 13}}, memory::precision::f32, memory::format::nchw);
+    const uint32_t mb = 2;
+    const uint32_t groups = 2;
+    tensor::dims input_tz = {mb, 256, 13, 13};
+    tensor::dims weights_tz = {groups, 384/groups, 256/groups, 3, 3};
+    tensor::dims bias_tz = {384};
+    tensor::dims strides = {1, 1};
+    tensor::nd_offset padding = {0, 0};
+    tensor::dims output_tz = {mb, 384,
+        (input_tz[2] + 2*padding[0] - weights_tz[3])/strides[0] + 1,
+        (input_tz[3] + 2*padding[1] - weights_tz[4])/strides[1] + 1,
+    };
 
-    auto c3_src = memory({c3_src_desc, cpu_engine});
-    auto c3_weights = memory({c3_weights_desc, cpu_engine});
+    /* prepare actual data */
+    float *input = new float[tensor_volume(input_tz)];
+    for (size_t i = 0; i < tensor_volume(input_tz); ++i) input[i] = 0;
+    float *weights = new float[tensor_volume(weights_tz)];
+    for (size_t i = 0; i < tensor_volume(weights_tz); ++i) weights[i] = 0;
+    float *bias = new float[tensor_volume(bias_tz)];
+    for (size_t i = 0; i < tensor_volume(bias_tz); ++i) bias[i] = i;
+    float *output = new float[tensor_volume(output_tz)];
+    for (size_t i = 0; i < tensor_volume(output_tz); ++i) output[i] = 0;
+
+    /* mkl-dnn starts here */
+    auto c3_src_desc = memory::desc({1, 1, 2, input_tz}, memory::precision::f32, memory::format::nchw);
+    auto c3_weights_desc = memory::desc({1, 2, 2, weights_tz}, memory::precision::f32, memory::format::goihw);
+    auto c3_bias_desc = memory::desc({0, 0, 1, bias_tz}, memory::precision::f32, memory::format::n);
+    auto c3_dst_desc = memory::desc({1, 1, 2, output_tz}, memory::precision::f32, memory::format::nchw);
+
+    auto c3_src = memory({c3_src_desc, cpu_engine}, input);
+    auto c3_weights = memory({c3_weights_desc, cpu_engine}, weights);
     auto c3_bias = memory({c3_bias_desc, cpu_engine});
     auto c3_dst = memory({c3_dst_desc, cpu_engine});
-
-#if 0
-    auto c3_src = memory({{{1, 1, 2, {2, 256, 13, 13}}, memory::format::nchw}, cpu_engine});
-    auto c3_weights = memory({{{0, 2, 2, {384, 256, 3, 3}}, memory::format::oihw}, cpu_engine});
-    auto c3_bias = memory({{{0, 0, 1, {384}}, memory::format::n}, cpu_engine});
-    auto c3_dst = memory({{{1, 1, 2, {2, 384, 13, 13}}, memory::format::nchw}, cpu_engine});
-
-    auto c3_desc = convolution::desc(prop_kind::forward, convolution::direct,
-            c3_src, c3_weights, c3_bias, c3_dst,
-            {0, 0, 1, 1}, {0, 0, 1, 1}, padding_kind::zero);
-    auto c3_primitive_desc = convolution::primitive_desc(c3_desc, cpu_engine);
-    auto c3 = convolution(c3_primitive_desc,
-            c3_src, c3_weights, c3_bias, c3_dst);
-#endif
 
 #if 0
     auto c3_desc = convolution::desc(prop_kind::forward, convolution::direct,
@@ -59,7 +75,7 @@ int doit(bool lazy) {
 #else
     auto c3 = convolution(prop_kind::forward, convolution::direct,
             c3_src, c3_weights, c3_bias, c3_dst,
-            {1, 1}, {1, 1}, padding_kind::zero);
+            {1, 1}, {0, 0}, padding_kind::zero);
 #endif
 
     stream().submit({c3}).wait();
