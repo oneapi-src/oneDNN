8d3fb6d73d20065a489b000b8b96cb32e0d29b0a
Kharitonchik, Andrey <andrey.kharitonchik@intel.com>
2016-08-04 15:47:28 +0700

    tests: removed useless api_check tests
    Change-Id: If917832ca6029b6d0fd11ea6ed1d4218aed16bbd

--- tests/gtests/api_tests/api_check_c.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "mkl_dnn.h"
-#include "gtest/gtest.h"
-
-TEST(api_tests, api_check_c) {
-}
--- tests/gtests/api_tests/api_check_cxx.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstddef>
-
-#include "mkl_dnn.hpp"
-
-using std::ptrdiff_t;
-#include "gtest/gtest.h"
-
-TEST(api_tests, api_check_cxx) {
-}
