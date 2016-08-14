55f5960b9058bfd98461ec1f584ea12971fd0458
Roman Dubtsov <roman.s.dubtsov@intel.com>
2016-08-14 16:09:18 +0300

    build: autodetect MKL in external/

--- cmake/MKL.cmake
+++ cmake/MKL.cmake
@@ -1,14 +1,28 @@
 ##############################################################################
-# Locate MKL installation using MKLROOT
+# Locate MKL installation using MKLROOT or look in
+# ${CMAKE_CURRENT_SOURCE_DIR}/external
+#
 function(detect_mkl LIBNAME)
     if(HAVE_MKL)
         return()
     endif()
-    find_path(MKLINC mkl_blas.h
+
+    find_path(MKLINC mkl_cblas.h
         PATHS ${MKLROOT}/include $ENV{MKLROOT}/include)
+    if(NOT MKLINC)
+        file(GLOB_RECURSE MKLINC
+                ${CMAKE_CURRENT_SOURCE_DIR}/external/*/mkl_cblas.h)
+        if (MKLINC)
+            get_filename_component(MKLINC ${MKLINC} PATH)
+            set(MKLINC ${MKLINC} PARENT_SCOPE)
+        endif()
+    endif()
+
+    get_filename_component(__mklinc_root "${MKLINC}" PATH)
     find_library(MKLLIB NAMES ${LIBNAME}
-        PATHS ${MKLROOT}/lib ${MKLROOT}/lib/intel64
-            $ENV{MKLROOT}/lib $ENV{MKLROOT}/lib/intel64)
+        PATHS   ${MKLROOT}/lib ${MKLROOT}/lib/intel64
+                $ENV{MKLROOT}/lib $ENV{MKLROOT}/lib/intel64
+                ${__mklinc_root}/lib ${__mklinc_root}/lib/intel64)
     if(MKLINC AND MKLLIB)
         set(HAVE_MKL TRUE PARENT_SCOPE)
     endif()
@@ -22,11 +36,14 @@ if(HAVE_MKL)
     include_directories(AFTER ${MKLINC})
     list(APPEND mkldnn_LINKER_LIBS ${MKLLIB})
     message(STATUS "MKL found: include ${MKLINC}, lib ${MKLLIB}")
-elseif(DEFINED ENV{FAIL_WITHOUT_MKL} OR DEFINED FAIL_WITHOUT_MKL)
-    message(FATAL_ERROR "MKL not found. Please run scripts/prepare_mkl.sh to "
-            "download a minimal set of libraries or get a full version from "
-            "https://software.intel.com/en-us/intel-mkl")
 else()
-    message(WARNING "MKL not found. "
-            "Some performance features may be unavailable.")
+    if(DEFINED ENV{FAIL_WITHOUT_MKL} OR DEFINED FAIL_WITHOUT_MKL)
+        set(SEVERITY "FATAL_ERROR")
+    else()
+        set(SEVERITY "WARNING")
+    endif()
+    message(${SEVERITY} "MKL not found. Some performance features may not be "
+        "available. Please run scripts/prepare_mkl.sh to download a minimal "
+        "set of libraries or get a full version from "
+        "https://software.intel.com/en-us/intel-mkl")
 endif()
