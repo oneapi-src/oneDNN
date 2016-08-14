f0550d565e4ff5a96d5ef0e26cdbb60f463ff2cb
Roman Dubtsov <roman.s.dubtsov@intel.com>
2016-08-14 13:22:33 +0300

    build: improve MKL detection (allow forced mode; will be made default later)

--- cmake/MKL.cmake
+++ cmake/MKL.cmake
@@ -22,6 +22,11 @@ if(HAVE_MKL)
     include_directories(AFTER ${MKLINC})
     list(APPEND mkldnn_LINKER_LIBS ${MKLLIB})
     message(STATUS "MKL found: include ${MKLINC}, lib ${MKLLIB}")
+elseif(DEFINED ENV{FAIL_WITHOUT_MKL} OR DEFINED FAIL_WITHOUT_MKL)
+    message(FATAL_ERROR "MKL not found. Please run scripts/prepare_mkl.sh to "
+            "download a minimal set of libraries or get a full version from "
+            "https://software.intel.com/en-us/intel-mkl")
 else()
-    message(WARNING "MKL not found; some performance features may be unavailable.")
+    message(WARNING "MKL not found. "
+            "Some performance features may be unavailable.")
 endif()
--- scripts/prepare_mkl.sh
+++ scripts/prepare_mkl.sh
@@ -1,52 +1,12 @@
 #!/bin/sh
 
-# MKL
-DST=`dirname $0`
-SET_ENV_SCRIPT=$DST/"set_env_up.sh"
-OMP=0 
 MKLURL="https://github.com/intelcaffe/caffe/releases/download/self_contained_BU1/mklml_lnx_2017.0.b1.20160513.1.tgz"
-if [ $MKLROOT ]; then
-	VERSION_LINE=`grep __INTEL_MKL_BUILD_DATE $MKLROOT/include/mkl_version.h | sed -e 's/.* //'`
-fi
-LOCALMKL=`find $DST -name libmklml_gnu.so`
 
+DST=`dirname $0`/../external
+DST=`readlink -f $DST`
+mkdir -p $DST
+wget --no-check-certificate -P $DST $MKLURL
+tar -xzf $DST/mklml_lnx*.tgz -C $DST
 
-# Check if MKL_ROOT is set if positive then set one will be used..
-if [ -z $MKLROOT ] || [ $VERSION_LINE -lt 20160514 ]; then
-  # ..if MKLROOT is not set then check if we have MKL downloaded..
-  if [ -z $LOCALMKL ] || [ ! -f $LOCALMKL ]; then
-    #...If it is not then downloaded and unpacked
-    wget --no-check-certificate -P $DST $MKLURL
-    # TODO: make it pretty, hash progress print what it does actually eg. downloading unpacking
-    tar -xzf $DST/mklml_lnx*.tgz -C $DST
-  fi
-  if [ $1 -eq 1 ]; then
-	LOCALMKL=`find $DST -name libmklml_intel.so`   # name of MKL SDL lib 
-  else
-	LOCALMKL=`find $DST -name libmklml_gnu.so`   # name of MKL SDL lib 
-  fi
+echo "Downloaded an unpacked MKL libraries for machine learning to $DST"
 
-  # set MKL env vars are to be done via generated script
-  # this will help us export MKL env to existing shell
-  
-  MKLROOT=$PWD/`echo $LOCALMKL | sed -e 's/lib.*$//; s/\.\///'`
-
-  echo '#!/bin/sh' > $SET_ENV_SCRIPT
-  echo "export MKLROOT=$MKLROOT" >> $SET_ENV_SCRIPT
-  echo "export LD_LIBRARY_PATH=$MKLROOT/lib:\${LD_LIBRARY_PATH}" >> $SET_ENV_SCRIPT
-  echo "export CPATH=\${CPATH}:${MKLROOT}/include/" >> $SET_ENV_SCRIPT
-  chmod 755 $SET_ENV_SCRIPT
-  
-  OMP=1
-fi
-
-# Check what MKL lib we have in MKLROOT
-if [ -z `find $MKLROOT -name libmkl_rt.so -print -quit` ]; then
-  LIBRARIES=`basename $LOCALMKL | sed -e 's/^.*lib//' | sed -e 's/\.so.*$//'`
-else
-  LIBRARIES="mkl_rt"
-fi 
-
-
-# return value to calling script (Makefile,cmake)
-echo $MKLROOT $LIBRARIES $OMP
