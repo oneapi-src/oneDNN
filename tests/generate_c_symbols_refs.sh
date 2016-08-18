f950e4e73aa22fd56be103a52588f531d0d29715
Roman Dubtsov <roman.s.dubtsov@intel.com>
2016-08-18 18:02:48 +0700

    build: api -> include

--- tests/generate_c_symbols_refs.sh
+++ tests/generate_c_symbols_refs.sh
@@ -20,6 +20,6 @@ output="$2"
 
 echo -e '#include "mkldnn.h"' > "$output"
 echo -e "const void *c_functions[] = {" >> "$output"
-cpp "${mkldnn_root}/api/mkldnn.h" | /usr/bin/grep -o 'mkldnn_\w\+(' \
+cpp "${mkldnn_root}/include/mkldnn.h" | /usr/bin/grep -o 'mkldnn_\w\+(' \
     | sed 's/\(.*\)(/(void*)\1,/g' | sort -u >> "$output"
 echo -e "NULL};\nint main() { return 0; }" >> "$output"
