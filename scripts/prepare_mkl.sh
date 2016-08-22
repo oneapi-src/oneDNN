ea4c57e2c03322791a856c3dff4571fdc92d0883
Pirogov, Vadim O <vadim.o.pirogov@intel.com>
2016-08-22 22:52:48 +0700

    src: added copyright header

--- scripts/prepare_mkl.sh
+++ scripts/prepare_mkl.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+#===============================================================================
+# Copyright 2016 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#===============================================================================
 
 MKLURL="https://github.com/intelcaffe/caffe/releases/download/self_contained_BU1/mklml_lnx_2017.0.b1.20160513.1.tgz"
 
@@ -9,4 +24,3 @@ wget --no-check-certificate -P $DST $MKLURL
 tar -xzf $DST/mklml_lnx*.tgz -C $DST
 
 echo "Downloaded and unpacked Intel(R) MKL libraries for machine learning to $DST"
-
