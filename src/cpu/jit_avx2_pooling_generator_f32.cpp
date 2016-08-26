668263f7eb8b495305fe0315438a19e706032c9e
Shustrov, Nikita A <nikita.a.shustrov@intel.com>
2016-08-26 15:28:39 +0700

    cpu: pooling: improve and fix index calculations

--- src/cpu/jit_avx2_pooling_generator_f32.cpp
+++ src/cpu/jit_avx2_pooling_generator_f32.cpp
@@ -18,22 +18,22 @@
 
 #include "jit_avx2_pooling_generator_f32.hpp"
 
-#define ymm_store_mask Ymm(15)
-#define ymm_input      Ymm(14)
-
-#define ymm_index      Ymm(13)
-#define xmm_index      Xmm(13)
-#define ymm_iw_simd    Ymm(12)
-#define xmm_iw_simd    Xmm(12)
-#define ymm_simd       Ymm(11)
-#define xmm_simd       Xmm(11)
-#define ymm_stride_w   Ymm(10)
-#define xmm_stride_w   Xmm(10)
-
-#define ymm_tmp        Ymm(9)
-#define xmm_tmp        Xmm(9)
-#define ymm_init_reg   Ymm(8)
-#define xmm_init_reg   Xmm(8)
+#define ymm_store_mask      Ymm(15)
+#define ymm_input           Ymm(14)
+#define ymm_index           Ymm(13)
+#define xmm_index           Xmm(13)
+#define ymm_simd            Ymm(12)
+#define xmm_simd            Xmm(12)
+#define ymm_simd_stride_w   Ymm(11)
+#define xmm_simd_stride_w   Xmm(11)
+#define ymm_ki_offset       Ymm(10)
+#define xmm_ki_offset       Xmm(10)
+#define ymm_ji_offset       Ymm(9)
+#define xmm_ji_offset       Xmm(9)
+#define ymm_tmp             Ymm(8)
+#define xmm_tmp             Xmm(8)
+#define ymm_offset_base     Ymm(7)
+#define xmm_offset_base     Xmm(7)
 
 namespace mkldnn {
 namespace impl {
@@ -58,37 +58,52 @@ inline void jit_avx2_pooling_generator_f32::oh_step(
     uint32_t stride_w = params->stride_w;
 
     vpxor(ymm_store_mask, ymm_store_mask);
-    // Init output
-    mov(tmp_gpr, cvt._flt_max_int);
-    movq(xmm_init_reg, tmp_gpr);
-    vbroadcastss(ymm_init_reg, xmm_init_reg);    // output
 
+    mov(tmp_gpr, cvt._flt_max_int);
+    movq(xmm_tmp, tmp_gpr);
+    vbroadcastss(ymm_tmp, xmm_tmp);
     for (uint32_t jj = 0; jj < ur_w; jj++)
-        vmovaps(Ymm(jj), ymm_init_reg);
+        vmovaps(Ymm(jj), ymm_tmp);
 
     mov(aux_reg_input , reg_input);
     xor_(kj, kj);
     L(kh_lable); {
-        if (this->_is_training)
-            vpaddd(ymm_index, ymm_index, ymm_iw_simd);
+        if (this->_is_training) {
+            vpxor(ymm_ki_offset, ymm_ki_offset);
+        }
         for (uint32_t ki = 0; ki < KW; ki++) {
             int jj_start = nstl::max(0, pad_l-(int)ki);
             int jj_end   = (int)ur_w -
                 nstl::max(0, (int)ki+pad_r - (int)(KW-1));
-            if (this->_is_training)
-                vpaddd(ymm_index, ymm_index, ymm_simd);
+            if (this->_is_training) {
+                vmovaps(ymm_index, ymm_ki_offset);
+                vmovaps(ymm_ji_offset, ymm_offset_base);
+                if (jj_start != 0) {
+                    mov(tmp_gpr,(jj_start * stride_w * params->c_block));
+                    movq(xmm_tmp, tmp_gpr);
+                    vpbroadcastd(ymm_tmp, xmm_tmp);
+                    vpaddd(ymm_ji_offset, ymm_ji_offset, ymm_tmp);
+                }
+            }
             for (int jj = jj_start; jj  < jj_end; jj++) {
                 int aux_input_offset = (ki+jj*stride_w-pad_l)*params->c_block;
                 if (aux_input_offset > (int)IW*(int)params->c_block)
                     continue;
+                if (this->_is_training) {
+                    vpaddd(ymm_index, ymm_ki_offset, ymm_ji_offset);
+                }
                 vmovups(ymm_input,
                     ptr [ aux_reg_input + sizeof(float)*aux_input_offset ]);
-                if (this->_is_training)
-                    vpaddd(ymm_index, ymm_index, ymm_tmp);
                 vcmpps(ymm_store_mask, Ymm(jj), ymm_input, _cmp);
                 vblendvps(Ymm(jj), Ymm(jj), ymm_input, ymm_store_mask);
-                if (this->_is_training)
-                    vblendvps(Ymm(ur_w+jj), Ymm(ur_w+jj), ymm_index, ymm_store_mask);
+                if (this->_is_training) {
+                    vblendvps(Ymm(ur_w+jj), Ymm(ur_w+jj), ymm_index,
+                        ymm_store_mask);
+                    vpaddd(ymm_ji_offset, ymm_ji_offset , ymm_simd_stride_w);
+                }
+            }
+            if (this->_is_training) {
+                vpaddd(ymm_ki_offset, ymm_ki_offset , ymm_simd);
             }
         }
         add(aux_reg_input,  sizeof(float)*IW*params->c_block);
@@ -125,29 +140,21 @@ jit_avx2_pooling_generator_f32::jit_avx2_pooling_generator_f32(
         mov(reg_arr_init, ptr [ this->param1 + 80]);
 
     if (this->_is_training) {
-        vmovdqu(ymm_index, ptr [ reg_arr_init ]); // array init
-
-        if (params->l_pad > 0) {
-            mov(tmp_gpr,(params->l_pad*params->c_block));
-            movq(xmm_tmp, tmp_gpr);
-            vpbroadcastd(ymm_tmp, xmm_tmp);
-            vpsubd(ymm_index, ymm_index, ymm_tmp);
-        } else {
-            vpxor(ymm_index, ymm_index);
-        }
-        mov(tmp_gpr,(params->iw*params->c_block));
-        movq(xmm_iw_simd, tmp_gpr);
-        vpbroadcastd(ymm_iw_simd, xmm_iw_simd);
-
         mov(tmp_gpr,(params->c_block));
         movq(xmm_simd, tmp_gpr);
         vpbroadcastd(ymm_simd, xmm_simd);
 
-        mov(tmp_gpr,(params->stride_w));
-        movq(xmm_stride_w, tmp_gpr);
-        vpbroadcastd(ymm_stride_w, xmm_stride_w);
+        mov(tmp_gpr,(params->stride_w * params->c_block));
+        movq(xmm_simd_stride_w, tmp_gpr);
+        vpbroadcastd(ymm_simd_stride_w, xmm_simd_stride_w);
 
-        vpmuldq(ymm_tmp, ymm_stride_w, ymm_simd);
+        vmovdqu(ymm_offset_base, ptr [ reg_arr_init ]);
+        if (params->l_pad > 0) {
+            mov(tmp_gpr,(params->l_pad * params->c_block));
+            movq(xmm_tmp, tmp_gpr);
+            vpbroadcastd(ymm_tmp, xmm_tmp);
+            vpsubd(ymm_offset_base, ymm_offset_base, ymm_tmp);
+        }
     }
 
     int r_pad  = nstl::max(0, (int)((params->ow-1)*params->stride_w) +
