bb194dd2275a19d80c2e5672aa8e67b37554a80a
Istomin, Mikhail <mikhail.istomin@intel.com>
2016-12-13 11:18:20 +0300

    tests: relu: merge forward and backward tests

--- tests/gtests/test_relu.cpp
+++ tests/gtests/test_relu.cpp
@@ -22,10 +22,10 @@
 namespace mkldnn {
 
 template <typename data_t>
-struct relu_bwd_test_params {
-    data_t negative_slope;
+struct relu_test_params {
     engine::kind engine_kind;
     memory::format memory_format;
+    data_t negative_slope;
     memory::dims dims;
 };
 
@@ -73,7 +73,7 @@ void check_relu_bwd(data_t negative_slope, const memory::desc &md,
 }
 
 template <typename data_t>
-class relu_bwd_test : public ::testing::TestWithParam<relu_bwd_test_params<data_t>> {
+class relu_test : public ::testing::TestWithParam<relu_test_params<data_t>> {
 private:
     std::shared_ptr<memory> src;
     std::shared_ptr<memory> diff_src;
@@ -82,13 +82,13 @@ private:
     std::shared_ptr<memory::desc> src_desc;
     std::shared_ptr<memory::desc> dst_desc;
     std::shared_ptr<relu_forward::primitive_desc> relu_prim_desc;
-    relu_bwd_test_params<data_t> p;
+    relu_test_params<data_t> p;
     std::shared_ptr<engine> eng;
     memory::data_type data_type;
 
 protected:
     virtual void SetUp() {
-        p = ::testing::TestWithParam<relu_bwd_test_params<data_t>>::GetParam();
+        p = ::testing::TestWithParam<relu_test_params<data_t>>::GetParam();
 
         ASSERT_EQ(p.memory_format, memory::format::nchw);
         ASSERT_TRUE(p.engine_kind == engine::kind::cpu);
@@ -152,18 +152,53 @@ protected:
     }
 };
 
-using relu_bwd_test_float = relu_bwd_test<float>;
-using relu_bwd_test_params_float = relu_bwd_test_params<float>;
+using relu_test_float = relu_test<float>;
+using relu_test_params_float = relu_test_params<float>;
 
-TEST_P(relu_bwd_test_float, TestsReLUBackward) { }
-INSTANTIATE_TEST_CASE_P(TestReLUBackward, relu_bwd_test_float,
-        ::testing::Values(
-            relu_bwd_test_params_float{0, engine::kind::cpu,
-            memory::format::nchw, {10, 10, 10, 10}},
-            relu_bwd_test_params_float{.1f, engine::kind::cpu,
-            memory::format::nchw, {256, 64, 8, 16}},
-            relu_bwd_test_params_float{.1f, engine::kind::cpu,
-            memory::format::nchw, {1, 1, 1, 1}},
-            relu_bwd_test_params_float{.1f, engine::kind::cpu,
-            memory::format::nchw, {3, 5, 7, 11}}));
+TEST_P(relu_test_float, TestsReLU)
+{
+}
+
+#define EXPAND_SIZES(mb, c, h, w) { mb, c, h, w }
+#define EXPAND_FORMATS(data) memory::format::data
+
+#define ENGINE engine::kind::cpu
+
+#define PARAMS(data, ns, mb, c, h, w) \
+    relu_test_params_float { ENGINE, \
+    EXPAND_FORMATS(data), ns, EXPAND_SIZES(mb, c, h, w) }
+
+#define INST_TEST_CASE(str, ...) INSTANTIATE_TEST_CASE_P( \
+        str, relu_test_float, ::testing::Values(__VA_ARGS__))
+
+INST_TEST_CASE(SimpleZeroNegativeSlope_NCHW,
+    PARAMS(nchw, 0.f, 2, 8, 4, 4),
+    PARAMS(nchw, 0.f, 2, 16, 4, 4),
+    PARAMS(nchw, 0.f, 2, 16, 8, 8),
+    PARAMS(nchw, 0.f, 2, 16, 16, 8),
+    PARAMS(nchw, 0.f, 2, 16, 10, 8),
+    PARAMS(nchw, 0.f, 10, 10, 10, 10),
+    PARAMS(nchw, 0.f, 256, 64, 8, 16),
+    PARAMS(nchw, 0.f, 1, 1, 1, 1),
+    PARAMS(nchw, 0.f, 3, 5, 7, 11)
+);
+
+INST_TEST_CASE(Simple_NCHW,
+    PARAMS(nchw, 0.1f, 2, 8, 4, 4),
+    PARAMS(nchw, 0.1f, 2, 16, 4, 4),
+    PARAMS(nchw, 0.1f, 2, 16, 8, 8),
+    PARAMS(nchw, 0.1f, 2, 16, 16, 8),
+    PARAMS(nchw, 0.1f, 2, 16, 10, 8),
+    PARAMS(nchw, 0.1f, 10, 10, 10, 10),
+    PARAMS(nchw, 0.1f, 256, 64, 8, 16),
+    PARAMS(nchw, 0.1f, 1, 1, 1, 1),
+    PARAMS(nchw, 0.1f, 3, 5, 7, 11)
+);
+
+INST_TEST_CASE(AlexNet_NCHW,
+    PARAMS(nchw, 0.f, 2, 96, 55, 55),
+    PARAMS(nchw, 0.f, 2, 256, 27, 27),
+    PARAMS(nchw, 0.f, 2, 384, 13, 13)
+);
+
 }
