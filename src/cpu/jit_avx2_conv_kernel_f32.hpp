69cf5370bbe64453c9ef8c1550dd7ec3b1a83f7b
Kazakov, Sergey <sergey.kazakov@intel.com>
2016-12-28 08:21:43 +0300

    cpu: jit: create jit_primitive_conf.hpp for common config structures

--- src/cpu/jit_avx2_conv_kernel_f32.hpp
+++ src/cpu/jit_avx2_conv_kernel_f32.hpp
@@ -19,43 +19,11 @@
 
 #include "c_types_map.hpp"
 #include "jit_generator.hpp"
+#include "jit_primitive_conf.hpp"
 
 namespace mkldnn {
 namespace impl {
 namespace cpu {
-
-struct jit_conv_conf_t {
-    int mb;
-    int ngroups, ic, oc;
-    int ih, iw, oh, ow;
-    int l_pad, t_pad;
-    int kh, kw;
-    int stride_h, stride_w;
-    memory_format_t src_fmt;
-    bool with_bias, with_relu;
-    double relu_negative_slope;
-
-    int ihp, iwp, ohp, owp;
-    int nb_ic, ic_block;
-    int nb_oc, oc_block;
-    int nb_ic_blocking, nb_oc_blocking; // blocking of nb_ic and nb_ic
-    int ur_h, ur_w;
-    int ur_w_tail;
-};
-
-struct __attribute__((__packed__)) jit_conv_call_s {
-    const float *src; /* hack, non-const for backward_data */
-    const float *dst; /* hack, non-const for forward */
-    const float *filt; /* hack, non-const for backward_weights */
-    const float *bias; /* hack, non-const for backward_bias */
-    const float *src_prf;
-    const float *dst_prf;
-    const float *filt_prf;
-    size_t kh_padding;
-    size_t kh_padding_prf;
-    size_t kw_padding;
-    int ic_flag;
-};
 
 struct jit_avx2_conv_fwd_kernel_f32: public jit_generator {
     enum { IC_FLAG_FIRST = 1, IC_FLAG_LAST = 2 };
