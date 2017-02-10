7ad4986a165bad6b7a9359eaa14a20b09fc50afc
Kazakov, Sergey <sergey.kazakov@intel.com>
2017-02-10 08:33:06 +0300

    cpu: avx512_mic fwd conv: improve pipeline and threading

--- src/cpu/jit_avx512_mic_convolution.cpp
+++ src/cpu/jit_avx512_mic_convolution.cpp
@@ -18,6 +18,7 @@
 
 #include "c_types_map.hpp"
 #include "jit_avx512_mic_convolution.hpp"
+#include "mkldnn_thread.hpp"
 #include "type_helpers.hpp"
 
 namespace mkldnn {
@@ -26,6 +27,7 @@ namespace cpu {
 
 using namespace mkldnn::impl::status;
 using namespace mkldnn::impl::memory_format;
+using namespace mkldnn::impl::utils;
 
 template <bool with_relu>
 void _jit_avx512_mic_convolution_fwd_t<with_relu>::execute_forward()
@@ -42,78 +44,88 @@ void _jit_avx512_mic_convolution_fwd_t<with_relu>::execute_forward()
 
     const auto &jcp = kernel_->jcp;
 
-    auto ker = [&](
-            int g, int n, int oc, int ic, int oh, jit_conv_call_s *par_conv) {
+    auto ker = [&](const int ithr, const int nthr) {
+        size_t start{0}, end{0};
+        const int oc_dim = jcp.nb_oc / jcp.nb_oc_blocking;
+        const size_t work_amount = jcp.mb * jcp.ngroups * oc_dim;
+        size_t n, g, oc;
+        jit_conv_call_s par_conv = {};
 
-        const int ij = oh * jcp.stride_h;
-        const int i_t_overflow = nstl::max(0, jcp.t_pad - ij);
-        const int i_b_overflow
-                = nstl::max(jcp.ih, ij + jcp.kh - jcp.t_pad) - jcp.ih;
+        balance211(work_amount, nthr, ithr, start, end);
+        nd_iterator_init(start, n, jcp.mb, g, jcp.ngroups, oc, oc_dim);
 
-        par_conv->src = par_conv->src_prf;
-        par_conv->dst = par_conv->dst_prf;
-        par_conv->filt = par_conv->filt_prf;
-        par_conv->bias = par_conv->bias_prf;
-        par_conv->current_ic = par_conv->current_ic_prf;
+        par_conv.src_prf = NULL;
+        par_conv.dst_prf = NULL;
+        par_conv.filt_prf = NULL;
+        par_conv.bias_prf = NULL;
 
-        const int ih = nstl::max(ij - jcp.t_pad, 0);
-        par_conv->src_prf = const_cast<data_t *>(&src[src_d.blk_off(
-                n, jcp.ic == 3 ? 0 : g * jcp.nb_ic + ic, ih, 0)]);
-        par_conv->dst_prf = const_cast<data_t *>(
-                &dst[dst_d.blk_off(n, g * jcp.nb_oc + oc, oh, 0)]);
-        par_conv->bias_prf = const_cast<data_t *>(
-                &bias[bias_d.blk_off((g * jcp.nb_oc + oc) * jcp.oc_block)]);
-        par_conv->filt_prf = const_cast<data_t *>(&weights[conf_.with_groups() ?
-                        weights_d.blk_off(
-                                g, oc, jcp.ic == 3 ? 0 : ic, i_t_overflow, 0) :
-                        weights_d.blk_off(
-                                oc, jcp.ic == 3 ? 0 : ic, i_t_overflow, 0)]);
+        for (size_t iwork = start; iwork < end; ++iwork) {
+            for (int ic = 0; ic < jcp.nb_ic; ++ic) {
+                for (int oh = 0; oh < jcp.oh; ++oh) {
 
-        par_conv->kh_padding = par_conv->kh_padding_prf;
-        par_conv->kh_padding_prf = jcp.kh - i_t_overflow - i_b_overflow;
-        par_conv->kw_padding = 0;
-        par_conv->current_ic_prf = ic;
+                    const int ij = oh * jcp.stride_h;
+                    const int i_t_overflow = nstl::max(0, jcp.t_pad - ij);
+                    const int i_b_overflow
+                            = nstl::max(jcp.ih, ij + jcp.kh - jcp.t_pad)
+                            - jcp.ih;
 
-        if (par_conv->src != NULL)
-            kernel_->jit_ker(par_conv);
+                    par_conv.src = par_conv.src_prf;
+                    par_conv.dst = par_conv.dst_prf;
+                    par_conv.filt = par_conv.filt_prf;
+                    par_conv.bias = par_conv.bias_prf;
+                    par_conv.current_ic = par_conv.current_ic_prf;
+
+                    const int ih = nstl::max(ij - jcp.t_pad, 0);
+                    par_conv.src_prf = const_cast<data_t *>(&src[src_d.blk_off(
+                            n, jcp.ic == 3 ? 0 : g * jcp.nb_ic + ic, ih, 0)]);
+                    par_conv.dst_prf = const_cast<data_t *>(
+                            &dst[dst_d.blk_off(n, g * jcp.nb_oc + oc, oh, 0)]);
+                    par_conv.bias_prf
+                            = const_cast<data_t *>(&bias[bias_d.blk_off(
+                                    (g * jcp.nb_oc + oc) * jcp.oc_block)]);
+                    par_conv.filt_prf = const_cast<data_t *>(
+                            &weights[conf_.with_groups() ?
+                                            weights_d.blk_off(g, oc,
+                                                    jcp.ic == 3 ? 0 : ic,
+                                                    i_t_overflow, 0) :
+                                            weights_d.blk_off(oc,
+                                                    jcp.ic == 3 ? 0 : ic,
+                                                    i_t_overflow, 0)]);
+
+                    par_conv.kh_padding = par_conv.kh_padding_prf;
+                    par_conv.kh_padding_prf
+                            = jcp.kh - i_t_overflow - i_b_overflow;
+                    par_conv.kw_padding = 0;
+                    par_conv.current_ic_prf = ic;
+
+                    if (par_conv.src != NULL)
+                        kernel_->jit_ker(&par_conv);
+                }
+            }
+            nd_iterator_step(n, jcp.mb, g, jcp.ngroups, oc, oc_dim);
+        }
+
+        par_conv.src = par_conv.src_prf;
+        par_conv.dst = par_conv.dst_prf;
+        par_conv.filt = par_conv.filt_prf;
+        par_conv.bias = par_conv.bias_prf;
+        par_conv.current_ic = par_conv.current_ic_prf;
+
+        par_conv.src_prf = NULL;
+        par_conv.dst_prf = NULL;
+        par_conv.filt_prf = NULL;
+        par_conv.bias_prf = NULL;
+
+        par_conv.kh_padding = par_conv.kh_padding_prf;
+        par_conv.kw_padding = 0;
+
+        if (par_conv.src != NULL)
+            kernel_->jit_ker(&par_conv);
     };
 
-#pragma omp parallel for collapse(3) schedule(static)
-    for (int n = 0; n < jcp.mb; ++n) {
-        for (int g = 0; g < jcp.ngroups; ++g) {
-            for (int oc = 0; oc < jcp.nb_oc; ++oc) {
-
-                jit_conv_call_s par_conv = {};
-
-                par_conv.src_prf = NULL;
-                par_conv.dst_prf = NULL;
-                par_conv.filt_prf = NULL;
-                par_conv.bias_prf = NULL;
-
-                for (int ic = 0; ic < jcp.nb_ic; ++ic) {
-                    for (int oh = 0; oh < jcp.oh; ++oh) {
-                        ker(g, n, oc, ic, oh, &par_conv);
-                    }
-                }
-
-                par_conv.src = par_conv.src_prf;
-                par_conv.dst = par_conv.dst_prf;
-                par_conv.filt = par_conv.filt_prf;
-                par_conv.bias = par_conv.bias_prf;
-                par_conv.current_ic = par_conv.current_ic_prf;
-
-                par_conv.src_prf = NULL;
-                par_conv.dst_prf = NULL;
-                par_conv.filt_prf = NULL;
-                par_conv.bias_prf = NULL;
-
-                par_conv.kh_padding = par_conv.kh_padding_prf;
-                par_conv.kw_padding = 0;
-
-                if (par_conv.src != NULL)
-                    kernel_->jit_ker(&par_conv);
-            }
-        }
+#pragma omp parallel
+    {
+        ker(omp_get_thread_num(), omp_get_num_threads());
     }
 }
 template void _jit_avx512_mic_convolution_fwd_t<true>::execute_forward();
