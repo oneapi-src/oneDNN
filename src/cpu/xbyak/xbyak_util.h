85d68636d787ece62f12ecd9271adb4645d08374
Pirogov, Vadim O <vadim.o.pirogov@intel.com>
2017-02-10 03:11:45 +0300

    all: updated copyright banners

--- src/cpu/xbyak/xbyak_util.h
+++ src/cpu/xbyak/xbyak_util.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2016 Intel Corporation
+* Copyright 2016-2017 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *******************************************************************************/
+
 /*******************************************************************************
 * Copyright (c) 2007 MITSUNARI Shigeo
 * All rights reserved.
