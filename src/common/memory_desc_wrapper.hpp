62746fe5967694ae1804ffbf4983c2289d8bd01a
Soto Flores, Manuel <manuel.soto.flores@intel.com>
2017-06-07 13:13:49 -0700

    common: win: template compatibility for msvs

--- src/common/memory_desc_wrapper.hpp
+++ src/common/memory_desc_wrapper.hpp
@@ -217,6 +217,12 @@ private:
     template<typename ...Void>
     inline size_t _blk_off() const { return blocking_desc().offset_padding; }
 
+#ifdef _MSVC_LANG
+    template<typename T> inline size_t _blk_off(T x0) const {
+        return size_t(x0)*blocking_desc().strides[0][0] + _blk_off();
+    }
+#endif
+
     template<typename T, typename ...Args>
     inline size_t _blk_off(Args ...args, T xn) const {
         return size_t(xn)*blocking_desc().strides[0][sizeof...(args)] +
