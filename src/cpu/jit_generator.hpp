6a280a33b321b316a97527590f4c435ae5af772f
Roman Dubtsov <roman.s.dubtsov@intel.com>
2017-06-12 04:20:56 -0700

    cpu: jit: fix a comment

--- src/cpu/jit_generator.hpp
+++ src/cpu/jit_generator.hpp
@@ -442,8 +442,8 @@ public:
         if (!code)
             return 0;
 
-        // TODO (Roma): add a virtual name() function that would used to -
-        // notify profiles like vtune about generated code - generate a
+        // TODO (Roma): add a virtual name() function that would be used to
+        // notify profilers like vtune about generated code and generate a
         // meaningful file name
 
         FILE *fp = fopen("mkldnn_jit_dump.bin", "w+");
