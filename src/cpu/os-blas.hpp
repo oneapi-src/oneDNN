2bfb5258d6ab080f2e660eaac834a5a6ce841fa2
Erik Kruus <kruus@nec-labs.com>
2017-06-30 09:58:26 -0400

    shorten last patch

--- src/cpu/os-blas.hpp
+++ src/cpu/os-blas.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2016-2017 Intel Corporation
+* Copyright 2016-2017 Intel Corporation, NEC Laboratories America LLC
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
