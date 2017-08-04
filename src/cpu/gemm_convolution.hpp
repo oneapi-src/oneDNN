c6b742d5af16581081aafa52894ee59c12662a64
kruus <ejkruus@gmail.com>
2017-08-04 07:27:27 -1000

    common: mkl cblas options (#85)
    the patch contains the following improvements:
     - cleaner usage of USE_MKL and USE_CBLAS
     - better handling the situation when several MKL binaries
       are available in extrenal subdirectory
     - some extra type conversions
     - some clean-ups
    
    * begin USE_MKL USE_CBLAS harmonization
    
    - A subset of my SX branch that seems generally "A Good Thing"
      - minor code dedup for gemm-convolution-supported logic
        (handy single-point-of-change)
      - handle multiple external/*/cblas.h graciously (with warning)
    
    * comment change
    
    * gcc wanted 1-liner constexpr fn return
    
    * Update os-blas.hpp comment
    
    * avoid a compiler-bug and better safety check
    
    * shorten last patch
    
    * typo & small SX mod for an example
    
    * style
    
    * minor style changes

--- src/cpu/gemm_convolution.hpp
+++ src/cpu/gemm_convolution.hpp
@@ -29,6 +29,20 @@ namespace mkldnn {
 namespace impl {
 namespace cpu {
 
+namespace {
+/** Can be more complicated to support other OSes
+ * (e.g. without JIT support, no mayiuse available).
+ * Some compilers (gcc) still insist on one-liner returns here :( */
+template<bool run_jit, cpu_isa_t isa>
+static inline bool constexpr _gemm_convolution_implemented() {
+#if defined(USE_MKL) || defined(USE_CBLAS)
+    return run_jit ? mayiuse(isa) : true;
+#else
+    return run_jit ? mayiuse(isa) : false;
+#endif
+}
+}
+
 template <bool with_relu, bool run_jit, cpu_isa_t isa>
 struct _gemm_convolution_fwd_t: public cpu_primitive_t {
     struct pd_t: public _cpu_convolution_fwd_pd_t<with_relu> {
@@ -46,15 +60,8 @@ struct _gemm_convolution_fwd_t: public cpu_primitive_t {
 
             assert(this->engine()->kind() == engine_kind::cpu);
 
-            if (run_jit) {
-                if (!mayiuse(isa)) return status::unimplemented;
-            } else {
-#ifndef USE_CBLAS
-                return status::unimplemented;
-#endif
-            }
-
             bool ok = true
+                && _gemm_convolution_implemented<run_jit, isa>()
                 && this->set_default_params() == status::success
                 && utils::one_of(this->cdesc_().prop_kind, forward_training,
                         forward_inference)
@@ -157,15 +164,8 @@ struct _gemm_convolution_bwd_data_t: public cpu_primitive_t {
 
             assert(this->engine()->kind() == engine_kind::cpu);
 
-            if (run_jit) {
-                if (!mayiuse(isa)) return status::unimplemented;
-            } else {
-#ifndef USE_CBLAS
-                return status::unimplemented;
-#endif
-            }
-
             bool ok = true
+                && _gemm_convolution_implemented<run_jit, isa>()
                 && this->set_default_params() == status::success
                 && utils::one_of(this->desc()->prop_kind, backward,
                         backward_data)
@@ -265,15 +265,8 @@ struct _gemm_convolution_bwd_weights_t: public cpu_primitive_t {
 
             assert(this->engine()->kind() == engine_kind::cpu);
 
-            if (run_jit) {
-                if (!mayiuse(isa)) return status::unimplemented;
-            } else {
-#ifndef USE_CBLAS
-                return status::unimplemented;
-#endif
-            }
-
             bool ok = true
+            && _gemm_convolution_implemented<run_jit, isa>()
             && this->set_default_params() == status::success
             && utils::one_of(this->desc()->prop_kind, backward,
                     backward_weights)
