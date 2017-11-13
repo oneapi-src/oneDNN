46a599230bcdd8e90309d3d0f44298a9df0421f0
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2017-11-13 19:25:48 +0000

    common: pd iter: do not use attrs from the stack
    Great thanks to @kruus for catching the problem and suggest the
    solution. The implementation is slightly different but still
    should work fine.
    
    This closes #149.
    The fix also incorporates #135 PR (from @kruus) as well.
    Adding tests to prevent such a shame in future.

--- src/common/primitive_iterator.cpp
+++ src/common/primitive_iterator.cpp
@@ -31,8 +31,8 @@ struct mkldnn_primitive_desc_iterator: public c_compatible {
 
     mkldnn_primitive_desc_iterator(engine_t *engine, const op_desc_t *op_desc,
             const primitive_attr_t *attr, const primitive_desc_t *hint_fwd_pd)
-        : idx_(-1), engine_(engine), pd_(nullptr)
-        , op_desc_(op_desc), attr_(attr), hint_fwd_pd_(hint_fwd_pd)
+        : idx_(-1), engine_(engine), pd_(nullptr), op_desc_(op_desc)
+        , attr_(attr ? *attr : primitive_attr_t()), hint_fwd_pd_(hint_fwd_pd)
         , impl_list_(engine_->get_implementation_list()), last_idx_(0)
     {
         while (impl_list_[last_idx_] != nullptr) ++last_idx_;
@@ -48,9 +48,9 @@ struct mkldnn_primitive_desc_iterator: public c_compatible {
     { return mkldnn_primitive_desc_iterator(engine_, last_idx_); }
 
     primitive_desc_iterator_t &operator++() {
-        if (pd_) delete pd_;
+        if (pd_) { delete pd_; pd_ = nullptr; }
         while (++idx_ != last_idx_) {
-            auto s = impl_list_[idx_](&pd_, op_desc_, attr_, engine_,
+            auto s = impl_list_[idx_](&pd_, op_desc_, &attr_, engine_,
                     hint_fwd_pd_);
             if (s == success) break;
         }
@@ -67,7 +67,7 @@ protected:
     engine_t *engine_;
     primitive_desc_t *pd_;
     const op_desc_t *op_desc_;
-    const primitive_attr_t *attr_;
+    const primitive_attr_t attr_;
     const primitive_desc_t *hint_fwd_pd_;
     const pd_create_f *impl_list_;
     int last_idx_;
@@ -75,7 +75,7 @@ protected:
 private:
     mkldnn_primitive_desc_iterator(engine_t *engine, int last_idx)
         : idx_(last_idx), engine_(engine), pd_(nullptr)
-        , op_desc_(nullptr), attr_(nullptr), hint_fwd_pd_(nullptr)
+        , op_desc_(nullptr), hint_fwd_pd_(nullptr)
         , impl_list_(nullptr), last_idx_(last_idx) {}
 };
 
@@ -84,10 +84,6 @@ status_t mkldnn_primitive_desc_iterator_create_v2(
         const primitive_attr_t *attr, engine_t *engine,
         const primitive_desc_t *hint_fwd_pd) {
     const op_desc_t *op_desc = (const op_desc_t *)c_op_desc;
-
-    const primitive_attr_t dummy_attr;
-    if (attr == nullptr)
-        attr = &dummy_attr;
 
     auto it = new primitive_desc_iterator_t(engine, op_desc, attr, hint_fwd_pd);
     if (it == nullptr) return out_of_memory;
@@ -142,10 +138,6 @@ status_t mkldnn_primitive_desc_create_v2(primitive_desc_t **primitive_desc,
         const_c_op_desc_t c_op_desc, const primitive_attr_t *attr,
         engine_t *engine, const primitive_desc_t *hint_fwd_pd) {
     const op_desc_t *op_desc = (const op_desc_t *)c_op_desc;
-
-    const primitive_attr_t dummy_attr;
-    if (attr == nullptr)
-        attr = &dummy_attr;
 
     mkldnn_primitive_desc_iterator it(engine, op_desc, attr, hint_fwd_pd);
     ++it;
