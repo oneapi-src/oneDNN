f20dc84f6500db6bff5c32be9bb2d63a32faaf1e
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2017-12-28 22:00:38 +0000

    benchdnn: do not use variadic arrays
    ... to make msvs cl happy

--- tests/benchdnn/conv/ref_conv.cpp
+++ tests/benchdnn/conv/ref_conv.cpp
@@ -101,7 +101,7 @@ void compute_ref_fwd(const prb_t *p, dnn_mem_t &src_m,
 
 void compute_ref_bwd_d(const prb_t *p, dnn_mem_t &diff_src_m,
         dnn_mem_t &wei_m, dnn_mem_t &diff_dst_m) {
-    const int precompute_size = 16;
+    enum { precompute_size = 16 };
     const bool fast = MAX2(p->kh, p->kw) <= precompute_size;
 
     /* pre-computes arrays of oh(ow) and kh(kw) for traversing in kernel */
