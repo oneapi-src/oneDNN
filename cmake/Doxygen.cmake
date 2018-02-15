4f5ddc921bb97d7d320ac6a4e62a563a835928bb
Pirogov, Vadim O <vadim.o.pirogov@intel.com>
2018-02-15 10:48:13 -0800

    all: updated copyright notice

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -1,5 +1,5 @@
 #===============================================================================
-# Copyright 2016-2017 Intel Corporation
+# Copyright 2016-2018 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #===============================================================================
+
 # Locates Doxygen and configures documentation generation
 #===============================================================================
 
--- tests/gtests/test_convolution_backward_data_s16s16s32.cpp
+++ tests/gtests/test_convolution_backward_data_s16s16s32.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2016-2017 Intel Corporation
+* Copyright 2016-2018 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *******************************************************************************/
+
 #include <stdint.h>
 #include "mkldnn_test_common.hpp"
 #include "gtest/gtest.h"
