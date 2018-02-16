c6a3f1d4fbe786d758de81da0ae3da9beb69a56a
Kazakov, Sergey <sergey.kazakov@intel.com>
2018-02-16 10:42:09 -0800

    gtests: add tests for 4vnni/vnni bwd_w conv

--- tests/gtests/test_convolution_backward_weights_common.hpp
+++ tests/gtests/test_convolution_backward_weights_common.hpp
@@ -13,6 +13,8 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *******************************************************************************/
+#ifndef TEST_CONVOLUTION_BACKWARD_WEIGHTS_COMMON_H
+#define TEST_CONVOLUTION_BACKWARD_WEIGHTS_COMMON_H
 
 #include "mkldnn_test_common.hpp"
 #include "gtest/gtest.h"
@@ -21,12 +23,15 @@
 
 namespace mkldnn {
 
-template <typename data_t>
+template <typename data_t_src, typename data_t_diff_dst,
+          typename data_t_diff_bias>
 void compute_ref_conv_bwd_bias(const test_convolution_sizes_t &c,
         const memory &diff_dst, const memory &diff_bias)
 {
-    data_t *diff_bias_data = (data_t *)diff_bias.get_data_handle();
-    data_t *diff_dst_data = (data_t *)diff_dst.get_data_handle();
+    data_t_diff_bias *diff_bias_data
+        = (data_t_diff_bias *)diff_bias.get_data_handle();
+    data_t_diff_dst *diff_dst_data
+        = (data_t_diff_dst *)diff_dst.get_data_handle();
 
     const memory::desc bias_d = diff_bias.get_primitive_desc().desc();
     const memory::desc dst_d = diff_dst.get_primitive_desc().desc();
@@ -51,13 +56,16 @@ void compute_ref_conv_bwd_bias(const test_convolution_sizes_t &c,
     }
 }
 
-template <typename data_t>
+template <typename data_t_src, typename data_t_diff_dst,
+          typename data_t_diff_weights>
 void compute_ref_conv_bwd_weights(const test_convolution_sizes_t &c,
         const memory &src, const memory &diff_dst, const memory &diff_weights)
 {
-    data_t *src_data = (data_t *)src.get_data_handle();
-    data_t *diff_weights_data = (data_t *)diff_weights.get_data_handle();
-    data_t *diff_dst_data = (data_t *)diff_dst.get_data_handle();
+    data_t_src *src_data = (data_t_src *)src.get_data_handle();
+    data_t_diff_weights *diff_weights_data
+        = (data_t_diff_weights *)diff_weights.get_data_handle();
+    data_t_diff_dst *diff_dst_data
+        = (data_t_diff_dst *)diff_dst.get_data_handle();
 
     const memory::desc src_d = src.get_primitive_desc().desc();
     const memory::desc weights_d = diff_weights.get_primitive_desc().desc();
@@ -110,7 +118,8 @@ void compute_ref_conv_bwd_weights(const test_convolution_sizes_t &c,
     }
 }
 
-template <typename data_t>
+template <typename data_t_src, typename data_t_diff_dst,
+          typename data_t_diff_weights, typename data_t_diff_bias>
 class convolution_backward_weights_test
             : public ::testing::TestWithParam<test_convolution_params_t> {
 protected:
@@ -119,37 +128,49 @@ protected:
         test_convolution_params_t p
                 = ::testing::TestWithParam<
                 test_convolution_params_t>::GetParam();
+
         ASSERT_TRUE(p.engine_kind == engine::kind::cpu);
         ASSERT_EQ(p.aalgorithm, convolution_direct);
         auto eng = engine(p.engine_kind, 0);
-        memory::data_type data_type = data_traits<data_t>::data_type;
-        ASSERT_EQ(data_type, mkldnn::memory::data_type::f32);
+        memory::data_type data_type_src = data_traits<data_t_src>::data_type;
+        memory::data_type data_type_diff_dst
+            = data_traits<data_t_diff_dst>::data_type;
+        memory::data_type data_type_diff_weights
+            = data_traits<data_t_diff_weights>::data_type;
+        memory::data_type data_type_diff_bias
+            = data_traits<data_t_diff_bias>::data_type;
 
         test_convolution_sizes_t cd = p.sizes;
 
-        auto c_src_desc = create_md({ cd.mb, cd.ic, cd.ih, cd.iw }, data_type,
-                p.formats.src_format);
-        auto c_diff_weights_desc = cd.ng > 1 ?
-                create_md({ cd.ng, cd.oc / cd.ng, cd.ic / cd.ng, cd.kh, cd.kw },
-                        data_type, p.formats.weights_format) :
-                create_md(
-                        { cd.oc, cd.ic, cd.kh, cd.kw }, data_type,
-                        p.formats.weights_format);
-        auto c_diff_bias_desc
-                = create_md({ cd.oc }, data_type, p.formats.bias_format);
-        auto c_diff_dst_desc
-                = create_md({ cd.mb, cd.oc, cd.oh, cd.ow }, data_type,
-                        p.formats.dst_format);
-
+        auto c_src_desc = create_md({ cd.mb, cd.ic, cd.ih, cd.iw },
+            data_type_src, p.formats.src_format);
+        auto c_diff_weights_desc = cd.ng > 1
+            ? create_md({ cd.ng, cd.oc / cd.ng, cd.ic / cd.ng, cd.kh, cd.kw },
+              data_type_diff_weights, p.formats.weights_format)
+            : create_md({ cd.oc, cd.ic, cd.kh, cd.kw }, data_type_diff_weights,
+              p.formats.weights_format);
+        auto c_diff_bias_desc = create_md({ cd.oc }, data_type_diff_bias,
+            p.formats.bias_format);
+        auto c_diff_dst_desc = create_md({ cd.mb, cd.oc, cd.oh, cd.ow },
+            data_type_diff_dst, p.formats.dst_format);
+        auto c_weights_desc_f = cd.ng > 1
+            ? create_md({ cd.ng, cd.oc / cd.ng, cd.ic / cd.ng, cd.kh, cd.kw },
+              data_type_diff_dst, p.formats.weights_format)
+            : create_md({ cd.oc, cd.ic, cd.kh, cd.kw }, data_type_diff_dst,
+              p.formats.weights_format);
+        auto c_dst_desc_f = create_md({ cd.mb, cd.oc, cd.oh, cd.ow },
+            data_type_diff_weights, p.formats.dst_format);
         auto c_src = test_memory(c_src_desc, eng);
         auto c_diff_weights = test_memory(c_diff_weights_desc, eng);
         auto c_diff_bias = test_memory(c_diff_bias_desc, eng);
         auto c_diff_dst = test_memory(c_diff_dst_desc, eng);
-
-        fill_data<data_t>(c_diff_dst.get_size() / sizeof(data_t),
-                (data_t *)c_diff_dst.get().get_data_handle());
-        fill_data<data_t>(c_src.get_size() / sizeof(data_t),
-                (data_t *)c_src.get().get_data_handle());
+        auto weights_primitive_desc_f = test_memory(c_weights_desc_f, eng);
+        auto dst_primitive_desc_f = test_memory(c_dst_desc_f, eng);
+        fill_data<data_t_diff_dst>(
+            c_diff_dst.get_size() / sizeof(data_t_diff_dst),
+            (data_t_diff_dst *)c_diff_dst.get().get_data_handle());
+        fill_data<data_t_src>(c_src.get_size() / sizeof(data_t_src),
+            (data_t_src *)c_src.get().get_data_handle());
 
         std::vector<int> padR = { cd.padh, cd.padw };
         for (int i = 0; i < 2; ++i) {
@@ -164,7 +185,7 @@ protected:
         auto test = [&]() {
             auto conv_desc = convolution_forward::desc(
                     prop_kind::forward_training, p.aalgorithm, c_src_desc,
-                    c_diff_weights_desc, c_diff_bias_desc, c_diff_dst_desc,
+                    c_weights_desc_f, c_diff_bias_desc, c_dst_desc_f,
                     { cd.strh, cd.strw }, { cd.dilh, cd.dilw },
                     { cd.padh, cd.padw }, padR, padding_kind::zero);
 
@@ -193,13 +214,15 @@ protected:
             auto ref_diff_weights = memory({c_diff_weights_desc, eng});
             auto ref_diff_bias = memory({c_diff_bias_desc, eng});
 
-            compute_ref_conv_bwd_weights<data_t>(cd, c_src.get(),
-                    c_diff_dst.get(), ref_diff_weights);
-            compare_data<data_t>(ref_diff_weights, c_diff_weights.get());
+            compute_ref_conv_bwd_weights<data_t_src, data_t_diff_dst,
+                data_t_diff_weights>(cd, c_src.get(), c_diff_dst.get(),
+                ref_diff_weights);
+            compare_data<data_t_diff_weights>(ref_diff_weights,
+                c_diff_weights.get());
 
-            compute_ref_conv_bwd_bias<data_t>(cd, c_diff_dst.get(),
-                    ref_diff_bias);
-            compare_data<data_t>(ref_diff_bias, c_diff_bias.get());
+            compute_ref_conv_bwd_bias<data_t_src, data_t_diff_dst,
+                data_t_diff_bias>(cd, c_diff_dst.get(), ref_diff_bias);
+            compare_data<data_t_diff_bias>(ref_diff_bias, c_diff_bias.get());
         };
 
         if (catch_expected_failures(test, p.expect_to_fail, p.expected_status))
@@ -207,14 +230,5 @@ protected:
     }
 };
 
-using convolution_test = convolution_backward_weights_test<float>;
-
-TEST_P(convolution_test, TestConvolution)
-{
 }
-
-#define DIRECTION_BACKWARD_WEIGHTS
-#include "convolution_common.h"
-#include "diluted_convolution.h"
-
-}
+#endif
