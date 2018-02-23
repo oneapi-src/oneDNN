09c614ffd883983e8bf8e4dcb57b0d3baaa34829
Kazakov, Sergey <sergey.kazakov@intel.com>
2018-02-23 13:55:57 -0800

    fixup: cpu: avx512 bwd_w convolution msvs warning fix

--- src/cpu/jit_avx512_common_convolution.cpp
+++ src/cpu/jit_avx512_common_convolution.cpp
@@ -821,7 +821,7 @@ void jit_avx512_common_convolution_bwd_weights_t<src_type, diff_dst_type,
 
             if (img == img_start)
                 for (int o = 0; o < 16; ++o)
-                    d_bias[o] = 0.;
+                    d_bias[o] = 0;
             for (int hw = 0; hw < jcp.oh * jcp.ow; ++hw) {
 #               pragma omp simd
                 for (int o = 0; o < 16; ++o)
