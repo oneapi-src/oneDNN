fd9f1bd781f3d8695a79741536d8a1fb6161ad06
Roman Dubtsov <roman.s.dubtsov@intel.com>
2018-05-08 17:06:35 -0700

    src: common: remove superfluous alignas(64) from scales_t

--- src/common/primitive_attr.hpp
+++ src/common/primitive_attr.hpp
@@ -60,7 +60,7 @@ struct scales_t: public c_compatible {
 
 private:
     enum { scales_buf_size = 16 };
-    alignas(64) float scales_buf_[scales_buf_size];
+    float scales_buf_[scales_buf_size];
 
     void cleanup() {
         if (scales_ != scales_buf_ && scales_ != nullptr)
