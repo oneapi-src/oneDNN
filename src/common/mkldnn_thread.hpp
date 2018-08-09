cc2e053a33ce25b43c8851fd1781eb4a314d6643
Kazakov, Sergey <sergey.kazakov@intel.com>
2018-08-09 09:59:01 -0700

    common: introduce parallel_nd threading

--- src/common/mkldnn_thread.hpp
+++ src/common/mkldnn_thread.hpp
@@ -60,6 +60,185 @@ inline void balance211(T n, U team, U tid, T &n_start, T &n_end) {
     n_end += n_start;
 }
 
+/*
+Threading based on nd_iterator.
+"Copy-paste" approach because of performance issues under Intel Compiler:
+More aggressive usage of templates/lambda usually causes performance degradation
+on Xeon Phi.
+In particular, #pagma opm parallel if(cond) may bring significant performance
+issue on KNL.
+*/
+template <typename T0, typename T1, typename F>
+void parallel_nd(const T0 D0, const T1 D1, F f) {
+#pragma omp parallel
+    {
+        size_t work_amount = (size_t)D0 * D1;
+        const int ithr = omp_get_thread_num();
+        const int nthr = omp_get_num_threads();
+        size_t start{0}, end{0};
+        balance211(work_amount, nthr, ithr, start, end);
+        T0 d0{0}; T1 d1{0};
+        utils::nd_iterator_init(start, d0, D0, d1, D1);
+        for (size_t iwork = start; iwork < end; ++iwork) {
+            f(d0, d1);
+            utils::nd_iterator_step(d0, D0, d1, D1);
+        }
+    }
+}
+template <typename T0, typename T1, typename T2, typename F>
+void parallel_nd(const T0 D0, const T1 D1, const T2 D2, F f) {
+#pragma omp parallel
+    {
+        size_t work_amount = (size_t)D0 * D1 * D2;
+        const int ithr = omp_get_thread_num();
+        const int nthr = omp_get_num_threads();
+        size_t start{0}, end{0};
+        balance211(work_amount, nthr, ithr, start, end);
+        T0 d0{0}; T1 d1{0}; T2 d2{0};
+        utils::nd_iterator_init(start, d0, D0, d1, D1, d2, D2);
+        for (size_t iwork = start; iwork < end; ++iwork) {
+            f(d0, d1, d2);
+            utils::nd_iterator_step(d0, D0, d1, D1, d2, D2);
+        }
+    }
+}
+template <typename T0, typename T1, typename T2, typename T3, typename F>
+void parallel_nd(const T0 D0, const T1 D1, const T2 D2, const T3 D3, F f) {
+#pragma omp parallel
+    {
+        size_t work_amount = (size_t)D0 * D1 * D2 * D3;
+        const int ithr = omp_get_thread_num();
+        const int nthr = omp_get_num_threads();
+        size_t start{0}, end{0};
+        balance211(work_amount, nthr, ithr, start, end);
+        T0 d0{0}; T1 d1{0}; T2 d2{0}; T3 d3{0};
+        utils::nd_iterator_init(start, d0, D0, d1, D1, d2, D2, d3, D3);
+        for (size_t iwork = start; iwork < end; ++iwork) {
+            f(d0, d1, d2, d3);
+            utils::nd_iterator_step(d0, D0, d1, D1, d2, D2, d3, D3);
+        }
+    }
+}
+template <typename T0, typename T1, typename T2, typename T3, typename T4,
+    typename F>
+void parallel_nd(const T0 D0, const T1 D1, const T2 D2, const T3 D3,
+        const T4 D4, F f) {
+#pragma omp parallel
+    {
+        size_t work_amount = (size_t)D0 * D1 * D2 * D3 * D4;
+        const int ithr = omp_get_thread_num();
+        const int nthr = omp_get_num_threads();
+        size_t start{0}, end{0};
+        balance211(work_amount, nthr, ithr, start, end);
+        T0 d0{0}; T1 d1{0}; T2 d2{0}; T3 d3{0}; T4 d4{0};
+        utils::nd_iterator_init(start, d0, D0, d1, D1, d2, D2, d3, D3, d4, D4);
+        for (size_t iwork = start; iwork < end; ++iwork) {
+            f(d0, d1, d2, d3, d4);
+            utils::nd_iterator_step(d0, D0, d1, D1, d2, D2, d3, D3, d4, D4);
+        }
+    }
+}
+template <typename T0, typename T1, typename T2, typename T3, typename T4,
+    typename T5, typename F>
+void parallel_nd(const T0 D0, const T1 D1, const T2 D2, const T3 D3,
+        const T4 D4, const T5 D5, F f) {
+#pragma omp parallel
+    {
+        size_t work_amount = (size_t)D0 * D1 * D2 * D3 * D4 * D5;
+        const int ithr = omp_get_thread_num();
+        const int nthr = omp_get_num_threads();
+        size_t start{0}, end{0};
+        balance211(work_amount, nthr, ithr, start, end);
+        T0 d0{0}; T1 d1{0}; T2 d2{0}; T3 d3{0}; T4 d4{0}; T5 d5{0};
+        utils::nd_iterator_init(start, d0, D0, d1, D1, d2, D2, d3, D3, d4, D4,
+            d5, D5);
+        for (size_t iwork = start; iwork < end; ++iwork) {
+            f(d0, d1, d2, d3, d4, d5);
+            utils::nd_iterator_step(d0, D0, d1, D1, d2, D2, d3, D3, d4, D4,
+                d5, D5);
+        }
+    }
+}
+
+/* For use inside of parallel section */
+
+template <typename T0, typename T1, typename F>
+void parallel_nd_in_omp(const T0 D0, const T1 D1, F f) {
+    size_t work_amount = (size_t)D0 * D1;
+    const int ithr = omp_get_thread_num();
+    const int nthr = omp_get_num_threads();
+    size_t start{0}, end{0};
+    balance211(work_amount, nthr, ithr, start, end);
+    T0 d0{0}; T1 d1{0};
+    utils::nd_iterator_init(start, d0, D0, d1, D1);
+    for (size_t iwork = start; iwork < end; ++iwork) {
+        f(d0, d1);
+        utils::nd_iterator_step(d0, D0, d1, D1);
+    }
+}
+template <typename T0, typename T1, typename T2, typename F>
+void parallel_nd_in_omp(const T0 D0, const T1 D1, const T2 D2, F f) {
+    size_t work_amount = (size_t)D0 * D1 * D2;
+    const int ithr = omp_get_thread_num();
+    const int nthr = omp_get_num_threads();
+    size_t start{0}, end{0};
+    balance211(work_amount, nthr, ithr, start, end);
+    T0 d0{0}; T1 d1{0}; T2 d2{0};
+    utils::nd_iterator_init(start, d0, D0, d1, D1, d2, D2);
+    for (size_t iwork = start; iwork < end; ++iwork) {
+        f(d0, d1, d2);
+        utils::nd_iterator_step(d0, D0, d1, D1, d2, D2);
+    }
+}
+template <typename T0, typename T1, typename T2, typename T3, typename F>
+void parallel_nd_in_omp(const T0 D0, const T1 D1, const T2 D2, const T3 D3,
+    F f) {
+    size_t work_amount = (size_t)D0 * D1 * D2 * D3;
+    const int ithr = omp_get_thread_num();
+    const int nthr = omp_get_num_threads();
+    size_t start{0}, end{0};
+    balance211(work_amount, nthr, ithr, start, end);
+    T0 d0{0}; T1 d1{0}; T2 d2{0}; T3 d3{0};
+    utils::nd_iterator_init(start, d0, D0, d1, D1, d2, D2, d3, D3);
+    for (size_t iwork = start; iwork < end; ++iwork) {
+        f(d0, d1, d2, d3);
+        utils::nd_iterator_step(d0, D0, d1, D1, d2, D2, d3, D3);
+    }
+}
+template <typename T0, typename T1, typename T2, typename T3, typename T4,
+    typename F>
+void parallel_nd_in_omp(const T0 D0, const T1 D1, const T2 D2, const T3 D3,
+    const T4 D4, F f) {
+    size_t work_amount = (size_t)D0 * D1 * D2 * D3 * D4;
+    const int ithr = omp_get_thread_num();
+    const int nthr = omp_get_num_threads();
+    size_t start{0}, end{0};
+    balance211(work_amount, nthr, ithr, start, end);
+    T0 d0{0}; T1 d1{0}; T2 d2{0}; T3 d3{0}; T4 d4{0};
+    utils::nd_iterator_init(start, d0, D0, d1, D1, d2, D2, d3, D3, d4, D4);
+    for (size_t iwork = start; iwork < end; ++iwork) {
+        f(d0, d1, d2, d3, d4);
+        utils::nd_iterator_step(d0, D0, d1, D1, d2, D2, d3, D3, d4, D4);
+    }
+}
+template <typename T0, typename T1, typename T2, typename T3, typename T4,
+    typename T5, typename F>
+void parallel_nd_in_omp(const T0 D0, const T1 D1, const T2 D2, const T3 D3,
+    const T4 D4, const T5 D5, F f) {
+    size_t work_amount = (size_t)D0 * D1 * D2 * D3 * D4 * D5;
+    const int ithr = omp_get_thread_num();
+    const int nthr = omp_get_num_threads();
+    size_t start{0}, end{0};
+    balance211(work_amount, nthr, ithr, start, end);
+    T0 d0{0}; T1 d1{0}; T2 d2{0}; T3 d3{0}; T4 d4{0}; T5 d5{0};
+    utils::nd_iterator_init(start, d0, D0, d1, D1, d2, D2, d3, D3, d4, D4,
+        d5, D5);
+    for (size_t iwork = start; iwork < end; ++iwork) {
+        f(d0, d1, d2, d3, d4, d5);
+        utils::nd_iterator_step(d0, D0, d1, D1, d2, D2, d3, D3, d4, D4, d5, D5);
+    }
+}
+
 } // namespace impl
 } // namespace mkldnn
 
