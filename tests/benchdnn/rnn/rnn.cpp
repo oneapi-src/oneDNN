e36521fe935f7c277396de6d5478c6555ea7bdb5
Mourad Gouicem <mourad.gouicem@intel.com>
2018-08-24 10:48:15 -0700

    tests: fix an OS build issue

--- tests/benchdnn/rnn/rnn.cpp
+++ tests/benchdnn/rnn/rnn.cpp
@@ -19,7 +19,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <random>
-#include "omp.h"
+#if defined(_OPENMP)
+#include <omp.h>
+#else // defined(_OPENMP)
+inline int omp_get_max_threads() { return 1; }
+inline int omp_get_num_threads() { return 1; }
+inline int omp_get_thread_num() { return 0; }
+inline int omp_in_parallel() { return 0; }
+#endif
+
 #include "mkldnn.h"
 
 #include "mkldnn_common.hpp"
