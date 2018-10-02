72dcfe80bcd39681e601aaec902afbca6e50f2aa
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2018-10-02 00:22:17 +0000

    build: compile with -openmp whenever possible
    even when compile with TBB to be able to use `#pragma omp simd`

--- cmake/OpenMP.cmake
+++ cmake/OpenMP.cmake
@@ -22,19 +22,33 @@ if(OpenMP_cmake_included)
 endif()
 set(OpenMP_cmake_included true)
 
-if(NOT MKLDNN_THREADING STREQUAL "OMP")
-    return()
-endif()
-
 include("cmake/MKL.cmake")
+
+macro(forbid_link_compiler_omp_rt)
+    if (NOT WIN32)
+        set_if(OpenMP_C_FOUND CMAKE_C_CREATE_SHARED_LIBRARY_FORBIDDEN_FLAGS ${OpenMP_C_FLAGS})
+        set_if(OpenMP_CXX_FOUND CMAKE_CXX_CREATE_SHARED_LIBRARY_FORBIDDEN_FLAGS ${OpenMP_CXX_FLAGS})
+    endif()
+endmacro()
+
+macro(use_intel_omp_rt)
+    # Do not link with compiler-native OpenMP library if Intel MKL is present.
+    # Rationale: Intel MKL comes with Intel OpenMP library which is compatible
+    # with all libraries shipped with compilers that Intel MKL-DNN supports.
+    if(HAVE_MKL AND NOT WIN32 AND NOT CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+        forbid_link_compiler_omp_rt()
+        list(APPEND EXTRA_LIBS ${MKLIOMP5LIB})
+    endif()
+endmacro()
 
 if(WIN32 AND ${CMAKE_CXX_COMPILER_ID} STREQUAL MSVC)
     add_definitions(/Qpar)
     add_definitions(/openmp)
-    add_definitions(-DMKLDNN_THR=MKLDNN_THR_OMP)
+    set(OpenMP_CXX_FOUND true)
 elseif(MSVC AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     append(CMAKE_C_FLAGS "-Xclang -fopenmp")
     append(CMAKE_CXX_FLAGS "-Xclang -fopenmp")
+    set(OpenMP_CXX_FOUND true)
     list(APPEND EXTRA_LIBS ${MKLIOMP5LIB})
 else()
     find_package(OpenMP)
@@ -50,18 +64,19 @@ else()
     endif()
     append_if(OpenMP_C_FOUND CMAKE_C_FLAGS "${OpenMP_C_FLAGS}")
     append_if(OpenMP_CXX_FOUND CMAKE_CXX_FLAGS "${OpenMP_CXX_FLAGS}")
-    if(OpenMP_CXX_FOUND)
-        add_definitions(-DMKLDNN_THR=MKLDNN_THR_OMP)
-    endif()
 endif()
 
-# Do not link with compiler-native OpenMP library if MKL is present.
-# Rationale: MKL comes with Intel OpenMP library which is compatible with all
-# libraries shipped with compilers that MKL-DNN supports.
-if(HAVE_MKL AND NOT WIN32 AND NOT CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
-    set_if(OpenMP_C_FOUND CMAKE_C_CREATE_SHARED_LIBRARY_FORBIDDEN_FLAGS ${OpenMP_C_FLAGS})
-    set_if(OpenMP_CXX_FOUND CMAKE_CXX_CREATE_SHARED_LIBRARY_FORBIDDEN_FLAGS ${OpenMP_CXX_FLAGS})
-    list(APPEND EXTRA_LIBS ${MKLIOMP5LIB})
+if (MKLDNN_THREADING STREQUAL "OMP")
+    if (NOT OpenMP_CXX_FOUND)
+        message(FATAL_ERROR "OpenMP library could not be found")
+    endif()
+    add_definitions(-DMKLDNN_THR=MKLDNN_THR_OMP)
+    use_intel_omp_rt()
+else()
+    # Compilation happens with OpenMP to enable `#pragma omp simd`
+    # but during linkage OpenMP dependency should be avoided
+    forbid_link_compiler_omp_rt()
+    return()
 endif()
 
 set_ternary(_omp_lib_msg MKLIOMP5LIB "${MKLIOMP5LIB}" "provided by compiler")
