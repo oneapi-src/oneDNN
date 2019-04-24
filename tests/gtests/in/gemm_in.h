345462cb093882f8883134fe0e1d3f68e4fa2f23
Peter Caday <peter.caday@intel.com>
2019-04-24 08:47:34 -0700

    tests: gemm: add transpose B blocking test

--- tests/gtests/in/gemm_in.h
+++ tests/gtests/in/gemm_in.h
@@ -14,6 +14,7 @@ INST_TEST_CASE(TestGEMM,
     test_params{'t', 'n', 2, 100, 100, 1.0, 2.0, 100, 100, 100, {}, false},
     test_params{'t', 't', 2, 100, 100, 1.0, 2.0, 100, 100, 100, {}, false},
     test_params{'n', 'n', 2, 2, 10000, 1.0, 2.0, 2, 10000, 2, {}, false},
+    test_params{'t', 't', 2, 2, 10000, 1.0, 2.0, 10000, 2, 2, {}, false},
 
     make_test_params_with_offset({1, 2, 3}, 'n', 'n', 100, 100, 2, 1.0f, 2.0f, 100, 100, 100),
     make_test_params_with_offset({30, 20, 10}, 'n', 't', 100, 2, 100, 1.0f, 2.0f, 100, 100, 100),
