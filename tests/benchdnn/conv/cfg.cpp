33051136c3652a975926b5bd55075b763d0c36a5
Andrey Kharitonchik <andrey.kharitonchik@intel.com>
2019-05-17 19:08:01 -0700

    tests: benchdnn: supported bf16 conv testing

--- tests/benchdnn/conv/cfg.cpp
+++ tests/benchdnn/conv/cfg.cpp
@@ -75,6 +75,41 @@ const _dt_conf_t conf_f32_wino = {
     {mkldnn_f32,},
 };
 
+const _dt_conf_t conf_bf16bf16f32 = {
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_f32,  -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_f32,  -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_f32,},
+};
+
+const _dt_conf_t conf_bf16bf16bf16 = {
+    /* eps is 1e-2 because of loss in precision of
+     * output when converted from fp32 to bf16.
+     * MKL-DNN output is compared against reference computed in fp32.*/
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 1e-2},
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 1e-2},
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 1e-2},
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 1e-2},
+    {mkldnn_f32,},
+};
+
+const _dt_conf_t conf_f32bf16bf16 = {
+    {mkldnn_f32,  -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_f32,},
+};
+
+const _dt_conf_t conf_bf16f32bf16 = {
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_f32,  -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_f32,  -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_bf16, -int_max_exact, int_max_exact, -32, 32, 0, 1, .75, 0.},
+    {mkldnn_f32,},
+};
+
 const _dt_conf_t conf_u8s8f32s32 = {
     {mkldnn_u8,          0, UINT8_MAX,    0,   8, 0, 1, .25, 0.},
     {mkldnn_s8,   INT8_MIN,  INT8_MAX,   -5,   5, 0, 1, .25, 0.},
@@ -191,6 +226,10 @@ const dt_conf_t *str2cfg(const char *str) {
     CASE(u8s8s32s32_wino);
     CASE(u8s8s8s32_wino);
     CASE(u8s8u8s32_wino);
+    CASE(bf16bf16f32);
+    CASE(bf16bf16bf16);
+    CASE(f32bf16bf16);
+    CASE(bf16f32bf16);
 #undef CASE
     []() { SAFE(FAIL, CRIT); return 0; }();
     return (const dt_conf_t *)1;
@@ -215,6 +254,10 @@ const char *cfg2str(const dt_conf_t *cfg) {
     CASE(u8s8s32s32_wino);
     CASE(u8s8s8s32_wino);
     CASE(u8s8u8s32_wino);
+    CASE(bf16bf16f32);
+    CASE(bf16bf16bf16);
+    CASE(f32bf16bf16);
+    CASE(bf16f32bf16);
 #undef CASE
     []() { SAFE(FAIL, CRIT); return 0; }();
     return NULL;
--- tests/benchdnn/conv/conv.cpp
+++ tests/benchdnn/conv/conv.cpp
@@ -152,7 +152,8 @@ inline int compare_dat(const prb_t *p, data_kind_t kind, dnn_mem_t &mem_dt,
         const float fp0 = ((float*)mem_fp)[i];
 
         float fp = fp0;
-        if (p->cfg[kind].dt != mkldnn_f16 && p->cfg[kind].dt != mkldnn_f32)
+        if (p->cfg[kind].dt != mkldnn_f16 && p->cfg[kind].dt != mkldnn_f32
+		        && p->cfg[kind].dt != mkldnn_bf16)
             fp = mxcsr_round(fp0);
 
         const float diff = fabsf(fp - dt);
