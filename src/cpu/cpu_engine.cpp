54548a5a57f11db9c4f9dab3c42717ea603a4c92
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2019-05-20 12:57:12 -0700

    src: cpu: softmax: add jit impl

--- src/cpu/cpu_engine.cpp
+++ src/cpu/cpu_engine.cpp
@@ -65,6 +65,7 @@
 #include "cpu/jit_avx512_core_u8s8s32x_wino_convolution.hpp"
 #include "cpu/jit_avx512_core_fp32_wino_conv_2x3.hpp"
 #include "cpu/jit_uni_batch_normalization_s8.hpp"
+#include "cpu/jit_uni_softmax.hpp"
 
 namespace mkldnn {
 namespace impl {
@@ -212,6 +213,9 @@ static const pd_create_f cpu_impl_list[] = {
     INSTANCE(ref_eltwise_fwd_t<u8>),
     INSTANCE(ref_eltwise_bwd_t<s32>),
     /* softmax */
+    INSTANCE(jit_uni_softmax_fwd_t<avx512_common>),
+    INSTANCE(jit_uni_softmax_fwd_t<avx2>),
+    INSTANCE(jit_uni_softmax_fwd_t<sse41>),
     INSTANCE(ref_softmax_fwd_t<f32>),
     INSTANCE(ref_softmax_bwd_t<f32>),
     /* pool */
