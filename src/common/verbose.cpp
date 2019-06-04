b27939bb9c7eeab4b2d9705459e74fdec9a02d3e
Roman Dubtsov <roman.s.dubtsov@intel.com>
2019-06-04 14:33:29 -0700

    temporary: port mkldnn_getenv from master

--- src/common/verbose.cpp
+++ src/common/verbose.cpp
@@ -53,7 +53,7 @@ const verbose_t *mkldnn_verbose() {
     if (!initialized) {
         const int len = 2;
         char val[len] = {0};
-        if (mkldnn_getenv(val, "MKLDNN_VERBOSE", len) == 1)
+        if (mkldnn_getenv("MKLDNN_VERBOSE", val, len) == 1)
             verbose.level = atoi(val);
         initialized = true;
     }
