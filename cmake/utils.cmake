d2f86520e47a7c82d5319f3b5645507fb8cd1e9d
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2019-06-15 16:59:09 -0700

    build: generate vcxproj.user for VS CMake generator only

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -28,8 +28,10 @@ function(maybe_configure_windows_test name kind)
     if((WIN32 OR MINGW) AND (NOT MKLDNN_BUILD_FOR_CI))
         string(REPLACE  ";" "\;" PATH "${CTESTCONFIG_PATH};$ENV{PATH}")
         set_property(${kind} ${name} PROPERTY ENVIRONMENT "PATH=${PATH}")
-        configure_file(${PROJECT_SOURCE_DIR}/cmake/template.vcxproj.user
-            ${name}.vcxproj.user @ONLY)
+        if(CMAKE_GENERATOR MATCHES "Visual Studio")
+            configure_file(${PROJECT_SOURCE_DIR}/cmake/template.vcxproj.user
+                ${name}.vcxproj.user @ONLY)
+        endif()
     endif()
 endfunction()
 
