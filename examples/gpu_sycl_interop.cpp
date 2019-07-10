3af59fb4cadb76160ceba8cbcaa03673b6183c5d
ltsai1 <louie.tsai@intel.com>
2019-07-10 16:20:17 -0700

    example: add a new GPU example using SYCL extension API

---
+++ examples/gpu_sycl_interop.cpp
@@ -0,0 +1,247 @@
+/*******************************************************************************
+* Copyright 2019 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+/// @example  gpu_sycl_interop.cpp
+/// Annotated version: @ref gpu_sycl_interop_cpp
+///
+/// @page  gpu_sycl_interop_cpp Getting started on GPU with SYCL extensions API
+/// Full example text: @ref gpu_sycl_interop.cpp
+///
+/// This C++ API example demonstrates programming for Intel(R) Processor
+/// Graphics with SYCL extensions API in Intel MKL-DNN.
+/// The workflow includes following steps:
+///   - Create a GPU engine. It uses SYCL as the runtime in this sample.
+///   - Create a GPU memory descriptor/object.
+///   - Create a SYCL kernel for GPU data initialization.
+///   - Access a GPU memory via SYCL interoperability interface.
+///   - Access a GPU queue via SYCL interoperability interface.
+///   - Execute a SYCL kernel with related GPU queue and GPU memory.
+///   - Create operation descriptor/operation primitives descriptor/primitive.
+///   - Execute the primitive with the initialized GPU memory.
+///   - Validate the result through a host accessor.
+///
+/// @page gpu_sycl_interop_cpp
+
+/// @section gpu_sycl_interop_cpp_headers Public headers
+///
+/// To start using Intel MKL-DNN, we must first include the @ref mkldnn.hpp
+/// header file in the application. We also include CL/sycl.hpp from DPC++ for
+/// using SYCL APIs and @ref mkldnn_debug.h, which  contains some debugging
+/// facilities such as returning a string representation
+/// for common Intel MKL-DNN C types.
+/// All C++ API types and functions reside in the `mkldnn` namespace, and
+/// SYCL API types and functions reside in the `cl::sycl` namespace.
+/// For simplicity of the example we import both namespaces.
+/// @page gpu_sycl_interop_cpp
+/// @snippet  gpu_sycl_interop.cpp Prologue
+// [Prologue]
+
+#include <CL/sycl.hpp>
+#include <mkldnn.hpp>
+#include "mkldnn_debug.h"
+
+#include <cassert>
+#include <iostream>
+#include <numeric>
+
+using namespace mkldnn;
+using namespace cl::sycl;
+// [Prologue]
+
+class kernel_tag;
+
+/// @page gpu_sycl_interop_cpp
+/// @section gpu_sycl_interop_cpp_tutorial gpu_sycl_interop_tutorial() function
+/// @page gpu_sycl_interop_cpp
+void gpu_sycl_interop_tutorial() {
+
+    /// @page gpu_sycl_interop_cpp
+    /// @subsection gpu_sycl_interop_cpp_sub1 Engine and stream
+    ///
+    /// All Intel MKL-DNN primitives and memory objects are attached to a
+    /// particular @ref mkldnn::engine, which is an abstraction of a
+    /// computational device (see also @ref dev_guide_basic_concepts). The
+    /// primitives are created and optimized for the device to which they are
+    /// attached, and the memory objects refer to memory residing on the
+    /// corresponding device. In particular, that means neither memory objects
+    /// nor primitives that were created for one engine can be used on
+    /// another.
+    ///
+    /// To create engines, we must specify the @ref mkldnn::engine::kind
+    /// and the index of the device of the given kind. In this example we use
+    /// the first available GPU engine, so the index for the engine is 0.
+    /// This example assumes SYCL being a runtime for GPU. In such case,
+    /// during engine creation, an SYCL context is also created and attaches
+    /// to the created engine.
+    ///
+    /// @snippet  gpu_sycl_interop.cpp Initialize engine
+    // [Initialize engine]
+    engine eng(engine::kind::gpu, 0);
+    // [Initialize engine]
+
+    /// In addition to an engine, all primitives require a @ref mkldnn::stream
+    /// for the execution. The stream encapsulates an execution context and is
+    /// tied to a particular engine.
+    ///
+    /// In this example, a GPU stream is created.
+    /// This example assumes SYCL being a runtime for GPU. During stream creation,
+    /// a SYCL queue is also created and attaches to this stream.
+    ///
+    /// @snippet  gpu_sycl_interop.cpp Initialize stream
+    // [Initialize stream]
+    mkldnn::stream strm(eng);
+    // [Initialize stream]
+
+    /// @subsection  gpu_sycl_interop_cpp_sub2 Wrapping data into Intel MKL-DNN memory object
+    ///
+    /// Next, we create a memory object. We need to specify dimensions of our
+    /// memory by passing a memory::dims object. Then we create a memory
+    /// descriptor with these dimensions, with the mkldnn::memory::data_type::f32
+    /// data type, and with the mkldnn::memory::format_tag::nchw memory format.
+    /// Finally, we construct a memory object and pass the memory descriptor.
+    /// The library allocates memory internally.
+    /// @snippet  gpu_sycl_interop.cpp memory alloc
+    //  [memory alloc]
+    memory::dims tz_dims = { 2, 3, 4, 5 };
+    const size_t N = std::accumulate(tz_dims.begin(), tz_dims.end(), (size_t)1,
+            std::multiplies<size_t>());
+
+    memory::desc mem_d(
+            tz_dims, memory::data_type::f32, memory::format_tag::nchw);
+
+    memory mem(mem_d, eng);
+    //  [memory alloc]
+
+    /// @subsection  gpu_sycl_interop_cpp_sub3 Initialize the data executing a custom SYCL kernel
+    ///
+    /// The underlying SYCL buffer can be extracted from the memory object using
+    /// the interoperability interface: `mkldnn::memory::get_sycl_buffer<T>()`.
+    ///
+    /// @snippet  gpu_sycl_interop.cpp get sycl buf
+    // [get sycl buf]
+    auto sycl_buf = mem.get_sycl_buffer<float>();
+    // [get sycl buf]
+
+    /// We are going to create an SYCL kernel that should initialize our data.
+    /// To execute SYCL kernel we need a SYCL queue.
+    /// For simplicity we can construct a stream and extract the SYCL queue from it.
+    /// The kernel initializes the data by the `0, -1, 2, -3, ...` sequence: `data[i] = (-1)^i * i`.
+    /// @snippet gpu_sycl_interop.cpp sycl kernel exec
+    // [sycl kernel exec]
+    queue q = strm.get_sycl_queue();
+    q.submit([&](handler &cgh) {
+        auto a = sycl_buf.get_access<access::mode::write>(cgh);
+        cgh.parallel_for<kernel_tag>(range<1>(N), [=](id<1> i) {
+            int idx = i[0];
+            a[idx] = (idx % 2) ? -idx : idx;
+        });
+    });
+    // [sycl kernel exec]
+
+    /// @subsection gpu_sycl_interop_cpp_sub4 Create and execute a primitive
+    /// There are three steps to create an operation primitive in Intel MKL-DNN:
+    /// 1. Create an operation descriptor.
+    /// 2. Create a primitive descriptor.
+    /// 3. Create a primitive.
+    ///
+    /// Let's create the primitive to perform the ReLU (rectified linear unit)
+    /// operation: x = max(0, x). An operation descriptor has no dependency on a
+    /// specific engine - it just describes some operation. On the contrary,
+    /// primitive descriptors are attached to a specific engine and represent
+    /// some implementation for this engine. A primitive object is a realization
+    /// of a primitive descriptor, and its construction is usually much
+    /// "heavier".
+    /// @snippet gpu_sycl_interop.cpp relu creation
+    //  [relu creation]
+    auto relu_d = eltwise_forward::desc(
+            prop_kind::forward, algorithm::eltwise_relu, mem_d, 0.0f);
+    auto relu_pd = eltwise_forward::primitive_desc(relu_d, eng);
+    auto relu = eltwise_forward(relu_pd);
+    //  [relu creation]
+
+    /// Next, execute the primitive.
+    /// @snippet gpu_sycl_interop.cpp relu exec
+    // [relu exec]
+    relu.execute(strm, { { MKLDNN_ARG_SRC, mem }, { MKLDNN_ARG_DST, mem } });
+    strm.wait();
+    // [relu exec]
+    ///
+    ///@note
+    ///    With SYCL runtime, both CPU and GPU have asynchronous execution; However, the user can
+    ///    call mkldnn::stream::wait() to synchronize the stream and ensure that all
+    ///    previously submitted primitives are completed.
+    ///
+
+    /// @page gpu_sycl_interop_cpp
+    /// @subsection gpu_sycl_interop_cpp_sub5 Validate the results
+    ///
+    /// Before running validation codes, we need to access the SYCL memory on
+    /// the host.
+    /// The simplest way to access the SYCL-backed memory on the host is to
+    /// construct a host accessor. Then we can directly read and write this data on the host.
+    /// However no any conflicting operations are allowed until the host accessor is destroyed.
+    /// We can run validation codes on the host accordingly.
+    /// @snippet gpu_sycl_interop.cpp Check the results
+    // [Check the results]
+    auto host_acc = sycl_buf.get_access<access::mode::read>();
+    for (size_t i = 0; i < N; i++) {
+        float exp_value = (i % 2) ? 0.0f : i;
+        if (host_acc[i] != (float)exp_value)
+            throw std::string(
+                    "Unexpected output, find a negative value after the ReLU "
+                    "execution");
+    }
+    // [Check the results]
+}
+
+/// @section gpu_sycl_interop_cpp_main main() function
+///
+/// We now just call everything we prepared earlier.
+///
+/// Because we are using the Intel MKL-DNN C++ API, we use exceptions to handle
+/// errors (see @ref dev_guide_c_and_cpp_apis). The Intel MKL-DNN C++ API throws
+/// exceptions of type @ref mkldnn::error, which contains the error status
+/// (of type @ref mkldnn_status_t) and a human-readable error message accessible
+/// through the regular `what()` method.
+/// @page gpu_sycl_interop_cpp
+/// @snippet gpu_sycl_interop.cpp Main
+// [Main]
+int main(int argc, char **argv) {
+    try {
+        gpu_sycl_interop_tutorial();
+    } catch (mkldnn::error &e) {
+        std::cerr << "Intel MKL-DNN error: " << e.what() << std::endl
+                  << "Error status: " << mkldnn_status2str(e.status)
+                  << std::endl;
+        return 1;
+    } catch (std::string &e) {
+        std::cerr << "Error in the example: " << e << std::endl;
+        return 2;
+    }
+
+    std::cout << "Example passes" << std::endl;
+    return 0;
+}
+// [Main]
+/// <b></b>
+///
+/// Upon compiling and running the example, the output should be just:
+///
+/// ~~~
+/// Example passes
+/// ~~~
+///
+/// @page gpu_sycl_interop_cpp
