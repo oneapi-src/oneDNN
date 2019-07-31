68115c60a91c999b6084538596e8b135526a9c7e
Pirogov, Vadim <vadim.o.pirogov@intel.com>
2019-07-31 09:38:25 -0700

    common: cleaned up verbose output

--- src/common/verbose.cpp
+++ src/common/verbose.cpp
@@ -40,22 +40,19 @@
 #include "lrn_pd.hpp"
 #include "gemm_pd.hpp"
 
-/* MKL-DNN CPU ISA info */
-#define ISA_ANY "No instruction set specific optimizations"
-#define SSE41 "Intel(R) Streaming SIMD Extensions 4.1 (Intel(R) SSE4.1)"
-#define AVX "Intel(R) Advanced Vector Extensions (Intel(R) AVX)"
-#define AVX2 "Intel(R) Advanced Vector Extensions 2 (Intel(R) AVX2)"
-#define AVX512_COMMON "Intel(R) Advanced Vector Extensions 512 (Intel(R) " \
-                      "AVX-512)"
-#define AVX512_CORE "Intel(R) Advanced Vector Extensions 512 (Intel(R) " \
-                    "AVX-512) with AVX512BW, AVX512VL, and AVX512DQ extensions"
-#define AVX512_CORE_VNNI "Intel(R) Advanced Vector Instructions (Intel(R) " \
-                         "AVX512) and Intel Deep Learning Boost (Intel(R) " \
-                         "DL Boost)"
-#define AVX512_MIC "Intel(R) Advanced Vector Extensions 512 (Intel(R) " \
-                   "AVX-512) with AVX512CD, AVX512ER, and AVX512PF extensions"
-#define AVX512_MIC_4OPS "Intel(R) Advanced Vector Extensions 512 (Intel(R) " \
-                   "AVX-512) with AVX512_4FMAPS and AVX512_4VNNIW extensions"
+/* Intel MKL-DNN CPU ISA info */
+#define ISA_ANY "Intel 64"
+#define SSE41 "Intel SSE4.1"
+#define AVX "Intel AVX"
+#define AVX2 "Intel AVX2"
+#define AVX512_COMMON "Intel AVX-512"
+#define AVX512_CORE \
+    "Intel AVX-512 with AVX512BW, AVX512VL, and AVX512DQ extensions"
+#define AVX512_CORE_VNNI "Intel AVX-512 with Intel DL Boost"
+#define AVX512_MIC \
+    "Intel AVX-512 with AVX512CD, AVX512ER, and AVX512PF extensions"
+#define AVX512_MIC_4OPS \
+    "Intel AVX-512 with AVX512_4FMAPS and AVX512_4VNNIW extensions"
 
 namespace mkldnn {
 namespace impl {
@@ -74,12 +71,11 @@ const verbose_t *mkldnn_verbose() {
         initialized = true;
     }
     if (!version_printed && verbose.level > 0) {
-         printf("mkldnn_verbose,info,"
-                 "Intel(R) MKL-DNN v%d.%d.%d (Git Hash %s),%s\n",
+        printf("mkldnn_verbose,info,Intel MKL-DNN v%d.%d.%d (commit %s)\n",
                 mkldnn_version()->major, mkldnn_version()->minor,
-                mkldnn_version()->patch, mkldnn_version()->hash,
-                get_isa_info());
-         version_printed = true;
+                mkldnn_version()->patch, mkldnn_version()->hash);
+        printf("mkldnn_verbose,info,Detected ISA is %s\n", get_isa_info());
+        version_printed = true;
     }
 #else
     verbose.level = 0;
