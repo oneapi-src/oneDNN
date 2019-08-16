5eea0b3386e6133e0ca18dfaa7e30fe23947e6d5
Roy Oursler <roy.j.oursler@intel.com>
2019-08-16 16:02:12 -0700

    cpu: Fix bf16 verbose output for batch normalization

--- src/cpu/jit_uni_batch_normalization.hpp
+++ src/cpu/jit_uni_batch_normalization.hpp
@@ -44,7 +44,13 @@ struct jit_uni_batch_normalization_fwd_t : public primitive_impl_t {
             : cpu_batch_normalization_fwd_pd_t(
                     engine, adesc, attr, hint_fwd_pd) {}
 
-        DECLARE_COMMON_PD_T(JIT_IMPL_NAME_HELPER("bnorm_jit:", isa, ""),
+        DECLARE_COMMON_PD_T(
+                JIT_IMPL_NAME_HELPER("bnorm_jit:",
+                        (this->desc()->data_desc.data_type == data_type::bf16)
+                                ? (mayiuse(avx512_core_bf16) ? avx512_core_bf16
+                                                             : avx512_core)
+                                : isa,
+                        ""),
                 jit_uni_batch_normalization_fwd_t);
 
         status_t init();
@@ -70,7 +76,13 @@ struct jit_uni_batch_normalization_bwd_t : public primitive_impl_t {
             : cpu_batch_normalization_bwd_pd_t(
                     engine, adesc, attr, hint_fwd_pd) {}
 
-        DECLARE_COMMON_PD_T(JIT_IMPL_NAME_HELPER("bnorm_jit:", isa, ""),
+        DECLARE_COMMON_PD_T(
+                JIT_IMPL_NAME_HELPER("bnorm_jit:",
+                        (this->desc()->data_desc.data_type == data_type::bf16)
+                                ? (mayiuse(avx512_core_bf16) ? avx512_core_bf16
+                                                             : avx512_core)
+                                : isa,
+                        ""),
                 jit_uni_batch_normalization_bwd_t);
 
         status_t init();
