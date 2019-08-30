6e4df48555f1e7bdc1278b86949716feaa91123a
Srinivas Putta <srinivas.putta@intel.com>
2019-08-29 17:19:25 -0700

    src: cpu: dw: bwd_d: fixup stride_w

--- src/cpu/jit_uni_dw_convolution.cpp
+++ src/cpu/jit_uni_dw_convolution.cpp
@@ -204,6 +204,8 @@ void _jit_uni_dw_convolution_bwd_data_t<isa, diff_dst_type,
         return par_conv;
     };
 
+    const int aux_w
+            = nstl::min(jcp.iw, jcp.iw - jcp.kw + jcp.r_pad + jcp.stride_w);
     const int chb_work = utils::div_up(jcp.nb_ch, jcp.nb_ch_blocking);
     parallel_nd(jcp.mb, chb_work, jcp.ih,
         [&](int n, int chb, int ih) {
@@ -233,8 +235,7 @@ void _jit_uni_dw_convolution_bwd_data_t<isa, diff_dst_type,
             }
 
             // main loop
-            ur_str_w = nstl::min((jcp.iw - jcp.kw + jcp.r_pad - iw)
-                 / jcp.stride_w, jcp.iw);
+            ur_str_w = (aux_w - iw) / jcp.stride_w;
             if (ur_str_w > 0) {
                 jit_conv_call_s par_conv = kernel_params(ur_str_w, iw, oh,
                                              ih, i_t_overflow, i_b_overflow,
