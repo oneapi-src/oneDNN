d2bbe352c3d64f181ca1cc03be38f80bfd9257c3
Wierschem, Keola <keola.wierschem@intel.com>
2019-09-18 13:47:17 -0700

    cpu: conv: avoid zmm usage in xmm and ymm versions of int8 jit

--- src/cpu/jit_avx512_core_x8s8s32x_conv_kernel.cpp
+++ src/cpu/jit_avx512_core_x8s8s32x_conv_kernel.cpp
@@ -217,15 +217,7 @@ void _jit_avx512_core_x8s8s32x_fwd_kernel<Vmm>::store_output(
                 vmaxps(vmm, vmm_zero, vmm);
             }
 
-            if (jcp.dst_dt != data_type::f32) {
-                /* Note: using Zmm for rounding in Xmm/Ymm kernel
-                   because there is no instruction to do rounding
-                   from Xmm/Ymm -> Xmm/Ymm.
-                   Embedded rounding is not supported for Xmm.
-                   TODO: maybe avoid Zmm if it helps performance.*/
-                Zmm zmm = zmm_out(j, k);
-                vcvtps2dq(zmm, zmm);
-            }
+            if (jcp.dst_dt != data_type::f32) vcvtps2dq(vmm, vmm);
         }
 
         for (int j = 0; j < ur_w; j++) {
