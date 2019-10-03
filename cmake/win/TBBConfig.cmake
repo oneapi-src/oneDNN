e787043a057a3fa38d35390996d361f40fa68cfa
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2019-10-02 23:15:17 -0700

    copyright: update to 2019, fix some banners

--- cmake/win/TBBConfig.cmake
+++ cmake/win/TBBConfig.cmake
@@ -1,4 +1,5 @@
-# Copyright (c) 2017-2018 Intel Corporation
+#===============================================================================
+# Copyright 2017-2019 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,10 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#
-#
-#
+#===============================================================================
 
 # TBB_FOUND should not be set explicitly. It is defined automatically by CMake.
 # Handling of TBB_VERSION is in TBBConfigVersion.cmake.
