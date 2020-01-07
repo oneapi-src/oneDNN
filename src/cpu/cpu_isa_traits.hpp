923bfb5f5f84353217d43ae294bca24e3788af19
Andrey Kharitonchik <andrey.kharitonchik@intel.com>
2020-01-07 11:50:33 -0800

    src: cpu: get_cache_size() is moved to cpu_isa_traits

--- src/cpu/cpu_isa_traits.hpp
+++ src/cpu/cpu_isa_traits.hpp
@@ -21,6 +21,7 @@
 
 #include "dnnl_types.h"
 
+#include "dnnl_thread.hpp"
 #include "utils.hpp"
 
 #define XBYAK64
@@ -185,6 +186,29 @@ static inline bool mayiuse(const cpu_isa_t cpu_isa, bool soft = false) {
 
 inline bool isa_has_bf16(cpu_isa_t isa) {
     return isa == avx512_core_bf16;
+}
+
+inline unsigned int get_cache_size(int level, bool per_core = true) {
+    unsigned int l = level - 1;
+    // Currently, if XByak is not able to fetch the cache topology
+    // we default to 32KB of L1, 512KB of L2 and 1MB of L3 per core.
+    if (cpu.getDataCacheLevels() == 0) {
+        const int L1_cache_per_core = 32000;
+        const int L2_cache_per_core = 512000;
+        const int L3_cache_per_core = 1024000;
+        int num_cores = per_core ? 1 : dnnl_get_max_threads();
+        switch (l) {
+            case (0): return L1_cache_per_core * num_cores;
+            case (1): return L2_cache_per_core * num_cores;
+            case (2): return L3_cache_per_core * num_cores;
+            default: return 0;
+        }
+    }
+    if (l < cpu.getDataCacheLevels()) {
+        return cpu.getDataCacheSize(l)
+                / (per_core ? cpu.getCoresSharingDataCache(l) : 1);
+    } else
+        return 0;
 }
 
 } // namespace
