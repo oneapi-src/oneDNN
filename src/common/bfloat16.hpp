593aa56edb1645cbecd8d651b3a5f9ada46d429f
Niels Dekker <C.E.Dekker@lumc.nl>
2020-02-10 14:44:27 +0100

    common: prevent constructing signaling nans and denormals bfloat16
    Adjusts `bfloat16_t(uint16_t, bool)` constructor according to "BFLOAT16 – Hardware Numerics Definition" White Paper, Intel, November 2018:
    
    > Treat denormal source as zero
      ...
    > NaNs and Infinities are supported normally (with SNaNs getting masked response)

--- src/common/bfloat16.hpp
+++ src/common/bfloat16.hpp
@@ -31,7 +31,11 @@ namespace impl {
 struct bfloat16_t {
     uint16_t raw_bits_;
     bfloat16_t() = default;
-    constexpr bfloat16_t(uint16_t r, bool) : raw_bits_(r) {}
+    constexpr bfloat16_t(uint16_t r, bool) :
+      raw_bits_{ is_signaling_NaN(r) ? signaling_to_quiet_NaN(r)
+                                     : is_denormal(r) ? make_signed_zero(r)
+                                                      : r }
+    {}
     bfloat16_t(float f) { (*this) = f; }
 
     bfloat16_t DNNL_API &operator=(float f);
@@ -41,6 +45,35 @@ struct bfloat16_t {
     bfloat16_t &operator+=(bfloat16_t a) {
         (*this) = (float)(*this) + (float)a;
         return *this;
+    }
+  private:
+
+    static constexpr uint16_t remove_signbit(const uint16_t r) {
+        return r & 0x7FFF;
+    }
+
+    static constexpr uint16_t signaling_to_quiet_NaN(const uint16_t sNaN) {
+        return sNaN | (1u << 6);
+    }
+
+    static constexpr bool is_signaling_NaN_without_signbit(const uint16_t r) {
+        return (r > 0x7F80) && (r < 0x7FC0);
+    }
+  
+    static constexpr bool is_signaling_NaN(const uint16_t r) {
+        return is_signaling_NaN_without_signbit(remove_signbit(r));
+    }
+
+    static constexpr bool is_denormal_without_signbit(const uint16_t r) {
+        return (r > 0) && (r < (1u << 7));
+    }
+
+    static constexpr bool is_denormal(const uint16_t r) {
+        return is_denormal_without_signbit(remove_signbit(r));
+    }
+
+    static constexpr uint16_t make_signed_zero(const uint16_t r) {
+        return r & (1u << 15);
     }
 };
 
