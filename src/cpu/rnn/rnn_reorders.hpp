14fb7ec56511559daaf644b61ad2cfc80ae7afbe
Mourad Gouicem <mourad.gouicem@intel.com>
2020-04-16 10:32:46 -0700

    src: cpu: rnn: default initialize parameters to balance211

--- src/cpu/rnn/rnn_reorders.hpp
+++ src/cpu/rnn/rnn_reorders.hpp
@@ -103,7 +103,7 @@ private:
         const dim_t inner_dim = input_d.dims()[input_d.ndims() - 1];
 
         parallel(0, [&](const int ithr, const int nthr) {
-            dim_t start, end;
+            dim_t start {0}, end {0};
             balance211(outer_dim, nthr, ithr, start, end);
             for (int i = start; i < end; ++i) {
                 const dim_t off_in = input_d.off_l(i * inner_dim);
@@ -285,7 +285,7 @@ private:
         const int mask = pd()->attr()->rnn_weights_qparams_.mask_;
 
         parallel(0, [&](const int ithr, const int nthr) {
-            int start, end;
+            int start {0}, end {0};
             balance211(L * D * I, nthr, ithr, start, end);
             for (int ldi = start; ldi < end; ldi++) {
                 for (int go = 0; go < G * O; go++) {
