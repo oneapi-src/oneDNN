c8d0fadf09f8301c78287d29b63bba8c1d17021f
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2020-04-29 22:23:23 +0000

    build: sycl: re-enable L0 again

--- cmake/SYCL.cmake
+++ cmake/SYCL.cmake
@@ -44,6 +44,7 @@ if(DPCPP_SUPPORTED)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsycl")
 
     if(UNIX AND LevelZero_FOUND)
+        set(DNNL_WITH_LEVEL_ZERO TRUE)
         include_directories(${LevelZero_INCLUDE_DIRS})
     endif()
 
