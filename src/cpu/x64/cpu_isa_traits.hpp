9a60f8828d3bb50ece24a935cf5d5d06f2fdd83d
Ilya Lavrenov <ilya.lavrenov@intel.com>
2020-05-18 05:59:36 +0300

    Fixes for MKLDNN to enable LTO

--- src/cpu/x64/cpu_isa_traits.hpp
+++ src/cpu/x64/cpu_isa_traits.hpp
@@ -31,7 +31,7 @@
 #define XBYAK_NO_OP_NAMES
 /* in order to make selinux happy memory that would be marked with X-bit should
  * be obtained with mmap */
-#define XBYAK_USE_MMAP_ALLOCATOR
+//#define XBYAK_USE_MMAP_ALLOCATOR
 /* Use Xbyak's memfd-based allocation, if available */
 #define XBYAK_USE_MEMFD
 #define XBYAK_NO_EXCEPTION
