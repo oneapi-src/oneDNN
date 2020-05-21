b36952a3fd5a01f08da0d9f7ff2383028991c817
Bhavani Subramanian <bhavani1.subramanian@intel.com>
2020-05-21 16:26:19 -0700

    cpu: conv: sse4: support nspc format in non-1x1 f32 jit kernel
    supports non-unit strides

--- src/cpu/x64/jit_sse41_conv_kernel_f32.hpp
+++ src/cpu/x64/jit_sse41_conv_kernel_f32.hpp
@@ -81,6 +81,48 @@ private:
     inline void width_blk_step(int ur_w, int pad_l, int pad_r, int oc_blocks);
     inline void solve_common(int oc_blocks);
 
+    inline dim_t filter_w_to_input(int ki, int oi = 0, int pad_l = 0) {
+        return ki * (jcp.dilate_w + 1) + oi * jcp.stride_w - pad_l;
+    }
+
+    inline dim_t filter_h_to_input(int ki) {
+        return ki * (jcp.dilate_h + 1) * jcp.iw;
+    }
+
+    inline dim_t get_input_offset(int i_ic, int i_iw) {
+        dim_t offset;
+        if (utils::one_of(jcp.src_tag, format_tag::ncw, format_tag::nchw,
+                    format_tag::ncdhw)) {
+            offset = i_ic * jcp.ih * jcp.iw + i_iw;
+        } else if (utils::one_of(jcp.src_tag, format_tag::nwc, format_tag::nhwc,
+                           format_tag::ndhwc)) {
+            offset = i_iw * jcp.ic * jcp.ngroups + i_ic;
+        } else {
+            offset = i_iw * jcp.ic_block + i_ic;
+        }
+        return sizeof(float) * offset;
+    }
+
+    inline dim_t get_output_offset(int i_oc_block, int i_ow) {
+        dim_t offset;
+        if (utils::one_of(jcp.dst_tag, format_tag::nwc, format_tag::nhwc,
+                    format_tag::ndhwc)) {
+            offset = i_ow * jcp.oc * jcp.ngroups + i_oc_block * jcp.oc_block;
+        } else {
+            offset = (i_oc_block * jcp.oh * jcp.ow + i_ow) * jcp.oc_block;
+        }
+        return sizeof(float) * offset;
+    }
+
+    inline dim_t get_kernel_offset(int i_oc_block, int ki, int i_ic) {
+        dim_t block_step_size = jcp.ic_block * jcp.oc_block;
+        dim_t ic_block_step_size = jcp.kh * jcp.kw * block_step_size;
+        dim_t oc_block_step_size = jcp.nb_ic * ic_block_step_size;
+        dim_t offset = i_oc_block * oc_block_step_size + ki * block_step_size
+                + i_ic * jcp.oc_block;
+        return sizeof(float) * offset;
+    }
+
     void generate();
 };
 
