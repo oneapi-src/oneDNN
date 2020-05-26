4c52de57b2a0bd93eefaba93af2539f706236b3e
Denis Samoilov <denis.samoylov@intel.com>
2020-05-26 16:21:08 -0700

    common: remove unused ctor from exec_ctx_t

--- src/common/primitive_exec_types.hpp
+++ src/common/primitive_exec_types.hpp
@@ -56,8 +56,6 @@ status_t cvt_primtive_args(const primitive_desc_t *pd, int nargs,
 struct resource_mapper_t;
 struct exec_ctx_t {
     exec_ctx_t(stream_t *stream) : stream_(stream) {}
-    exec_ctx_t(stream_t *stream, exec_args_t &&args)
-        : stream_(stream), args_(std::move(args)) {}
     exec_ctx_t(const exec_ctx_t &other, exec_args_t &&args)
         : stream_(other.stream_)
         , args_(std::move(args))
