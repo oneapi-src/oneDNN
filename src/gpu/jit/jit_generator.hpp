2d58b717867892656bdf51ff93953fa90085fe44
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2020-07-15 09:57:24 -0700

    gpu: do not use SVM if OpenCL version is older than 2.0

--- src/gpu/jit/jit_generator.hpp
+++ src/gpu/jit/jit_generator.hpp
@@ -86,6 +86,7 @@ constexpr gpu_gen_t gpu_gen12lp = ngen::HW::Gen12LP;
 template <gpu_gen_t hw>
 class jit_generator : public ngen::OpenCLCodeGenerator<hw> {
 private:
+#ifdef CL_VERSION_2_0
     struct svm_deleter {
         cl_context context_;
 
@@ -94,14 +95,18 @@ private:
         }
     };
     std::unique_ptr<void, svm_deleter> dbg_memory_ = nullptr;
+#endif
 
 public:
     jit_generator() = default;
 
+#ifdef CL_VERSION_2_0
     void dbg_alloc(cl_context context);
     void *dbg_memory() const { return dbg_memory_.get(); }
+#endif
 };
 
+#ifdef CL_VERSION_2_0
 template <gpu_gen_t hw>
 void jit_generator<hw>::dbg_alloc(cl_context context) {
     constexpr size_t size = 1048576;
@@ -110,6 +115,7 @@ void jit_generator<hw>::dbg_alloc(cl_context context) {
     dbg_memory_ = decltype(dbg_memory_)(mem, svm_deleter {context});
     memset(mem, 0xcd, size);
 }
+#endif
 
 } // namespace jit
 } // namespace gpu
