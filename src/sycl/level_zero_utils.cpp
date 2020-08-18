4302005e98d094eed8bb2fd7a13f670c01806576
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2020-08-18 22:25:16 +0000

    sycl: level zero: support gold api

--- src/sycl/level_zero_utils.cpp
+++ src/sycl/level_zero_utils.cpp
@@ -42,6 +42,9 @@
 #define LEVEL_ZERO_NOT_SUPPORTED
 #endif
 #else // __SYCL_COMPILER_VERSION <= 20200630
+#if (__SYCL_COMPILER_VERSION >= 20200818)
+#define USE_GOLD_LEVEL_ZERO_SYCL_INTEROP
+#endif
 #include <CL/sycl/backend/level_zero.hpp>
 #endif // __SYCL_COMPILER_VERSION <= 20200630
 
@@ -90,7 +93,9 @@ namespace {
     } while (false)
 
 void *find_ze_symbol(const char *symbol) {
-#if defined(__linux__)
+#if defined(__linux__) && defined(USE_GOLD_LEVEL_ZERO_SYCL_INTEROP)
+    void *handle = dlopen("libze_loader.so.1", RTLD_NOW | RTLD_LOCAL);
+#elif defined(__linux__)
     void *handle = dlopen("libze_loader.so", RTLD_NOW | RTLD_LOCAL);
 #elif defined(_WIN32)
     HMODULE handle = LoadLibraryA("ze_loader.dll");
@@ -121,6 +126,18 @@ F find_ze_symbol(const char *symbol) {
     return (F)find_ze_symbol(symbol);
 }
 
+#if defined(USE_GOLD_LEVEL_ZERO_SYCL_INTEROP)
+status_t func_zeModuleCreate(ze_context_handle_t hContext,
+        ze_device_handle_t hDevice, const ze_module_desc_t *desc,
+        ze_module_handle_t *phModule,
+        ze_module_build_log_handle_t *phBuildLog) {
+    static auto f = find_ze_symbol<decltype(&zeModuleCreate)>("zeModuleCreate");
+
+    if (!f) return status::runtime_error;
+    ZE_CHECK(f(hContext, hDevice, desc, phModule, phBuildLog));
+    return status::success;
+}
+#else
 status_t func_zeModuleCreate(ze_device_handle_t hDevice,
         const ze_module_desc_t *desc, ze_module_handle_t *phModule,
         ze_module_build_log_handle_t *phBuildLog) {
@@ -130,6 +147,7 @@ status_t func_zeModuleCreate(ze_device_handle_t hDevice,
     ZE_CHECK(f(hDevice, desc, phModule, phBuildLog));
     return status::success;
 }
+#endif
 
 status_t func_zeModuleDestroy(ze_module_handle_t hModule) {
     static auto f
@@ -161,7 +179,11 @@ device_uuid_t get_device_uuid(const cl::sycl::device &dev) {
             "ZE_MAX_DEVICE_UUID_SIZE is expected to be 16");
 
     ze_device_properties_t ze_device_properties;
+#if defined(USE_GOLD_LEVEL_ZERO_SYCL_INTEROP)
+    auto ze_device = dev.get_native<cl::sycl::backend::level_zero>();
+#else
     auto ze_device = (ze_device_handle_t)dev.get();
+#endif
     auto status = func_zeDeviceGetProperties(ze_device, &ze_device_properties);
     assert(status == status::success);
 
@@ -180,7 +202,12 @@ status_t sycl_create_kernel_with_level_zero(
         const sycl_gpu_engine_t *sycl_engine,
         const std::vector<unsigned char> &binary,
         const std::string &kernel_name, void **handle_to_destroy) {
-    ze_module_desc_t desc {ZE_MODULE_DESC_VERSION_CURRENT};
+    auto desc = ze_module_desc_t();
+#if defined(USE_GOLD_LEVEL_ZERO_SYCL_INTEROP)
+    desc.stype = ZE_STRUCTURE_TYPE_MODULE_DESC;
+#else
+    desc.version = ZE_MODULE_DESC_VERSION_CURRENT;
+#endif
     desc.format = ZE_MODULE_FORMAT_NATIVE;
     desc.inputSize = binary.size();
     desc.pInputModule = binary.data();
@@ -189,6 +216,15 @@ status_t sycl_create_kernel_with_level_zero(
 
     ze_module_handle_t ze_module;
 
+#if defined(USE_GOLD_LEVEL_ZERO_SYCL_INTEROP)
+    auto ze_device
+            = sycl_engine->device().get_native<cl::sycl::backend::level_zero>();
+    auto ze_ctx = sycl_engine->context()
+                          .get_native<cl::sycl::backend::level_zero>();
+    CHECK(func_zeModuleCreate(ze_ctx, ze_device, &desc, &ze_module, nullptr));
+    auto sycl_program = cl::sycl::level_zero::make<cl::sycl::program>(
+            sycl_engine->context(), ze_module);
+#else
     auto ze_device = (ze_device_handle_t)sycl_engine->device().get();
     CHECK(func_zeModuleCreate(ze_device, &desc, &ze_module, nullptr));
 #if defined(__INTEL_CLANG_COMPILER)
@@ -197,6 +233,7 @@ status_t sycl_create_kernel_with_level_zero(
 #else
     auto sycl_program = cl::sycl::level_zero::make<cl::sycl::program>(
             sycl_engine->context(), ze_module);
+#endif
 #endif
 
     sycl_kernel.reset(
