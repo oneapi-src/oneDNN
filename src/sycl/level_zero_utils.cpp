64c200c6c8433beea77b188e14926dc6a72bcf6a
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2020-08-18 21:16:56 +0000

    sycl: style: reorganize level zero interop functions

--- src/sycl/level_zero_utils.cpp
+++ src/sycl/level_zero_utils.cpp
@@ -14,19 +14,76 @@
 * limitations under the License.
 *******************************************************************************/
 
-#ifdef __linux__
+#include "dnnl_config.h"
+
+#if defined(DNNL_WITH_LEVEL_ZERO)
+
+#if defined(__linux__)
 #include <dlfcn.h>
-#endif
-
-#ifdef _WIN32
+#elif defined(_WIN32)
 #include "windows.h"
+#else
+#error "Level Zero is supported on Linux and Windows only"
 #endif
 
+#include <CL/sycl.hpp>
+#include <level_zero/ze_api.h>
+
+#if !defined(__SYCL_COMPILER_VERSION)
+#error "Unsupported compiler"
+#endif
+
+#if (__SYCL_COMPILER_VERSION <= 20200630)
+#if defined(__INTEL_CLANG_COMPILER)
+#include <CL/sycl/backend/Intel_level0.hpp>
+#else
+#define LEVEL_ZERO_NOT_SUPPORTED
+#endif
+#else // __SYCL_COMPILER_VERSION <= 20200630
+#include <CL/sycl/backend/level_zero.hpp>
+#endif // __SYCL_COMPILER_VERSION <= 20200630
+
+#include "common/c_types_map.hpp"
 #include "common/verbose.hpp"
+
+#include "sycl/sycl_gpu_engine.hpp"
+
+#include "sycl/level_zero_utils.hpp"
 
 namespace dnnl {
 namespace impl {
 namespace sycl {
+
+#if defined(LEVEL_ZERO_NOT_SUPPORTED)
+
+device_uuid_t get_device_uuid(const cl::sycl::device &) {
+    return device_uuid_t(0, 0);
+}
+
+status_t sycl_create_kernel_with_level_zero(std::unique_ptr<cl::sycl::kernel> &,
+        const sycl_gpu_engine_t *, const std::vector<unsigned char> &,
+        const std::string &, void **) {
+    return status::unimplemented;
+}
+
+status_t sycl_destroy_kernel_with_level_zero(
+        std::unique_ptr<cl::sycl::kernel> &, void *) {
+    return status::unimplemented;
+}
+
+#else
+
+namespace {
+
+#define ZE_CHECK(f) \
+    do { \
+        ze_result_t res_ = (f); \
+        if (res_ != ZE_RESULT_SUCCESS) { \
+            if (get_verbose()) \
+                printf("dnnl_verbose,gpu,ze_error,%d\n", (int)(res_)); \
+            return status::runtime_error; \
+        } \
+    } while (false)
 
 void *find_ze_symbol(const char *symbol) {
 #if defined(__linux__)
@@ -55,6 +112,115 @@ void *find_ze_symbol(const char *symbol) {
     return f;
 }
 
+template <typename F>
+F find_ze_symbol(const char *symbol) {
+    return (F)find_ze_symbol(symbol);
+}
+
+status_t func_zeModuleCreate(ze_device_handle_t hDevice,
+        const ze_module_desc_t *desc, ze_module_handle_t *phModule,
+        ze_module_build_log_handle_t *phBuildLog) {
+    using func_type
+            = ze_result_t (*)(ze_device_handle_t, const ze_module_desc_t *,
+                    ze_module_handle_t *, ze_module_build_log_handle_t *);
+    static auto f = find_ze_symbol<func_type>("zeModuleCreate");
+
+    if (!f) return status::runtime_error;
+    ZE_CHECK(f(hDevice, desc, phModule, phBuildLog));
+    return status::success;
+}
+
+status_t func_zeModuleDestroy(ze_module_handle_t hModule) {
+    using func_type = ze_result_t (*)(ze_module_handle_t);
+    static auto f = find_ze_symbol<func_type>("zeModuleDestroy");
+
+    if (!f) return status::runtime_error;
+    ZE_CHECK(f(hModule));
+    return status::success;
+}
+
+status_t func_zeDeviceGetProperties(
+        ze_device_handle_t hDevice, ze_device_properties_t *pDeviceProperties) {
+    using func_type
+            = ze_result_t (*)(ze_device_handle_t, ze_device_properties_t *);
+    static auto f = find_ze_symbol<func_type>("zeDeviceGetProperties");
+
+    if (!f) return status::runtime_error;
+    ZE_CHECK(f(hDevice, pDeviceProperties));
+    return status::success;
+}
+
+} // namespace
+
+// FIXME: Currently SYCL doesn't provide any API to get device UUID so
+// we query it directly from Level0 with the zeDeviceGetProperties function.
+// The `get_device_uuid` function packs 128 bits of the device UUID, which are
+// represented as an uint8_t array of size 16, to 2 uint64_t values.
+device_uuid_t get_device_uuid(const cl::sycl::device &dev) {
+    static_assert(ZE_MAX_DEVICE_UUID_SIZE == 16,
+            "ZE_MAX_DEVICE_UUID_SIZE is expected to be 16");
+
+    ze_device_properties_t ze_device_properties;
+    auto ze_device = (ze_device_handle_t)dev.get();
+    auto status = func_zeDeviceGetProperties(ze_device, &ze_device_properties);
+    assert(status == status::success);
+
+    const auto &ze_device_id = ze_device_properties.uuid.id;
+
+    uint64_t uuid[ZE_MAX_DEVICE_UUID_SIZE / sizeof(uint64_t)] = {};
+    for (size_t i = 0; i < ZE_MAX_DEVICE_UUID_SIZE; ++i) {
+        size_t shift = i % sizeof(uint64_t) * CHAR_BIT;
+        uuid[i / sizeof(uint64_t)] |= (((uint64_t)ze_device_id[i]) << shift);
+    }
+    return device_uuid_t(uuid[0], uuid[1]);
+}
+
+status_t sycl_create_kernel_with_level_zero(
+        std::unique_ptr<cl::sycl::kernel> &sycl_kernel,
+        const sycl_gpu_engine_t *sycl_engine,
+        const std::vector<unsigned char> &binary,
+        const std::string &kernel_name, void **handle_to_destroy) {
+    ze_module_desc_t desc {ZE_MODULE_DESC_VERSION_CURRENT};
+    desc.format = ZE_MODULE_FORMAT_NATIVE;
+    desc.inputSize = binary.size();
+    desc.pInputModule = binary.data();
+    desc.pBuildFlags = "";
+    desc.pConstants = nullptr;
+
+    ze_module_handle_t ze_module;
+
+    auto ze_device = (ze_device_handle_t)sycl_engine->device().get();
+    CHECK(func_zeModuleCreate(ze_device, &desc, &ze_module, nullptr));
+#if defined(__INTEL_CLANG_COMPILER)
+    auto sycl_program = cl::sycl::level0::make<cl::sycl::program>(
+            sycl_engine->context(), ze_module);
+#else
+    auto sycl_program = cl::sycl::level_zero::make<cl::sycl::program>(
+            sycl_engine->context(), ze_module);
+#endif
+
+    sycl_kernel.reset(
+            new cl::sycl::kernel(sycl_program.get_kernel(kernel_name)));
+
+    *handle_to_destroy = ze_module;
+
+    return status::success;
+}
+
+status_t sycl_destroy_kernel_with_level_zero(
+        std::unique_ptr<cl::sycl::kernel> &sycl_kernel,
+        void *handle_to_destroy) {
+    // L0 module should be destroyed manually.
+    sycl_kernel.reset();
+    auto ze_module = reinterpret_cast<ze_module_handle_t>(handle_to_destroy);
+
+    return func_zeModuleDestroy(ze_module);
+}
+
+#endif // defined(LEVEL_ZERO_NOT_SUPPORTED)
+
 } // namespace sycl
 } // namespace impl
 } // namespace dnnl
+
+#endif // DNNL_WITH_LEVEL_ZERO
--- src/sycl/level_zero_utils.hpp
+++ src/sycl/level_zero_utils.hpp
@@ -17,99 +17,34 @@
 #ifndef SYCL_LEVEL_ZERO_UTILS_HPP
 #define SYCL_LEVEL_ZERO_UTILS_HPP
 
-#if defined(DNNL_WITH_LEVEL_ZERO)
+#include <memory>
+#include <string>
+#include <vector>
 
-#if !(defined(__linux__) || defined(_WIN32))
-#error "Level Zero is supported with Linux and Windows only"
-#endif
-
-#include <level_zero/ze_api.h>
-
-#include "common/verbose.hpp"
 #include <CL/sycl.hpp>
-
-#define ZE_CHECK(f) \
-    do { \
-        ze_result_t res_ = (f); \
-        if (res_ != ZE_RESULT_SUCCESS) { \
-            if (get_verbose()) \
-                printf("dnnl_verbose,gpu,ze_error,%d\n", (int)(res_)); \
-            return status::runtime_error; \
-        } \
-    } while (false)
 
 namespace dnnl {
 namespace impl {
 namespace sycl {
 
-void *find_ze_symbol(const char *symbol);
-template <typename F>
-F find_ze_symbol(const char *symbol) {
-    return (F)find_ze_symbol(symbol);
-}
-
-inline status_t func_zeModuleCreate(ze_device_handle_t hDevice,
-        const ze_module_desc_t *desc, ze_module_handle_t *phModule,
-        ze_module_build_log_handle_t *phBuildLog) {
-    using func_type
-            = ze_result_t (*)(ze_device_handle_t, const ze_module_desc_t *,
-                    ze_module_handle_t *, ze_module_build_log_handle_t *);
-    static auto f = find_ze_symbol<func_type>("zeModuleCreate");
-
-    if (!f) return status::runtime_error;
-    ZE_CHECK(f(hDevice, desc, phModule, phBuildLog));
-    return status::success;
-}
-
-inline status_t func_zeModuleDestroy(ze_module_handle_t hModule) {
-    using func_type = ze_result_t (*)(ze_module_handle_t);
-    static auto f = find_ze_symbol<func_type>("zeModuleDestroy");
-
-    if (!f) return status::runtime_error;
-    ZE_CHECK(f(hModule));
-    return status::success;
-}
-
-inline status_t func_zeDeviceGetProperties(
-        ze_device_handle_t hDevice, ze_device_properties_t *pDeviceProperties) {
-    using func_type
-            = ze_result_t (*)(ze_device_handle_t, ze_device_properties_t *);
-    static auto f = find_ze_symbol<func_type>("zeDeviceGetProperties");
-
-    if (!f) return status::runtime_error;
-    ZE_CHECK(f(hDevice, pDeviceProperties));
-    return status::success;
-}
-
 using device_uuid_t = std::tuple<uint64_t, uint64_t>;
+device_uuid_t get_device_uuid(const cl::sycl::device &dev);
 
-// FIXME: Currently SYCL doesn't provide any API to get device UUID so
-// we query it directly from Level0 with the zeDeviceGetProperties function.
-// The `get_device_uuid` function packs 128 bits of the device UUID, which are
-// represented as an uint8_t array of size 16, to 2 uint64_t values.
-inline device_uuid_t get_device_uuid(const cl::sycl::device &dev) {
-    static_assert(ZE_MAX_DEVICE_UUID_SIZE == 16,
-            "ZE_MAX_DEVICE_UUID_SIZE is expected to be 16");
+// including sycl_gpu_engine.hpp leads to circular dependencies, w/a for now.
+struct sycl_gpu_engine_t;
 
-    ze_device_properties_t ze_device_properties;
-    auto ze_device = (ze_device_handle_t)dev.get();
-    auto status = func_zeDeviceGetProperties(ze_device, &ze_device_properties);
-    assert(status == status::success);
+status_t sycl_create_kernel_with_level_zero(
+        std::unique_ptr<cl::sycl::kernel> &sycl_kernel,
+        const sycl_gpu_engine_t *sycl_engine,
+        const std::vector<unsigned char> &binary,
+        const std::string &kernel_name, void **handle_to_destroy);
 
-    const auto &ze_device_id = ze_device_properties.uuid.id;
-
-    uint64_t uuid[ZE_MAX_DEVICE_UUID_SIZE / sizeof(uint64_t)] = {};
-    for (size_t i = 0; i < ZE_MAX_DEVICE_UUID_SIZE; ++i) {
-        size_t shift = i % sizeof(uint64_t) * CHAR_BIT;
-        uuid[i / sizeof(uint64_t)] |= (((uint64_t)ze_device_id[i]) << shift);
-    }
-    return device_uuid_t(uuid[0], uuid[1]);
-}
+status_t sycl_destroy_kernel_with_level_zero(
+        std::unique_ptr<cl::sycl::kernel> &sycl_kernel,
+        void *handle_to_destroy);
 
 } // namespace sycl
 } // namespace impl
 } // namespace dnnl
-
-#endif
 
 #endif // SYCL_LEVEL_ZERO_UTILS_HPP
