26aafacedc323accc4fa558eeea0f1dbc195e7fc
Denis Samoilov <denis.samoylov@intel.com>
2020-08-24 15:47:15 -0700

    cpu: xbyak: disallow calling to primary template for mov

--- src/cpu/x64/xbyak/xbyak.h
+++ src/cpu/x64/xbyak/xbyak.h
@@ -2552,7 +2552,7 @@ public:
 	// When the 2nd argument is 0 the call goes to
 	// `void mov(const Operand& op, uint64 imm)`.
 	template <typename T1, typename T2>
-	void mov(const T1& reg, const T2 *label);
+	void mov(const T1& reg, const T2 *label) { T1::unimplemented_function; }
 	void mov(const NativeReg& reg, const Label& label)
 	{
 		mov_imm(reg, dummyAddr);
