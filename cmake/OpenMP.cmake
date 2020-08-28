7aec833d54d6857121c1b60383d698a4150051c0
Denis Samoilov <denis.samoylov@intel.com>
2020-08-27 19:24:37 -0700

    build: workaround an issue with linking OpenMP on Windows for ICX

--- cmake/OpenMP.cmake
+++ cmake/OpenMP.cmake
@@ -66,7 +66,11 @@ if(NOT OpenMP_CXX_FOUND AND MSVC AND CMAKE_CXX_COMPILER_ID STREQUAL "Clang" AND 
     # clang-cl and icx will fall under this condition
     # CAVEAT: undocumented variable, may be inappropriate
     if(CMAKE_BASE_NAME STREQUAL "icx")
-        set(OpenMP_FLAGS "/Qopenmp")
+        # XXX: Use `-Xclang --dependent-lib=libiomp5md` to workaround an issue
+        # with linking OpenMP on Windows.
+        # The ICX driver doesn't link OpenMP library even if `/Qopenmp`
+        # was specified.
+        set(OpenMP_FLAGS "/Qopenmp -Xclang --dependent-lib=libiomp5md")
     else()
         if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "10.0")
             # version < 10 can't pass cl-style `/openmp` flag
