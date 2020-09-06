14a00013ee1bad90857ef2ea6eab476e62b8ccfb
Denis Samoilov <denis.samoylov@intel.com>
2020-09-06 00:14:54 -0700

    common: fix potential memory leak

--- src/common/sum.cpp
+++ src/common/sum.cpp
@@ -71,8 +71,10 @@ status_t dnnl_sum_primitive_desc_create(primitive_desc_iface_t **sum_pd_iface,
         sum_pd_t *sum_pd = nullptr;
         if ((*s)(&sum_pd, engine, attr, dst_md, n, scales, src_mds)
                 == success) {
-            return safe_ptr_assign(
+            auto status = safe_ptr_assign(
                     *sum_pd_iface, new primitive_desc_iface_t(sum_pd, engine));
+            if (status != status::success) delete sum_pd;
+            return status;
         }
     }
     return unimplemented;
