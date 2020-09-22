46bbdfa121207cdd57d04142782a1542637eb64f
Fomenko, Evarist M <evarist.m.fomenko@intel.com>
2020-09-22 04:19:08 +0000

    benchdnn: rnn: int8: use power-of-two weights range...
    ... to improve test stability

--- tests/benchdnn/rnn/cfg.cpp
+++ tests/benchdnn/rnn/cfg.cpp
@@ -93,8 +93,8 @@ CFG(f16) {
 #define MEAN_U8 28.f
 #define STDDEV_U8 16.f
 
-#define MIN_S8 (-63.f)
-#define MAX_S8 63.f
+#define MIN_S8 (-64.f)
+#define MAX_S8 64.f
 #define MEAN_S8 0.f
 #define STDDEV_S8 32.f
 
