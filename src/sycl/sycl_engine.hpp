fac73854863aacbb13dcc59f8e646e7af710c717
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2020-10-01 13:10:26 -0700

    sycl: engine: move function bodies to cpp file

--- src/sycl/sycl_engine.hpp
+++ src/sycl/sycl_engine.hpp
@@ -86,70 +86,10 @@ public:
         return get_intel_sycl_devices(dev_type).size();
     }
 
-    status_t engine_create(engine_t **engine, size_t index) const override {
-        assert(index < count());
-        auto dev_type = (engine_kind_ == engine_kind::cpu)
-                ? cl::sycl::info::device_type::cpu
-                : cl::sycl::info::device_type::gpu;
-        auto devices = get_intel_sycl_devices(dev_type);
-        auto &dev = devices[index];
-
-        auto exception_handler = [](cl::sycl::exception_list eptr_list) {
-            for (auto &eptr : eptr_list) {
-                if (get_verbose()) {
-                    try {
-                        std::rethrow_exception(eptr);
-                    } catch (const cl::sycl::exception &e) {
-                        printf("dnnl_verbose,gpu,sycl_exception,%s\n",
-                                e.what());
-                    }
-                } else {
-                    std::rethrow_exception(eptr);
-                }
-            }
-        };
-
-        // XXX: we could use the platform to construct the context to cover
-        // more devices. However in this case SYCL runtime may build a SYCL
-        // kernel for all devices from the context (e.g. build both CPU and
-        // GPU). This doesn't work for the CPU thunk kernel which works on CPU
-        // only because it calls a native CPU function.
-        cl::sycl::context ctx(dev, exception_handler);
-        return engine_create(engine, dev, ctx);
-    }
+    status_t engine_create(engine_t **engine, size_t index) const override;
 
     status_t engine_create(engine_t **engine, const cl::sycl::device &dev,
-            const cl::sycl::context &ctx) const {
-
-        status_t status = status::success;
-
-        // Validate device and context.
-        auto ctx_devs = ctx.get_devices();
-        auto it = std::find_if(ctx_devs.begin(), ctx_devs.end(),
-                [&](const cl::sycl::device &ctx_dev) {
-                    return are_equal(ctx_dev, dev);
-                });
-        if (it == ctx_devs.end()) return status::invalid_arguments;
-
-        if (engine_kind_ == engine_kind::cpu && !dev.is_cpu() && !dev.is_host())
-            return status::invalid_arguments;
-        if (engine_kind_ == engine_kind::gpu && !dev.is_gpu())
-            return status::invalid_arguments;
-
-        std::unique_ptr<sycl_engine_base_t> sycl_engine(
-                (engine_kind_ == engine_kind::cpu)
-                        ? static_cast<sycl_engine_base_t *>(
-                                new sycl_cpu_engine_t(dev, ctx))
-                        : static_cast<sycl_engine_base_t *>(
-                                new sycl_gpu_engine_t(dev, ctx)));
-        if (!sycl_engine) return status::out_of_memory;
-
-        status = sycl_engine->init();
-        if (status != status::success) return status;
-
-        *engine = sycl_engine.release();
-        return status::success;
-    }
+            const cl::sycl::context &ctx) const;
 
 private:
     engine_kind_t engine_kind_;
