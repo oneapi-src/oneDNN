8f744fa23cc2b68a3bc7f3dfa87cedbbb85012f1
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2020-10-01 17:35:33 -0700

    gtests: iface_wino: improve internal check logic

--- tests/gtests/test_iface_wino_convolution.cpp
+++ tests/gtests/test_iface_wino_convolution.cpp
@@ -36,8 +36,8 @@ protected:
     struct input_data_t {
         data_type dat_dt;
         data_type wei_dt;
-        bool wino_supported;
-        bool backward_supported;
+        bool wino_supported = false;
+        bool backward_supported = false;
     } input_f32, input_f16, input_int8;
 
     void SetUp() override {
@@ -46,35 +46,30 @@ protected:
 
         input_f16.dat_dt = data_type::f16;
         input_f16.wei_dt = data_type::f16;
-        input_f16.backward_supported = false;
 
         input_int8.dat_dt = data_type::u8;
         input_int8.wei_dt = data_type::s8;
-        input_int8.backward_supported = false;
 
-#if DNNL_X64
+        const bool is_cpu = get_test_engine_kind() == engine::kind::cpu;
+        const bool is_gpu = get_test_engine_kind() == engine::kind::gpu;
+        static const auto isa = get_effective_cpu_isa();
         input_f32.wino_supported
-                = (get_test_engine_kind() == engine::kind::cpu
-                          && impl::cpu::x64::mayiuse(
-                                  impl::cpu::x64::avx512_common))
-                || get_test_engine_kind() == engine::kind::gpu;
-        input_f16.wino_supported = false;
-        input_int8.wino_supported = get_test_engine_kind() == engine::kind::cpu
-                && impl::cpu::x64::mayiuse(impl::cpu::x64::avx512_core);
+                = (is_cpu && isa >= cpu_isa::avx512_mic) || is_gpu;
+        input_int8.wino_supported = (is_cpu && isa >= cpu_isa::avx512_core);
+#if DNNL_X64
         input_f32.backward_supported
                 = get_test_engine_kind() == engine::kind::cpu
                 && impl::dnnl_thr_syncable();
-
-#else
-        input_f32.wino_supported = false;
-        input_f16.wino_supported = false;
-        input_int8.wino_supported = false;
 #endif // DNNL_X64
     }
 };
 
 TEST_F(wino_conv_test_t, TestSmallPadding) {
     for (const auto &input : {input_f32, input_f16, input_int8}) {
+        if (unsupported_data_type(input.dat_dt)
+                || unsupported_data_type(input.wei_dt))
+            continue;
+
         memory::desc src_md {{1, 16, 7, 7}, input.dat_dt, tag::any};
         memory::desc wei_md {{32, 16, 3, 3}, input.wei_dt, tag::any};
         memory::desc dst_md {{1, 32, 7, 7}, input.dat_dt, tag::any};
@@ -108,6 +103,10 @@ TEST_F(wino_conv_test_t, TestSmallPadding) {
 
 TEST_F(wino_conv_test_t, TestLargePadding) {
     for (const auto &input : {input_f32, input_f16, input_int8}) {
+        if (unsupported_data_type(input.dat_dt)
+                || unsupported_data_type(input.wei_dt))
+            continue;
+
         memory::desc src_md {{1, 16, 7, 7}, input.dat_dt, tag::any};
         memory::desc wei_md {{32, 16, 3, 3}, input.wei_dt, tag::any};
         memory::desc dst_md {{1, 32, 9, 9}, input.dat_dt, tag::any};
@@ -115,18 +114,30 @@ TEST_F(wino_conv_test_t, TestLargePadding) {
                 algorithm::convolution_winograd, src_md, wei_md, dst_md, {1, 1},
                 {2, 2}, {2, 2});
 
-        EXPECT_ANY_THROW(convolution_forward::primitive_desc(fwd_op_desc, eng));
+        bool large_pad_is_supported = false;
+        if (input.wino_supported && large_pad_is_supported) {
+            EXPECT_NO_THROW(
+                    convolution_forward::primitive_desc(fwd_op_desc, eng));
+        } else {
+            EXPECT_ANY_THROW(
+                    convolution_forward::primitive_desc(fwd_op_desc, eng));
+        }
     }
 }
 
 TEST_F(wino_conv_test_t, TestUnsupportedKernel) {
     for (const auto &input : {input_f32, input_f16, input_int8}) {
+        if (unsupported_data_type(input.dat_dt)
+                || unsupported_data_type(input.wei_dt))
+            continue;
+
         memory::desc src_md {{1, 16, 5, 5}, input.dat_dt, tag::any};
         memory::desc wei_md {{32, 16, 2, 2}, input.wei_dt, tag::any};
         memory::desc dst_md {{1, 32, 6, 6}, input.dat_dt, tag::any};
         auto fwd_op_desc = convolution_forward::desc(prop_kind::forward,
                 algorithm::convolution_winograd, src_md, wei_md, dst_md, {1, 1},
                 {1, 1}, {1, 1});
+
         EXPECT_ANY_THROW(convolution_forward::primitive_desc(fwd_op_desc, eng));
     }
 }
