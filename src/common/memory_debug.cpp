d30815e27ec126323fe6530e0c79dda5ced10998
Yuri <yuri@rawbw.com>
2020-11-06 07:55:18 -0800

    src: common: enable building on FreeBSD

--- src/common/memory_debug.cpp
+++ src/common/memory_debug.cpp
@@ -21,7 +21,7 @@
 #include <windows.h>
 #endif
 
-#if defined __linux__ || defined __APPLE__
+#if defined __linux__ || defined __APPLE__ || defined __FreeBSD__
 #include <unistd.h>
 #include <sys/mman.h>
 #endif
--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -19,7 +19,7 @@
 #include <windows.h>
 #endif
 
-#if defined __linux__ || defined __APPLE__
+#if defined __linux__ || defined __APPLE__ || defined __FreeBSD__
 #include <unistd.h>
 #endif
 
