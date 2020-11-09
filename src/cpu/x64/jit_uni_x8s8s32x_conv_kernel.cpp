7f79b1a1e644b0b8160ba97d2affa22919fcc231
Dariusz Sciebura <dariusz.sciebura@intel.com>
2020-11-08 22:25:25 -0800

    cpu: x64: conv: fix uni conv kernel for non-grouped conv cases

--- src/cpu/x64/jit_uni_x8s8s32x_conv_kernel.cpp
+++ src/cpu/x64/jit_uni_x8s8s32x_conv_kernel.cpp
@@ -61,7 +61,10 @@ _jit_uni_x8s8s32x_fwd_kernel<isa, Vmm>::_jit_uni_x8s8s32x_fwd_kernel(
         static constexpr bool preserve_gpr = true;
         static constexpr bool preserve_vmm = false;
         static constexpr size_t helper_vmm_idx = 15;
-        const size_t tail_size = jcp.oc_block % isa_simd_width_;
+        const size_t oc_block_tail = jcp.oc_block % isa_simd_width_;
+        const size_t tail_size = oc_block_tail
+                ? oc_block_tail
+                : jcp.oc_without_padding % isa_simd_width_;
 
         const rhs_arg_static_params_t rhs_arg_static_params {helper_vmm_idx,
                 r13, r14, preserve_gpr, preserve_vmm,
