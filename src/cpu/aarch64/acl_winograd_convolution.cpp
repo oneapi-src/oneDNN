49a27fb0a9bed07e6114aa58798652e29dd21a03
Aleksandr Nikolaev <aleksandr.nikolaev@arm.com>
2020-11-11 13:24:44 +0000

    src: cpu: aarch64: add ACL-based Winograd convolution

--- src/cpu/aarch64/acl_winograd_convolution.cpp
+++ src/cpu/aarch64/acl_winograd_convolution.cpp
@@ -20,7 +20,8 @@
 #include "common/dnnl_thread.hpp"
 #include "common/type_helpers.hpp"
 #include "common/utils.hpp"
-#include "cpu/aarch64/acl_gemm_convolution.hpp"
+#include "cpu/aarch64/acl_convolution_utils.hpp"
+#include "cpu/aarch64/acl_winograd_convolution.hpp"
 
 #include <cstring>
 
@@ -33,7 +34,7 @@ using namespace dnnl::impl::status;
 using namespace dnnl::impl::memory_tracking::names;
 using namespace dnnl::impl::utils;
 
-status_t acl_gemm_convolution_fwd_t::execute_forward(
+status_t acl_wino_convolution_fwd_t::execute_forward(
         const exec_ctx_t &ctx) const {
     status_t status = status::success;
     auto src_base = CTX_IN_MEM(const data_t *, DNNL_ARG_SRC);
@@ -44,32 +45,33 @@ status_t acl_gemm_convolution_fwd_t::execute_forward(
     bool with_bias = pd()->acp_.with_bias;
 
     // Retrieve primitive resource and configured Compute Library objects
-    auto *acl_resource = ctx.get_resource_mapper()->get<acl_resource_t>(this);
-    acl_obj_t<arm_compute::NEGEMMConvolutionLayer> &acl_obj
+    auto *acl_resource
+            = ctx.get_resource_mapper()->get<acl_wino_resource_t>(this);
+    acl_obj_t<arm_compute::NEWinogradConvolutionLayer> &acl_wino_obj
             = acl_resource->get_acl_obj();
 
-    acl_obj.src_tensor.allocator()->import_memory(
+    acl_wino_obj.src_tensor.allocator()->import_memory(
             const_cast<data_t *>(src_base));
-    acl_obj.wei_tensor.allocator()->import_memory(
+    acl_wino_obj.wei_tensor.allocator()->import_memory(
             const_cast<data_t *>(wei_base));
-    acl_obj.dst_tensor.allocator()->import_memory(dst_base);
+    acl_wino_obj.dst_tensor.allocator()->import_memory(dst_base);
 
     // Retrieve extra bias memory from the scratchpad and copy from user memory
     if (with_bias) {
         const auto scratchpad = ctx.get_scratchpad_grantor();
         data_t *bia_memory = scratchpad.template get<data_t>(
                 memory_tracking::names::key_none);
-        size_t oc = acl_obj.bia_tensor.info()->tensor_shape()[0];
+        size_t oc = acl_wino_obj.bia_tensor.info()->tensor_shape()[0];
         std::memcpy(bia_memory, bia_base, oc * sizeof(data_t));
-        acl_obj.bia_tensor.allocator()->import_memory(bia_memory);
+        acl_wino_obj.bia_tensor.allocator()->import_memory(bia_memory);
     }
 
-    acl_obj.conv.run();
+    acl_wino_obj.conv.run();
 
-    acl_obj.src_tensor.allocator()->free();
-    acl_obj.wei_tensor.allocator()->free();
-    acl_obj.dst_tensor.allocator()->free();
-    if (with_bias) { acl_obj.bia_tensor.allocator()->free(); }
+    acl_wino_obj.src_tensor.allocator()->free();
+    acl_wino_obj.wei_tensor.allocator()->free();
+    acl_wino_obj.dst_tensor.allocator()->free();
+    if (with_bias) { acl_wino_obj.bia_tensor.allocator()->free(); }
 
     return status;
 }
--- src/cpu/aarch64/acl_winograd_convolution.hpp
+++ src/cpu/aarch64/acl_winograd_convolution.hpp
@@ -14,89 +14,93 @@
 * limitations under the License.
 *******************************************************************************/
 
-#ifndef CPU_AARCH64_ACL_GEMM_CONVOLUTION_HPP
-#define CPU_AARCH64_ACL_GEMM_CONVOLUTION_HPP
+#ifndef CPU_AARCH64_ACL_WINOGRAD_CONVOLUTION_HPP
+#define CPU_AARCH64_ACL_WINOGRAD_CONVOLUTION_HPP
 
 #include "common/c_types_map.hpp"
+#include "common/dnnl_thread.hpp"
 #include "common/memory_tracking.hpp"
 #include "common/primitive.hpp"
 
-#include "cpu/aarch64/acl_convolution_utils.hpp"
-#include "cpu/gemm/gemm.hpp"
-
 #include "cpu/cpu_convolution_pd.hpp"
+#include "cpu/platform.hpp"
+
+#include "cpu/aarch64/acl_convolution_utils.hpp"
 
 #include "arm_compute/runtime/NEON/NEFunctions.h"
-#include "arm_compute/runtime/Scheduler.h"
+#include "arm_compute/runtime/NEON/NEScheduler.h"
 
 namespace dnnl {
 namespace impl {
 namespace cpu {
 namespace aarch64 {
 
-struct acl_resource_t : public resource_t {
-    acl_resource_t()
-        : acl_obj_(utils::make_unique<
-                acl_obj_t<arm_compute::NEGEMMConvolutionLayer>>()) {}
+struct acl_wino_resource_t : public resource_t {
+    acl_wino_resource_t()
+        : acl_wino_obj_(utils::make_unique<
+                acl_obj_t<arm_compute::NEWinogradConvolutionLayer>>()) {}
 
     status_t configure(const acl_conv_conf_t &acp) {
-        if (!acl_obj_) return status::out_of_memory;
+        if (!acl_wino_obj_) return status::out_of_memory;
 
         // Init Compute Library tensors based on info from descriptor
-        acl_obj_->src_tensor.allocator()->init(acp.src_info);
-        acl_obj_->wei_tensor.allocator()->init(acp.wei_info);
-        acl_obj_->dst_tensor.allocator()->init(acp.dst_info);
-        acl_obj_->bia_tensor.allocator()->init(acp.bia_info);
+        acl_wino_obj_->src_tensor.allocator()->init(acp.src_info);
+        acl_wino_obj_->wei_tensor.allocator()->init(acp.wei_info);
+        acl_wino_obj_->dst_tensor.allocator()->init(acp.dst_info);
+        acl_wino_obj_->bia_tensor.allocator()->init(acp.bia_info);
 
         // clang-format off
-        acl_obj_->conv.configure(
-            &acl_obj_->src_tensor,
-            &acl_obj_->wei_tensor,
-            acp.with_bias ? &acl_obj_->bia_tensor : nullptr,
-            &acl_obj_->dst_tensor,
+        acl_wino_obj_->conv.configure(
+            &acl_wino_obj_->src_tensor,
+            &acl_wino_obj_->wei_tensor,
+            acp.with_bias ? &acl_wino_obj_->bia_tensor : nullptr,
+            &acl_wino_obj_->dst_tensor,
             acp.padstride_info,
-            acp.weights_info,
-            acp.dilation_info,
-            acp.act_info);
+            acp.act_info,
+            true); // to support 5x5, 7x7 filter shapes in addition to 3x3
         // clang-format on
 
         return status::success;
     }
 
-    acl_obj_t<arm_compute::NEGEMMConvolutionLayer> &get_acl_obj() const {
-        return *acl_obj_;
+    acl_obj_t<arm_compute::NEWinogradConvolutionLayer> &get_acl_obj() const {
+        return *acl_wino_obj_;
     }
 
-    DNNL_DISALLOW_COPY_AND_ASSIGN(acl_resource_t);
+    DNNL_DISALLOW_COPY_AND_ASSIGN(acl_wino_resource_t);
 
 private:
-    std::unique_ptr<acl_obj_t<arm_compute::NEGEMMConvolutionLayer>> acl_obj_;
+    std::unique_ptr<acl_obj_t<arm_compute::NEWinogradConvolutionLayer>>
+            acl_wino_obj_;
+}; // acl_wino_resource_t
 
-}; // acl_resource_t
-
-struct acl_gemm_convolution_fwd_t : public primitive_t {
+struct acl_wino_convolution_fwd_t : public primitive_t {
     struct pd_t : public cpu_convolution_fwd_pd_t {
         pd_t(const convolution_desc_t *adesc, const primitive_attr_t *attr,
                 const typename pd_t::base_class *hint_fwd_pd)
             : cpu_convolution_fwd_pd_t(adesc, attr, hint_fwd_pd), acp_() {}
 
         DECLARE_COMMON_PD_T(
-                "gemm:acl", acl_gemm_convolution_fwd_t, USE_GLOBAL_SCRATCHPAD);
+                "wino:acl", acl_wino_convolution_fwd_t, USE_GLOBAL_SCRATCHPAD);
 
         status_t init(engine_t *engine) {
             bool ok = true && is_fwd()
-                    && set_default_alg_kind(alg_kind::convolution_direct)
+                    && utils::one_of(desc()->alg_kind,
+                            alg_kind::convolution_auto,
+                            alg_kind::convolution_winograd)
                     && expect_data_types(data_type::f32, data_type::f32,
                             data_type::f32, data_type::f32, data_type::f32)
-                    && !has_zero_dim_memory()
                     && attr()->has_default_values(
-                            primitive_attr_t::skip_mask_t::post_ops)
-                    && post_ops_ok();
+                            primitive_attr_t::skip_mask_t::post_ops,
+                            data_type::f32)
+                    && !has_zero_dim_memory() && post_ops_ok();
             if (!ok) return status::unimplemented;
 
-            auto conf_status = acl_convolution_utils::init_conf_gemm(acp_,
+            auto conf_status = acl_convolution_utils::init_conf_wino(acp_,
                     src_md_, weights_md_, dst_md_, bias_md_, *desc(), *attr());
             if (conf_status != status::success) return status::unimplemented;
+
+            set_default_alg_kind(alg_kind::convolution_winograd);
 
             // Number of threads in Compute Library is set by OMP_NUM_THREADS
             // dnnl_get_max_threads() == OMP_NUM_THREADS
@@ -134,13 +138,13 @@ struct acl_gemm_convolution_fwd_t : public primitive_t {
         }
     };
 
-    acl_gemm_convolution_fwd_t(const pd_t *apd) : primitive_t(apd) {}
+    acl_wino_convolution_fwd_t(const pd_t *apd) : primitive_t(apd) {}
 
     status_t create_resource(
             engine_t *engine, resource_mapper_t &mapper) const override {
         if (mapper.has_resource(this)) return status::success;
 
-        auto r = utils::make_unique<acl_resource_t>();
+        auto r = utils::make_unique<acl_wino_resource_t>();
         if (!r) return status::out_of_memory;
 
         // Configure the resource based on information from primitive descriptor
@@ -150,7 +154,7 @@ struct acl_gemm_convolution_fwd_t : public primitive_t {
         return st;
     }
 
-    ~acl_gemm_convolution_fwd_t() {}
+    ~acl_wino_convolution_fwd_t() {}
 
     typedef typename prec_traits<data_type::f32>::type data_t;
 
@@ -162,11 +166,11 @@ private:
     status_t execute_forward(const exec_ctx_t &ctx) const;
     const pd_t *pd() const { return (const pd_t *)primitive_t::pd().get(); }
 
-}; // acl_gemm_convolution_fwd_t
+}; // acl_wino_convolution_fwd_t
 
 } // namespace aarch64
 } // namespace cpu
 } // namespace impl
 } // namespace dnnl
 
-#endif
+#endif // CPU_AARCH64_ACL_WINOGRAD_CONVOLUTION_HPP
