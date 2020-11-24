e542f25a713fa666ee9be0b3459ed8939145535f
Roy Oursler <roy.j.oursler@intel.com>
2020-11-23 16:31:28 -0800

    tests: benchdnn: use unordered_map for zero_points
    Change map to unordered map for zero_points since unordered_map is faster for
    our lookup heavy usecase.

--- tests/benchdnn/dnn_types.hpp
+++ tests/benchdnn/dnn_types.hpp
@@ -25,6 +25,7 @@
 #include <map>
 #include <memory>
 #include <vector>
+#include <unordered_map>
 
 #include "common.hpp"
 #include "oneapi/dnnl/dnnl_types.h"
@@ -156,16 +157,15 @@ struct attr_t {
             return it == points.end() ? entry_t() : it->second;
         }
 
-        std::map<int, entry_t>::const_iterator begin() const {
+        std::unordered_map<int, entry_t>::const_iterator begin() const {
             return points.begin();
         }
-        std::map<int, entry_t>::const_iterator end() const {
+        std::unordered_map<int, entry_t>::const_iterator end() const {
             return points.end();
         }
 
         zero_points_t() : points() {} // needed for debug icc190 build;
-
-        std::map<int, entry_t> points;
+        std::unordered_map<int, entry_t> points;
     };
 
     struct arg_scales_t {
