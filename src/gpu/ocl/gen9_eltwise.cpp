7dea4fb6b2696ea1da4654608dc2497f24cfb365
Lukasz Golebiowski <lukasz.golebiowski@intel.com>
2020-12-21 17:31:18 -0800

    add failed cases

--- src/gpu/ocl/gen9_eltwise.cpp
+++ src/gpu/ocl/gen9_eltwise.cpp
@@ -91,9 +91,28 @@ status_t gen9_eltwise_fwd_t::execute_forward_dense(
 }
 
 status_t gen9_eltwise_bwd_t::pd_t::init_conf(engine_t *engine) {
+    using namespace dnnl::impl::format_tag;
+
     const memory_desc_wrapper data_d(src_md());
+    const memory_desc_wrapper diff_data_d(diff_src_md());
     conf.with_zero_padding = data_d.nelems(false) != data_d.nelems(true);
     conf.vector_size = 8;
+
+    //gen9_eltwise has not supported when formats data for src and diff_src are different
+    //In this case we return status unimplemented in order to exec ref kernel
+    //Below is the list when src data format is different from diff_src one.
+    bool format_src_and_diff_src_are_diff
+            = (data_d.matches_one_of_tag(abcde)
+                      && diff_data_d.matches_one_of_tag(aBcde8b))
+            || (data_d.matches_one_of_tag(aBcde8b)
+                    && diff_data_d.matches_one_of_tag(abcde))
+            || (data_d.matches_one_of_tag(acdeb)
+                    && diff_data_d.matches_one_of_tag(abcde))
+            || (data_d.matches_one_of_tag(abcde)
+                    && diff_data_d.matches_one_of_tag(acdeb));
+
+    if (format_src_and_diff_src_are_diff) return status::unimplemented;
+
     return status::success;
 }
 
