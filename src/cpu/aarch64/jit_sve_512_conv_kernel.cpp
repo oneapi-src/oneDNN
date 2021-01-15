53cb4296dbea391ca51bd94bcb6de7266a5212b0
m.yamazaki <m.yamazaki@fujitsu.com>
2021-01-15 16:32:47 +0900

    src: aarch64: conv: bwd_w: fixup hw_transpose optimization
      (c.f. #832a5aaad by nivas-x86)

--- src/cpu/aarch64/jit_sve_512_conv_kernel.cpp
+++ src/cpu/aarch64/jit_sve_512_conv_kernel.cpp
@@ -2435,15 +2435,16 @@ void jit_sve_512_conv_bwd_weights_kernel_f32::oh_step_comeback_pointers() {
     mov(kj, reg_kh);
     L(kh_comeback_label);
     {
+        int kw = jcp.is_hw_transp ? 1 : jcp.kw;
         int inp_mult = is_src_layout_nxc()
                 ? jcp.ngroups * jcp.ic
                 : (jcp.is_1stconv ? 1 : jcp.ic_block);
-        int iw = jcp.iw;
+        int iw = jcp.is_hw_transp ? 1 : jcp.iw;
         sub_imm(reg_input, reg_input,
                 jcp.typesize_in * (jcp.dilate_h + 1) * iw * inp_mult,
                 reg_tmp_imm);
         sub_imm(reg_kernel, reg_kernel,
-                jcp.typesize_out * jcp.kw * jcp.ic_block * jcp.oc_block,
+                jcp.typesize_out * kw * jcp.ic_block * jcp.oc_block,
                 reg_tmp_imm);
         sub(kj, kj, 1);
         cmp(kj, 0);
@@ -2876,7 +2877,8 @@ void jit_sve_512_conv_bwd_weights_kernel_f32 ::compute_oh_step_unroll_ow(
     int ic_block = jcp.ic_block;
     int oc_block = jcp.oc_block;
 
-    int ow = jcp.ow;
+    int inp_icb_sp_stride = jcp.is_hw_transp ? 1 : jcp.iw;
+    int ow = jcp.is_hw_transp ? jcp.oh : jcp.ow;
 
     int r_pad = nstl::max(0, jcp.r_pad);
     int l_pad = jcp.l_pad;
@@ -2925,8 +2927,8 @@ void jit_sve_512_conv_bwd_weights_kernel_f32 ::compute_oh_step_unroll_ow(
         }
         L(icb_block_label_end);
 
-        const int input_shift
-                = jcp.typesize_in * (jcp.dilate_h + 1) * jcp.iw * inp_mul;
+        const int input_shift = jcp.typesize_in * (jcp.dilate_h + 1)
+                * inp_icb_sp_stride * inp_mul;
 
         if (generate_icb_loop || ic_tail) {
             const size_t kernel_icb_shift = (size_t)jcp.typesize_out * jcp.kd
@@ -2992,7 +2994,7 @@ void jit_sve_512_conv_bwd_weights_kernel_f32 ::compute_oh_step_unroll_ow(
                     input_shift - jcp.typesize_in * jcp.ic_block, reg_tmp_imm);
         }
 
-        if (!(generate_icb_loop || ic_tail))
+        if (!jcp.is_hw_transp && !(generate_icb_loop || ic_tail))
             add_imm(reg_kernel, reg_kernel,
                     jcp.typesize_out * (jcp.kw - 1) * ic_block * oc_block,
                     reg_tmp_imm);
