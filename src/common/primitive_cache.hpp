a675e366173c843c9647400343a6cb8fc9bde1e9
Bogusevschi, Silviu <silviu.bogusevschi@intel.com>
2021-01-22 12:51:26 -0800

    common: use timestamps for lru primitive cache

--- src/common/primitive_cache.hpp
+++ src/common/primitive_cache.hpp
@@ -18,7 +18,6 @@
 #define COMMON_PRIMITIVE_CACHE_HPP
 
 #include <future>
-#include <list>
 #include <memory>
 #include <unordered_map>
 
@@ -84,9 +83,17 @@ private:
     value_t get(const key_t &key);
 
     size_t capacity_;
-    using cache_list_t = std::list<std::pair<key_t, value_t>>;
-    cache_list_t cache_list_;
-    std::unordered_map<key_t, cache_list_t::iterator> cache_mapper_;
+    struct timed_entry_t {
+        value_t value_;
+        std::atomic<size_t> timestamp_;
+        timed_entry_t(const value_t &value, size_t timestamp)
+            : value_(value), timestamp_(timestamp) {}
+    };
+    // Each entry in the cache has a corresponding key and timestamp.
+    // NOTE: pairs that contain atomics cannot be stored in an unordered_map *as
+    // an element*, since it invokes the copy constructor of std::atomic, which
+    // is deleted.
+    std::unordered_map<key_t, timed_entry_t> cache_mapper_;
 };
 
 primitive_cache_t &primitive_cache();
