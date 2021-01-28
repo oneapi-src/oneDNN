b5fc62e783e4d41d4d6e882f2e55ca3c996f2af3
Roy Oursler <roy.j.oursler@intel.com>
2021-01-28 12:00:22 -0800

    gpu: ocl: winograd: remove unsupported dimension logic
    Additionally changes weights format to avoid likely reorders due to switching
    between OIhw16i16 and IOhw16i16o formats.

--- src/gpu/ocl/gen9_wino_convolution.cpp
+++ src/gpu/ocl/gen9_wino_convolution.cpp
@@ -92,9 +92,9 @@ status_t gen9_wino_convolution_fwd_t::pd_t::init_conf() {
     conf.ic = utils::rnd_up(conf.ic_without_padding, 16);
     conf.oc = utils::rnd_up(conf.oc_without_padding, 16);
 
-    const bool is_wino_shape = conf.kh == 3 && conf.kw == 3 && conf.ngroups == 1
-            && conf.stride_h == 1 && conf.stride_w == 1 && conf.dilate_h == 0
-            && conf.dilate_w == 0 && conf.l_pad < conf.kw
+    const bool is_wino_shape = conf.ndims == 4 && conf.kh == 3 && conf.kw == 3
+            && conf.ngroups == 1 && conf.stride_h == 1 && conf.stride_w == 1
+            && conf.dilate_h == 0 && conf.dilate_w == 0 && conf.l_pad < conf.kw
             && conf.r_pad < conf.kw && conf.t_pad < conf.kh
             && conf.b_pad < conf.kh;
     if (!is_wino_shape) return status::unimplemented;
@@ -176,22 +176,14 @@ status_t gen9_wino_convolution_fwd_t::pd_t::init_conf() {
 
     switch (conf.ver) {
         case ver_16mb16c:
-            src_tag = utils::pick(
-                    conf.ndims - 3, NCw16n16c, NChw16n16c, NCdhw16n16c);
-            dst_tag = utils::pick(
-                    conf.ndims - 3, NCw16n16c, NChw16n16c, NCdhw16n16c);
-            wei_tag = (conf.with_groups ? utils::pick(conf.ndims - 3,
-                               gIOw16i16o, gIOhw16i16o, gIOdhw16i16o)
-                                        : utils::pick(conf.ndims - 3, IOw16i16o,
-                                                IOhw16i16o, IOdhw16i16o));
+            src_tag = NChw16n16c;
+            dst_tag = NChw16n16c;
+            wei_tag = conf.with_groups ? gOIhw16i16o : OIhw16i16o;
             break;
         case ver_8ow16c:
-            src_tag = utils::pick(conf.ndims - 3, nCw16c, nChw16c, nCdhw16c);
-            dst_tag = utils::pick(conf.ndims - 3, nCw16c, nChw16c, nCdhw16c);
-            wei_tag = (conf.with_groups ? utils::pick(conf.ndims - 3,
-                               gIOw16i16o, gIOhw16i16o, gIOdhw16i16o)
-                                        : utils::pick(conf.ndims - 3, IOw16i16o,
-                                                IOhw16i16o, IOdhw16i16o));
+            src_tag = nChw16c;
+            dst_tag = nChw16c;
+            wei_tag = conf.with_groups ? gOIhw16i16o : OIhw16i16o;
             break;
         default: return status::unimplemented;
     }
@@ -290,16 +282,18 @@ status_t gen9_wino_convolution_fwd_t::pd_t::init_kernel_ctx(
     kernel_ctx.define_int("SRC_16N16C",
             utils::one_of(conf.src_tag, NCw16n16c, NChw16n16c, NCdhw16n16c));
     kernel_ctx.define_int(
-            "SRC_W16C", utils::one_of(conf.src_tag, nCw16c, nChw16c, nCdhw16c));
+            "SRC_16N16C", utils::one_of(conf.src_tag, NChw16n16c));
+    kernel_ctx.define_int("SRC_W16C", utils::one_of(conf.src_tag, nChw16c));
 
-    kernel_ctx.define_int("WEI_16I16O_FLIPPED",
-            utils::one_of(conf.wei_tag, gIOw16i16o, gIOhw16i16o, gIOdhw16i16o,
-                    IOw16i16o, IOhw16i16o, IOdhw16i16o));
-
-    kernel_ctx.define_int("DST_16N16C",
-            utils::one_of(conf.dst_tag, NCw16n16c, NChw16n16c, NCdhw16n16c));
     kernel_ctx.define_int(
-            "DST_W16C", utils::one_of(conf.dst_tag, nCw16c, nChw16c, nCdhw16c));
+            "WEI_16I16O", utils::one_of(conf.wei_tag, gOIhw16i16o, OIhw16i16o));
+    kernel_ctx.define_int("WEI_16I16O_FLIPPED",
+            utils::one_of(conf.wei_tag, gIOhw16i16o, IOhw16i16o));
+
+    kernel_ctx.define_int(
+            "DST_16N16C", utils::one_of(conf.dst_tag, NChw16n16c));
+    kernel_ctx.define_int("DST_W16C", utils::one_of(conf.dst_tag, nChw16c));
+
     kernel_ctx.define_int("WITH_BIAS", conf.with_bias);
 
     def_attr_info(kernel_ctx, conf.attr_info);
