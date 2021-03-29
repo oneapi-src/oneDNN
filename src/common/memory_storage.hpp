75b5a15f712b2ebedb93d96fab89f0ce90aec2af
Denis Samoilov <denis.samoylov@intel.com>
2021-03-29 16:28:20 -0700

    common: use delegating constructor

--- src/common/memory_storage.hpp
+++ src/common/memory_storage.hpp
@@ -33,11 +33,10 @@ namespace impl {
 // Memory storage is engine-specific and has different implementations for
 // different engines.
 struct memory_storage_t : public c_compatible {
-    memory_storage_t(engine_t *engine)
-        : engine_(engine), parent_storage_(this) {}
-
     memory_storage_t(engine_t *engine, const memory_storage_t *parent_storage)
         : engine_(engine), parent_storage_(parent_storage) {}
+
+    memory_storage_t(engine_t *engine) : memory_storage_t(engine, this) {}
 
     virtual ~memory_storage_t() = default;
 
