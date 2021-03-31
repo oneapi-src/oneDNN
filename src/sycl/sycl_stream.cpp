d0cbee489c31e5748771ce2082cae3371c4b1672
Denis Samoilov <denis.samoylov@intel.com>
2021-03-31 13:03:25 -0700

    sycl: remove dead code for ComputeCpp

--- src/sycl/sycl_stream.cpp
+++ src/sycl/sycl_stream.cpp
@@ -46,31 +46,11 @@ status_t sycl_stream_t::init() {
         // If service stream is NULL then the current stream will be service
         // so construct it from scratch.
         if (!sycl_engine.is_service_stream_created()) {
-#ifdef DNNL_SYCL_DPCPP
             cl::sycl::property_list props = (flags() & stream_flags::in_order)
                     ? cl::sycl::property_list {cl::sycl::property::queue::
                                     in_order {}}
                     : cl::sycl::property_list {};
             queue_.reset(new cl::sycl::queue(sycl_ctx, sycl_dev, props));
-#else
-            // XXX: Create a queue based on the SYCL context and engine kind.
-            // This is not reliable but there is no constructor from
-            // context and device in SYCL 1.2.1.
-            if (sycl_dev.is_cpu()) {
-                assert(sycl_engine.kind() == engine_kind::cpu);
-                queue_.reset(new cl::sycl::queue(
-                        sycl_ctx, cl::sycl::cpu_selector {}));
-            } else if (sycl_dev.is_host()) {
-                assert(sycl_engine.kind() == engine_kind::cpu);
-                queue_.reset(new cl::sycl::queue(
-                        sycl_ctx, cl::sycl::host_selector {}));
-            } else {
-                assert(sycl_engine.kind() == engine_kind::gpu);
-                queue_.reset(new cl::sycl::queue(
-                        sycl_ctx, cl::sycl::gpu_selector {}));
-            }
-#endif
-
         } else {
             // XXX: multiple queues support has some issues, so always re-use
             // the same queue from the service stream.
