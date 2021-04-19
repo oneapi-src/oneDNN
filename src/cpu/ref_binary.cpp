234af773ed228bcc6f0962b8f89931a25d82c95e
Robert Dobrowolski <robert.dobrowolski@intel.com>
2021-04-19 09:01:14 +0200

    cpu: binary: remove zero padding macro from ref binary

--- src/cpu/ref_binary.cpp
+++ src/cpu/ref_binary.cpp
@@ -35,11 +35,9 @@ namespace cpu {
 template <data_type_t src0_type, data_type_t src1_type, data_type_t dst_type>
 status_t ref_binary_t<src0_type, src1_type, dst_type>::execute_ref(
         const exec_ctx_t &ctx) const {
-    status_t status = status::success;
     const auto src0 = CTX_IN_MEM(const src0_data_t *, DNNL_ARG_SRC_0);
     const auto src1 = CTX_IN_MEM(const src1_data_t *, DNNL_ARG_SRC_1);
-    auto dst = CTX_OUT_CLEAN_MEM(dst_data_t *, DNNL_ARG_DST, status);
-    CHECK(status);
+    auto dst = CTX_OUT_MEM(dst_data_t *, DNNL_ARG_DST);
 
     const float *scales[2];
     ASSIGN_INPUT_SCALE_VALUE(scales[0], DNNL_ARG_SRC_0);
@@ -52,7 +50,33 @@ status_t ref_binary_t<src0_type, src1_type, dst_type>::execute_ref(
     const auto alg = pd()->desc()->alg_kind;
 
     const auto nelems = dst_d.nelems();
+    const auto nelems_pad = dst_d.nelems(true);
     const auto ndims = pd()->ndims();
+    const auto has_padding = !dst_d.is_dense();
+    const auto has_postops = pd()->attr()->post_ops_.len() != 0;
+    const auto is_inplace
+            = static_cast<const void *>(src0) == static_cast<void *>(dst);
+
+    if (has_padding && !is_inplace) {
+        if (has_postops) {
+            // Use zero-padding implementation as we cannot memset over populated memory
+            const auto memory = ctx.memory(DNNL_ARG_TO);
+            memory->zero_pad(ctx);
+        } else {
+            const auto res = std::div(
+                    static_cast<int>(nelems_pad * dst_d.data_type_size()),
+                    PAGE_4K);
+            if (!res.quot)
+                std::memset(dst, 0, res.rem);
+            else
+                parallel_nd(res.quot, [&](dim_t i) {
+                    const auto tail = (i + 1 == res.quot) ? res.rem : 0;
+                    const auto ptr = reinterpret_cast<unsigned char *>(dst)
+                            + i * PAGE_4K;
+                    std::memset(ptr, 0, PAGE_4K + tail);
+                });
+        }
+    }
 
     parallel_nd(nelems, [&](dim_t i) {
         dims_t dims_src0, dims_src1; // decomposition for physical offsets
@@ -78,12 +102,14 @@ status_t ref_binary_t<src0_type, src1_type, dst_type>::execute_ref(
 
         float acc = compute_binary_scalar(alg, x_f, y_f);
 
-        ref_post_ops_t::args_t args;
-        args.dst_val = dst_f;
-        args.ctx = &ctx;
-        args.l_offset = i;
-        args.dst_md = pd()->dst_md();
-        ref_post_ops->execute(acc, args);
+        if (has_postops) {
+            ref_post_ops_t::args_t args;
+            args.dst_val = dst_f;
+            args.ctx = &ctx;
+            args.l_offset = i;
+            args.dst_md = pd()->dst_md();
+            ref_post_ops->execute(acc, args);
+        }
 
         dst[off_C] = cpu::saturate_and_round<dst_data_t>(acc);
     });
