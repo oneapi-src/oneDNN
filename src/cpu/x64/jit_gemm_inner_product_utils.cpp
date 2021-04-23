87fd4893eaf8415b5eebfa9ddfda2c81b9363caa
Alok Bakshi <alok.bakshi@intel.com>
2021-04-23 10:23:30 -0700

    cpu: x64: gemm_bf16_ip: no advance in dst/acc pointers in jit prologue

--- src/cpu/x64/jit_gemm_inner_product_utils.cpp
+++ src/cpu/x64/jit_gemm_inner_product_utils.cpp
@@ -483,7 +483,6 @@ void jit_pp_kernel_t<acc_type, dst_type>::compute_oc_channel_blk() {
         cmp(reg_tmp, reg_len);
         cmovg(reg_tmp, reg_len);
         sub(reg_len, reg_tmp);
-        maybe_advance_mb_stride();
         process_runtime_oc();
         rewind_ptrs();
     }
