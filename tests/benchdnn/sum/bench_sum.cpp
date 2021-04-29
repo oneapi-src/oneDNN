f0313aff2d5c697de7b4d1ad3c0d0288c3566fee
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2021-04-29 14:21:53 -0700

    benchdnn: fix broadcast approach on Windows
    std::vector::assign method may invalidate its own reference to an object
    what happens on Win, but not on Lnx. Copy value from original vector
    (double precaution) to update temporary vector.

--- tests/benchdnn/sum/bench_sum.cpp
+++ tests/benchdnn/sum/bench_sum.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2020 Intel Corporation
+* Copyright 2019-2021 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -35,7 +35,10 @@ void check_correctness(const settings_t &s) {
     for (const auto &i_scratchpad_mode : s.scratchpad_mode) {
         // broadcast tag if needed
         auto i_stag = i_stag_;
-        if (i_stag.size() == 1) i_stag.assign(i_sdt.size(), i_stag[0]);
+        if (i_stag.size() == 1) {
+            const auto val = i_stag_[0];
+            i_stag.assign(i_sdt.size(), val);
+        }
 
         if (i_sdt.size() != i_stag.size()) // expect 1:1 match of dt and tag
             SAFE_V(FAIL);
