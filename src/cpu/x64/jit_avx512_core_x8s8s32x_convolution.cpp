35968f860f9bcb473ac7989961522d373c0d4a7c
Soto Flores, Manuel <manuel.soto.flores@intel.com>
2021-05-04 14:41:59 -0700

    src: cpu: x64: dw_conv: fix zero-point buffer offset calculation
    This fixes an incorrect buffer offset calculation when ngroups
    dimension is padded. The error occurred in depthwise convolution
    with 1D shapes.

--- src/cpu/x64/jit_avx512_core_x8s8s32x_convolution.cpp
+++ src/cpu/x64/jit_avx512_core_x8s8s32x_convolution.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2016-2020 Intel Corporation
+* Copyright 2016-2021 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -80,14 +80,17 @@ status_t jit_avx512_core_x8s8s32x_convolution_fwd_t<src_type,
         oscales = local_scales;
     }
 
-    size_t offset = weights_d.size() - weights_d.additional_buffer_size();
+    size_t extra_data_offset
+            = weights_d.size() - weights_d.additional_buffer_size();
+    size_t ch_offset = jcp.is_depthwise ? jcp.nb_ch * jcp.ch_block
+                                        : jcp.ngroups * jcp.oc;
     auto w = const_cast<wei_data_t *>(weights);
     int32_t *compensation = (jcp.signed_input)
-            ? reinterpret_cast<int32_t *>(&w[offset])
+            ? reinterpret_cast<int32_t *>(&w[extra_data_offset])
             : nullptr;
     int32_t *zp_compensation = jcp.src_zero_point
-            ? reinterpret_cast<int32_t *>(&w[offset])
-                    + (jcp.signed_input ? jcp.ngroups * jcp.oc : 0)
+            ? reinterpret_cast<int32_t *>(&w[extra_data_offset])
+                    + (jcp.signed_input ? ch_offset : 0)
             : nullptr;
 
     int oc_chunks = jcp.nb_oc / jcp.nb_oc_blocking;
