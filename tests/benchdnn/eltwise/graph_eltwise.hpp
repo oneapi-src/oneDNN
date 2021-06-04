c24b5a824900e72ca2f50b5023b0eb1149b3e70c
gandhimathi anand <gandhimathi.anand@intel.com>
2021-06-03 20:42:52 -0700

    tests: benchdnn: eltwise: enable post-op eltwise-binary check
    Eltwise + Binary infra support

--- tests/benchdnn/eltwise/graph_eltwise.hpp
+++ tests/benchdnn/eltwise/graph_eltwise.hpp
@@ -32,6 +32,17 @@ struct eltwise_graph_prb_t : public graph_prb_t {
 
         ctor_status = handle_main_op_();
         if (stop_work(ctor_status)) return;
+        for (const auto &po : prb->attr.post_ops.entry) {
+            if (po.is_binary_kind()) {
+                has_post_bin_ = true;
+                ctor_status = handle_bin_(po);
+                if (stop_work(ctor_status)) return;
+            } else {
+                //TODO - can be removed after adding eltwise and sum support
+                ctor_status = fill_status::UNHANDLED_CONFIG_OPTIONS;
+                if (stop_work(ctor_status)) return;
+            }
+        }
 
         ctor_status = fill_status::DONE;
     };
@@ -44,12 +55,15 @@ struct eltwise_graph_prb_t : public graph_prb_t {
         dims_t dims;
         dt eltwise_dt;
         dnnl::graph::op::kind op_kind;
+        std::string dst_tag;
     };
     spec_t spec_;
+    po_handlers_t po_handler;
     fill_status_t handle_main_op_();
     dnnl::graph::op::kind get_main_op_kind() const override {
         return spec_.op_kind;
     }
+    fill_status_t handle_bin_(const attr_t::post_ops_t::entry_t &po_entry);
 };
 
 int doit(const ::eltwise::prb_t *prb, res_t *res);
