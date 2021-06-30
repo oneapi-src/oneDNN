f22ca83427a344bbcbd6af2737c2b71b742cce7a
Denis Samoilov <denis.samoylov@intel.com>
2021-06-30 12:34:54 -0700

    gtests: ocl: add tests for USM

--- tests/gtests/ocl/api/test_memory_buffer.cpp
+++ tests/gtests/ocl/api/test_memory_buffer.cpp
@@ -27,7 +27,7 @@
 
 namespace dnnl {
 
-class ocl_memory_test_c_t : public ::testing::Test {
+class ocl_memory_buffer_test_c_t : public ::testing::Test {
 protected:
     HANDLE_EXCEPTIONS_FOR_TEST_SETUP() {
         if (!find_ocl_device(CL_DEVICE_TYPE_GPU)) { return; }
@@ -60,11 +60,17 @@ protected:
     dnnl_memory_t memory = nullptr;
 };
 
-class ocl_memory_test_cpp_t : public ::testing::Test {};
+class ocl_memory_buffer_test_cpp_t : public ::testing::Test {};
 
-HANDLE_EXCEPTIONS_FOR_TEST_F(ocl_memory_test_c_t, BasicInteropC) {
+// Use runtime agnostic API to create memory. The created memory expected to
+// contain an OpenCL buffer.
+HANDLE_EXCEPTIONS_FOR_TEST_F(ocl_memory_buffer_test_c_t, BasicInteropC) {
     SKIP_IF(!find_ocl_device(CL_DEVICE_TYPE_GPU),
             "OpenCL GPU devices not found.");
+
+    dnnl_ocl_interop_memory_kind_t memory_kind;
+    DNNL_CHECK(dnnl_ocl_interop_memory_get_memory_kind(memory, &memory_kind));
+    ASSERT_EQ(dnnl_ocl_interop_buffer, memory_kind);
 
     cl_mem ocl_mem;
     DNNL_CHECK(dnnl_ocl_interop_memory_get_mem_object(memory, &ocl_mem));
@@ -92,7 +98,7 @@ HANDLE_EXCEPTIONS_FOR_TEST_F(ocl_memory_test_c_t, BasicInteropC) {
     TEST_OCL_CHECK(clReleaseMemObject(interop_ocl_mem));
 }
 
-HANDLE_EXCEPTIONS_FOR_TEST(ocl_memory_test_cpp_t, BasicInteropCpp) {
+HANDLE_EXCEPTIONS_FOR_TEST(ocl_memory_buffer_test_cpp_t, BasicInteropCpp) {
     SKIP_IF(!find_ocl_device(CL_DEVICE_TYPE_GPU),
             "OpenCL GPU devices not found.");
 
@@ -111,6 +117,9 @@ HANDLE_EXCEPTIONS_FOR_TEST(ocl_memory_test_cpp_t, BasicInteropCpp) {
                 tz, memory::data_type::f32, memory::format_tag::nchw);
         auto mem = test::make_memory(mem_d, eng);
 
+        ASSERT_EQ(ocl_interop::memory_kind::buffer,
+                ocl_interop::get_memory_kind(mem));
+
         cl_mem ocl_mem = ocl_interop::get_mem_object(mem);
         ASSERT_NE(ocl_mem, nullptr);
 
@@ -127,6 +136,40 @@ HANDLE_EXCEPTIONS_FOR_TEST(ocl_memory_test_cpp_t, BasicInteropCpp) {
     ASSERT_EQ(i_ref_count, 1);
 
     TEST_OCL_CHECK(clReleaseMemObject(interop_ocl_mem));
+}
+
+// Use interop API to create memory.
+HANDLE_EXCEPTIONS_FOR_TEST(ocl_memory_buffer_test_cpp_t, BasicInteropCtor) {
+    SKIP_IF(engine::get_count(engine::kind::gpu) == 0, "Engine not found.");
+
+    engine eng(engine::kind::gpu, 0);
+    memory::dims tz = {4, 4, 4, 4};
+
+    size_t sz = size_t(tz[0]) * tz[1] * tz[2] * tz[3];
+
+    cl_context ocl_ctx = ocl_interop::get_context(eng);
+
+    cl_int err;
+    cl_mem ocl_mem = clCreateBuffer(
+            ocl_ctx, CL_MEM_READ_WRITE, sizeof(float) * sz, nullptr, &err);
+
+    memory::desc mem_d(tz, memory::data_type::f32, memory::format_tag::nchw);
+
+    cl_mem ocl_mem_from_mem;
+    {
+        auto mem = ocl_interop::make_memory(mem_d, eng, ocl_mem);
+        ocl_mem_from_mem = ocl_interop::get_mem_object(mem);
+    }
+
+    ASSERT_EQ(ocl_mem, ocl_mem_from_mem);
+    TEST_OCL_CHECK(err);
+    cl_uint ref_count;
+    TEST_OCL_CHECK(clGetMemObjectInfo(ocl_mem, CL_MEM_REFERENCE_COUNT,
+            sizeof(cl_uint), &ref_count, nullptr));
+    int i_ref_count = int(ref_count);
+    ASSERT_EQ(i_ref_count, 1);
+
+    TEST_OCL_CHECK(clReleaseMemObject(ocl_mem));
 }
 
 } // namespace dnnl
