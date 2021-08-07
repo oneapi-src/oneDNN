822489d50426acac4c6af74c91e5d61869a3c172
Srinivas Putta <srinivas.putta@intel.com>
2021-08-06 19:18:24 -0700

    cpu: fix clang undefined sanitizer: addition to nullptr

--- src/cpu/rnn/brgemm_cell_common.cpp
+++ src/cpu/rnn/brgemm_cell_common.cpp
@@ -77,7 +77,9 @@ rnn_cell_execution_sig((_ref_rnn_common_t<aprop, src_type, weights_type,
 
             const auto curr_ws_gates_
                     = ws_gates_ + (m * rnn.ws_gates_ld) + nb_i * rnn.n_block;
-            const float *weights_peephole_n = weights_peephole_ + n;
+            const float *weights_peephole_n = weights_peephole_
+                    ? weights_peephole_ + n
+                    : weights_peephole_;
             auto weights_scales_n = weights_scales + (mask ? n : 0);
             const auto Aic_n
                     = inc_ptr(src_iter_c_, rnn.src_iter_c_dt, m * LDAic + n);
