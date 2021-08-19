977a4793807978eecdb0ddba75df808ce0e4989a
Andrzejewski, Kamil <kamil.andrzejewski@intel.com>
2021-08-19 11:26:22 +0200

    cpu: add missing eltwise alg tags for validity check

--- src/cpu/primitive_attr_postops.cpp
+++ src/cpu/primitive_attr_postops.cpp
@@ -152,13 +152,14 @@ ref_eltwise_scalar_fwd_t::ref_eltwise_scalar_fwd_t(
     : alg_(alg), alpha_(alpha), beta_(beta), scale_(scale) {
     assert(utils::one_of(alg_, eltwise_relu, eltwise_tanh, eltwise_elu,
             eltwise_square, eltwise_abs, eltwise_sqrt, eltwise_linear,
-            eltwise_bounded_relu, eltwise_soft_relu, eltwise_logistic,
-            eltwise_exp, eltwise_gelu_tanh, eltwise_swish, eltwise_log,
-            eltwise_clip, eltwise_clip_v2, eltwise_pow, eltwise_gelu_erf,
-            eltwise_round, eltwise_hardswish, eltwise_relu_use_dst_for_bwd,
-            eltwise_tanh_use_dst_for_bwd, eltwise_elu_use_dst_for_bwd,
-            eltwise_sqrt_use_dst_for_bwd, eltwise_logistic_use_dst_for_bwd,
-            eltwise_exp_use_dst_for_bwd, eltwise_clip_v2_use_dst_for_bwd));
+            eltwise_bounded_relu, eltwise_soft_relu, eltwise_logsigmoid,
+            eltwise_mish, eltwise_logistic, eltwise_exp, eltwise_gelu_tanh,
+            eltwise_swish, eltwise_log, eltwise_clip, eltwise_clip_v2,
+            eltwise_pow, eltwise_gelu_erf, eltwise_round, eltwise_hardswish,
+            eltwise_relu_use_dst_for_bwd, eltwise_tanh_use_dst_for_bwd,
+            eltwise_elu_use_dst_for_bwd, eltwise_sqrt_use_dst_for_bwd,
+            eltwise_logistic_use_dst_for_bwd, eltwise_exp_use_dst_for_bwd,
+            eltwise_clip_v2_use_dst_for_bwd));
 }
 
 ref_eltwise_scalar_fwd_t::ref_eltwise_scalar_fwd_t(
