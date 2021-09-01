eb9582f1e1d1aebb42b974221e4f76bb1b31d365
Anton Voronov <anton.voronov@intel.com>
2021-09-02 00:32:40 +0300

    Perf fixes for Ref poolings

--- src/cpu/ref_pooling.cpp
+++ src/cpu/ref_pooling.cpp
@@ -47,12 +47,9 @@ template <data_type_t src_type, data_type_t dst_type, data_type_t acc_type>
 status_t ref_pooling_fwd_t<src_type, dst_type, acc_type>::execute_forward(
         const exec_ctx_t &ctx) const {
 
-    status_t status = status::success;
     auto src = CTX_IN_MEM(const src_data_t *, DNNL_ARG_SRC);
-    auto dst = CTX_OUT_CLEAN_MEM(dst_data_t *, DNNL_ARG_DST, status);
-    CHECK(status);
-    auto ws = CTX_OUT_CLEAN_MEM(unsigned char *, DNNL_ARG_WORKSPACE, status);
-    CHECK(status);
+    auto dst = CTX_OUT_MEM(dst_data_t *, DNNL_ARG_DST);
+    auto ws = CTX_OUT_MEM(unsigned char *, DNNL_ARG_WORKSPACE);
 
     auto MB = CTX_IN_BATCH(DNNL_ARG_SRC);
 
