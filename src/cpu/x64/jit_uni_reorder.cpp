736c63223299432b85d4569a48716594514fb196
Dariusz Sciebura <dariusz.sciebura@intel.com>
2021-09-02 23:58:35 -0700

    cpu: x64: reorder: optimized nodes split

--- src/cpu/x64/jit_uni_reorder.cpp
+++ src/cpu/x64/jit_uni_reorder.cpp
@@ -1861,10 +1861,10 @@ kernel_t *kernel_t::create(const kernel_t::desc_t &desc) {
 static void prb_block_for_cache(tr::prb_t &prb) {
     /* If strides for 0th and 1st nodes are cache friendly
      * then one can altogether do away with blocking ! */
+    static constexpr int num_elems_thr = 16;
     const bool cache_blocking_needed
-            = ((prb.nodes[0].is % 64 == 0 && prb.nodes[0].n > 16)
-                      || (prb.ndims > 1 && prb.nodes[1].is % 64 == 0
-                              && prb.nodes[1].n > 16))
+            = ((prb.nodes[0].is % 64 == 0 && prb.nodes[0].n > num_elems_thr)
+                      || (prb.ndims > 1 && prb.nodes[1].n > num_elems_thr))
             && !prb.is_tail_present;
     if (!cache_blocking_needed) return;
 
@@ -1881,9 +1881,11 @@ static void prb_block_for_cache(tr::prb_t &prb) {
         const auto output_stride = prb.nodes[unit_input_stride_idx].os;
         const auto num_elems = prb.nodes[unit_input_stride_idx].n;
 
-        const bool split_needed = (num_elems > 16) && (num_elems % 16 == 0);
+        const bool split_needed = (num_elems > num_elems_thr)
+                && (num_elems % num_elems_thr == 0);
         const int move_location = (output_stride % 4 != 0) ? 0 : 1;
-        if (split_needed) prb_node_split(prb, unit_input_stride_idx, 16);
+        if (split_needed)
+            prb_node_split(prb, unit_input_stride_idx, num_elems_thr);
 
         /* Because of cache-unfriendly nature of unit-output stride node, let
          * us move unit-input stride node on or near front! */
@@ -1894,15 +1896,26 @@ static void prb_block_for_cache(tr::prb_t &prb) {
      * is=1 between them for better cache reuse:
      * [n0:is0:1][n1:1:os1] --> [16n0:is0:1][n1:1:os1][n0/16:is0*16:16] */
     if (prb.ndims >= 2 && prb.nodes[0].os == 1 && prb.nodes[1].is == 1) {
-        const auto input_stride = prb.nodes[0].is;
         const auto num_elems = prb.nodes[0].n;
 
-        const bool split_needed = true && (num_elems > 16)
-                && (num_elems % 16 == 0) && (input_stride >= 256)
-                && (input_stride % 64 == 0);
+        const bool split_needed = (num_elems > num_elems_thr)
+                && (num_elems % num_elems_thr == 0);
         if (split_needed) {
-            prb_node_split(prb, 0, 16);
+            prb_node_split(prb, 0, num_elems_thr);
             prb_node_move(prb, 1, 2);
+
+            // heuristics - looping over the unrolled dims should maximize reuse
+            // of the already cached data; observation is choosing the smallest
+            // dim from the remaining (from 2 up to ndims) gives good results
+            const auto dim_beg_it = std::begin(prb.nodes);
+            const auto dim_two_it = dim_beg_it + 2;
+            const auto dim_last_it = dim_beg_it + prb.ndims;
+            const auto min_n_node_it = std::min_element(dim_two_it, dim_last_it,
+                    [](const tr::node_t &lhs, const tr::node_t &rhs) {
+                        return lhs.n < rhs.n;
+                    });
+            const auto min_idx = std::distance(dim_beg_it, min_n_node_it);
+            if (min_idx > 2) prb_node_move(prb, min_idx, 2);
         }
     }
 }
