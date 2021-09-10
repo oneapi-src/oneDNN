fa8e496c4c0d6f99c649914b1b16204fe6292b5f
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2021-09-09 19:04:24 -0700

    benchdnn: matmul: enable fast-ref-gpu

--- tests/benchdnn/matmul/matmul.hpp
+++ tests/benchdnn/matmul/matmul.hpp
@@ -302,9 +302,8 @@ inline int64_t dst_off_f(const prb_t *prb, int64_t mb, int64_t m, int64_t n) {
     return (mb * prb->m + m) * prb->n + n;
 }
 
-void compute_ref(const prb_t *prb, dnn_mem_t &src_m, dnn_mem_t &wei_m,
-        dnn_mem_t &bia_m, const std::vector<dnn_mem_t> &binary_po,
-        dnn_mem_t &dst_m);
+void compute_ref(
+        const prb_t *prb, dnnl_primitive_t prim_ref, const args_t &args);
 
 int doit(const prb_t *prb, res_t *res);
 
