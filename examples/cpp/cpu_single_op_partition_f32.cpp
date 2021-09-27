e92326edadc8bea973e8f35bd0d24d9a91556fda
Lv, Tao A <tao.a.lv@intel.com>
2021-09-27 15:26:43 +0800

    examples: cpp: cpu single op partition example
    Signed-off-by: Lv, Tao A <tao.a.lv@intel.com>

--- examples/cpp/cpu_single_op_partition_f32.cpp
+++ examples/cpp/cpu_single_op_partition_f32.cpp
@@ -14,31 +14,24 @@
 * limitations under the License.
 *******************************************************************************/
 
-/// @example cpu_single_op_partition_matmul.cpp
-/// @copybrief cpu_single_op_partition_matmul_cpp
-/// Annotated version: @ref cpu_single_op_partition_matmul_cpp
+/// @example cpu_single_op_partition_f32.cpp
+/// @copybrief cpu_single_op_partition_f32_cpp
+/// Annotated version: @ref cpu_single_op_partition_f32_cpp
 
-/// @page cpu_single_op_partition_matmul_cpp CPU example for single operator partition
+/// @page cpu_single_op_partition_f32_cpp CPU example for single operator partition
 ///
-/// > Example code: @ref cpu_single_op_partition_matmul.cpp
+/// > Example code: @ref cpu_single_op_partition_f32.cpp
 
 #include "oneapi/dnnl/dnnl_graph.hpp"
-
-#include <iostream>
 
 using namespace dnnl::graph;
 using data_type = logical_tensor::data_type;
 using layout_type = logical_tensor::layout_type;
 
-// clang-format off
 int main(int argc, char **argv) {
-    std::cout << "======== Example: Single operator partition ========\n";
-
     const engine::kind ekind = engine::kind::cpu;
 
     /// Create logical tensor
-    std::cout << "Create logical tensors -------------------------------";
-
     std::vector<int64_t> src0_dims {32, 1024};
     std::vector<int64_t> src1_dims {1024, 2048};
     std::vector<int64_t> dst_dims {32, 2048};
@@ -46,10 +39,8 @@ int main(int argc, char **argv) {
     logical_tensor src0 {0, data_type::f32, src0_dims, layout_type::strided};
     logical_tensor src1 {1, data_type::f32, src1_dims, layout_type::strided};
     logical_tensor dst {2, data_type::f32, dst_dims, layout_type::strided};
-    std::cout << "Success!\n";
 
     /// Create matmul operator
-    std::cout << "Create matmul operator -------------------------------";
     op matmul {3, op::kind::MatMul, "matmul"};
 
     matmul.add_inputs({src0, src1});
@@ -57,24 +48,18 @@ int main(int argc, char **argv) {
 
     matmul.set_attr<bool>("transpose_a", false);
     matmul.set_attr<bool>("transpose_b", false);
-    std::cout << "Success!\n";
 
     /// Create partition
-    std::cout << "Create a partition with the matmul operator ----------";
     partition part {matmul, ekind};
-    std::cout << "Success!\n";
 
     /// Compile the partition
-    std::cout << "Compile the partition --------------------------------";
     /// construct a new engine
     engine eng {ekind, 0};
 
     // compilation
     compiled_partition cp = part.compile({src0, src1}, {dst}, eng);
-    std::cout << "Success!\n";
 
     /// Execute the compiled partition
-    std::cout << "Execute the compiled partition -----------------------";
     // construct tensors
     std::vector<float> data0(32 * 1024);
     std::vector<float> data1(1024 * 2048);
@@ -88,9 +73,6 @@ int main(int argc, char **argv) {
     stream stm {eng};
     /// execute the compile partition
     cp.execute(stm, {t0, t1}, {t2});
-    std::cout << "Success!\n";
-    std::cout << "============ Run Example Successfully ==============\n";
 
     return 0;
 }
-// clang-format on
