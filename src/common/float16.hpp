8243e1b9c021fb5e4f5facb642818f9a1cae4de0
Arthur Mitrano <arthur.araujo.mitrano@intel.com>
2021-09-29 23:18:31 +0000

    common: fix f32 -> f16 conversion for subnormal range

--- src/common/float16.hpp
+++ src/common/float16.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2020 Intel Corporation
+* Copyright 2019-2021 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -86,12 +86,11 @@ inline float16_t &float16_t::operator=(float f) {
         ee = 0x1F;
         mm = 0;
     } else {
-        // Underflow. Scale the input float, converting it
-        // into an equivalent denormal.
-        float ff = f * utils::bit_cast<float, uint32_t>(0x01000000);
+        // Underflow.
+        float ff = fabsf(f) + 0.5;
         uint32_t ii = utils::bit_cast<uint32_t>(ff);
         ee = 0;
-        mm = ii;
+        mm = ii & 0x7FF;
     }
 
     this->raw = (ss << 15) | (ee << 10) | mm;
