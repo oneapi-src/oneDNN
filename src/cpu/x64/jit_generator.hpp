1494d4e9affc9e46e23ab1efd379a68cf656f433
Thiago Macieira <thiago.macieira@intel.com>
2021-11-10 12:17:30 -0800

    cpu: hacky solution to remove the global variables per .cpp
    Removing the macros left as an exercise for the reader.
    
    This wouldn't be necessary if we could depend on C++14, because then
    Xbyak::util::rax and family are also constexpr.

--- src/cpu/x64/jit_generator.hpp
+++ src/cpu/x64/jit_generator.hpp
@@ -95,6 +95,8 @@ static inline void tc_configure_tile(
 //
 // (Roma)
 
+} // namespace
+
 #ifdef XBYAK64
 constexpr Xbyak::Operand::Code abi_save_gpr_regs[] = {
         Xbyak::Operand::RBX,
@@ -109,19 +111,32 @@ constexpr Xbyak::Operand::Code abi_save_gpr_regs[] = {
 #endif
 };
 
+constexpr Xbyak::Operand::Code abi_param_regs[] = {
 #ifdef _WIN32
-static const Xbyak::Reg64 abi_param1(Xbyak::Operand::RCX),
-        abi_param2(Xbyak::Operand::RDX), abi_param3(Xbyak::Operand::R8),
-        abi_param4(Xbyak::Operand::R9), abi_not_param1(Xbyak::Operand::RDI);
+        Xbyak::Operand::RCX, Xbyak::Operand::RDX, Xbyak::Operand::R8,
+        Xbyak::Operand::R9
 #else
-static const Xbyak::Reg64 abi_param1(Xbyak::Operand::RDI),
-        abi_param2(Xbyak::Operand::RSI), abi_param3(Xbyak::Operand::RDX),
-        abi_param4(Xbyak::Operand::RCX), abi_param5(Xbyak::Operand::R8),
-        abi_param6(Xbyak::Operand::R9), abi_not_param1(Xbyak::Operand::RCX);
+        Xbyak::Operand::RDI, Xbyak::Operand::RSI, Xbyak::Operand::RDX,
+        Xbyak::Operand::RCX, Xbyak::Operand::R8, Xbyak::Operand::R9
 #endif
+};
+
+constexpr Xbyak::Operand::Code abi_not_param_reg =
+#ifdef _WIN32
+        Xbyak::Operand::RDI;
+#else
+        Xbyak::Operand::RCX;
 #endif
 
-} // namespace
+#define abi_param1 Xbyak::Reg64(abi_param_regs[0])
+#define abi_param2 Xbyak::Reg64(abi_param_regs[1])
+#define abi_param3 Xbyak::Reg64(abi_param_regs[2])
+#define abi_param4 Xbyak::Reg64(abi_param_regs[3])
+#define abi_param5 Xbyak::Reg64(abi_param_regs[4])
+#define abi_param6 Xbyak::Reg64(abi_param_regs[5])
+#define abi_not_param1 Xbyak::Reg64(abi_not_param_reg)
+
+#endif
 
 class jit_generator : public Xbyak::CodeGenerator, public c_compatible {
 public:
