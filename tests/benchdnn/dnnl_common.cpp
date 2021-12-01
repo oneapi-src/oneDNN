4b0a6c046e707ec05e0fae1b23e41be472d72122
Denis Samoilov <denis.samoylov@intel.com>
2021-12-01 12:01:14 -0800

    style: use term cache_blob_id

--- tests/benchdnn/dnnl_common.cpp
+++ tests/benchdnn/dnnl_common.cpp
@@ -80,7 +80,8 @@ size_t set_primitive_cache_capacity_without_clearing(size_t capacity) {
     return size_t(0);
 }
 
-int get_cache_blob_id(std::vector<uint8_t> &id, dnnl_primitive_desc_t pd) {
+int get_cache_blob_id(
+        std::vector<uint8_t> &cache_blob_id, dnnl_primitive_desc_t pd) {
     dnnl_dim_t count;
     const uint8_t *c_id;
     DNN_SAFE(dnnl_primitive_desc_query(
@@ -89,7 +90,7 @@ int get_cache_blob_id(std::vector<uint8_t> &id, dnnl_primitive_desc_t pd) {
     DNN_SAFE(dnnl_primitive_desc_query(
                      pd, dnnl_query_cache_blob_id, 0, (void **)&c_id),
             WARN);
-    id = {c_id, c_id + count};
+    cache_blob_id = {c_id, c_id + count};
     return OK;
 }
 
@@ -160,10 +161,10 @@ int test_persistent_cache_api(benchdnn_dnnl_wrapper_t<dnnl_primitive_t> &prim,
     // 1. Disable primitive cache to make sure that the next primitive will
     // be created from the cache blob and not fetched from the primitive cache.
     const auto old_capacity = set_primitive_cache_capacity_without_clearing(0);
-    // 2. Get primitive descriptor ID to use it as a key for the `test_cache`.
-    std::vector<uint8_t> id;
-    SAFE(get_cache_blob_id(id, pd), WARN);
-    // 3. Check if a cache blob for the obtained ID is present in the
+    // 2. Get cache blob ID to use it as a key for the `test_cache`.
+    std::vector<uint8_t> cache_blob_id;
+    SAFE(get_cache_blob_id(cache_blob_id, pd), WARN);
+    // 3. Check if a cache blob for the obtained cache blob ID is present in the
     //    `test_cache`.
     //    a) If the cache blob is found the primitive is created from it.
     //    b) If the cache blob is not found then get it from the previously
@@ -171,7 +172,7 @@ int test_persistent_cache_api(benchdnn_dnnl_wrapper_t<dnnl_primitive_t> &prim,
     //       from it.
     dnnl_primitive_t p {};
     auto &cache = get_test_cache();
-    auto cache_value = cache.get(id);
+    auto cache_value = cache.get(cache_blob_id);
     if (!cache_value.empty()) {
         const size_t size = cache_value.size();
         const uint8_t *cache_blob = cache_value.data();
@@ -199,7 +200,7 @@ int test_persistent_cache_api(benchdnn_dnnl_wrapper_t<dnnl_primitive_t> &prim,
         DNN_SAFE(dnnl_primitive_create_from_cache_blob(
                          &p, pd, cache_blob.size(), cache_blob.data()),
                 WARN);
-        cache.add(id, cache_blob);
+        cache.add(cache_blob_id, cache_blob);
     }
     prim.reset(p);
 
