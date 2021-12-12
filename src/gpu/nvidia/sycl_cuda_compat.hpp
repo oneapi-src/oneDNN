7e5bc3f849919e9ceb586f89bf991375c2b6f58d
Denis Samoilov <denis.samoylov@intel.com>
2021-12-11 16:20:28 -0800

    gpu: nvidia: use ext_oneapi_cuda instead of cuda backend

--- src/gpu/nvidia/sycl_cuda_compat.hpp
+++ src/gpu/nvidia/sycl_cuda_compat.hpp
@@ -53,7 +53,8 @@ native_object_t get_native(const sycl_object_t &sycl_object) {
 using interop_handle = ::sycl::interop_handle;
 template <typename T, typename U>
 T get_native_mem(const interop_handle &ih, U acc) {
-    return reinterpret_cast<T>(ih.get_native_mem<::sycl::backend::cuda>(acc));
+    return reinterpret_cast<T>(
+            ih.get_native_mem<::sycl::backend::ext_oneapi_cuda>(acc));
 }
 
 template <typename T>
@@ -63,7 +64,8 @@ void host_task(::sycl::handler &cgh, const T &task) {
 
 template <typename native_object_t, typename sycl_object_t>
 native_object_t get_native(const sycl_object_t &sycl_object) {
-    auto handle = ::sycl::get_native<::sycl::backend::cuda>(sycl_object);
+    auto handle
+            = ::sycl::get_native<::sycl::backend::ext_oneapi_cuda>(sycl_object);
     return reinterpret_cast<native_object_t>(handle);
 }
 #endif
