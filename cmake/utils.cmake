d9c8bbeb884ab916d47aa57b0bec9abbd7f89542
Mourad Gouicem <mourad.gouicem@intel.com>
2022-01-06 14:49:51 +0100

    build: osx: fix find LIBM issues with OSX11 and older cmake

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,5 +1,5 @@
 #===============================================================================
-# Copyright 2018-2021 Intel Corporation
+# Copyright 2018-2022 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -146,4 +146,20 @@ function(target_link_libraries_install target list)
         target_link_libraries(${target} LINK_PUBLIC
             "$<INSTALL_INTERFACE:${base}>")
     endforeach(lib)
+endfunction()
+
+function(find_libm var)
+    # This is to account for the linker cache in OSX11.  might work
+    # with lower than 3.9.4, but was not able to test with anything
+    # between 2.8 and 3.9. See here for more details:
+    # https://gitlab.kitware.com/cmake/cmake/-/issues/20863
+    if (APPLE AND (${CMAKE_HOST_SYSTEM_VERSION} VERSION_GREATER "20.0.0")
+           AND (${CMAKE_VERSION} VERSION_LESS "3.9.4"))
+        message(INFO "Using OSX11 and above with CMAKE older than 3.18 can cause linking issues.")
+        set(OSX11_AND_OLDER_CMAKE TRUE)
+    endif()
+
+    if(UNIX AND (NOT (APPLE AND OSX11_AND_OLDER_CMAKE)))
+        find_library(${var} m REQUIRED)
+    endif()
 endfunction()
