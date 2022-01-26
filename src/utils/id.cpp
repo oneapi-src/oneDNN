28d59754c602617b732bb849b848993a7c54b557
Safonov, Igor <igor.safonov@intel.com>
2022-01-25 17:17:48 -0800

    src: utils: id: avoid ambiguous references

--- src/utils/id.cpp
+++ src/utils/id.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2020-2021 Intel Corporation
+* Copyright 2020-2022 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -16,6 +16,14 @@
 
 #include "utils/id.hpp"
 
-using namespace dnnl::graph::impl::utils;
+namespace dnnl {
+namespace graph {
+namespace impl {
+namespace utils {
 
 std::atomic<id_t::value_type> id_t::counter {100000};
+
+} // namespace utils
+} // namespace impl
+} // namespace graph
+} // namespace dnnl
