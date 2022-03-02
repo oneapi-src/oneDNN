a6b171b9f6a9760b1f5c5244f013580cd2950eb1
Damian Szwichtenberg <damian.szwichtenberg@intel.com>
2022-03-02 09:59:14 +0100

    tests: benchdnn: prelu: add support for PReLUBackprop benchmarks

--- tests/benchdnn/prelu/graph_prelu.cpp
+++ tests/benchdnn/prelu/graph_prelu.cpp
@@ -21,6 +21,12 @@ namespace benchdnnext {
 namespace prelu {
 
 prelu_graph_prb_t::spec_t::spec_t(const ::prelu::prb_t *prb) noexcept {
+    using graph_op = dnnl::graph::op;
+
+    is_bwd_pass = prb->dir & FLAG_BWD;
+    op_kind = is_bwd_pass ? graph_op::kind::PReLUBackprop
+                          : graph_op::kind::PReLU;
+
     data_dims = prb->vdims[0];
     slope_dims = prb->vdims[1];
 
@@ -38,6 +44,7 @@ void check_known_skipped_case_graph(
 }
 
 fill_status_t prelu_graph_prb_t::handle_main_op_() {
+    using logical_tensor = dnnl::graph::logical_tensor;
     using op = dnnl::graph::op;
 
     const size_t new_op_id = ops_.size();
@@ -45,22 +52,49 @@ fill_status_t prelu_graph_prb_t::handle_main_op_() {
     tensor_id["main"].push_back(TENSOR_ID);
     const std::string SRC {TENSOR_ID + "_SRC"};
     const std::string SLOPE {TENSOR_ID + "_SLOPE"};
-    const std::string DST {TENSOR_ID + "_DST"};
 
     tensor_descs_.emplace(
             SRC, spec_.data_dt, spec_.data_dims, spec_.raw_data_tag);
     tensor_descs_.emplace(
             SLOPE, spec_.slope_dt, spec_.slope_dims, spec_.raw_slope_tag);
-    tensor_descs_.emplace(
-            DST, spec_.data_dt, spec_.data_dims, spec_.raw_data_tag);
-    op prelu_op(new_op_id, op::kind::PReLU,
-            {tensor_descs_[SRC], tensor_descs_[SLOPE]}, {tensor_descs_[DST]},
-            "prelu");
+
+    std::string name;
+    std::vector<logical_tensor> inputs;
+    std::vector<logical_tensor> outputs;
+    if (spec_.is_bwd_pass) {
+        name = "prelu_bwd";
+        const std::string DIFF_DST {TENSOR_ID + "_DIFF_DST"};
+        const std::string DIFF_SRC {TENSOR_ID + "_DIFF_SRC"};
+        const std::string DIFF_SLOPE {TENSOR_ID + "_DIFF_SLOPE"};
+
+        tensor_descs_.emplace(
+                DIFF_DST, spec_.data_dt, spec_.data_dims, spec_.raw_data_tag);
+        tensor_descs_.emplace(
+                DIFF_SRC, spec_.data_dt, spec_.data_dims, spec_.raw_data_tag);
+        tensor_descs_.emplace(DIFF_SLOPE, spec_.slope_dt, spec_.slope_dims,
+                spec_.raw_slope_tag);
+        inputs = {tensor_descs_[SRC], tensor_descs_[SLOPE],
+                tensor_descs_[DIFF_DST]};
+        outputs = {tensor_descs_[DIFF_SRC], tensor_descs_[DIFF_SLOPE]};
+    } else { // fwd
+        name = "prelu";
+        const std::string DST {TENSOR_ID + "_DST"};
+
+        tensor_descs_.emplace(
+                DST, spec_.data_dt, spec_.data_dims, spec_.raw_data_tag);
+        inputs = {tensor_descs_[SRC], tensor_descs_[SLOPE]};
+        outputs = {tensor_descs_[DST]};
+    }
+
+    op prelu_op(new_op_id, spec_.op_kind, inputs, outputs, name);
     prelu_op.set_attr<std::string>("data_format", spec_.data_format);
-    prelu_op.set_attr<bool>(
-            "per_channel_broadcast", spec_.broadcast_to_channel);
+    if (!spec_.is_bwd_pass)
+        prelu_op.set_attr<bool>(
+                "per_channel_broadcast", spec_.broadcast_to_channel);
+
     ops_.emplace_back(prelu_op);
     curr_out_map_ids_.assign({TENSOR_ID});
+
     return fill_status::DONE;
 }
 
@@ -95,12 +129,9 @@ int doit(const ::prelu::prb_t *prb, res_t *res) {
     auto cp = compile_partition(::prelu::init_pd, prb, res, par, ins, outs);
 
     auto src_fp = make_dnn_mem(ins[0], dt::f32, tag::abx);
-    auto slope_fp = make_dnn_mem(ins[1], dt::f32, tag::abx);
-    auto dst_fp = make_dnn_mem(outs[0], dt::f32, tag::abx);
-
     auto src_dt = make_dnn_mem(ins[0], prb->stag[0]);
+    auto slope_fp = make_dnn_mem(ins[1], dt::f32, tag::abx);
     auto slope_dt = make_dnn_mem(ins[1], prb->stag[1]);
-    auto dst_dt = make_dnn_mem(outs[0], prb->stag[0]);
 
     SAFE(::prelu::fill_data(SRC, src_dt, src_fp), WARN);
     SAFE(::prelu::fill_data(WEI, slope_dt, slope_fp), WARN);
@@ -113,26 +144,80 @@ int doit(const ::prelu::prb_t *prb, res_t *res) {
             dnnl::graph::tensor(ins[0], eng, static_cast<void *>(src_dt)));
     tensors_in.emplace_back(
             dnnl::graph::tensor(ins[1], eng, static_cast<void *>(slope_dt)));
-    tensors_out.emplace_back(
-            dnnl::graph::tensor(outs[0], eng, static_cast<void *>(dst_dt)));
 
-    SAFE(execute_and_wait(cp, tensors_in, tensors_out, res), WARN);
-    if (is_bench_mode(CORR)) {
-        args_t ref_args;
-        ref_args.set(DNNL_ARG_SRC, src_fp);
-        ref_args.set(DNNL_ARG_WEIGHTS, slope_fp);
-        ref_args.set(DNNL_ARG_DST, dst_fp);
+    if (prb->dir & FLAG_FWD) {
+        auto dst_fp = make_dnn_mem(outs[0], dt::f32, tag::abx);
+        auto dst_dt = make_dnn_mem(outs[0], prb->stag[0]);
 
-        TIME_REF(::prelu::compute_ref(prb, ref_args));
+        tensors_out.emplace_back(
+                dnnl::graph::tensor(outs[0], eng, static_cast<void *>(dst_dt)));
 
-        compare::compare_t cmp;
-        cmp.set_threshold(2 * epsilon_dt(prb->sdt[0]));
-        cmp.set_zero_trust_percent(50.f); // Due to filling
-        cmp.set_data_kind(DST);
-        SAFE(cmp.compare(dst_fp, dst_dt, prb->attr, res), WARN);
+        SAFE(execute_and_wait(cp, tensors_in, tensors_out, res), WARN);
+
+        if (is_bench_mode(CORR)) {
+            args_t ref_args;
+            ref_args.set(DNNL_ARG_SRC, src_fp);
+            ref_args.set(DNNL_ARG_WEIGHTS, slope_fp);
+            ref_args.set(DNNL_ARG_DST, dst_fp);
+
+            TIME_REF(::prelu::compute_ref(prb, ref_args));
+
+            compare::compare_t cmp;
+            cmp.set_threshold(2 * epsilon_dt(prb->sdt[0]));
+            cmp.set_zero_trust_percent(50.f); // Due to filling
+            cmp.set_data_kind(DST);
+            SAFE(cmp.compare(dst_fp, dst_dt, prb->attr, res), WARN);
+        }
+    } else if (prb->dir & FLAG_BWD) {
+        auto d_dst_fp = make_dnn_mem(ins[2], dt::f32, tag::abx);
+        auto d_dst_dt = make_dnn_mem(ins[2], prb->stag[0]);
+        auto d_src_fp = make_dnn_mem(outs[0], dt::f32, tag::abx);
+        auto d_src_dt = make_dnn_mem(outs[0], prb->stag[0]);
+        auto d_slope_fp = make_dnn_mem(outs[1], dt::f32, tag::abx);
+        auto d_slope_dt = make_dnn_mem(outs[1], prb->stag[1]);
+
+        SAFE(::prelu::fill_data(DST, d_dst_dt, d_dst_fp), WARN);
+
+        tensors_in.emplace_back(dnnl::graph::tensor(
+                ins[2], eng, static_cast<void *>(d_dst_dt)));
+        tensors_out.emplace_back(dnnl::graph::tensor(
+                outs[0], eng, static_cast<void *>(d_src_dt)));
+        tensors_out.emplace_back(dnnl::graph::tensor(
+                outs[1], eng, static_cast<void *>(d_slope_dt)));
+
+        SAFE(execute_and_wait(cp, tensors_in, tensors_out, res), WARN);
+
+        if (is_bench_mode(CORR)) {
+            args_t ref_args;
+            ref_args.set(DNNL_ARG_SRC, src_fp);
+            ref_args.set(DNNL_ARG_WEIGHTS, slope_fp);
+            ref_args.set(DNNL_ARG_DIFF_DST, d_dst_fp);
+            ref_args.set(DNNL_ARG_DIFF_SRC, d_src_fp);
+            ref_args.set(DNNL_ARG_DIFF_WEIGHTS, d_slope_fp);
+
+            TIME_REF(::prelu::compute_ref(prb, ref_args));
+
+            compare::compare_t cmp_d_src;
+            cmp_d_src.set_threshold(2 * epsilon_dt(prb->sdt[0]));
+            cmp_d_src.set_zero_trust_percent(50.f); // Due to filling
+            cmp_d_src.set_data_kind(SRC);
+            SAFE(cmp_d_src.compare(d_src_fp, d_src_dt, prb->attr, res), WARN);
+
+            compare::compare_t cmp_d_slope;
+            cmp_d_slope.set_threshold(2 * epsilon_dt(prb->sdt[1]));
+            // Weights are very sparse, no sense to test for trust.
+            cmp_d_slope.set_zero_trust_percent(100.f);
+            cmp_d_slope.set_data_kind(WEI);
+            SAFE(cmp_d_slope.compare(d_slope_fp, d_slope_dt, prb->attr, res),
+                    WARN);
+        }
+    } else {
+        SAFE(FAIL, CRIT);
     }
+
     SAFE(measure_perf(res->timer_map.perf_timer(), cp, tensors_in, tensors_out),
             WARN);
+
     return OK;
 }
 } // namespace prelu
