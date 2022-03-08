20b9e8089ea71cbf2ef30b216326fb4de797d5f6
ganand1 <gandhimathi.anand@intel.com>
2022-03-07 17:39:07 -0800

    tests: benchdnn graph: enable mlp inference pattern

---
+++ tests/benchdnn_graph/mlp/mlp.hpp
@@ -0,0 +1,170 @@
+/*******************************************************************************
+* Copyright 2022 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+#ifndef MLP_HPP
+#define MLP_HPP
+
+#include "common.hpp"
+#include "dnn_types.hpp"
+#include "dnnl_common.hpp"
+#include "dnnl_debug.hpp"
+#include "dnnl_graph_common.hpp"
+#include "utils/perf_report.hpp"
+
+#include "matmul/matmul.hpp"
+
+namespace mlp {
+
+using namespace matmul;
+using namespace benchdnnext;
+using graph_dt = dnnl::graph::logical_tensor::data_type;
+
+enum tensor_desc {
+    DATA_INT8_, //only for int8
+    DATA_,
+    DATA_OUT_, //only for int8
+    WEI_INT8_,
+    WEI_,
+    BIA_,
+    MATMUL_
+};
+
+struct settings_t {
+    settings_t() = default;
+
+    // ctor to save certain fields from resetting
+    settings_t(const char *perf_template) : settings_t() {
+        this->perf_template = perf_template;
+    }
+    prb_dims_t prb_dims;
+    std::vector<const dt_conf_t *> cfg {conf_f32};
+    std::vector<dnnl_data_type_t> bia_dt {dnnl_data_type_undef};
+    std::vector<std::string> stag {tag::abx}, wtag {tag::abx}, dtag {tag::abx};
+    std::vector<attr_t::post_ops_t> actfunc;
+    std::vector<attr_t::scale_t> scales {attr_t::scale_t()};
+    std::vector<attr_t::zero_points_t> zero_points {attr_t::zero_points_t()};
+
+    const char *perf_template_csv
+            = "perf,%engine%,%DESC%,"
+              "%-time%,%0time%";
+    const char *perf_template_def
+            = "perf,%engine%,%impl%,%prb%,%-time%,%0time%";
+    const char *perf_template = perf_template_def;
+
+    void reset() { *this = settings_t(perf_template); }
+};
+
+struct mlp_graph_spec_t {
+    mlp_graph_spec_t(const prb_dims_t &a_dims, const std::string &wtag,
+            const std::string &dtag, const dnnl_data_type_t &bia_dt,
+            const dt_conf_t *cfg, std::vector<attr_t::post_ops_t> actfunc,
+            attr_t::scale_t scales, attr_t::zero_points_t zps)
+        : prb_dims(a_dims)
+        , cfg(cfg)
+        , actfunc(actfunc)
+        , raw_data_tag(dtag)
+        , raw_wei_tag(wtag)
+        , batch_sz(a_dims.dims[0]) {
+        assert(actfunc[0].entry.size() == prb_dims.ndims - 2);
+        ;
+        num_hidden_layers = prb_dims.ndims - 2;
+        for (int i = 1; i < prb_dims.ndims - 1; i++) {
+            layer_dims.push_back({batch_sz, prb_dims.dims[i]});
+            weight_dims.push_back({prb_dims.dims[i], prb_dims.dims[i + 1]});
+            bias_dims.push_back({prb_dims.dims[i + 1]});
+            activation_func.push_back(benchdnnext::convert_alg_kind(
+                    actfunc[0].entry[i - 1].eltwise.alg));
+        }
+        layer_dims.push_back({batch_sz, prb_dims.dims[prb_dims.ndims - 1]});
+        mlp_src_dt = benchdnnext::convert_dt(cfg[SRC].dt);
+        mlp_wei_dt = benchdnnext::convert_dt(cfg[WEI].dt);
+        mlp_bias_dt = benchdnnext::convert_dt(bia_dt);
+        mlp_dst_dt = benchdnnext::convert_dt(cfg[DST].dt);
+        assert(mlp_src_dt == mlp_dst_dt);
+        has_bias = (mlp_bias_dt != graph_dt::undef);
+        mlp_layer_dt
+                = (mlp_src_dt == graph_dt::bf16) ? mlp_src_dt : graph_dt::f32;
+        is_mlp_int8 = mlp_src_dt == graph_dt::s8 || mlp_src_dt == graph_dt::u8;
+        attr.insert(actfunc[0]);
+        if (is_mlp_int8) {
+            attr.insert(scales);
+            attr.insert(zps);
+        }
+    }
+    ~mlp_graph_spec_t() {}
+    prb_dims_t prb_dims;
+    const dt_conf_t *cfg;
+    std::vector<attr_t::post_ops_t> actfunc;
+    int num_hidden_layers {0};
+    attr_t attr;
+    std::string raw_data_tag;
+    std::string raw_wei_tag;
+
+    int batch_sz;
+    bool has_bias {false};
+    std::vector<dnnl::graph::logical_tensor::dims_t> layer_dims, weight_dims,
+            bias_dims;
+    graph_dt mlp_src_dt, mlp_wei_dt, mlp_bias_dt, mlp_dst_dt, mlp_layer_dt;
+    bool is_mlp_int8;
+    std::vector<dnnl::graph::op::kind> activation_func;
+};
+
+std::ostream &operator<<(std::ostream &s, const mlp_graph_spec_t &spec);
+
+struct perf_report_t : public base_perf_report_t {
+    perf_report_t(const mlp_graph_spec_t *spec, const char *perf_template)
+        : base_perf_report_t(perf_template), spec_(spec) {}
+    void dump_desc(std::ostream &s) const override { s << *spec_; }
+    void dump_desc_csv(std::ostream &s) const override { dump_desc(s); }
+
+private:
+    const mlp_graph_spec_t *spec_;
+};
+
+struct mlp_graph_prb_t : public ::benchdnnext::graph_prb_t {
+    mlp_graph_prb_t(const mlp_graph_spec_t &spec) {
+        const auto stop_work = [](const fill_status_t s) {
+            return s != fill_status::DONE
+                    && s != fill_status::UNHANDLED_CONFIG_OPTIONS;
+        };
+        ctor_status = build_mlp_subgraph(spec);
+        if (stop_work(ctor_status)) return;
+
+        ctor_status = fill_status::DONE;
+    };
+    fill_status_t ctor_status;
+
+    ~mlp_graph_prb_t() {}
+    void build_tensor_desc(const mlp_graph_spec_t &spec);
+    fill_status_t build_mlp_subgraph(const mlp_graph_spec_t &spec);
+    dnnl::graph::op::kind get_main_op_kind() const noexcept override {
+        return dnnl::graph::op::kind::MatMul;
+    }
+
+private:
+    void addQuanDequanOp(const mlp_graph_spec_t &spec, const std::string src,
+            const std::string dst, std::vector<float> scales,
+            std::vector<int64_t> zps, bool isQuanOp);
+    void addMatmulActFuncOp(const mlp_graph_spec_t &spec, int layer_num);
+};
+
+void compute_ref_mlp(
+        const mlp_graph_spec_t *spec, const std::vector<args_t> &args);
+int doit(const mlp_graph_spec_t *spec, res_t *res);
+
+int bench(int argc, char **argv);
+} // namespace mlp
+
+#endif
