08385949d8c1b4195be72fb85d2ed230e565c292
Zhang, Wuxun <wuxun.zhang@intel.com>
2022-03-15 15:40:39 +0800

    backend: dnnl: prelu: fix undefined function

--- src/backend/dnnl/kernels/prelu.hpp
+++ src/backend/dnnl/kernels/prelu.hpp
@@ -197,9 +197,8 @@ public:
         BACKEND_DNNL_CHECK(pipeline.run(subgraph_));
 
         for (size_t i = 0; i < outputs.size(); i++) {
-            BACKEND_DNNL_CHECK(set_shape_and_layout(
-                    const_cast<impl::logical_tensor_t &>(outputs[i]),
-                    subgraph_->outs_[i]));
+            auto &out = const_cast<impl::logical_tensor_t &>(outputs[i]);
+            out = subgraph_->outs_[i];
         }
 
         resource_ctor_ = [this]() {
