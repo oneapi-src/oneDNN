69e898df9f8a52824f56e3fd3a3d626262807ca3
Damian Szwichtenberg <damian.szwichtenberg@intel.com>
2022-06-22 07:55:37 +0200

    tests: benchdnn: reorder: start using new graph infrastructure

--- tests/benchdnn/reorder/graph_reorder.cpp
+++ tests/benchdnn/reorder/graph_reorder.cpp
@@ -14,6 +14,7 @@
 * limitations under the License.
 *******************************************************************************/
 
+#include <tuple>
 #include <utility>
 #include "utils/compare.hpp"
 
@@ -22,7 +23,7 @@
 namespace benchdnnext {
 namespace reorder {
 
-void check_known_skipped_case_graph(
+static void check_known_skipped_case_graph(
         const ::reorder::prb_t *prb, res_t *res) noexcept {
     /* reorder op requires source and destination data types to be same.
        four possible cases:
@@ -69,26 +70,26 @@ void check_known_skipped_case_graph(
     check_graph_scales_and_zps_support(prb->attr, res);
 }
 
-bool is_quantize(graph_dt src_dt, graph_dt dst_dt) {
+static bool is_quantize(graph_dt src_dt, graph_dt dst_dt) {
     return src_dt == graph_dt::f32 && is_low_precision({dst_dt});
 }
 
-bool is_dequantize(graph_dt src_dt, graph_dt dst_dt) {
+static bool is_dequantize(graph_dt src_dt, graph_dt dst_dt) {
     return dst_dt == graph_dt::f32 && is_low_precision({src_dt});
 }
 
-void set_quant_op_attr(dnnl::graph::op &op_, const std::string &qtype,
+static void set_quant_op_attr(dnnl::graph::op &op, const std::string &qtype,
         const std::vector<float> &scales, const std::vector<int64_t> &zps,
         const int64_t &axis) {
-    op_.set_attr("qtype", qtype);
+    op.set_attr("qtype", qtype);
     //TODO: use zps - revisit
-    op_.set_attr("zps", zps);
-    op_.set_attr("scales", scales);
+    op.set_attr("zps", zps);
+    op.set_attr("scales", scales);
     //TODO: axis doesnt support PER_DIM_01
-    if (qtype == "per_channel") op_.set_attr("axis", axis);
+    if (qtype == "per_channel") op.set_attr("axis", axis);
 }
 
-int fill_zps(const ::reorder::prb_t *prb, const int64_t axis,
+static int fill_zps(const ::reorder::prb_t *prb, const int64_t axis,
         std::vector<int64_t> &src_zps, std::vector<int64_t> &dst_zps) {
     if (prb->attr.oscale.policy == attr_t::policy_t::COMMON) {
         if (prb->attr.zero_points.is_def()) {
@@ -117,7 +118,7 @@ int fill_zps(const ::reorder::prb_t *prb, const int64_t axis,
     return OK;
 }
 
-int fill_scales(const ::reorder::prb_t *prb, const int64_t axis,
+static int fill_scales(const ::reorder::prb_t *prb, const int64_t axis,
         std::vector<float> &scales) {
     if (prb->attr.oscale.policy == attr_t::policy_t::COMMON) {
         scales.emplace_back(prb->scales[0]);
@@ -136,9 +137,9 @@ int fill_scales(const ::reorder::prb_t *prb, const int64_t axis,
     return OK;
 }
 
-void prepare_runtime_scales(const ::reorder::prb_t *prb, dnn_mem_t &scales_dt,
-        const dnnl::graph::logical_tensor &in, std::vector<float> scales,
-        int64_t axis) {
+static void prepare_runtime_scales(const ::reorder::prb_t *prb,
+        dnn_mem_t &scales_dt, const dnnl::graph::logical_tensor &in,
+        std::vector<float> scales, int64_t axis) {
     // scales is required input for dynamic q/deq
     scales_dt = make_dnn_mem(in, dt::f32, tag::x);
     fill_scales(prb, axis, scales);
@@ -147,7 +148,7 @@ void prepare_runtime_scales(const ::reorder::prb_t *prb, dnn_mem_t &scales_dt,
     }
 }
 
-void maybe_prepare_runtime_zero_points(const ::reorder::prb_t *prb,
+static void maybe_prepare_runtime_zero_points(const ::reorder::prb_t *prb,
         dnn_mem_t &zps_dt, const dnnl::graph::logical_tensor &in,
         std::vector<int64_t> src_zps, std::vector<int64_t> dst_zps,
         int64_t axis) {
@@ -168,18 +169,12 @@ void maybe_prepare_runtime_zero_points(const ::reorder::prb_t *prb,
     }
 }
 
-fill_status_t reorder_graph_prb_t::handle_main_op_(
-        const ::reorder::prb_t *prb) {
-    using op = dnnl::graph::op;
+fill_status_t append_graph_with_block(const ::reorder::prb_t *prb) {
+    using graph_dt = dnnl::graph::logical_tensor::data_type;
 
-    const size_t new_op_id = ops_.size();
-    const std::string TENSOR_ID = std::to_string(new_op_id);
-    tensor_id["main"].push_back(TENSOR_ID);
-    const std::string SRC {TENSOR_ID + "_SRC"};
-    const std::string DST {TENSOR_ID + "_DST"};
-    // specific for dynamic q/deq
-    const std::string SCALES {TENSOR_ID + "_SCALES"};
-    const std::string ZPS {TENSOR_ID + "_ZPS"};
+    graph_t &graph = graph_t::get();
+
+    const auto connect_to_previous_block = graph.has_blocks();
 
     const auto src_dt = convert_dt(prb->sdt);
     const auto dst_dt = convert_dt(prb->ddt);
@@ -188,116 +183,173 @@ fill_status_t reorder_graph_prb_t::handle_main_op_(
     // axis is used only for PER_DIM_0 and PER_DIM_1 policies
     int64_t axis
             = prb->attr.oscale.policy == attr_t::policy_t::PER_DIM_1 ? 1 : 0;
+    std::vector<float> default_scales({1.0});
+    const auto sz_dims
+            = qtype == "per_channel" ? dims_t {prb->dims[axis]} : dims_t {1};
 
     std::vector<float> scales;
-    std::vector<float> default_scales({1.0});
     std::vector<int64_t> src_zps, dst_zps;
-
     if (!runtime) {
         fill_scales(prb, axis, scales);
         fill_zps(prb, axis, src_zps, dst_zps);
     }
 
-    const auto sz_dims
-            = qtype == "per_channel" ? dims_t {prb->dims[axis]} : dims_t {1};
-
-    tensor_descs_.emplace(SRC, src_dt, prb->dims, prb->stag);
-    tensor_descs_.emplace(DST, dst_dt, prb->dims, prb->dtag);
-
+    const auto is_reorder = src_dt == dst_dt;
+    const auto is_typecast
+            = (src_dt == graph_dt::f32 && dst_dt == graph_dt::bf16)
+            || (src_dt == graph_dt::bf16 && dst_dt == graph_dt::f32);
+    const auto is_qdq
+            = is_quantize(src_dt, dst_dt) || is_dequantize(src_dt, dst_dt);
     if (is_low_precision({src_dt}) && is_low_precision({dst_dt})) {
-        //SRC->SRC_F32->DST_F32->DST
-        const std::string SRC_F32 {TENSOR_ID + "_SRC_F32"};
-        const std::string DST_F32 {TENSOR_ID + "_DST_F32"};
-        tensor_descs_.emplace(SRC_F32, graph_dt::f32, prb->dims, prb->stag);
-        tensor_descs_.emplace(DST_F32, graph_dt::f32, prb->dims, prb->dtag);
+        // SRC->SRC_F32->DST_F32->DST
+        const auto op0_id = graph.generate_id_for(entry_kind::DEQUANTIZE);
+        const auto op0_src_id = connect_to_previous_block
+                ? graph.get_last_block_out_id()
+                : graph.generate_id_for(op0_id, lt_kind::SRC);
+        const auto op0_dst_id = graph.generate_id_for(op0_id, lt_kind::DST);
 
-        op dequantize_op(new_op_id, op::kind::Dequantize, {tensor_descs_[SRC]},
-                {tensor_descs_[SRC_F32]}, "dequantize");
+        graph.create_lt(op0_src_id, src_dt, prb->dims, prb->stag);
+        graph.create_lt(op0_dst_id, graph_dt::f32, prb->dims, prb->stag);
+
+        dnnl::graph::op dequantize_op(op0_id, dnnl::graph::op::kind::Dequantize,
+                graph.stringify_id(op0_id));
         set_quant_op_attr(dequantize_op, qtype, default_scales, src_zps, axis);
-        ops_.emplace_back(dequantize_op);
 
-        op reorder_op(ops_.size(), op::kind::Reorder, {tensor_descs_[SRC_F32]},
-                {tensor_descs_[DST_F32]}, "reorder");
-        ops_.emplace_back(reorder_op);
+        graph.append(op0_id, dequantize_op, {op0_src_id}, {op0_dst_id});
 
-        op quantize_op(ops_.size(), op::kind::Quantize,
-                {tensor_descs_[DST_F32]}, {tensor_descs_[DST]}, "quantize");
+        const auto op1_id = graph.generate_id_for(entry_kind::REORDER);
+        const auto op1_dst_id = graph.generate_id_for(op1_id, lt_kind::DST);
+
+        graph.create_lt(op1_dst_id, graph_dt::f32, prb->dims, prb->dtag);
+
+        dnnl::graph::op reorder_op(op1_id, dnnl::graph::op::kind::Reorder,
+                graph.stringify_id(op1_id));
+
+        graph.append(op1_id, reorder_op, {op0_dst_id}, {op1_dst_id});
+
+        const auto op2_id = graph.generate_id_for(entry_kind::QUANTIZE);
+        const auto op2_dst_id = graph.generate_id_for(op2_id, lt_kind::DST);
+
+        graph.create_lt(op2_dst_id, dst_dt, prb->dims, prb->dtag);
+
+        dnnl::graph::op quantize_op(op2_id, dnnl::graph::op::kind::Quantize,
+                graph.stringify_id(op2_id));
         quantize_op.set_attr("qtype", qtype);
         set_quant_op_attr(quantize_op, qtype, scales, dst_zps, axis);
-        ops_.emplace_back(quantize_op);
-    } else if (src_dt == dst_dt) {
-        op reorder_op(new_op_id, op::kind::Reorder, {tensor_descs_[SRC]},
-                {tensor_descs_[DST]}, "reorder");
-        ops_.emplace_back(reorder_op);
-    } else if ((src_dt == graph_dt::f32 && dst_dt == graph_dt::bf16)
-            || (src_dt == graph_dt::bf16 && dst_dt == graph_dt::f32)) {
-        op typecast_op(new_op_id, op::kind::TypeCast, {tensor_descs_[SRC]},
-                {tensor_descs_[DST]}, "typecast");
-        ops_.emplace_back(typecast_op);
-    } else if (is_quantize(src_dt, dst_dt)) {
-        if (!runtime) {
-            op quantize_op(new_op_id, op::kind::Quantize, {tensor_descs_[SRC]},
-                    {tensor_descs_[DST]}, "quantize");
-            set_quant_op_attr(quantize_op, qtype, scales, dst_zps, axis);
-            ops_.emplace_back(quantize_op);
-        } else {
-            tensor_descs_.emplace(SCALES, graph_dt::f32, sz_dims, lt::strided);
-            std::vector<dnnl::graph::logical_tensor> inputs
-                    = {tensor_descs_[SRC], tensor_descs_[SCALES]};
-            if (!prb->attr.zero_points.is_def()) {
-                tensor_descs_.emplace(ZPS, graph_dt::s32, sz_dims, lt::strided);
-                inputs.push_back(tensor_descs_[ZPS]);
-            }
-            op dync_quantize(new_op_id, op::kind::DynamicQuantize, inputs,
-                    {tensor_descs_[DST]}, "dynamic_quantize");
-            dync_quantize.set_attr<std::string>("qtype", qtype);
-            if (qtype == "per_channel")
-                dync_quantize.set_attr<int64_t>("axis", axis);
-            ops_.emplace_back(dync_quantize);
-        }
-    } else if (is_dequantize(src_dt, dst_dt)) {
-        if (!runtime) {
-            op dequantize_op(new_op_id, op::kind::Dequantize,
-                    {tensor_descs_[SRC]}, {tensor_descs_[DST]}, "dequantize");
-            set_quant_op_attr(dequantize_op, qtype, scales, src_zps, axis);
-            ops_.emplace_back(dequantize_op);
-        } else {
-            tensor_descs_.emplace(SCALES, graph_dt::f32, sz_dims, lt::strided);
-            std::vector<dnnl::graph::logical_tensor> inputs
-                    = {tensor_descs_[SRC], tensor_descs_[SCALES]};
 
-            if (!prb->attr.zero_points.is_def()) {
-                tensor_descs_.emplace(ZPS, graph_dt::s32, sz_dims, lt::strided);
-                inputs.push_back(tensor_descs_[ZPS]);
-            }
-            op dync_dequantize(new_op_id, op::kind::DynamicDequantize, inputs,
-                    {tensor_descs_[DST]}, "dynamic_dequantize");
-            dync_dequantize.set_attr<std::string>("qtype", qtype);
-            if (qtype == "per_channel")
-                dync_dequantize.set_attr<int64_t>("axis", axis);
-            ops_.emplace_back(dync_dequantize);
+        graph.append(op2_dst_id, quantize_op, {op1_dst_id}, {op2_dst_id});
+    } else if (is_reorder || is_typecast || (!runtime && is_qdq)) {
+        entry_kind_t e_kind;
+        dnnl::graph::op::kind op_kind;
+        if (is_reorder) {
+            e_kind = entry_kind::REORDER;
+            op_kind = dnnl::graph::op::kind::Reorder;
+        } else if (is_typecast) {
+            e_kind = entry_kind::TYPECAST;
+            op_kind = dnnl::graph::op::kind::TypeCast;
+        } else {
+            e_kind = is_quantize(src_dt, dst_dt) ? entry_kind::QUANTIZE
+                                                 : entry_kind::DEQUANTIZE;
+            op_kind = is_quantize(src_dt, dst_dt)
+                    ? dnnl::graph::op::kind::Quantize
+                    : dnnl::graph::op::kind::Dequantize;
         }
+
+        const auto op_id = graph.generate_id_for(e_kind);
+        const auto src_id = connect_to_previous_block
+                ? graph.get_last_block_out_id()
+                : graph.generate_id_for(op_id, lt_kind::SRC);
+        const auto dst_id = graph.generate_id_for(op_id, lt_kind::DST);
+
+        graph.create_lt(src_id, src_dt, prb->dims, prb->stag);
+        graph.create_lt(dst_id, dst_dt, prb->dims, prb->dtag);
+
+        dnnl::graph::op aop(op_id, op_kind, graph.stringify_id(op_id));
+        if (is_qdq) {
+            const auto zps = is_quantize(src_dt, dst_dt) ? dst_zps : src_zps;
+            set_quant_op_attr(aop, qtype, scales, zps, axis);
+        }
+
+        graph.append(op_id, aop, {src_id}, {dst_id});
+    } else if (runtime && is_qdq) {
+        const auto e_kind = is_quantize(src_dt, dst_dt)
+                ? entry_kind::QUANTIZE
+                : entry_kind::DEQUANTIZE;
+        const auto op_kind = is_quantize(src_dt, dst_dt)
+                ? dnnl::graph::op::kind::DynamicQuantize
+                : dnnl::graph::op::kind::DynamicDequantize;
+
+        const auto op_id = graph.generate_id_for(e_kind);
+        const auto src0_id = connect_to_previous_block
+                ? graph.get_last_block_out_id()
+                : graph.generate_id_for(op_id, lt_kind::SRC_I, size_t(0));
+        const auto src1_id
+                = graph.generate_id_for(op_id, lt_kind::SRC_I, size_t(1));
+        const auto dst_id = graph.generate_id_for(op_id, lt_kind::DST);
+
+        graph.create_lt(src0_id, src_dt, prb->dims, prb->stag);
+        graph.create_lt(src1_id, graph_dt::f32, sz_dims,
+                dnnl::graph::logical_tensor::layout_type::strided);
+        graph.create_lt(dst_id, dst_dt, prb->dims, prb->dtag);
+
+        std::vector<size_t> src_ids = {src0_id, src1_id};
+        std::vector<size_t> dst_ids = {dst_id};
+        if (!prb->attr.zero_points.is_def()) {
+            const auto src2_id
+                    = graph.generate_id_for(op_id, lt_kind::SRC_I, size_t(2));
+            graph.create_lt(src2_id, graph_dt::s32, sz_dims,
+                    dnnl::graph::logical_tensor::layout_type::strided);
+            src_ids.push_back(src2_id);
+        }
+
+        dnnl::graph::op dynamic_qdq_op(
+                op_id, op_kind, graph.stringify_id(op_id));
+        dynamic_qdq_op.set_attr<std::string>("qtype", qtype);
+        if (qtype == "per_channel")
+            dynamic_qdq_op.set_attr<int64_t>("axis", axis);
+
+        graph.append(op_id, dynamic_qdq_op, src_ids, dst_ids);
     } else if (src_dt == graph_dt::bf16 && is_low_precision({dst_dt})) {
-        const std::string SRC_F32 {TENSOR_ID + "_SRC_F32"};
-        tensor_descs_.emplace(SRC_F32, graph_dt::f32, prb->dims, prb->stag);
-        op typecast_op(new_op_id, op::kind::TypeCast, {tensor_descs_[SRC]},
-                {tensor_descs_[SRC_F32]}, "typecast");
-        ops_.emplace_back(typecast_op);
-        op quantize_op(ops_.size(), op::kind::Quantize,
-                {tensor_descs_[SRC_F32]}, {tensor_descs_[DST]}, "quantize");
+        const auto op0_id = graph.generate_id_for(entry_kind::TYPECAST);
+        const auto op0_src_id = connect_to_previous_block
+                ? graph.get_last_block_out_id()
+                : graph.generate_id_for(op0_id, lt_kind::SRC);
+        const auto op0_dst_id = graph.generate_id_for(op0_id, lt_kind::DST);
+
+        graph.create_lt(op0_src_id, src_dt, prb->dims, prb->stag);
+        graph.create_lt(op0_dst_id, graph_dt::f32, prb->dims, prb->stag);
+
+        dnnl::graph::op typecast_op(op0_id, dnnl::graph::op::kind::TypeCast,
+                graph.stringify_id(op0_id));
+
+        graph.append(op0_id, typecast_op, {op0_src_id}, {op0_dst_id});
+
+        const auto op1_id = graph.generate_id_for(entry_kind::QUANTIZE);
+        const auto op1_dst_id = graph.generate_id_for(op1_id, lt_kind::DST);
+
+        graph.create_lt(op1_dst_id, dst_dt, prb->dims, prb->dtag);
+
+        dnnl::graph::op quantize_op(op1_id, dnnl::graph::op::kind::Quantize,
+                graph.stringify_id(op1_id));
         set_quant_op_attr(quantize_op, qtype, scales, dst_zps, axis);
-        ops_.emplace_back(quantize_op);
+
+        graph.append(op1_id, quantize_op, {op0_dst_id}, {op1_dst_id});
     } else {
         return fill_status::UNHANDLED_CONFIG_OPTIONS;
     }
 
-    curr_out_map_ids_.assign({TENSOR_ID});
+    // handle post ops
+    fill_status_t status;
+    for (const auto &entry : prb->attr.post_ops.entry) {
+        if (entry.is_sum_kind()) {
+            std::tie(status, std::ignore) = append_graph_with_sum(entry);
+            BENCHDNNEXT_VERIFY(status);
+        }
+    }
+
+    graph.close_block();
 
     return fill_status::DONE;
-}
-
-fill_status_t reorder_graph_prb_t::handle_sum_() {
-    return po_handler.reorder.sum_handler(*this);
 }
 
 int doit(const ::reorder::prb_t *prb, res_t *res) {
@@ -312,20 +364,27 @@ int doit(const ::reorder::prb_t *prb, res_t *res) {
     check_known_skipped_case_graph(prb, res);
     if (res->state == SKIPPED) return OK;
 
-    reorder_graph_prb_t graph_prb(prb);
-    if (graph_prb.ctor_status != fill_status::DONE
-            && graph_prb.ctor_status != fill_status::UNHANDLED_CONFIG_OPTIONS) {
+    const auto status = append_graph_with_block(prb);
+    if (status != fill_status::DONE
+            && status != fill_status::UNHANDLED_CONFIG_OPTIONS) {
+        cleanup();
         return res->state = UNIMPLEMENTED, FAIL;
     }
 
-    auto graph_h = graph_prb.to_graph();
+    auto &graph = graph_t::get();
 
-    const auto partitions = graph_h.get_partitions();
-    if (partitions.empty() || partitions.size() > 1)
+    // Filter partitions
+    const auto partitions = graph.get_partitions();
+    if (partitions.empty() || partitions.size() > 1) {
+        cleanup();
         return res->state = FAILED, FAIL;
+    }
 
     const auto par = partitions[0];
-    if (!par.is_supported()) return res->state = UNIMPLEMENTED, FAIL;
+    if (!par.is_supported()) {
+        cleanup();
+        return res->state = UNIMPLEMENTED, FAIL;
+    }
 
     const auto ins = par.get_in_ports();
     const auto outs = par.get_out_ports();
@@ -374,7 +433,7 @@ int doit(const ::reorder::prb_t *prb, res_t *res) {
     tensors_out.emplace_back(
             dnnl::graph::tensor(outs[0], eng, static_cast<void *>(dst_dt)));
 
-    if (graph_prb.has_post_sum()) {
+    if (!prb->attr.post_ops.entry.empty()) {
         dnnl::graph::tensor sum_src1_tensor = dnnl::graph::tensor(
                 ins.back(), eng, static_cast<void *>(dst_dt));
         tensors_in.emplace_back(sum_src1_tensor);
@@ -427,6 +486,8 @@ int doit(const ::reorder::prb_t *prb, res_t *res) {
     }
     SAFE(measure_perf(res->timer_map.perf_timer(), cp, tensors_in, tensors_out),
             WARN);
+
+    cleanup();
 
     return OK;
 }
--- tests/benchdnn/reorder/graph_reorder.hpp
+++ tests/benchdnn/reorder/graph_reorder.hpp
@@ -23,33 +23,7 @@
 namespace benchdnnext {
 namespace reorder {
 
-struct reorder_graph_prb_t : public graph_prb_t {
-    reorder_graph_prb_t(const ::reorder::prb_t *prb) {
-        const auto stop_work = [](const fill_status_t s) {
-            return s != fill_status::DONE
-                    && s != fill_status::UNHANDLED_CONFIG_OPTIONS;
-        };
-
-        ctor_status = handle_main_op_(prb);
-        if (stop_work(ctor_status)) return;
-
-        for (const auto &po : prb->attr.post_ops.entry) {
-            if (po.is_sum_kind()) {
-                has_post_sum_ = true;
-                ctor_status = handle_sum_();
-                if (stop_work(ctor_status)) return;
-            }
-        }
-        ctor_status = fill_status::DONE;
-    };
-
-private:
-    po_handlers_t po_handler;
-
-    fill_status_t handle_main_op_(const ::reorder::prb_t *prb);
-    fill_status_t handle_sum_();
-};
-
+fill_status_t append_graph_with_block(const ::reorder::prb_t *prb);
 int doit(const ::reorder::prb_t *prb, res_t *res);
 
 } // namespace reorder
