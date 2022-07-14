0df7aa2668ab8a20f7eaeb69a0ad1436fc3f69a7
Daniel Youssif <daniel.youssif@intel.com>
2022-07-14 01:14:58 +0000

    gpu: jit: conv: fix slm bank parameterizations

--- src/gpu/compute/device_info.hpp
+++ src/gpu/compute/device_info.hpp
@@ -218,6 +218,7 @@ public:
     static int max_slm_size_per_tg(
             gpu_arch_t gpu_arch, bool large_grf_mode = false);
     static int slm_memory_bank_count(gpu_arch_t gpu_arch);
+    static int slm_memory_bank_granularity(gpu_arch_t gpu_arch);
     size_t llc_cache_size() const { return llc_cache_size_; }
 
     const runtime_version_t &runtime_version() const {
