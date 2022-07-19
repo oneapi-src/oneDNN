0cd28dfbc4e9945b7a03c5cbcd3806bc4bb73ed4
Peter Caday <peter.caday@intel.com>
2022-07-19 09:19:19 -0700

    gpu: jit: gemm: provide preferred thread count

--- src/gpu/jit/gemm/gen_gemm.hpp
+++ src/gpu/jit/gemm/gen_gemm.hpp
@@ -218,9 +218,11 @@ struct gen_gemm_t : public gpu_gemm_t {
 
         status_t query(query_t what, int idx, void *result) const override {
             switch ((int)what) {
-                case (int)query::preferred_gpu_threads_per_eu:
-                    *(int *)result = 4;
+                case (int)query::preferred_gpu_threads_per_eu: {
+                    int grfs = kernel_desc_.driver_info()->grfCount;
+                    *(int *)result = (grfs > 128) ? 4 : 8;
                     break;
+                }
                 default: return gpu_gemm_pd_t::query(what, idx, result);
             }
             return status::success;
