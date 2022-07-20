ce85a56e0b536723b54c86609407cb853958085d
Lv, Tao A <tao.a.lv@intel.com>
2022-07-20 23:28:01 +0800

    benchdnn: eltwise: adjust hardsigmoid to accept alpha and beta

--- tests/benchdnn/dnn_types.cpp
+++ tests/benchdnn/dnn_types.cpp
@@ -1339,7 +1339,8 @@ float compute_eltwise_fwd(
         case pk_t::GELU_ERF: return scale * gelu_erf_fwd(src);
         case pk_t::ROUND: return scale * round_fwd(src);
         case pk_t::HARDSWISH: return scale * hardswish_fwd(src);
-        case pk_t::HARDSIGMOID: return scale * hardsigmoid_fwd(src);
+        case pk_t::HARDSIGMOID:
+            return scale * hardsigmoid_fwd(src, alpha, beta);
         case pk_t::RELU_DST: return scale * relu_fwd(src, alpha);
         case pk_t::TANH_DST: return scale * tanh_fwd(src);
         case pk_t::ELU_DST: return scale * elu_fwd(src, alpha);
@@ -1380,7 +1381,7 @@ float compute_eltwise_bwd(
         case pk_t::POW: return pow_bwd(d_dst, src, alpha, beta);
         case pk_t::GELU_ERF: return gelu_erf_bwd(d_dst, src);
         case pk_t::HARDSWISH: return hardswish_bwd(d_dst, src);
-        case pk_t::HARDSIGMOID: return hardsigmoid_bwd(d_dst, src);
+        case pk_t::HARDSIGMOID: return hardsigmoid_bwd(d_dst, src, alpha, beta);
 
         case pk_t::RELU_DST: return relu_bwd_use_dst(d_dst, src, alpha);
         case pk_t::TANH_DST: return tanh_bwd_use_dst(d_dst, src);
--- tests/benchdnn/doc/knobs_attr.md
+++ tests/benchdnn/doc/knobs_attr.md
@@ -156,7 +156,6 @@ Operations may be called in any order, e.g. apply `SUM` at first and then apply
       - `exp_dst`
       - `gelu_erf`
       - `gelu_tanh`
-      - `hardsigmoid`
       - `hardswish`
       - `log`
       - `logistic`
@@ -182,6 +181,7 @@ Operations may be called in any order, e.g. apply `SUM` at first and then apply
       - `clip`
       - `clip_v2`
       - `clip_v2_dst`
+      - `hardsigmoid`
       - `linear`
       - `pow`
 
