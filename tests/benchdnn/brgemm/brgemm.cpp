441602f284c81913f936cf2f76db4b78449b6910
Wierschem, Keola <keola.wierschem@intel.com>
2022-07-20 14:06:46 -0700

    benchdnn: brgemm: warn after oneDNN call failures

--- tests/benchdnn/brgemm/brgemm.cpp
+++ tests/benchdnn/brgemm/brgemm.cpp
@@ -311,21 +311,22 @@ int doit(const prb_t *prb, res_t *res) {
     auto dnnl_attr = make_benchdnn_dnnl_wrapper(
             create_dnnl_attr(prb->attr, attr_args));
 
-    DNN_SAFE_STATUS(brgemm_desc_set_postops(
-            &brgemm_desc, dnnl_attr, &dst_md, prb->get_ldd(), prb->bia_dt));
+    DNN_SAFE(brgemm_desc_set_postops(&brgemm_desc, dnnl_attr, &dst_md,
+                     prb->get_ldd(), prb->bia_dt),
+            WARN);
 
     brgemm_attr_t brgemm_attr;
-    DNN_SAFE_STATUS(brgemm_attr_init(&brgemm_attr, prb));
-    DNN_SAFE_STATUS(brgemm_desc_set_attr(&brgemm_desc, brgemm_attr));
+    DNN_SAFE(brgemm_attr_init(&brgemm_attr, prb), WARN);
+    DNN_SAFE(brgemm_desc_set_attr(&brgemm_desc, brgemm_attr), WARN);
 
     brgemm_kernel_t *brgemm_kernel_;
-    DNN_SAFE_STATUS(brgemm_kernel_create(&brgemm_kernel_, brgemm_desc));
+    DNN_SAFE(brgemm_kernel_create(&brgemm_kernel_, brgemm_desc), WARN);
     auto brgemm_kernel = make_benchdnn_dnnl_wrapper(brgemm_kernel_);
 
     char palette[AMX_PALETTE_SIZE] = {};
     const auto init_tile_status = brgemm_init_tiles(brgemm_desc, palette);
     if (init_tile_status == dnnl_success)
-        DNN_SAFE_STATUS(amx_tile_configure(palette));
+        DNN_SAFE(amx_tile_configure(palette), WARN);
 
     const auto &test_engine = get_test_engine();
     const auto &ref_engine = get_cpu_engine();
@@ -487,7 +488,7 @@ int doit(const prb_t *prb, res_t *res) {
         check_correctness(prb, {DST}, args, ref_args, setup_cmp, res);
     }
 
-    if (init_tile_status == dnnl_success) DNN_SAFE_STATUS(amx_tile_release());
+    if (init_tile_status == dnnl_success) DNN_SAFE(amx_tile_release(), WARN);
 
     return OK;
 }
