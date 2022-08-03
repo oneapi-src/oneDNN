87415c4ffc8c8a30b626d9b6a1a19c12b8e602f1
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2022-08-02 19:10:58 -0700

    gpu: jit: conv: revert layout_iterator_t extension

--- src/gpu/jit/conv/tensor.hpp
+++ src/gpu/jit/conv/tensor.hpp
@@ -1113,45 +1113,27 @@ private:
 // layouts.
 class layout_iterator_t {
 public:
-    layout_iterator_t(const layout_t &l) : l_(&l), block_idx_(-1), block_(1) {}
-
-    layout_iterator_t &operator=(const layout_iterator_t &other) {
-        l_ = other.l_;
-        block_idx_ = other.block_idx_;
-        block_ = other.block_;
-        return *this;
-    }
+    layout_iterator_t(const layout_t &l) : l_(l), block_idx_(-1), block_(1) {}
 
     bool has_next() const {
         dim_t b = block_;
         int b_idx = block_idx_;
         while (b == 1) {
             b_idx++;
-            if (b_idx >= int(l_->blocks().size())) return false;
-            b = int(l_->blocks()[b_idx].block);
+            if (b_idx >= int(l_.blocks().size())) return false;
+            b = int(l_.blocks()[b_idx].block);
         }
         return true;
     }
 
-    layout_iterator_t &operator++() { return next(); }
-
-    // If factor hint is set, searches for the maximum factor that is less or
-    // equal than the hint. If no such factor or hint is unset, searches for
-    // the smallest factor.
-    layout_iterator_t &next(int factor_hint = -1) {
+    layout_iterator_t &operator++() {
         ir_assert(has_next());
         while (block_ == 1) {
             block_idx_++;
-            block_ = int(l_->blocks()[block_idx_].block);
-        }
-        // Handle factor hint.
-        if (factor_hint != -1) {
-            int factor = utils::max_div(block_, factor_hint);
-            block_ /= factor;
-            if (factor != 1) return *this;
+            block_ = int(l_.blocks()[block_idx_].block);
         }
         // Find smallest factor.
-        for (int factor = 2; factor <= (int)block_; factor++) {
+        for (int factor = 2; factor <= int(block_); factor++) {
             if (block_ % factor == 0) {
                 block_ /= factor;
                 return *this;
@@ -1163,9 +1145,9 @@ public:
     }
 
     tensor_t tile() const {
-        std::vector<dim_t> dims(l_->ndims(), 1);
+        std::vector<dim_t> dims(l_.ndims(), 1);
         for (int i = 0; i <= block_idx_; i++) {
-            auto &b = l_->blocks()[i];
+            auto &b = l_.blocks()[i];
             int b_block = b.block;
             if (i == block_idx_) b_block /= block_;
             dims[b.dim_idx] *= b_block;
@@ -1176,7 +1158,7 @@ public:
     int nblocks() const { return block_idx_ + 1; }
 
     layout_t outer_layout() const {
-        auto &blocks = l_->blocks();
+        auto &blocks = l_.blocks();
         std::vector<block_t> outer_blocks;
         if (block_ > 1) {
             auto &b = blocks[block_idx_];
@@ -1186,11 +1168,11 @@ public:
         }
         outer_blocks.insert(outer_blocks.end(), blocks.begin() + block_idx_ + 1,
                 blocks.end());
-        return layout_t(l_->type(), l_->ndims(), l_->offset(), outer_blocks);
+        return layout_t(l_.type(), l_.ndims(), l_.offset(), outer_blocks);
     }
 
 private:
-    const layout_t *l_;
+    const layout_t &l_;
 
     int block_idx_;
     dim_t block_;
