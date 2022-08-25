4f221b00e16eb0974ab03a2f350dd84f1572c42a
Denis Samoilov <denis.samoylov@intel.com>
2022-08-24 21:13:13 -0700

    gtests: remove redundant SYCL headers

--- tests/gtests/sycl/api/test_stream.cpp
+++ tests/gtests/sycl/api/test_stream.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2021 Intel Corporation
+* Copyright 2019-2022 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -21,8 +21,6 @@
 #include "oneapi/dnnl/dnnl_sycl.hpp"
 
 #include <memory>
-#include <CL/cl.h>
-#include <CL/sycl.hpp>
 
 using namespace cl::sycl;
 
