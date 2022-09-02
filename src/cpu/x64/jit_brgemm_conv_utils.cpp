8db125ef86947b99a81e5daaf6cf98548b418a3c
Tomasz Czeszun <tomasz.czeszun@intel.com>
2022-09-02 04:16:38 -0700

    src: cpu: x64: conv: enable per_tensor post-ops in brgemm conv

--- src/cpu/x64/jit_brgemm_conv_utils.cpp
+++ src/cpu/x64/jit_brgemm_conv_utils.cpp
@@ -80,8 +80,8 @@ bool post_ops_ok(jit_brgemm_conv_conf_t &jcp, primitive_attr_t &attr,
             {sum, eltwise, binary}, post_ops, &dst_d,
             false /*sum_at_pos_0_only*/, false /*sum_requires_scale_one*/,
             false /*sum_requires_zp_zero*/,
-            {broadcasting_strategy_t::per_oc,
-                    broadcasting_strategy_t::scalar}));
+            {broadcasting_strategy_t::per_oc, broadcasting_strategy_t::scalar,
+                    broadcasting_strategy_t::no_broadcast}));
 }
 
 bool is_groups_ok(jit_brgemm_conv_conf_t &jcp) {
