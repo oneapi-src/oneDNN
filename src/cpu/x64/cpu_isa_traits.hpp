b08e747226192c0e5b1d93c4ea026c6a6d2c010c
Denis Kotov <denys.kotov@altran.com>
2022-09-08 14:13:54 +0300

    [FIX] Added support for exceptions during code generation

--- src/cpu/x64/cpu_isa_traits.hpp
+++ src/cpu/x64/cpu_isa_traits.hpp
@@ -31,10 +31,8 @@
 /* in order to make selinux happy memory that would be marked with X-bit should
  * be obtained with mmap */
 // #define XBYAK_USE_MMAP_ALLOCATOR
-
+#if !defined(DNNL_ENABLE_EXCEPTIONS) && defined(NDEBUG)
 #define XBYAK_NO_EXCEPTION
-#ifndef NDEBUG
-#undef XBYAK_NO_EXCEPTION
 #endif
 
 #if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
