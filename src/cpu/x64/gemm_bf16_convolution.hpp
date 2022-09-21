a2c7d7ffee170fd12833f0deddcd15f7f279486e
Srinivas Putta <srinivas.putta@intel.com>
2022-09-21 11:48:34 -0700

    x64: gemm bf16 conv: check for isa support

--- src/cpu/x64/gemm_bf16_convolution.hpp
+++ src/cpu/x64/gemm_bf16_convolution.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2021 Intel Corporation
+* Copyright 2019-2022 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ struct gemm_bf16_convolution_fwd_t : public primitive_t {
                 USE_GLOBAL_SCRATCHPAD);
 
         status_t init(engine_t *engine) {
-            bool ok = true && is_fwd()
+            bool ok = true && is_fwd() && mayiuse(avx512_core)
                     && set_default_alg_kind(alg_kind::convolution_direct)
                     && expect_data_types(data_type::bf16, data_type::bf16,
                             data_type::undef, dst_data_type, data_type::f32)
@@ -249,7 +249,8 @@ struct gemm_bf16_convolution_bwd_data_t : public primitive_t {
                 USE_GLOBAL_SCRATCHPAD);
 
         status_t init(engine_t *engine) {
-            bool ok = true && desc()->prop_kind == prop_kind::backward_data
+            bool ok = true && mayiuse(avx512_core)
+                    && desc()->prop_kind == prop_kind::backward_data
                     && set_default_alg_kind(alg_kind::convolution_direct)
                     && expect_data_types(diff_src_data_type, data_type::bf16,
                             data_type::undef, data_type::bf16, data_type::f32)
@@ -301,7 +302,8 @@ struct gemm_bf16_convolution_bwd_weights_t : public primitive_t {
                 USE_GLOBAL_SCRATCHPAD);
 
         status_t init(engine_t *engine) {
-            bool ok = true && desc()->prop_kind == prop_kind::backward_weights
+            bool ok = true && mayiuse(avx512_core)
+                    && desc()->prop_kind == prop_kind::backward_weights
                     && set_default_alg_kind(alg_kind::convolution_direct)
                     && expect_data_types(data_type::bf16, diff_wei_data_type,
                             data_type::undef, data_type::bf16, data_type::f32)
