3e29ae26dba137a6232669bd1c5d42ad4449b794
Mourad Gouicem <mourad.gouicem@intel.com>
2022-10-20 05:14:19 -0700

    cpu: x64: disable MEMFD allocation by default
    In some rare occasions, too many allocations may happen causing a
    ENOMEM error. Users still needing this capability can still enable it
    by passing -DXBYAK_USE_MEMFD as a compile flag.

--- src/cpu/x64/cpu_isa_traits.hpp
+++ src/cpu/x64/cpu_isa_traits.hpp
@@ -32,8 +32,7 @@
 /* in order to make selinux happy memory that would be marked with X-bit should
  * be obtained with mmap */
 #define XBYAK_USE_MMAP_ALLOCATOR
-/* Use Xbyak's memfd-based allocation, if available */
-#define XBYAK_USE_MEMFD
+
 #define XBYAK_NO_EXCEPTION
 #if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
 /* turn off `size_t to other-type implicit casting` warning
