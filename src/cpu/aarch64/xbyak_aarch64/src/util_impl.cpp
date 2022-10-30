ea323ab76bafc80d41571f8fd3aa3a1a5af6af8c
Kentaro Kawakami <kawakami.k@fujitsu.com>
2022-10-30 15:55:55 +0900

    cpu: aarch64: update xbyak_aarch64 into the latest version
    - extend error types
    - fix bug of Label handling
    - add experimental APIs
      - getCoresSharingDataCache
      - getDataCacheLevels
      - getDataCacheSize
      - getNumCores

---
+++ src/cpu/aarch64/xbyak_aarch64/src/util_impl.cpp
@@ -0,0 +1,267 @@
+/*******************************************************************************
+ * Copyright 2020-2022 FUJITSU LIMITED
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+#include "xbyak_aarch64_util.h"
+
+namespace Xbyak_aarch64 {
+namespace util {
+
+void Cpu::setCacheHierarchy() {
+  /* Cache size of AArch64 CPUs are described in the system registers,
+     which can't be read from user-space applications.
+     Linux provides `sysconf` API and `/sys/devices/system/cpu/`
+     device files to get cache size, but they dosen't always return
+     correct values. It may depend on Linux kernel version and
+     support status of CPUs. To avoid this situation, cahche size is
+     firstly read from `cacheInfoDict`, secondly get by `sysconf`.
+
+     `sysconf` example
+     #include <unistd.h>
+     int main() {
+       reutrn sysconf(_SC_LEVEL1_DCACHE_SIZE);
+     }
+   */
+  const cacheInfo_t *c = nullptr;
+
+  for (size_t j = 0; j < sizeof(cacheInfoDict) / sizeof(cacheInfo_t); j++) {
+    if (cacheInfoDict[j].midr_el1 == midr_el1_) {
+      c = cacheInfoDict + j;
+      break;
+    }
+  }
+
+  if (c != nullptr) {
+    dataCacheLevel_ = c->dataCacheLevel;
+    for (uint32_t i = 0; i < maxNumberCacheLevel; i++) {
+      if (i < c->highestInnerCacheLevel)
+        dataCacheSize_[i] = c->dataCacheSize[i];
+      else
+        dataCacheSize_[i] = c->dataCacheSize[i] / coresSharingDataCache_[i];
+    }
+  } else {
+    /**
+     * @ToDo Get chache information by `sysconf`
+     * for the case thd dictionary is unavailable.
+     */
+#define XBYAK_AARCH64_CACHE_SIZE(LEVEL, SIZE, ID, CORES, VAL)                                                                                                                                                                                                                                              \
+  cache_size = sysconf(ID);                                                                                                                                                                                                                                                                                \
+  VAL[LEVEL] = cache_size ? (cache_size / (CORES)) : ((SIZE) / (CORES));
+
+    uint32_t cache_size;
+
+    /* If `sysconf` returns zero as cache sizes, 32KiB, 1MiB, 0 and 0 is set as
+       1st, 2nd, 3rd and 4th level cache sizes. 2nd cahce is assumed as sharing cache. */
+    XBYAK_AARCH64_CACHE_SIZE(0, 1024 * 32, _SC_LEVEL1_DCACHE_SIZE, 1, coresSharingDataCache_);
+    XBYAK_AARCH64_CACHE_SIZE(1, 1024 * 1024, _SC_LEVEL2_CACHE_SIZE, 1, coresSharingDataCache_);
+    XBYAK_AARCH64_CACHE_SIZE(2, 0, _SC_LEVEL3_CACHE_SIZE, 1, coresSharingDataCache_);
+    XBYAK_AARCH64_CACHE_SIZE(3, 0, _SC_LEVEL4_CACHE_SIZE, 1, coresSharingDataCache_);
+
+    XBYAK_AARCH64_CACHE_SIZE(0, 1024 * 32, _SC_LEVEL1_DCACHE_SIZE, 1, dataCacheSize_);
+    XBYAK_AARCH64_CACHE_SIZE(1, 1024 * 1024, _SC_LEVEL2_CACHE_SIZE, 8, dataCacheSize_);
+    XBYAK_AARCH64_CACHE_SIZE(2, 0, _SC_LEVEL3_CACHE_SIZE, 1, dataCacheSize_);
+    XBYAK_AARCH64_CACHE_SIZE(3, 0, _SC_LEVEL4_CACHE_SIZE, 1, dataCacheSize_);
+#undef XBYAK_AARCH64_CACHE_SIZE
+  }
+}
+
+void Cpu::setNumCores() {
+#ifdef __linux__
+  /**
+   * @ToDo There are some methods to get # of cores.
+   Considering various kernel versions and CPUs, a combination of
+   multiple methods may be required.
+   1) sysconf(_SC_NPROCESSORS_ONLN)
+   2) /sys/devices/system/cpu/online
+   3) std::thread::hardware_concurrency()
+  */
+  numCores_[0] = numCores_[1] = sysconf(_SC_NPROCESSORS_ONLN);
+  coresSharingDataCache_[0] = 1;
+
+  /* # of numa nodes: /sys/devices/system/node/node[0-9]+
+     # of cores for each numa node: /sys/devices/system/node/node[0-9]+/cpu[0-9]+
+     It is assumed L2 cache is shared by each numa node. */
+  const int nodes = getFilePathMaxTailNumPlus1(XBYAK_AARCH64_PATH_NODES);
+  int cores = 1;
+
+  if (nodes > 0) {
+    cores = getFilePathMaxTailNumPlus1(XBYAK_AARCH64_PATH_CORES);
+    coresSharingDataCache_[1] = (cores > 0) ? cores : 1;
+  } else {
+    coresSharingDataCache_[1] = 1;
+  }
+#else
+  numCores_[0] = numCores_[1] = 1;
+  for (unsigned int i = 0; i < maxNumberCacheLevel; i++)
+    coresSharingDataCache_[i] = 1;
+
+  coresSharingDataCache_[1] = 8; // Set possible value.
+#endif
+}
+
+void Cpu::setSysRegVal() {
+#ifdef __linux__
+  XBYAK_AARCH64_READ_SYSREG(midr_el1_, MIDR_EL1);
+#endif
+}
+
+/**
+ * Return directory path
+ * @param[in] path ex. /sys/devices/system/node/node
+ * @param[out] buf ex. /sys/devices/system/node
+ */
+int Cpu::getRegEx(char *buf, const char *path, const char *regex) {
+  regex_t regexBuf;
+  regmatch_t match[1];
+
+  if (regcomp(&regexBuf, regex, REG_EXTENDED) != 0)
+    throw ERR_INTERNAL;
+
+  const int retVal = regexec(&regexBuf, path, 1, match, 0);
+  regfree(&regexBuf);
+
+  if (retVal != 0)
+    return -1;
+
+  const int startIdx = match[0].rm_so;
+  const int endIdx = match[0].rm_eo;
+
+  /* Something wrong (multiple match or not match) */
+  if (startIdx == -1 || endIdx == -1 || (endIdx - startIdx - 1) < 1)
+    return -1;
+
+  strncpy(buf, path + startIdx, endIdx - startIdx);
+  buf[endIdx - startIdx] = '\0';
+
+  return 0;
+}
+
+int Cpu::getFilePathMaxTailNumPlus1(const char *path) {
+#ifdef __linux__
+  char dir_path[max_path_len];
+  char file_pattern[max_path_len];
+  int retVal = 0;
+
+  getRegEx(dir_path, path, "/([^/]+/)+");
+  /* Remove last '/'. */
+  dir_path[strlen(dir_path) - 1] = '\0';
+  getRegEx(file_pattern, path, "[^/]+$");
+  strncat(file_pattern, "[0-9]+", 16);
+
+  fflush(stdout);
+
+  DIR *dir = opendir(dir_path);
+  struct dirent *dp;
+
+  dp = readdir(dir);
+  while (dp != NULL) {
+    if (getRegEx(dir_path, dp->d_name, file_pattern) == 0)
+      retVal++;
+    dp = readdir(dir);
+  }
+
+  if (dir != NULL)
+    closedir(dir);
+
+  return retVal;
+#else
+  return 0;
+#endif
+}
+
+Cpu::Cpu() : type_(tNONE), sveLen_(SVE_NONE) {
+#ifdef __linux__
+  unsigned long hwcap = getauxval(AT_HWCAP);
+  if (hwcap & HWCAP_ATOMICS) {
+    type_ |= tATOMIC;
+  }
+
+  if (hwcap & HWCAP_FP) {
+    type_ |= tFP;
+  }
+  if (hwcap & HWCAP_ASIMD) {
+    type_ |= tADVSIMD;
+  }
+#ifdef HWCAP_SVE
+  /* Some old <sys/auxv.h> may not define HWCAP_SVE.
+     In that case, SVE is treated as if it were not supported. */
+  if (hwcap & HWCAP_SVE) {
+    type_ |= tSVE;
+    // svcntb(); if arm_sve.h is available
+    sveLen_ = (sveLen_t)prctl(51); // PR_SVE_GET_VL
+  }
+#endif
+#elif defined(__APPLE__)
+  size_t val = 0;
+  size_t len = sizeof(val);
+
+  if (sysctlbyname(hw_opt_atomics, &val, &len, NULL, 0) != 0)
+    throw Error(ERR_INTERNAL);
+  else
+    type_ |= (val == 1) ? tATOMIC : 0;
+
+  if (sysctlbyname(hw_opt_fp, &val, &len, NULL, 0) != 0)
+    throw Error(ERR_INTERNAL);
+  else
+    type_ |= (val == 1) ? tFP : 0;
+
+  if (sysctlbyname(hw_opt_neon, &val, &len, NULL, 0) != 0)
+    throw Error(ERR_INTERNAL);
+  else
+    type_ |= (val == 1) ? tADVSIMD : 0;
+#endif
+
+  setSysRegVal();
+  setNumCores();
+  setCacheHierarchy();
+}
+
+Type Cpu::getType() const { return type_; }
+bool Cpu::has(Type type) const { return (type & type_) != 0; }
+uint64_t Cpu::getSveLen() const { return sveLen_; }
+bool Cpu::isAtomicSupported() const { return type_ & tATOMIC; }
+const char *Cpu::getImplementer() const {
+  uint64_t implementer = (midr_el1_ >> 24) & 0xff;
+
+  for (size_t i = 0; i < sizeof(implementers) / sizeof(implementer_t); i++) {
+    if (implementers[i].id == implementer)
+      return implementers[i].implementer;
+  }
+
+  return nullptr;
+}
+
+uint32_t Cpu::getCoresSharingDataCache(uint32_t i) const {
+  if (i >= dataCacheLevel_)
+    throw Error(ERR_BAD_PARAMETER);
+  return coresSharingDataCache_[i];
+}
+
+uint32_t Cpu::getDataCacheLevels() const { return dataCacheLevel_; }
+
+uint32_t Cpu::getDataCacheSize(uint32_t i) const {
+  if (i >= dataCacheLevel_)
+    throw Error(ERR_BAD_PARAMETER);
+  return dataCacheSize_[i];
+}
+uint32_t Cpu::getNumCores(Arm64CpuTopologyLevel level) const {
+  switch (level) {
+  case CoreLevel:
+    return numCores_[level - 1];
+  default:
+    throw Error(ERR_BAD_PARAMETER);
+  }
+}
+} // namespace util
+} // namespace Xbyak_aarch64
--- src/cpu/aarch64/xbyak_aarch64/xbyak_aarch64/xbyak_aarch64_util.h
+++ src/cpu/aarch64/xbyak_aarch64/xbyak_aarch64/xbyak_aarch64_util.h
@@ -1,6 +1,6 @@
 #pragma once
 /*******************************************************************************
- * Copyright 2020-2021 FUJITSU LIMITED
+ * Copyright 2020-2022 FUJITSU LIMITED
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +17,17 @@
 #ifndef XBYAK_AARCH64_UTIL_H_
 #define XBYAK_AARCH64_UTIL_H_
 
+#include <dirent.h>
+#include <regex.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #ifdef __linux__
 #include <sys/auxv.h>
 #include <sys/prctl.h>
+#include <unistd.h>
 
 /* In old Linux such as Ubuntu 16.04, HWCAP_ATOMICS, HWCAP_FP, HWCAP_ASIMD
    can not be found in <bits/hwcap.h> which is included from <sys/auxv.h>.
@@ -36,8 +43,20 @@
 
 #include "xbyak_aarch64_err.h"
 
+#define XBYAK_AARCH64_MIDR_EL1(I, V, A, P, R) ((I << 24) | (V << 20) | (A << 16) | (P << 4) | (R << 0))
+#define XBYAK_AARCH64_PATH_NODES "/sys/devices/system/node/node"
+#define XBYAK_AARCH64_PATH_CORES "/sys/devices/system/node/node0/cpu"
+#define XBYAK_AARCH64_READ_SYSREG(var, ID) asm("mrs %0, " #ID : "=r"(var));
+
 namespace Xbyak_aarch64 {
 namespace util {
+typedef uint64_t Type;
+
+constexpr uint32_t maxNumberCacheLevel = 4;
+constexpr uint32_t maxTopologyLevel = 2;
+constexpr uint32_t max_path_len = 1024;
+
+enum Arm64CpuTopologyLevel { SmtLevel = 1, CoreLevel = 2 };
 
 enum sveLen_t {
   SVE_NONE = 0,
@@ -59,11 +78,38 @@ enum sveLen_t {
   SVE_2048 = 16 * 16,
 };
 
+struct implementer_t {
+  uint32_t id;
+  const char *implementer;
+};
+
+struct cacheInfo_t {
+  uint64_t midr_el1;
+  uint32_t dataCacheLevel;
+  uint32_t highestInnerCacheLevel;
+  uint32_t dataCacheSize[maxNumberCacheLevel];
+};
+
 #ifdef __APPLE__
 constexpr char hw_opt_atomics[] = "hw.optional.armv8_1_atomics";
 constexpr char hw_opt_fp[] = "hw.optional.floatingpoint";
 constexpr char hw_opt_neon[] = "hw.optional.neon";
 #endif
+
+const struct implementer_t implementers[] = {{0x00, "Reserved for software use"},
+                                             {0xC0, "Ampere Computing"},
+                                             {0x41, "Arm Limited"},
+                                             {0x42, "Broadcom Corporation"},
+                                             {0x43, "Cavium Inc."},
+                                             {0x44, "Digital Equipment Corporation"},
+                                             {0x46, "Fujitsu Ltd."},
+                                             {0x49, "Infineon Technologies AG"},
+                                             {0x4D, "Motorola or Freescale Semiconductor Inc."},
+                                             {0x4E, "NVIDIA Corporation"},
+                                             {0x50, "Applied Micro Circuits Corporation"},
+                                             {0x51, "Qualcomm Inc."},
+                                             {0x56, "Marvell International Ltd."},
+                                             {0x69, "Intel Corporation"}};
 
 /**
    CPU detection class
@@ -72,63 +118,44 @@ class Cpu {
   uint64_t type_;
   sveLen_t sveLen_;
 
-public:
-  typedef uint64_t Type;
+private:
+  const struct cacheInfo_t cacheInfoDict[2] = {
+      {/* A64FX */ XBYAK_AARCH64_MIDR_EL1(0x46, 0x1, 0xf, 0x1, 0x0), 2, 1, {1024 * 64, 1024 * 1024 * 8 * 4, 0, 0}},
+      {/* A64FX */ XBYAK_AARCH64_MIDR_EL1(0x46, 0x2, 0xf, 0x1, 0x0), 2, 1, {1024 * 64, 1024 * 1024 * 8 * 4, 0, 0}},
+  };
 
+  uint32_t coresSharingDataCache_[maxNumberCacheLevel];
+  uint32_t dataCacheSize_[maxNumberCacheLevel];
+  uint32_t dataCacheLevel_;
+  uint64_t midr_el1_;
+  uint32_t numCores_[maxTopologyLevel];
+
+  void setCacheHierarchy();
+  void setNumCores();
+  void setSysRegVal();
+  int getRegEx(char *buf, const char *path, const char *regex);
+  int getFilePathMaxTailNumPlus1(const char *path);
+
+public:
   static const Type tNONE = 0;
   static const Type tADVSIMD = 1 << 1;
   static const Type tFP = 1 << 2;
   static const Type tSVE = 1 << 3;
   static const Type tATOMIC = 1 << 4;
 
-  Cpu() : type_(tNONE), sveLen_(SVE_NONE) {
-#ifdef __linux__
-    unsigned long hwcap = getauxval(AT_HWCAP);
-    if (hwcap & HWCAP_ATOMICS) {
-      type_ |= tATOMIC;
-    }
+  Cpu();
 
-    if (hwcap & HWCAP_FP) {
-      type_ |= tFP;
-    }
-    if (hwcap & HWCAP_ASIMD) {
-      type_ |= tADVSIMD;
-    }
-#ifdef HWCAP_SVE
-    /* Some old <sys/auxv.h> may not define HWCAP_SVE.
-       In that case, SVE is treated as if it were not supported. */
-    if (hwcap & HWCAP_SVE) {
-      type_ |= tSVE;
-      // svcntb(); if arm_sve.h is available
-      sveLen_ = (sveLen_t)prctl(51); // PR_SVE_GET_VL
-    }
-#endif
-#elif defined(__APPLE__)
-    size_t val = 0;
-    size_t len = sizeof(val);
-
-    if (sysctlbyname(hw_opt_atomics, &val, &len, NULL, 0) != 0)
-      throw Error(ERR_INTERNAL);
-    else
-      type_ |= (val == 1) ? tATOMIC : 0;
-
-    if (sysctlbyname(hw_opt_fp, &val, &len, NULL, 0) != 0)
-      throw Error(ERR_INTERNAL);
-    else
-      type_ |= (val == 1) ? tFP : 0;
-
-    if (sysctlbyname(hw_opt_neon, &val, &len, NULL, 0) != 0)
-      throw Error(ERR_INTERNAL);
-    else
-      type_ |= (val == 1) ? tADVSIMD : 0;
-#endif
-  }
-
-  Type getType() const { return type_; }
-  bool has(Type type) const { return (type & type_) != 0; }
-  uint64_t getSveLen() const { return sveLen_; }
-  bool isAtomicSupported() const { return type_ & tATOMIC; }
+  Type getType() const;
+  bool has(Type type) const;
+  uint64_t getSveLen() const;
+  bool isAtomicSupported() const;
+  const char *getImplementer() const;
+  uint32_t getCoresSharingDataCache(uint32_t i) const;
+  uint32_t getDataCacheLevels() const;
+  uint32_t getDataCacheSize(uint32_t i) const;
+  uint32_t getNumCores(Arm64CpuTopologyLevel level) const;
 };
+
 } // namespace util
 } // namespace Xbyak_aarch64
 #endif
