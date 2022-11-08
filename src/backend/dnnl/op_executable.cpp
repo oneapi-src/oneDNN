a5f24331c6188d8ce65916dbdb1202f1029f99c9
Lv, Tao A <tao.a.lv@intel.com>
2022-11-08 11:29:00 +0800

    backend: dnnl: fixed typo in rebasing

--- src/backend/dnnl/op_executable.cpp
+++ src/backend/dnnl/op_executable.cpp
@@ -1586,7 +1586,7 @@ bn_folding_t::desc_t bn_folding_t::create_desc(std::shared_ptr<impl::op_t> &op,
     // need permute c channel to the first dimension
     if (desc.filter_format_ == "OIX") { // conv case
         auto perm = dnnl_impl::utils::cast_to_int32(get_permutation(
-                desc.new_scale_desc_.get_ndsim(), "XIO", "OIX"));
+                desc.new_scale_desc_.get_ndims(), "XIO", "OIX"));
         desc.new_scale_desc_ = desc.new_scale_desc_.permute_axes(perm);
         desc.new_variance_desc_ = desc.new_variance_desc_.permute_axes(perm);
     }
