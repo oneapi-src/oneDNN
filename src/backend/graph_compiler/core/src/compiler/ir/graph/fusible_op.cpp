8c1df80b69baf105f802fa31c431a0f059ba9acb
Yun-Fly <yunfei.song@intel.com>
2022-11-16 12:48:15 +0800

    backend: compiler: fusion: fix last use check for buffer inplace

--- src/backend/graph_compiler/core/src/compiler/ir/graph/fusible_op.cpp
+++ src/backend/graph_compiler/core/src/compiler/ir/graph/fusible_op.cpp
@@ -127,8 +127,8 @@ void fusible_op_t::create_mixed_partition(mixed_parti_t *parti) {
 void fusible_op_t::append_mixed_partition(mixed_parti_t *parti) {
     search_anchor(parti);
     COMPILE_ASSERT(parti->ready_for_op(this),
-            "Not suitable anchor found for " << op_name_ << "_"
-                                             << logical_op_id_);
+            "No suitable anchor found for " << op_name_ << "_"
+                                            << logical_op_id_);
 
     if (!parti->empty()) {
         parti->buf_alloc_.allocate_buffer(this);
