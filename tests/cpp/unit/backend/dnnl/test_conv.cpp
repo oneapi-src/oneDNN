08c20046a5bf4e0374dac4f1a1dbd3d6c901c60c
Xinyu Chen <xinyu1.chen@intel.com>
2022-11-16 21:27:58 +0800

    backend: dnnl: fix relu mul_scale swapping

--- tests/cpp/unit/backend/dnnl/test_conv.cpp
+++ tests/cpp/unit/backend/dnnl/test_conv.cpp
@@ -3466,6 +3466,160 @@ TEST(ExecuteSubgraphInt8, Conv2dSumReluNxc) {
     }
 }
 
+TEST(ExecuteSubgraphInt8, Conv2dAddRelu) {
+    using dims = impl::dnnl_impl::dims;
+
+    impl::engine_t &engine = get_engine();
+    impl::stream_t &strm = get_stream();
+
+    static auto isa = dnnl_get_effective_cpu_isa();
+
+    std::string wei_qtype = "per_channel";
+    // prepare data
+    int64_t in_channel = 8, out_channel = 8;
+    int64_t kernel_size = 3;
+    std::vector<int64_t> src_shape {1, in_channel, 112, 112};
+    std::vector<int64_t> weight_shape {
+            out_channel, in_channel, kernel_size, kernel_size};
+    std::vector<int64_t> bias_shape {out_channel};
+    std::vector<int64_t> dst_shape {1, out_channel, 110, 110};
+
+    test::vector<uint8_t> src_u8_data(product(src_shape));
+    test::vector<int8_t> weight_s8_data(product(weight_shape));
+    test::vector<float> other_f32_data(product(dst_shape));
+    test::vector<int8_t> case1_out_data(product(dst_shape));
+    test::vector<int8_t> case2_out_data(product(dst_shape));
+
+    // random generate src, weight and bias data random seed = 7
+    std::default_random_engine generator(7);
+    std::uniform_real_distribution<float> u8_distribution(0.0f, 255.0f);
+    std::uniform_real_distribution<float> s8_distribution(-127.0f, 128.0f);
+    std::uniform_real_distribution<float> f32_distribution(0.0f, 1.0f);
+    std::generate(src_u8_data.begin(), src_u8_data.end(),
+            [&]() { return static_cast<uint8_t>(u8_distribution(generator)); });
+    std::generate(weight_s8_data.begin(), weight_s8_data.end(),
+            [&]() { return static_cast<int8_t>(s8_distribution(generator)); });
+    std::generate(other_f32_data.begin(), other_f32_data.end(),
+            [&]() { return f32_distribution(generator); });
+
+    float scale_src = 1 / 255.f; // map to 0~255
+    float scale_out = 0.78;
+    int64_t zp_src = 0;
+    int64_t zp_out = 78;
+
+    size_t scale_size = out_channel;
+
+    std::vector<float> scale_wei(scale_size, 1 / 127.f);
+    std::vector<int64_t> zp_wei(scale_size, 0);
+
+    impl::op_t dqdata_node(1, impl::op_kind::Dequantize, "dqdata_node");
+    SET_Q_DQ_DATA_ATTR(dqdata_node)
+
+    impl::op_t dqweight_node(3, impl::op_kind::Dequantize, "dqweight_node");
+    SET_Q_DQ_WEIGHT_ATTR(dqweight_node, 0)
+
+    impl::op_t conv_node(4, impl::op_kind::Convolution, "conv_node");
+    int64_t g = 1;
+    SET_CONV_ATTR(conv_node, 2)
+
+    impl::op_t relu_node(5, impl::op_kind::ReLU, "relu_node");
+
+    impl::op_t qout_node(6, impl::op_kind::Quantize, "qout_node");
+    SET_Q_DQ_OUT_ATTR(qout_node)
+
+    impl::op_t add_node(9, impl::op_kind::Add, "add_node");
+
+    // prepare logical tensor
+    auto src_u8 = utils::logical_tensor_init(1, impl::data_type::u8);
+    auto src_f32_dq = utils::logical_tensor_init(2, impl::data_type::f32);
+    auto weight_s8 = utils::logical_tensor_init(4, impl::data_type::s8);
+    auto weight_f32_dq = utils::logical_tensor_init(5, impl::data_type::f32);
+    auto dst_f32 = utils::logical_tensor_init(7, impl::data_type::f32);
+    auto dst_relu_f32 = utils::logical_tensor_init(8, impl::data_type::f32);
+    auto dst_s8 = utils::logical_tensor_init(9, impl::data_type::s8);
+    auto other_f32 = utils::logical_tensor_init(12, impl::data_type::f32);
+    auto dst_add_f32 = utils::logical_tensor_init(13, impl::data_type::f32);
+
+    dqdata_node.add_input(src_u8);
+    dqdata_node.add_output(src_f32_dq);
+
+    dqweight_node.add_input(weight_s8);
+    dqweight_node.add_output(weight_f32_dq);
+
+    conv_node.add_input(src_f32_dq);
+    conv_node.add_input(weight_f32_dq);
+    conv_node.add_output(dst_f32);
+    add_node.add_input(dst_f32);
+    add_node.add_input(other_f32);
+    add_node.add_output(dst_add_f32);
+
+    relu_node.add_input(dst_add_f32);
+    relu_node.add_output(dst_relu_f32);
+
+    qout_node.add_input(dst_relu_f32);
+    qout_node.add_output(dst_s8);
+
+    impl::graph_t agraph(engine.kind());
+    agraph.add_op(&dqdata_node);
+    agraph.add_op(&dqweight_node);
+    agraph.add_op(&conv_node);
+    agraph.add_op(&add_node);
+    agraph.add_op(&relu_node);
+    agraph.add_op(&qout_node);
+    agraph.build_graph();
+
+    // prepare in/out with full shape
+    src_u8 = utils::logical_tensor_init(1, src_shape, impl::data_type::u8);
+    weight_s8
+            = utils::logical_tensor_init(4, weight_shape, impl::data_type::s8);
+    other_f32 = utils::logical_tensor_init(12, dst_shape, impl::data_type::f32);
+    dst_s8 = utils::logical_tensor_init(9, dst_shape, impl::data_type::s8);
+
+    impl::tensor_t src_u8_ts(src_u8, &engine, src_u8_data.data());
+    impl::tensor_t weight_s8_ts(weight_s8, &engine, weight_s8_data.data());
+    impl::tensor_t other_f32_ts(other_f32, &engine, other_f32_data.data());
+    impl::tensor_t dst_s8_ts(dst_s8, &engine, case1_out_data.data());
+    impl::tensor_t dst_s8_case2_ts(dst_s8, &engine, case2_out_data.data());
+
+    // -------------------------case 1----------------------------------
+    ASSERT_EQ(run_graph(agraph, {src_u8_ts, weight_s8_ts, other_f32_ts},
+                      {dst_s8_ts}, engine, strm),
+            impl::status::success);
+
+    // -------------------------case 2----------------------------------
+    impl::pass::pass_base_ptr apass
+            = get_pass(engine.kind() == impl::engine_kind::gpu
+                            ? "int8_conv_post_ops_fusion_gpu"
+                            : "int8_conv_post_ops_fusion_cpu");
+
+    apass->run(agraph);
+    ASSERT_EQ(agraph.get_num_partitions(), 1U);
+    auto part = agraph.get_partitions()[0];
+
+    // compile
+    impl::partition_t p;
+    p.init(part);
+
+    impl::compiled_partition_t cp(p);
+
+    std::vector<const impl::logical_tensor_t *> lt_ins {
+            &src_u8, &weight_s8, &other_f32};
+    std::vector<const impl::logical_tensor_t *> lt_outs {&dst_s8};
+
+    p.compile(&cp, lt_ins, lt_outs, &engine);
+    cp.execute(
+            &strm, {src_u8_ts, weight_s8_ts, other_f32_ts}, {dst_s8_case2_ts});
+    strm.wait();
+
+    if (engine.kind() == impl::engine_kind::cpu
+            && isa < dnnl_cpu_isa_avx512_core_vnni)
+        ASSERT_TRUE(allclose(case1_out_data, case2_out_data, /*rtol*/ 0.1f,
+                /*atol*/ 1.f));
+    else
+        ASSERT_TRUE(allclose(case1_out_data, case2_out_data, /*rtol*/ 0.01f,
+                /*atol*/ 1.f));
+}
+
 TEST(ExecuteSubgraphInt8, Conv1d2d3dX8s8f32) {
     using dims = impl::dnnl_impl::dims;
 
