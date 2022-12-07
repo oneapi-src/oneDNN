25600fbf657c36f0769641a8b1be881a1dc59cb8
Andrey Kharitonchik <andrey.kharitonchik@intel.com>
2022-12-06 16:25:40 -0800

    fixup: cpu: matmul: align allocation/usage logic for accumulation buffer size
    fixed copyright

--- src/cpu/matmul/matmul_utils.hpp
+++ src/cpu/matmul/matmul_utils.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2020 Intel Corporation
+* Copyright 2020-2022 Intel Corporation
 * Copyright 2022 Arm Ltd. and affiliates
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
