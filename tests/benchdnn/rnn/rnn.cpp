bf9a4d4876b17b16335eaa92cd22559ea586be55
Putta, Srinivas <srinivas.putta@intel.com>
2022-12-09 13:47:54 -0800

    benchdnn: skip bf16,f16 unsupported cases

--- tests/benchdnn/rnn/rnn.cpp
+++ tests/benchdnn/rnn/rnn.cpp
@@ -731,9 +731,12 @@ void skip_unimplemented_prb(const prb_t *prb_, res_t *res) {
 
 #if DNNL_CPU_RUNTIME != DNNL_RUNTIME_NONE
     static auto isa = dnnl_get_effective_cpu_isa();
-    const bool is_f16_not_ok = prb.cfg[SRC_LAYER].dt == dnnl_f16
-            && dnnl::is_superset(isa, dnnl_cpu_isa_avx512_core_fp16);
-    if (is_f16_not_ok) {
+    // f16 is not implemented on any x64 platform yet.
+    const bool is_f16_not_ok = prb.cfg[SRC_LAYER].dt == dnnl_f16;
+    // bf16 is currently supported only on avx512_core[+] platforms
+    const bool is_bf16_not_ok = prb.cfg[SRC_LAYER].dt == dnnl_bf16
+            && !dnnl::is_superset(isa, dnnl_cpu_isa_avx512_core);
+    if (is_f16_not_ok || is_bf16_not_ok) {
         res->state = SKIPPED, res->reason = CASE_NOT_SUPPORTED;
         return;
     }
