fc673d6364ad018a1b023cb671fabc9969ca8c98
Denis Samoilov <denis.samoylov@intel.com>
2023-01-30 22:48:14 -0800

    cpu: suppress false positive warning

--- src/cpu/x64/jit_brgemm_deconv.cpp
+++ src/cpu/x64/jit_brgemm_deconv.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2022 Intel Corporation
+* Copyright 2022-2023 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 *******************************************************************************/
 
 #include "common/c_types_map.hpp"
+#include "common/compiler_workarounds.hpp"
 #include "common/dnnl_thread.hpp"
 #include "common/nstl.hpp"
 #include "common/type_helpers.hpp"
