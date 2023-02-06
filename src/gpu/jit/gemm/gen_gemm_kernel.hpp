7c70c38b3f048e0daacaf595e8d4cdf6c9bb72e0
Peter Caday <peter.caday@intel.com>
2023-02-06 12:00:38 -0800

    gpu: jit: gemm: fix bia_mask={3,6} in xe_hp_systolic_gemm_t

--- src/gpu/jit/gemm/gen_gemm_kernel.hpp
+++ src/gpu/jit/gemm/gen_gemm_kernel.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2022 Intel Corporation
+* Copyright 2019-2023 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -122,8 +122,8 @@ struct gen_gemm_nocopy_kernel_desc_t : public gen_gemm_kernel_desc_t {
 
 struct gen_gemm_xe_systolic_kernel_desc_t : public gen_gemm_kernel_desc_t {
     status_t select_kernel(compute::gpu_arch_t arch, int stepping, int eu_count,
-            int batch_dims, bool packed_c, bool a_offset, bool b_offset,
-            bool c_offset, bool bias, float alpha, float beta,
+            int batch_dims, bool packed_c, bool trans_co, bool a_offset,
+            bool b_offset, bool c_offset, bool bias, float alpha, float beta,
             const post_ops_t &post_ops, data_type_t a_type, data_type_t b_type,
             data_type_t c_type, data_type_t co_type, data_type_t acc_type,
             dim_t m, dim_t n, dim_t k, dim_t batch, int unroll_m, int unroll_n,
