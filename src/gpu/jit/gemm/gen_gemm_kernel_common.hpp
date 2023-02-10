940ccf3fe0d2ebb30dbe456831904596a8b0c1b4
Peter Caday <peter.caday@intel.com>
2023-02-10 07:29:44 -0800

    gpu: jit: gemm: add missing <cstdint> include

--- src/gpu/jit/gemm/gen_gemm_kernel_common.hpp
+++ src/gpu/jit/gemm/gen_gemm_kernel_common.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2022 Intel Corporation
+* Copyright 2019-2023 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@
 
 #define STANDALONE 0
 
+#include <cstdint>
 #include <string>
 
 namespace dnnl {
