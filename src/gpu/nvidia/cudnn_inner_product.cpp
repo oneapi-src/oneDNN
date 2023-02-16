93f204e81d8af32eafb29c5cc1152f840e2867b9
Roy Oursler <roy.j.oursler@intel.com>
2023-02-16 11:26:59 -0800

    gpu: nvidia: fix nvidia fill dependency management

--- src/gpu/nvidia/cudnn_inner_product.cpp
+++ src/gpu/nvidia/cudnn_inner_product.cpp
@@ -133,12 +133,14 @@ status_t cudnn_inner_product_bwd_weights_t::execute(
 
         if (wei_sz != 0) {
             auto status = cuda_stream->fill(
-                    CTX_OUT_STORAGE(DNNL_ARG_DIFF_WEIGHTS), 0, wei_sz);
+                    CTX_OUT_STORAGE(DNNL_ARG_DIFF_WEIGHTS), 0, wei_sz,
+                    cuda_stream->ctx.get_deps(), cuda_stream->ctx.get_deps());
             if (status != status::success) return status;
         }
         if (bias_sz != 0) {
-            auto status = cuda_stream->fill(
-                    CTX_OUT_STORAGE(DNNL_ARG_DIFF_BIAS), 0, bias_sz);
+            auto status = cuda_stream->fill(CTX_OUT_STORAGE(DNNL_ARG_DIFF_BIAS),
+                    0, bias_sz, cuda_stream->ctx.get_deps(),
+                    cuda_stream->ctx.get_deps());
             if (status != status::success) return status;
         }
         return status::success;
