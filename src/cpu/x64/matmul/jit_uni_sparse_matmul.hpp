f19e4911df218ea9dddd5e57b52d0326a31a8196
Denis Samoilov <denis.samoylov@intel.com>
2023-03-02 13:06:08 -0800

    cpu: x64: matmul: add check for supported ISAs

--- src/cpu/x64/matmul/jit_uni_sparse_matmul.hpp
+++ src/cpu/x64/matmul/jit_uni_sparse_matmul.hpp
@@ -56,7 +56,7 @@ struct jit_uni_sparse_matmul_t : public primitive_t {
                     && utils::everyone_is(
                             s32, src_d.metadata_type(0), src_d.metadata_type(1))
                     && !with_bias() && attr()->has_default_values()
-                    && set_default_formats() && formats_ok();
+                    && mayiuse(avx2) && set_default_formats() && formats_ok();
             return ok ? status::success : status::unimplemented;
         }
 
