f20c042387627ad67a4468176fa7c10bc3255043
Soto Flores, Manuel <manuel.soto.flores@intel.com>
2023-03-06 23:50:06 -0800

    refactor: src: cpu: x64: brgemm: create separate call to calculate max_block

--- src/cpu/x64/brgemm/brgemm_utils.cpp
+++ src/cpu/x64/brgemm/brgemm_utils.cpp
@@ -171,6 +171,44 @@ int calculate_ldb_params(brgemm_t *brg, const int try_ld_block2) {
     return nstl::max(1, adj_ld_block2);
 }
 
+int calculate_max_block(brgemm_t *brg, const int adj_ld_block2) {
+
+    constexpr int max_bcst_regs = 1;
+    const bool req_compensation = brg->req_s8s8_compensation
+            || brg->zp_type_a != brgemm_broadcast_t::none;
+    const bool req_zp_a_comp_pads
+            = (brg->req_cal_comp_pads || brg->brgattr.max_top_vpad > 0
+                      || brg->brgattr.max_bottom_vpad > 0)
+            && brg->zp_type_a != brgemm_broadcast_t::none;
+    const int beta_regs = !one_of(brg->beta, 1.f, 0.f);
+
+    const int max_isa_regs = isa_num_vregs(brg->isa_impl);
+    // note: the 'adj_ld_block2' already removes the necessary registers
+    // for 'embd_bcst'
+    auto max_reg_count = max_isa_regs - max_bcst_regs - beta_regs
+            - req_compensation - req_zp_a_comp_pads;
+    if (req_zp_a_comp_pads)
+        max_reg_count
+                = nstl::min(max_reg_count, max_isa_regs - max_bcst_regs - 5);
+
+    int max_block = max_reg_count - adj_ld_block2;
+
+    if (brg->is_bf16_emu) {
+        // in theory, vmm bf16_emu register indices overlap with other vmm
+        // registers related to 'max_block'
+        assert(is_superset(brg->isa_impl, avx512_core));
+        constexpr int bf16_emu_reg_count = 28;
+        max_block = nstl::min(max_block, bf16_emu_reg_count);
+    }
+
+    // non-VNNI INT8 dot product required 2 temp vectors
+    if (brg->is_int8 && !brg->has_vnni) max_block -= 2;
+
+    max_block /= adj_ld_block2;
+
+    return max_block;
+}
+
 status_t brgemm_blocking(brgemm_t *brg) {
 
     set_isa_impl(brg);
@@ -185,38 +223,9 @@ status_t brgemm_blocking(brgemm_t *brg) {
         brg->ldb = brg->load_dim / brg->ld_block;
         brg->ldb_tail = brg->load_dim % brg->ld_block;
 
-        const int max_bcst_regs = 1;
-        const bool req_compensation = brg->req_s8s8_compensation
-                || brg->zp_type_a != brgemm_broadcast_t::none;
-        const bool req_zp_a_comp_pads
-                = (brg->req_cal_comp_pads || brg->brgattr.max_top_vpad > 0
-                          || brg->brgattr.max_bottom_vpad > 0)
-                && brg->zp_type_a != brgemm_broadcast_t::none;
-        const int beta_regs = !one_of(brg->beta, 1.f, 0.f);
+        const int adj_ld_block2 = calculate_ldb_params(brg, 4);
+        const int max_block = calculate_max_block(brg, adj_ld_block2);
 
-        const int max_isa_regs
-                = is_superset(brg->isa_impl, avx512_core) ? 32 : 16;
-        // note: the 'adj_ld_block2' already removes the necessary registers
-        // for 'embd_bcst'
-        auto max_reg_count = max_isa_regs - max_bcst_regs - beta_regs
-                - req_compensation - req_zp_a_comp_pads;
-
-        if (req_zp_a_comp_pads)
-            max_reg_count = nstl::min(
-                    max_reg_count, max_isa_regs - max_bcst_regs - 5);
-
-        const auto adj_ld_block2 = calculate_ldb_params(brg, 4);
-        max_reg_count -= adj_ld_block2;
-
-        if (brg->is_bf16_emu) {
-            assert(is_superset(brg->isa_impl, avx512_core));
-            max_reg_count = nstl::min(max_reg_count, 28);
-        }
-
-        // non-VNNI INT8 dot product required 2 temp vectors
-        if (brg->is_int8 && !brg->has_vnni) max_reg_count -= 2;
-
-        const int max_block = max_reg_count / adj_ld_block2;
         const int min_block = 1;
         float best_bd_block_eff = 0.f;
         brg->bd_block = 1;
