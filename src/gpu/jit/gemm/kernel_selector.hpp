02d983b45141736890403592dc608935f563c6d2
Roy Oursler <roy.j.oursler@intel.com>
2023-03-10 11:14:23 -0800

    gpu : jit : gemm : fix MatchParams copy behavior

--- src/gpu/jit/gemm/kernel_selector.hpp
+++ src/gpu/jit/gemm/kernel_selector.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2022 Intel Corporation
+* Copyright 2022-2023 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -31,19 +31,51 @@ namespace jit {
 
 // Basic kernel selection API.
 struct MatchParams {
+    MatchParams(const MatchParams &other) { *this = other; }
+    MatchParams &operator=(const MatchParams &other) {
+        selector = other.selector;
+        sizes = other.sizes;
+        precisionCExt = other.precisionCExt;
+        ignoreSizes = other.ignoreSizes;
+        stepping = other.stepping;
+        alignment = other.alignment;
+        unroll = other.unroll;
+        temp = other.temp;
+
+        copy_interned_values(other);
+        return *this;
+    }
     kcatalog::Selector selector;
     SizeParams sizes;
     char precisionCExt = 0;
     bool ignoreSizes = false;
     int stepping = 0;
-    int alignment[3] = {0, 0, 0};
+    std::array<int, 3> alignment = {0, 0, 0};
     kcatalog::string tags, lateTags;
-    int unroll[2] = {0, 0};
+    std::array<int, 2> unroll = {0, 0};
 
     MatchParams() {}
     MatchParams(ngen::HW hw, const GEMMProblem &problem);
 
 private:
+    void copy_interned_values(const MatchParams &other) {
+        auto interned = [&](const char *value) -> const char * {
+            if (other.temp.data() <= value
+                    && value < other.temp.data() + sizeof(temp)) {
+                return temp.data() + (value - other.temp.data());
+            }
+            return value;
+        };
+        selector.precisions[0] = interned(other.selector.precisions[0]);
+        selector.precisions[1] = interned(other.selector.precisions[1]);
+        selector.precisions[2] = interned(other.selector.precisions[2]);
+        selector.layouts[0] = interned(other.selector.layouts[0]);
+        selector.layouts[1] = interned(other.selector.layouts[1]);
+        selector.layouts[2] = interned(other.selector.layouts[2]);
+
+        tags = interned(other.tags);
+        lateTags = interned(other.lateTags);
+    }
     std::array<char, 32> temp;
 };
 
