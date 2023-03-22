6a2a4076bfa83e39796068846b9b1c5614bdcfa0
Safonov, Igor <igor.safonov@intel.com>
2023-03-22 14:58:12 -0700

    src: cpu: x64: brgemm: containers: free resources before exiting

--- src/cpu/x64/brgemm/brgemm_containers.cpp
+++ src/cpu/x64/brgemm/brgemm_containers.cpp
@@ -74,7 +74,11 @@ status_t brgemm_kernel_container_t::insert(int idx, const brgemm_t *brg) {
     const auto brgemm_it = brgemm_map_.find(brg);
     if (brgemm_it == brgemm_map_.end()) {
         brgemm_kernel_t *brg_kernel = nullptr;
-        CHECK(brgemm_kernel_create(&brg_kernel, *brg));
+        status_t s = brgemm_kernel_create(&brg_kernel, *brg);
+        if (s != status::success) {
+            delete brg_kernel;
+            return s;
+        }
         std::shared_ptr<brgemm_kernel_t> sptr(brg_kernel);
         lock_write();
         const auto kernel_ret = set_.insert(sptr);
