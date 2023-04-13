d461ef49b29a162c0964bb44b9b81cb298047b9e
Chen, Xinyu1 <xinyu1.chen@intel.com>
2023-04-13 19:38:28 +0800

    graph: backend: dnnl: create matmul with plain dst on gpu

--- src/graph/backend/dnnl/op_executable.cpp
+++ src/graph/backend/dnnl/op_executable.cpp
@@ -364,7 +364,18 @@ matmul_executable_t::desc_t matmul_executable_t::create_desc(
             = ((src.get_ndims() == 2 || src.get_ndims() == 3)
                       && p_engine.get_kind() == dnnl::engine::kind::gpu)
             || keep_dst_layout;
-    if (!use_strided_dst) { dst = to_format_any(dst); }
+    if (!use_strided_dst) {
+        dst = to_format_any(dst);
+    } else if (dst.get_format_kind() == dnnl::memory::format_kind::any
+            && !keep_dst_layout) {
+        // convert to strided for avoiding blocked activation. The format kind
+        // of dst is possible to be any when:
+        // 1) It is created with internal logical tensor
+        // 2) It is the partition output and defined by user
+        dst = to_ncx_format(dst);
+    } else {
+        // do nothing
+    }
 
     dnnl::matmul::primitive_desc pd;
     if (op->has_attr(op_attr::with_bias)
