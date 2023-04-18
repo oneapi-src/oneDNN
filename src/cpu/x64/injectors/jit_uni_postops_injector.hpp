15051b1219147fed243a1281ee1ff39f1edd72df
Wierschem, Keola <keola.wierschem@intel.com>
2023-04-18 15:41:49 -0700

    cpu: x64: injectors: remove unused default values

--- src/cpu/x64/injectors/jit_uni_postops_injector.hpp
+++ src/cpu/x64/injectors/jit_uni_postops_injector.hpp
@@ -139,10 +139,8 @@ enum post_op_type { sum = 0, eltwise, binary, prelu };
 struct post_ops_ok_args_t {
     post_ops_ok_args_t(const cpu_isa_t isa,
             const std::vector<post_op_type> &accepted_post_op_types,
-            const post_ops_t &post_ops,
-            const memory_desc_wrapper *dst_d = nullptr,
-            const bool sum_at_pos_0_only = false,
-            const bool sum_requires_scale_one = false,
+            const post_ops_t &post_ops, const memory_desc_wrapper *dst_d,
+            const bool sum_at_pos_0_only, const bool sum_requires_scale_one,
             const bool sum_requires_zp_zero = true,
             const bcast_set_t &enabled_bcast_strategy = default_strategies());
 
