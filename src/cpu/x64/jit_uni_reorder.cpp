e4b82d3091d816796677e553bf3e176fc6718fa1
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2023-05-31 12:56:35 -0700

    fixup: cpu: x64: jit_reorder: fix dispatch issue for direct copy and large dims

--- src/cpu/x64/jit_uni_reorder.cpp
+++ src/cpu/x64/jit_uni_reorder.cpp
@@ -69,13 +69,18 @@ static bool is_direct_copy(const prb_t &prb) {
 }
 
 static bool prb_has_small_strides(const prb_t &prb) {
-    constexpr ptrdiff_t max_stride = (1LL << 31) - 1;
+    static constexpr ptrdiff_t max_stride = (1LL << 31) - 1;
+
     for (int d = 0; d < prb.ndims; ++d) {
         const ptrdiff_t cms = max_stride / prb.nodes[d].n;
-        const bool small_strides = true
-                && prb.nodes[d].is < cms / (int)data_type_size(prb.itype)
-                && prb.nodes[d].os < cms / (int)data_type_size(prb.otype);
-        if (!small_strides) return false;
+        if (is_direct_copy(prb)) {
+            if (prb.nodes[d].n > max_stride) return false;
+        } else {
+            const bool small_strides
+                    = prb.nodes[d].is < cms / (int)data_type_size(prb.itype)
+                    && prb.nodes[d].os < cms / (int)data_type_size(prb.otype);
+            if (!small_strides) return false;
+        }
     }
     return true;
 }
@@ -185,7 +190,7 @@ struct jit_uni_reorder_kernel_f32_t : public kernel_t, public jit_generator {
                         mayiuse(avx512_core) || mayiuse(avx2_vnni_2))
                 && IMPLICATION(utils::one_of(f16, p.itype, p.otype),
                         mayiuse(avx512_core_fp16) || mayiuse(avx2_vnni_2))
-                && IMPLICATION(!is_direct_copy(p), prb_has_small_strides(p));
+                && prb_has_small_strides(p);
         return ok;
     }
 
