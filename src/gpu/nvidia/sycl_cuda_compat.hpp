21d41ea4a14215f3bdc32e40c4a50b32590e556d
Denis Samoilov <denis.samoylov@intel.com>
2023-06-09 11:55:38 -0700

    gpu: nvidia: fix circular dependency

--- src/gpu/nvidia/sycl_cuda_compat.hpp
+++ src/gpu/nvidia/sycl_cuda_compat.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2021-2022 Intel Corporation
+* Copyright 2021-2023 Intel Corporation
 * Copyright 2020 Codeplay Software Limited
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,7 +20,6 @@
 
 #include <cuda.h>
 
-#include "gpu/nvidia/sycl_cuda_utils.hpp"
 #include "sycl/sycl_compat.hpp"
 
 namespace dnnl {
