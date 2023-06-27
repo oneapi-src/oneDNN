c8c34da06a5ec1165514eb9fd4635e259a245d82
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2023-06-26 17:02:40 -0700

    gpu: jit: conv: revert group-specific layout check

--- src/gpu/jit/conv/config_plan.cpp
+++ src/gpu/jit/conv/config_plan.cpp
@@ -2110,11 +2110,6 @@ private:
                                    vec_size, a_layout, b_layout, 1, -1)) {
                     m_blk = vec_size;
                     c_blk_layout = c_blk_layout.add_outer_block(1, vec_size);
-                } else if (cfg_.prb().g > 1 && cfg_.prb().g < 16
-                        && (a_layout.blocks()[0].block
-                                   * a_layout.blocks()[1].block)
-                                        % vec_size
-                                == 0) {
                 } else {
                     return plan_status_t::invalid_layout;
                 }
