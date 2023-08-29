82ebb4d42e423ad605c72b4140d14a28d1bafa2c
Andrey Kalinin <andrey.kalinin@intel.com>
2023-08-29 09:02:43 -0700

    x64: brgemm convolution: update K_tail calculation

--- src/cpu/x64/jit_brgemm_conv_utils.cpp
+++ src/cpu/x64/jit_brgemm_conv_utils.cpp
@@ -567,10 +567,7 @@ status_t brg_blocking_t::estimate_brgemm_ur() {
         if (vnni_block > 1 && K_tail > simd_w) K_tail = rnd_up(K_tail, simd_w);
     } else {
         K = kh_sets * (ic >= ic_block ? ic_block : 0);
-        K_tail = kh_sets
-                * (exec_type == exec_trans && (!is_bf32)
-                                ? ic_block
-                                : rnd_up(ic % ic_block, vnni_block));
+        K_tail = kh_sets * rnd_up(ic % ic_block, vnni_block);
     }
 
     const auto vK = K > 0 ? K : K_tail;
