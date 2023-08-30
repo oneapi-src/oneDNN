f3336d57b1b461bac7ba2da50f35a3bacddae7ad
Lv, Tao A <tao.a.lv@intel.com>
2023-08-30 12:19:52 +0800

    gtests: graph: unit: interface: fix incorrect pointer free

--- tests/gtests/graph/unit/interface/sycl/test_allocator.cpp
+++ tests/gtests/graph/unit/interface/sycl/test_allocator.cpp
@@ -31,7 +31,7 @@ TEST(TestAllocator, DefaultSyclAllocator) {
     SKIP_IF(kind == graph::engine_kind::cpu,
             "skip sycl api test for native cpu runtime.");
 #endif
-    graph::allocator_t &alloc = *(new graph::allocator_t());
+    graph::allocator_t *alloc = new graph::allocator_t();
     sycl::queue q = kind == graph::engine_kind::gpu
             ? sycl::queue {dnnl::impl::sycl::compat::gpu_selector_v,
                     sycl::property::queue::in_order {}}
@@ -40,17 +40,17 @@ TEST(TestAllocator, DefaultSyclAllocator) {
 
     graph::allocator_t::mem_attr_t attr {
             graph::allocator_t::mem_type_t::persistent, 64};
-    void *mem_ptr = alloc.allocate(
+    void *mem_ptr = alloc->allocate(
             static_cast<size_t>(16), q.get_device(), q.get_context(), attr);
 
     if (mem_ptr == nullptr) {
         // release alloc before asserting.
-        delete &alloc;
+        delete alloc;
         ASSERT_NE(mem_ptr, nullptr);
     } else {
         sycl::event e;
-        alloc.deallocate(mem_ptr, q.get_device(), q.get_context(), e);
-        delete &alloc;
+        alloc->deallocate(mem_ptr, q.get_device(), q.get_context(), e);
+        delete alloc;
     }
 }
 
@@ -65,24 +65,24 @@ TEST(TestAllocator, SyclAllocator) {
     ASSERT_EQ(alloc_attr.type_, graph::allocator_t::mem_type_t::persistent);
 
     ASSERT_EQ(alloc_attr.alignment_, 1024U);
-    graph::allocator_t &sycl_alloc = *(
-            new graph::allocator_t(dnnl::graph::testing::sycl_malloc_wrapper,
-                    dnnl::graph::testing::sycl_free_wrapper));
+    graph::allocator_t *sycl_alloc
+            = new graph::allocator_t(dnnl::graph::testing::sycl_malloc_wrapper,
+                    dnnl::graph::testing::sycl_free_wrapper);
     sycl::device sycl_dev = (kind == graph::engine_kind::gpu)
             ? sycl::device {dnnl::impl::sycl::compat::gpu_selector_v}
             : sycl::device {dnnl::impl::sycl::compat::cpu_selector_v};
     sycl::context sycl_ctx {sycl_dev};
 
-    auto *mem_ptr = sycl_alloc.allocate(
+    auto *mem_ptr = sycl_alloc->allocate(
             static_cast<size_t>(16), sycl_dev, sycl_ctx, alloc_attr);
 
     if (mem_ptr == nullptr) {
         // release sycl_alloc before asserting.
-        delete &sycl_alloc;
+        delete sycl_alloc;
         ASSERT_NE(mem_ptr, nullptr);
     } else {
         sycl::event e;
-        sycl_alloc.deallocate(mem_ptr, sycl_dev, sycl_ctx, e);
-        delete &sycl_alloc;
+        sycl_alloc->deallocate(mem_ptr, sycl_dev, sycl_ctx, e);
+        delete sycl_alloc;
     }
 }
