f7987ab5315ba8a516399bd14c542cacf2f57bf5
Ilya Lavrenov <ilya.lavrenov@intel.com>
2023-09-20 20:26:55 +0400

    src: cpu: aarch64: updated xbyak_aarch64 to v1.1.2

--- src/cpu/aarch64/xbyak_aarch64/xbyak_aarch64/xbyak_aarch64_version.h
+++ src/cpu/aarch64/xbyak_aarch64/xbyak_aarch64/xbyak_aarch64_version.h
@@ -1,20 +1,5 @@
-/*******************************************************************************
- * Copyright 2022-2023 FUJITSU LIMITED
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
 static const int majorVersion = 1;
-static const int minorVersion = 0;
-static const int patchVersion = 0;
+static const int minorVersion = 1;
+static const int patchVersion = 2;
 static int getVersion() { return (majorVersion << 16) + (minorVersion << 8) + patchVersion; }
-static const char *getVersionString() { return "1.0.0"; }
+static const char *getVersionString() { return "1.1.2"; }
