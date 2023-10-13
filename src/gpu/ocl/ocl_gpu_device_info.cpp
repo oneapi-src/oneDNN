c83bf2c2ee917d3c8977b7ac70668b9922b0c137
Roy Oursler <roy.j.oursler@intel.com>
2023-10-13 11:47:03 -0700

    gpu: ocl: enable '-Werror` for OpenCL C code in DNNL_DEV_MODE

--- src/gpu/ocl/ocl_gpu_device_info.cpp
+++ src/gpu/ocl/ocl_gpu_device_info.cpp
@@ -172,6 +172,19 @@ std::string ocl_gpu_device_info_t::get_cl_ext_options() const {
                     device_ext_t::intel_dot_accumulate))
             opts += std::string("-D") + ext2cl_str(ext) + " ";
     }
+
+#ifdef DNNL_DEV_MODE
+    // Preferably this would be in `kernel_ctx::set_default_options()`, but
+    // warnings are emitted for the automatic down conversions of double
+    // literals to float. This behavior is desirable to avoid duplicate
+    // implementations, so -Werror is disabled when fp64 support is not
+    // available instead.
+    bool enabled_werror = gpu_utils::dev_getenv(
+            "enable_ocl_werror", has(device_ext_t::khr_fp64));
+
+    if (enabled_werror) opts += "-Werror ";
+#endif
+
     return opts;
 }
 
