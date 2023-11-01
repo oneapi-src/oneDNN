f43e3ff3561ca5f6e490091f2b20727809c61557
Ankit Manerikar <ankit.v.manerikar@intel.com>
2023-11-01 11:43:48 -0700

    cpu: convolution: fix undef conflicts in gemm_bf16_conv

--- src/cpu/x64/gemm_bf16_convolution.hpp
+++ src/cpu/x64/gemm_bf16_convolution.hpp
@@ -49,7 +49,8 @@ struct gemm_bf16_convolution_fwd_t : public primitive_t {
 
             VDISPATCH_CONV(is_fwd(), VERBOSE_BAD_PROPKIND);
             VDISPATCH_CONV(
-                    expect_data_types(bf16, bf16, undef, dst_data_type, f32),
+                    expect_data_types(bf16, bf16, dnnl::impl::data_type::undef,
+                            dst_data_type, f32),
                     VERBOSE_UNSUPPORTED_DT_CFG);
 
             VDISPATCH_CONV(set_default_alg_kind(alg_kind::convolution_direct),
@@ -265,8 +266,8 @@ struct gemm_bf16_convolution_bwd_data_t : public primitive_t {
 
             VDISPATCH_CONV(desc()->prop_kind == prop_kind::backward_data,
                     VERBOSE_BAD_PROPKIND);
-            VDISPATCH_CONV(expect_data_types(
-                                   diff_src_data_type, bf16, undef, bf16, f32),
+            VDISPATCH_CONV(expect_data_types(diff_src_data_type, bf16,
+                                   dnnl::impl::data_type::undef, bf16, f32),
                     VERBOSE_UNSUPPORTED_DT_CFG);
             VDISPATCH_CONV(set_default_alg_kind(alg_kind::convolution_direct),
                     VERBOSE_BAD_ALGORITHM);
@@ -324,8 +325,8 @@ struct gemm_bf16_convolution_bwd_weights_t : public primitive_t {
 
             VDISPATCH_CONV(desc()->prop_kind == prop_kind::backward_weights,
                     VERBOSE_BAD_PROPKIND);
-            VDISPATCH_CONV(expect_data_types(
-                                   bf16, diff_wei_data_type, undef, bf16, f32),
+            VDISPATCH_CONV(expect_data_types(bf16, diff_wei_data_type,
+                                   dnnl::impl::data_type::undef, bf16, f32),
                     VERBOSE_UNSUPPORTED_DT_CFG);
             VDISPATCH_CONV(set_default_alg_kind(alg_kind::convolution_direct),
                     VERBOSE_BAD_ALGORITHM);
