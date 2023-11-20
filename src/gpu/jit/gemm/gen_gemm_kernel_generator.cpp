c117639921f6f3710bf8a70088feefe6f29b4ca3
Peter Caday <peter.caday@intel.com>
2023-11-20 12:59:47 -0800

    xe2: jit: gemm: fix missing immediate offsets after adding remainder handling

--- src/gpu/jit/gemm/gen_gemm_kernel_generator.cpp
+++ src/gpu/jit/gemm/gen_gemm_kernel_generator.cpp
@@ -2946,6 +2946,7 @@ bool gemm_kernel_generator_t<hw>::tryAddRemainder(Type T, RegisterBlock &block,
                 return false;
         }
         blockNew.bytes = block.bytes;
+        blockNew.offsetAddr = block.offsetAddr;
     }
 
     block = blockNew;
