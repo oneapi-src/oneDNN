460823e2dec4c28d8b305bd5a360ef7f2c927332
Sergey Kazakov <sergey.kazakov@intel.com>
2023-11-22 16:20:45 -0800

    gpu: bnorm: fix debug verbose macro

--- src/gpu/ocl/bnorm/bnorm_utils.hpp
+++ src/gpu/ocl/bnorm/bnorm_utils.hpp
@@ -24,7 +24,8 @@
 #include <string.h>
 
 #ifndef __SHORT_FILE_NAME__
-#define __SHORT_FILE_NAME__ ((strrchr(__FILE__, '/') ? "" : __FILE__ - 1) + 1)
+#define __SHORT_FILE_NAME__ \
+    (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #endif
 #ifndef PRINTHEAD
 #define PRINTHEAD __SHORT_FILE_NAME__, __FUNCTION__, __LINE__
