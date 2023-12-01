69cef84c4f09398858393035eafa2bd4a29ec0b0
Peter Caday <peter.caday@intel.com>
2023-12-01 11:33:44 -0800

    gpu: jit: ngen: fix swsb edge case

--- src/gpu/jit/ngen/ngen_gen12.hpp
+++ src/gpu/jit/ngen/ngen_gen12.hpp
@@ -945,7 +945,9 @@ bool Instruction12::getOperandRegion(autoswsb::DependencyRegion &region, int opN
             auto sub = base[sr / getBytes(base.getType())];
             auto hs = (1 << o.direct.hs);
             if (opNum >= 0) hs >>= 1;
-            if ((opNum < 0) || (opNum == 2))
+            if (opNum < 0)
+                rd = sub(hs, 1, 0);
+            else if (opNum == 2)
                 rd = sub(hs);
             else
                 rd = sub((1 << vs) >> 1, hs);
@@ -987,7 +989,7 @@ bool Instruction12::getOperandRegion(autoswsb::DependencyRegion &region, int opN
             auto sub = base[sr / getBytes(base.getType())];
             auto hs = (1 << o.direct.hs) >> 1;
             if (opNum < 0)
-                rd = sub(hs);
+                rd = sub(hs, 1, 0);
             else
                 rd = sub((1 << vs) >> 1, 1 << o.direct.width, hs);
 
