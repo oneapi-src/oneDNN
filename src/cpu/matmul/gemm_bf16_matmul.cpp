5e346c6032be2835229cd62a874b6076cc5b474b
Palicki, Stefan <stefan.palicki@intel.com>
2023-12-18 13:33:23 -0800

    x64: matmul: gemm: disable prelu with runtime N
    Co-authored-by: Arthur Araujo Mitrano <arthur.araujo.mitrano@intel.com>

--- src/cpu/matmul/gemm_bf16_matmul.cpp
+++ src/cpu/matmul/gemm_bf16_matmul.cpp
@@ -130,11 +130,13 @@ status_t gemm_bf16_matmul_t<dst_type>::pd_t::check_and_configure_attributes(
                         binary_injector_utils::extract_bcast_strategies(
                                 post_ops.entry_, dst_md()),
                         broadcasting_strategy_t::per_oc);
+        const bool has_prelu = post_ops.find(prelu) != -1;
         return cpu::inner_product_utils::post_ops_ok(
                        post_ops, dst_md(), enabled_bcast_strategy)
                 && IMPLICATION(is_binary_po_per_oc,
                         gemm_based::check_gemm_binary_per_oc_compatible_formats(
-                                *this));
+                                *this))
+                && IMPLICATION(N() == DNNL_RUNTIME_DIM_VAL, !has_prelu);
     };
 
     // check basic attributes
