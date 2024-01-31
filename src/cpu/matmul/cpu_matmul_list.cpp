13481f27a1b0d388384f0d5ceebd98eabc9fbbd7
Makarevich, Alexey <alexey.makarevich@intel.com>
2024-01-31 12:07:04 -0800

    made f32  gemm impl higher priority than brgemm f32/avx2

--- src/cpu/matmul/cpu_matmul_list.cpp
+++ src/cpu/matmul/cpu_matmul_list.cpp
@@ -82,11 +82,11 @@ constexpr impl_list_item_t impl_list[] = REG_MATMUL_P({
         CPU_INSTANCE_AVX512(brgemm_matmul_t<avx512_core>)
         CPU_INSTANCE_AVX2(brgemm_matmul_t<avx2_vnni_2>)
         CPU_INSTANCE_AVX2(brgemm_matmul_t<avx2_vnni>)
-        CPU_INSTANCE_AVX2(brgemm_matmul_t<avx2>)
         CPU_INSTANCE(gemm_f32_matmul_t)
         CPU_INSTANCE(gemm_bf16_matmul_t<f32>)
         CPU_INSTANCE(gemm_bf16_matmul_t<bf16>)
         CPU_INSTANCE(gemm_x8s8s32x_matmul_t)
+        CPU_INSTANCE_AVX2(brgemm_matmul_t<avx2>)
         CPU_INSTANCE(ref_matmul_t)
         CPU_INSTANCE(ref_matmul_int8_t)
         // These implementations are enabled only when DNNL_EXPERIMENTAL_SPARSE
