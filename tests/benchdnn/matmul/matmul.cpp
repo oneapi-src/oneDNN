09b603c5f9b7f98b30c034ddb90099994e26292e
Kealan Barbieri <kealan.barbieri@intel.com>
2024-02-22 14:16:01 -0800

    gpu: jit: gemm: fix int8 decomp paths

--- tests/benchdnn/matmul/matmul.cpp
+++ tests/benchdnn/matmul/matmul.cpp
@@ -477,16 +477,19 @@ void skip_unimplemented_prb(const prb_t *prb, res_t *res) {
             return;
         }
 
-        // GPU supports only default sum_dt argument.
-        const auto &po = prb->attr.post_ops;
-        const int sum_idx = po.find(attr_t::post_ops_t::kind_t::SUM);
-        if (sum_idx != -1 && po.entry[sum_idx].sum.dt != dnnl_data_type_undef) {
+        // GPU does not support grouped scales or zero-points.
+        if (prb->attr.zero_points.get(DNNL_ARG_WEIGHTS).policy
+                        == policy_t::PER_OCIC
+                || prb->attr.scales.get(DNNL_ARG_WEIGHTS).policy
+                        == policy_t::PER_OCIC) {
             res->state = SKIPPED, res->reason = CASE_NOT_SUPPORTED;
             return;
         }
 
-        // GPU does not support weights decompression
-        if (prb->weights_decompression()) {
+        // GPU supports only default sum_dt argument.
+        const auto &po = prb->attr.post_ops;
+        const int sum_idx = po.find(attr_t::post_ops_t::kind_t::SUM);
+        if (sum_idx != -1 && po.entry[sum_idx].sum.dt != dnnl_data_type_undef) {
             res->state = SKIPPED, res->reason = CASE_NOT_SUPPORTED;
             return;
         }
@@ -519,12 +522,12 @@ void skip_unimplemented_prb(const prb_t *prb, res_t *res) {
             return;
         }
 
-        // GPU doesn't support f8_e5m2/f8_e4m3.
-        const bool is_xf8 = (prb->src_dt() == dnnl_f8_e5m2
-                                    || prb->src_dt() == dnnl_f8_e4m3)
-                && (prb->wei_dt() == dnnl_f8_e5m2
-                        || prb->wei_dt() == dnnl_f8_e4m3);
-        if (is_xf8) {
+        // GPU supports f8_e4m3 on all platformas and f8_e5m2 pre XeHPC through
+        // ref only, with limited post-op support.
+        if (((prb->src_dt() == dnnl_f8_e4m3 || prb->dst_dt() == dnnl_f8_e4m3)
+                    || (prb->src_dt() == dnnl_f8_e5m2
+                            || prb->dst_dt() == dnnl_f8_e5m2))
+                && (!po.is_def() || !prb->attr.scales.is_def())) {
             res->state = SKIPPED, res->reason = CASE_NOT_SUPPORTED;
             return;
         }
