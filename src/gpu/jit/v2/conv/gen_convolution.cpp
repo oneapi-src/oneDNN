834d540f74624ce3cd366bcaf2d7c67b4b7c8bb2
Roy Oursler <roy.j.oursler@intel.com>
2024-03-07 14:05:39 -0800

    gpu: jit: v2: conv: add bwd_d and bwd_w primitives

--- src/gpu/jit/v2/conv/gen_convolution.cpp
+++ src/gpu/jit/v2/conv/gen_convolution.cpp
@@ -23,6 +23,7 @@
 #include "common/utils.hpp"
 #include "common/verbose.hpp"
 #include "gpu/jit/codegen/kernel.hpp"
+#include "gpu/jit/conv/gen_convolution.hpp"
 #include "gpu/jit/ir/kernel_info.hpp"
 #include "gpu/jit/utils/utils.hpp"
 #include "gpu/jit/v2/conv/bridge.hpp"
@@ -70,12 +71,12 @@ public:
     static bool is_supported(T *pd, prop_kind_t prop) {
         bool enable_conv_v2 = gpu_utils::dev_getenv("enable_conv_v2", false);
         if (!enable_conv_v2) return false;
-        switch (prop) {
-            case prop_kind::forward:
-                if (!pd->is_fwd()) return false;
-                break;
-            default: return false;
-        }
+
+        // Non-trivial strides produces non-linear offset arithmetic which is
+        // not yet supported.
+        if (pd->is_bwd_d()
+                && !(pd->KSW() == 1 && pd->KSH() == 1 && pd->KSD() == 1))
+            return false;
 
         if (pd->with_bias()) return false;
         if (!pd->attr()->has_default_values()) return false;
@@ -163,6 +164,33 @@ status_t gen_convolution_fwd_t::init(engine_t *engine) {
 }
 
 status_t gen_convolution_fwd_t::execute(const exec_ctx_t &ctx) const {
+    return impl_->execute(this, ctx);
+}
+
+status_t gen_convolution_bwd_data_t::pd_t::init(engine_t *engine) {
+    return gen_convolution_t::init_pd(this, engine, prop_kind::backward_data);
+}
+
+status_t gen_convolution_bwd_data_t::init(engine_t *engine) {
+    impl_.reset(new gen_convolution_t());
+    return impl_->init(this, engine);
+}
+
+status_t gen_convolution_bwd_data_t::execute(const exec_ctx_t &ctx) const {
+    return impl_->execute(this, ctx);
+}
+
+status_t gen_convolution_bwd_weights_t::pd_t::init(engine_t *engine) {
+    return gen_convolution_t::init_pd(
+            this, engine, prop_kind::backward_weights);
+}
+
+status_t gen_convolution_bwd_weights_t::init(engine_t *engine) {
+    impl_.reset(new gen_convolution_t());
+    return impl_->init(this, engine);
+}
+
+status_t gen_convolution_bwd_weights_t::execute(const exec_ctx_t &ctx) const {
     return impl_->execute(this, ctx);
 }
 
