903831bf566ba5b5812d5beabbdaf3bf7d16d3ed
Safonov, Igor <igor.safonov@intel.com>
2024-03-12 16:29:19 -0700

    doc: sphinx: update copyright

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -1,5 +1,5 @@
 ################################################################################
-# Copyright 2021-2023 Intel Corporation
+# Copyright 2021-2024 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -51,7 +51,7 @@ sys.path.insert(1, os.path.abspath(doxyrest_share_path))
 # -- Project information -----------------------------------------------------
 
 project = 'oneDNN'
-copyright = '2016-2022 Intel Corporation'
+copyright = '2016-2024 Intel Corporation'
 author = ''
 
 # -- General configuration ---------------------------------------------------
