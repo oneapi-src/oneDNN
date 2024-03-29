762ccb1634715b95e3dc5a18f18cfa3443e242f2
Soto Flores, Manuel <manuel.soto.flores@intel.com>
2024-03-29 00:36:27 -0700

    x64: brgemm: enable fp8 for non-uker implementation

--- src/cpu/x64/brgemm/brgemm.cpp
+++ src/cpu/x64/brgemm/brgemm.cpp
@@ -556,9 +556,8 @@ status_t brgemm_desc_set_attr(brgemm_t *brg, const brgemm_attr_t &brgattr) {
             && brg->prfC.dist2 < 0)
         brg->prfC.dist2 = 0;
 
-    // TODO: update conditions once other brgemm implementations are enabled
-    if (brg->is_fp8 && brg->brgattr.use_uker && !brg->is_fp8_via_convert())
-        return status::unimplemented;
+    // TODO: update conditions once other implementations are enabled
+    if (brg->is_fp8 && !brg->is_fp8_via_convert()) return status::unimplemented;
 
     return status::success;
 }
