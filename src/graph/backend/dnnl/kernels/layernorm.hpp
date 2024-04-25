b22ceb043f794e73755eb8eff71ffc44e9ebe4fd
Lv, Tao A <tao.a.lv@intel.com>
2024-04-26 00:10:10 +0800

    graph: backend: dnnl: fix lnorm + tc + eltwise/binary fusions

--- src/graph/backend/dnnl/kernels/layernorm.hpp
+++ src/graph/backend/dnnl/kernels/layernorm.hpp
@@ -88,8 +88,8 @@ public:
         pass_pipeline_t pipeline(vis);
 
         BACKEND_DNNL_ADD_PASS(pipeline, lower_down);
-        BACKEND_DNNL_ADD_PASS(pipeline, fuse_post_ops);
         BACKEND_DNNL_ADD_PASS(pipeline, fuse_post_typecast_to_predecessor);
+        BACKEND_DNNL_ADD_PASS(pipeline, fuse_post_ops);
         BACKEND_DNNL_ADD_PASS(pipeline, remove_quant_data_with_no_effect);
         BACKEND_DNNL_ADD_PASS(pipeline, convert_to_runtime_dst_scales);
         BACKEND_DNNL_ADD_PASS(pipeline, fuse_dst_scales);
