cdb7c51843a82784a3eff0181bb51b9da2381672
Peter Caday <peter.caday@intel.com>
2024-05-07 15:17:00 -0700

    gpu: jit: gemm: separate strategy parameter for A/B block 2D path

--- src/gpu/intel/jit/gemm/strategy_parser.cpp
+++ src/gpu/intel/jit/gemm/strategy_parser.cpp
@@ -407,7 +407,7 @@ void parseStrategy(const char *str, HW hw, const GEMMProblem &problem,
             strategy.arbitrationMode
                     = ngen::ThreadArbitrationMode::RoundRobinOnStall;
         else if (mod == "l2d")
-            strategy.optAlignAB = GEMMStrategy::AlignBlock2D;
+            strategy.optAlignAB2D = true;
         else if (mod == "nq") {
             strategy.A.noExtraPad = strategy.A_prefetch.noExtraPad = true;
             strategy.B.noExtraPad = strategy.B_prefetch.noExtraPad = true;
@@ -539,7 +539,7 @@ void adjustStrategy(HW hw, const GEMMProblem &problem, GEMMStrategy &strategy,
             && !isPacked(problem.C.layout);
 
     // Notify kernel generator to downgrade block 2D prefetches if block 2D cannot be used.
-    if (tags && strategy.optAlignAB != GEMMStrategy::AlignBlock2D) {
+    if (tags && !strategy.optAlignAB2D) {
         bool block2DA = false, block2DB = false;
         while (*tags) {
             block2DA |= (*tags == kcatalog::ReqBlock2DA);
