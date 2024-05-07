40ae09468323797a2c805a3dadb51486e3b0e54e
Tomasz Czeszun <tomasz.czeszun@intel.com>
2024-05-07 13:40:13 -0700

    cpu: ref_eltwise: enable fp8

--- src/cpu/cpu_eltwise_list.cpp
+++ src/cpu/cpu_eltwise_list.cpp
@@ -75,6 +75,8 @@ const std::map<pk_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map() {
             CPU_INSTANCE(ref_eltwise_fwd_t<s32>)
             CPU_INSTANCE(ref_eltwise_fwd_t<s8>)
             CPU_INSTANCE(ref_eltwise_fwd_t<u8>)
+            CPU_INSTANCE(ref_eltwise_fwd_t<f8_e4m3>)
+            CPU_INSTANCE(ref_eltwise_fwd_t<f8_e5m2>)
             nullptr,
         }},
         {{backward}, REG_BWD_PK({
@@ -90,6 +92,8 @@ const std::map<pk_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map() {
             CPU_INSTANCE(ref_eltwise_bwd_t<f32>)
             CPU_INSTANCE(ref_eltwise_bwd_t<bf16>)
             CPU_INSTANCE(ref_eltwise_bwd_t<f16>)
+            CPU_INSTANCE(ref_eltwise_bwd_t<f8_e4m3>)
+            CPU_INSTANCE(ref_eltwise_bwd_t<f8_e5m2>)
             nullptr,
         })},
     });
