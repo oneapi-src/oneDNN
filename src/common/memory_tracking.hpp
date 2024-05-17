6f143650bd6e94930da56ea8a6633d26a077ee45
Annop Wongwathanarat <annop.wongwathanarat@arm.com>
2024-05-17 18:52:59 +0000

    cpu: aarch64: matmul: Move allocation of temporary tensors to scratchpad in acl_matmul
    Introduce 3 new scrathpad memory key names.

--- src/common/memory_tracking.hpp
+++ src/common/memory_tracking.hpp
@@ -1,5 +1,6 @@
 /*******************************************************************************
 * Copyright 2018-2024 Intel Corporation
+* Copyright 2024 Arm Ltd. and affiliates
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
