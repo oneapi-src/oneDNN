7f95cd9a42ca2f421c7a54ce2a69ead7a3f6dbca
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-05-23 14:04:12 -0700

    benchdnn: utils: fill: introduce a custom set of values

--- tests/benchdnn/utils/fill.hpp
+++ tests/benchdnn/utils/fill.hpp
@@ -40,12 +40,14 @@ struct fill_cfg_t {
         : dt_(dnnl_f32)
         , range_min_val_(-16.f)
         , range_max_val_(16.f)
+        , predefined_set_({})
         , only_integer_(false)
         , name_("") {}
 
     fill_cfg_t(dnnl_data_type_t dt, float range_min_val, float range_max_val,
             bool only_integer, attr_t::post_ops_t::kind_t alg,
             const std::string &name);
+    fill_cfg_t(const std::vector<float> &user_set, const std::string &name);
 
     std::string print_verbose() const;
 
@@ -55,6 +57,12 @@ struct fill_cfg_t {
     float range_min_val_;
     // The upper bound for the filling range.
     float range_max_val_;
+    // It's an alternative approach to fill the data. Instead of setting range
+    // borders, pass a set of fixed values. The function will use uniform_int
+    // randomizer to choose the value.
+    //
+    // Helpful to generate pow2 filling, by passing {2.f, 4.f, ...};
+    std::vector<float> predefined_set_;
     // A flag to generate only integer values.
     bool only_integer_;
     // Config name for verbosity.
