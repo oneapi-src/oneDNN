ddb0e1556d62866ed54b3dc8f8d7c16ad1e03744
Tadej Ciglarič <tadej.ciglaric@codeplay.com>
2024-05-23 11:23:48 +0200

    gpu: sycl: binary: added support for different memory formats

--- src/gpu/sycl/ref_binary.cpp
+++ src/gpu/sycl/ref_binary.cpp
@@ -36,7 +36,7 @@ status_t ref_binary_t::pd_t::init_conf() {
     conf_.block_size = 16;
     conf_.wg_size = 32;
 
-    conf_.wk_size = memory_desc_wrapper(src_md(0)).nelems();
+    conf_.wk_size = memory_desc_wrapper(dst_md()).nelems();
 
     conf_.alg_kind = desc()->alg_kind;
     // Limitations:
@@ -47,7 +47,10 @@ status_t ref_binary_t::pd_t::init_conf() {
             = !attr()->scales_.get(DNNL_ARG_SRC_1).has_default_values();
     conf_.is_tensor_op = is_tensor_op();
     for (size_t i = 0; i < xpu::sycl::md_t::max_dims; i++) {
-        conf_.broadcast_dims[i] = broadcast_dims()[i];
+        conf_.broadcast_dims0[i]
+                = conf_.src0_md.dims()[i] == 1 && conf_.src1_md.dims()[i] != 1;
+        conf_.broadcast_dims1[i]
+                = conf_.src0_md.dims()[i] != 1 && conf_.src1_md.dims()[i] == 1;
     }
 
     conf_.post_ops = sycl_post_ops_t(attr());
