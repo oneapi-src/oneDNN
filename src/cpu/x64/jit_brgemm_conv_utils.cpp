f7885a61d175340be05bbd7749285879b2163b5c
Xuxin, Zeng <xuxin.zeng@intel.com>
2024-05-28 10:52:28 -0700

    revert: cpu: x64: dispatch the shape that requires large/small cache to jit on avx2

--- src/cpu/x64/jit_brgemm_conv_utils.cpp
+++ src/cpu/x64/jit_brgemm_conv_utils.cpp
@@ -2216,28 +2216,6 @@ status_t init_conf(jit_brgemm_conv_conf_t &jcp, cpu_isa_t isa,
             !(jcp.ow_block == 0 || jcp.ic_block == 0 || jcp.oc_block == 0),
             VERBOSE_BLOCKING_FAIL, "bad blocking dimensions");
 
-    // Dispatch the shape that requires large or small cache to JIT
-    // for better performance on AVX2
-    // The threshold is empirical
-    const size_t w_cache_sz
-            = static_cast<size_t>(jcp.src_dsz) * jcp.ic_block * jcp.iwp
-            + jcp.dst_dsz * jcp.ow * jcp.oc_block;
-    const size_t wei_cache_sz = static_cast<size_t>(jcp.wei_dsz) * jcp.kd_block
-            * jcp.kh_block * jcp.kw_block * jcp.ic_block * jcp.oc_block;
-    const size_t nthr_work_amount
-            = div_up(static_cast<size_t>(jcp.mb) * jcp.ngroups * jcp.nb_od
-                            * jcp.nb_oh * jcp.nb_ow * jcp.nb_oc,
-                    jcp.nthr);
-    const bool req_large_cache = jcp.oc >= 256 && jcp.ic >= 256
-            && nstl::max(w_cache_sz, wei_cache_sz) * nthr_work_amount
-                    >= brg_blocking_t::L2 * 10;
-    const bool req_small_cache = jcp.ic <= jcp.acc_simd_w
-            && nstl::max(w_cache_sz, wei_cache_sz) <= 2048;
-    VDISPATCH_CONV_IC(
-            !((req_large_cache || req_small_cache) && jcp.isa == avx2),
-            VERBOSE_IMPL_HEURISTIC_FAIL,
-            "Dispatch the shape that requires large/small cache size to jit");
-
     // Dispatch the shape to VNNI for better performance on AMX
     const bool is_int8_small_ic = jcp.oc == 32 && jcp.ic < jcp.simd_w / 2
             && is_int8_convolution && is_amx(jcp.isa)
@@ -2458,28 +2436,6 @@ status_t init_1x1_conf(jit_brgemm_conv_conf_t &jcp, cpu_isa_t isa,
 
     VDISPATCH_CONV_IC(!(jcp.ic_block == 0 || jcp.oc_block == 0),
             VERBOSE_BLOCKING_FAIL, "bad blocking dimensions");
-
-    // Dispatch the shape that requires large or small cache to JIT
-    // for better performance on AVX2
-    // The threshold is empirical
-    const size_t w_cache_sz
-            = static_cast<size_t>(jcp.src_dsz) * jcp.ic_block * jcp.iwp
-            + jcp.dst_dsz * jcp.ow * jcp.oc_block;
-    const size_t wei_cache_sz = static_cast<size_t>(jcp.wei_dsz) * jcp.kd_block
-            * jcp.kh_block * jcp.kw_block * jcp.ic_block * jcp.oc_block;
-    const size_t nthr_work_amount
-            = div_up(static_cast<size_t>(jcp.mb) * jcp.ngroups * jcp.nb_od
-                            * jcp.nb_oh * jcp.nb_ow * jcp.nb_oc,
-                    jcp.nthr);
-    const bool req_large_cache = jcp.oc >= 256 && jcp.ic >= 256
-            && nstl::max(w_cache_sz, wei_cache_sz) * nthr_work_amount
-                    >= brg_blocking_t::L2 * 10;
-    const bool req_small_cache = jcp.ic <= jcp.acc_simd_w
-            && nstl::max(w_cache_sz, wei_cache_sz) <= 2048;
-    VDISPATCH_CONV_IC(
-            !((req_large_cache || req_small_cache) && jcp.isa == avx2),
-            VERBOSE_IMPL_HEURISTIC_FAIL,
-            "Dispatch the shapes that requie large cache size to jit");
 
     // Configure matrix sizes
 
