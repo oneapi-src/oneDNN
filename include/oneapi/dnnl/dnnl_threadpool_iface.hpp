e6ac9b9b4740f2a8e0d0b56f6434e375d7b26b55
Denis Samoilov <denis.samoylov@intel.com>
2024-05-30 00:13:20 -0700

    api: fix missing header

--- include/oneapi/dnnl/dnnl_threadpool_iface.hpp
+++ include/oneapi/dnnl/dnnl_threadpool_iface.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2020 Intel Corporation
+* Copyright 2020-2024 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 #ifndef ONEAPI_DNNL_DNNL_THREADPOOL_IFACE_HPP
 #define ONEAPI_DNNL_DNNL_THREADPOOL_IFACE_HPP
 
+#include <cstdint>
 #include <functional>
 
 /// @addtogroup dnnl_api
