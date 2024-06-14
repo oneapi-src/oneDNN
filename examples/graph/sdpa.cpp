18cb691c4857668f8d12a3fe1b31aa33f791b4c7
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-06-14 09:50:16 -0700

    examples: graph: sdpa: add primitive execution pass for comparison

--- examples/graph/sdpa.cpp
+++ examples/graph/sdpa.cpp
@@ -23,12 +23,15 @@
 #include <string>
 #include <vector>
 
+#include "oneapi/dnnl/dnnl.hpp"
 #include "oneapi/dnnl/dnnl_graph.hpp"
 
 #include "graph_example_utils.hpp"
 
+using namespace dnnl;
+using tag = memory::format_tag;
+
 using namespace dnnl::graph;
-using data_type = logical_tensor::data_type;
 using layout_type = logical_tensor::layout_type;
 using dim = logical_tensor::dim;
 using dims = logical_tensor::dims;
@@ -74,11 +77,173 @@ void fill_mask(std::vector<float> &mask, size_t seq_len) {
     }
 }
 
-const char *get_type_string(data_type dt) {
+void print_test_case(memory::data_type dt, const sdpa_dims_t &p) {
+    std::cout << '[' << std::setw(4) << dnnl_dt2str(memory::convert_to_c(dt));
+    std::cout << " mb = " << p.mb << ", seq_len = " << p.seq_len
+              << ", head_num = " << p.head_num
+              << ", head_size = " << p.head_size;
+    std::cout << "] " << std::flush;
+}
+
+void bench_sdpa_primitives(engine::kind ekind, memory::data_type dt,
+        const sdpa_dims_t &p, double time_limit = 0.) {
+    const bool quick_test = (time_limit == 0.);
+    if (!quick_test) print_test_case(dt, p);
+
+    // Create execution dnnl::engine.
+    dnnl::engine eng(ekind, 0);
+    // Create dnnl::stream.
+    dnnl::stream strm(eng);
+
+    // Prepare input and output shapes to construct the sdpa graph.
+    const memory::dims qv_sz = {p.mb, p.head_num, p.seq_len, p.head_size};
+    const memory::dims k_sz = {p.mb, p.head_num, p.head_size, p.seq_len};
+    const memory::dims score_sz = {p.mb, p.head_num, p.seq_len, p.seq_len};
+    const memory::dims scale_sz = {1, 1, 1, 1};
+    const memory::dims mask_sz = {p.mb, 1, 1, p.seq_len};
+
+    // score = query x key.T
+    // scaled_score = score / scale
+    // masked_score = scaled_score + mask
+    // All combined in a single matmul primitive.
+    auto query_md = memory::desc(qv_sz, dt, tag::acbd);
+    auto key_md = memory::desc(k_sz, dt, tag::acbd);
+    auto score_md = memory::desc(score_sz, dt, tag::abcd);
+    auto scale_md = memory::desc(scale_sz, dt, tag::abcd);
+    auto mask_md = memory::desc(mask_sz, dt, tag::abcd);
+
+    primitive_attr bmm1_attr;
+    bmm1_attr.set_scratchpad_mode(scratchpad_mode::user);
+    post_ops bmm1_po;
+    bmm1_po.append_binary(algorithm::binary_div, scale_md);
+    bmm1_po.append_binary(algorithm::binary_add, mask_md);
+    bmm1_attr.set_post_ops(bmm1_po);
+
+    auto bmm1_pd = matmul::primitive_desc(
+            eng, query_md, key_md, score_md, bmm1_attr);
+    auto bmm1_prim = matmul(bmm1_pd);
+
+    // attention_probs = softmax(masked_score)
+    primitive_attr softmax_attr;
+    softmax_attr.set_scratchpad_mode(scratchpad_mode::user);
+    auto softmax_pd = softmax_forward::primitive_desc(eng,
+            prop_kind::forward_inference, algorithm::softmax_accurate, score_md,
+            score_md, /* axis = */ score_md.get_ndims() - 1, softmax_attr);
+    auto softmax_prim = softmax_forward(softmax_pd);
+
+    // attention_output = attention_probs x value
+    auto value_md = memory::desc(qv_sz, dt, tag::acbd);
+    auto output_md = memory::desc(qv_sz, dt, tag::acbd);
+
+    primitive_attr bmm2_attr;
+    bmm2_attr.set_scratchpad_mode(scratchpad_mode::user);
+    auto bmm2_pd = matmul::primitive_desc(
+            eng, score_md, value_md, output_md, bmm2_attr);
+    auto bmm2_prim = matmul(bmm2_pd);
+
+    // Create memory objects
+    auto m_query = memory(query_md, eng);
+    auto m_key = memory(key_md, eng);
+    auto m_scale = memory(scale_md, eng);
+    auto m_mask = memory(mask_md, eng);
+    auto m_value = memory(value_md, eng);
+    auto m_output = memory(output_md, eng);
+
+    // Allocate user data.
+    std::vector<float> query_data(product(qv_sz));
+    std::vector<float> key_data(product(k_sz));
+    std::vector<float> scale_data(product(scale_sz), std::sqrt(p.head_size));
+    std::vector<float> mask_data(product(mask_sz));
+    std::vector<float> value_data(product(qv_sz));
+    std::vector<float> output_data(product(qv_sz));
+
+    fill_random(query_data);
+    fill_random(key_data);
+    fill_random(value_data);
+    fill_mask(mask_data, static_cast<size_t>(p.seq_len));
+
+    // Write data to tensor object's handle.
+    write_to_dnnl_memory(query_data.data(), m_query);
+    write_to_dnnl_memory(key_data.data(), m_key);
+    write_to_dnnl_memory(scale_data.data(), m_scale);
+    write_to_dnnl_memory(mask_data.data(), m_mask);
+    write_to_dnnl_memory(value_data.data(), m_value);
+
+    size_t max_scratchpad_size = 0;
+    auto bmm1_scratchpad = bmm1_pd.scratchpad_desc().get_size();
+    auto softmax_scratchpad = softmax_pd.scratchpad_desc().get_size();
+    auto bmm2_scratchpad = bmm2_pd.scratchpad_desc().get_size();
+    for (auto &sz : {bmm1_scratchpad, softmax_scratchpad, bmm2_scratchpad}) {
+        if (max_scratchpad_size < sz) max_scratchpad_size = sz;
+    }
+    auto scratchpad_md
+            = memory::desc({static_cast<memory::dim>(max_scratchpad_size)},
+                    memory::data_type::u8, tag::a);
+
+    // allocate intermediate memory
+    auto m_score = memory(score_md, eng);
+    auto m_scratchpad = memory(scratchpad_md, eng);
+
+    const auto loop = [&]() {
+        // each primitive will use all threads
+        bmm1_prim.execute(strm,
+                {{DNNL_ARG_SRC, m_query}, {DNNL_ARG_WEIGHTS, m_key},
+                        {DNNL_ARG_DST, m_score},
+                        {DNNL_ARG_ATTR_MULTIPLE_POST_OP(0) | DNNL_ARG_SRC_1,
+                                m_scale},
+                        {DNNL_ARG_ATTR_MULTIPLE_POST_OP(1) | DNNL_ARG_SRC_1,
+                                m_mask},
+                        {DNNL_ARG_SCRATCHPAD, m_scratchpad}});
+
+        softmax_prim.execute(strm,
+                {{DNNL_ARG_SRC, m_score}, {DNNL_ARG_DST, m_score},
+                        {DNNL_ARG_SCRATCHPAD, m_scratchpad}});
+
+        bmm2_prim.execute(strm,
+                {{DNNL_ARG_SRC, m_score}, {DNNL_ARG_WEIGHTS, m_value},
+                        {DNNL_ARG_DST, m_output},
+                        {DNNL_ARG_SCRATCHPAD, m_scratchpad}});
+    };
+
+    // Warmup run.
+    // Execute primitives of sdpa.
+    loop();
+
+    // Wait for the computation to finish.
+    strm.wait();
+
+    // First run.
+    auto start_first = std::chrono::steady_clock::now();
+    loop();
+    strm.wait();
+    auto end_first = std::chrono::steady_clock::now();
+    std::chrono::duration<double, std::milli> dur_first
+            = end_first - start_first;
+
+    if (quick_test) return;
+
+    // Timing runs.
+    const int runs = std::max(min_runs, int(time_limit / dur_first.count()));
+    auto start = std::chrono::steady_clock::now();
+    for (int i = 0; i <= runs; i++)
+        loop();
+    strm.wait();
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+
+    // Display the results.
+    double avg_time = (duration.count() - dur_first.count()) / runs;
+    std::cout << "primitive runs: " << runs + 1 << "; ";
+    std::cout << "avg_time: " << avg_time << " ms" << std::endl;
+
+    return;
+}
+
+const char *get_type_string(logical_tensor::data_type dt) {
     const char *type_string = "unknown";
 
 #define TYPE_CASE(T) \
-    if (dt == data_type::T) type_string = #T;
+    if (dt == logical_tensor::data_type::T) type_string = #T;
     TYPE_CASE(f16);
     TYPE_CASE(f32);
     TYPE_CASE(bf16);
@@ -87,7 +252,7 @@ const char *get_type_string(data_type dt) {
     return type_string;
 }
 
-void print_test_case(data_type dt, const sdpa_dims_t &p) {
+void print_test_case(logical_tensor::data_type dt, const sdpa_dims_t &p) {
     std::cout << '[' << std::setw(4) << get_type_string(dt);
     std::cout << " mb = " << p.mb << ", seq_len = " << p.seq_len
               << ", head_num = " << p.head_num
@@ -95,8 +260,8 @@ void print_test_case(data_type dt, const sdpa_dims_t &p) {
     std::cout << "] " << std::flush;
 }
 
-void bench_sdpa(engine::kind ekind, data_type dt, const sdpa_dims_t &p,
-        double time_limit = 0.) {
+void bench_sdpa(engine::kind ekind, logical_tensor::data_type dt,
+        const sdpa_dims_t &p, double time_limit = 0.) {
     const bool quick_test = (time_limit == 0.);
     if (!quick_test) print_test_case(dt, p);
 
@@ -154,7 +319,7 @@ void bench_sdpa(engine::kind ekind, data_type dt, const sdpa_dims_t &p,
     bmm2.add_outputs({output});
 
     // Construct a sdpa graph with engine kind and operations.
-    graph sdpa(ekind);
+    dnnl::graph::graph sdpa(ekind);
     sdpa.add_op(bmm1);
     sdpa.add_op(scale_div);
     sdpa.add_op(mask_add);
@@ -233,7 +398,7 @@ void bench_sdpa(engine::kind ekind, data_type dt, const sdpa_dims_t &p,
 
     // Display the results.
     double avg_time = (duration.count() - dur_first.count()) / runs;
-    std::cout << "runs: " << runs + 1 << " ";
+    std::cout << "graph runs: " << runs + 1 << "; ";
     std::cout << "avg_time: " << avg_time << " ms" << std::endl;
 
     return;
@@ -266,9 +431,14 @@ void sdpa_perf(engine::kind ekind, int argc, char **argv) {
         }
     }
 
-    bench_sdpa(ekind, data_type::f32, params, 4000.0 /*ms*/);
-    bench_sdpa(ekind, data_type::bf16, params, 4000.0 /*ms*/);
-    bench_sdpa(ekind, data_type::f16, params, 4000.0 /*ms*/);
+    bench_sdpa(ekind, logical_tensor::data_type::f32, params, 4000.0 /*ms*/);
+    bench_sdpa(ekind, logical_tensor::data_type::bf16, params, 4000.0 /*ms*/);
+    bench_sdpa(ekind, logical_tensor::data_type::f16, params, 4000.0 /*ms*/);
+
+    bench_sdpa_primitives(ekind, memory::data_type::f32, params, 4000.0 /*ms*/);
+    bench_sdpa_primitives(
+            ekind, memory::data_type::bf16, params, 4000.0 /*ms*/);
+    bench_sdpa_primitives(ekind, memory::data_type::f16, params, 4000.0 /*ms*/);
 }
 
 int main(int argc, char **argv) {
