42ebbb80406bf6614d71989d22ab8d03fdb27395
Pirogov, Vadim <vadim.o.pirogov@intel.com>
2024-06-25 07:14:37 -0700

    tests: benchdnn: fixed formatting in softmax.cpp

--- tests/benchdnn/softmax/softmax.cpp
+++ tests/benchdnn/softmax/softmax.cpp
@@ -236,7 +236,7 @@ void setup_cmp(compare::compare_t &cmp, const prb_t *prb, data_kind_t kind,
     // MIOpen and ACL softmax accumulate in F16, but oneDNN now expects accumulation in
     // F32, this partially reverts 6727bbe8. For more information on ACL softmax, see
     // https://github.com/oneapi-src/oneDNN/issues/1819
-    // Similarly, for bf16 on AArch64, the relaxed threshold is necessary due to 
+    // Similarly, for bf16 on AArch64, the relaxed threshold is necessary due to
     // minor accuracy drops observed compared to f32
     const float trh = trh_f32;
 #else
