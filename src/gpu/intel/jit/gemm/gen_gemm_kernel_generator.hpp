11fb657745f90822fbea135e9d9aea7bcbdf9710
Peter Caday <peter.caday@intel.com>
2024-07-01 14:45:34 -0700

    xe: jit: gemm: fix scale indexing in k remainder

--- src/gpu/intel/jit/gemm/gen_gemm_kernel_generator.hpp
+++ src/gpu/intel/jit/gemm/gen_gemm_kernel_generator.hpp
@@ -2487,7 +2487,7 @@ protected:
     void setupTeardownAccumulateSumSystolic(bool setup, Type Tother,
             const GEMMProblem &problem, const GEMMStrategy &strategy,
             GEMMState &state);
-    void outerProductRepackC(int x0, int xr0, int nx, int ha, int hb,
+    void outerProductRepackC(int x0, int xr0, int nx, int h,
             const GEMMProblem &problem, const GEMMStrategy &strategy,
             GEMMState &state);
 
