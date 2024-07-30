38c2226a8e8bd988b59571f89e954e2a7f5b0611
Hamza Butt <hamza.butt@arm.com>
2024-07-30 09:54:11 +0000

    cpu: aarch64 modularise indirect conv primitive (ACL backend)
    This can be shared by other convolution primitives.
    
    Signed-off-by: Hamza Butt <hamza.butt@arm.com>
    Change-Id: I80f4c12f8d0037d8502faccea5dc18aa925f44fd
    Reviewed-on: https://eu-gerrit-1.euhpc.arm.com/c/oncpuml/oneDNN/+/656297
    Tested-by: mdc-bbot
    Reviewed-by: Ryo Suzuki <ryo.suzuki@arm.com>

--- src/cpu/aarch64/acl_convolution_utils.hpp
+++ src/cpu/aarch64/acl_convolution_utils.hpp
@@ -17,6 +17,7 @@
 #ifndef CPU_AARCH64_ACL_CONVOLUTION_UTILS_HPP
 #define CPU_AARCH64_ACL_CONVOLUTION_UTILS_HPP
 
+#include <map>
 #include "acl_post_ops.hpp"
 #include "acl_utils.hpp"
 #include "cpu/cpu_convolution_pd.hpp"
@@ -80,6 +81,112 @@ status_t init_conf_wino(acl_conv_conf_t &acp, memory_desc_t &src_md,
         memory_desc_t &bias_md, const convolution_desc_t &cd,
         const primitive_attr_t &attr);
 } // namespace acl_convolution_utils
+
+// Keys are anonymous with local linkage. So deduce the type automagically.
+using conv_key_t = decltype(memory_tracking::names::key_gemm_tmp_buffer);
+
+template <typename op_t, typename post_ops_t>
+status_t init_scratchpad(op_t &conv, memory_tracking::registrar_t &scratchpad,
+        const std::map<int, conv_key_t> &conv_keys, engine_t *engine,
+        post_ops_t &post_ops, dnnl::impl::post_ops_t &attr_post_ops,
+        arm_compute::ActivationLayerInfo &act_info, bool &use_dst_acc_for_sum,
+        const dnnl::impl::memory_desc_t &dst_md) {
+
+    // Book temp mem.
+    const auto aux_mem_req = conv.workspace();
+    for (const auto &key : conv_keys) {
+        const auto id = key.first;
+        if (aux_mem_req[id].size > 0) {
+            scratchpad.book(key.second, aux_mem_req[id].size, 1,
+                    aux_mem_req[id].alignment, aux_mem_req[id].alignment);
+        }
+    }
+
+    CHECK(post_ops.init(engine, attr_post_ops, dst_md, act_info));
+    use_dst_acc_for_sum = post_ops.has_sum();
+
+    if (use_dst_acc_for_sum) {
+        const memory_desc_wrapper dst_d(&dst_md);
+        scratchpad.book(memory_tracking::names::key_generic_acc, dst_d.nelems(),
+                dst_d.data_type_size());
+    }
+
+    return status::success;
+}
+
+template <typename conv_obj_t, typename conv_pd_t, typename src_data_t,
+        typename wei_data_t = src_data_t, typename dst_data_t = src_data_t,
+        typename bia_data_t = src_data_t>
+status_t execute_forward_conv_acl(const exec_ctx_t &ctx,
+        conv_obj_t *acl_conv_obj, const conv_pd_t *pd,
+        const std::map<int, conv_key_t> &conv_keys) {
+
+    auto src_base = CTX_IN_MEM(const src_data_t *, DNNL_ARG_SRC);
+    auto wei_base = CTX_IN_MEM(const wei_data_t *, DNNL_ARG_WEIGHTS);
+
+    // import_memory() and free() methods do not allocate/free any additional
+    // memory, only acquire/release pointers.
+    arm_compute::Tensor src_tensor;
+    arm_compute::Tensor wei_tensor;
+    arm_compute::Tensor bia_tensor = nullptr;
+    arm_compute::Tensor dst_tensor;
+
+    auto const acp = pd->acp_;
+
+    src_tensor.allocator()->init(acp.src_tensor_info);
+    wei_tensor.allocator()->init(acp.wei_tensor_info);
+    dst_tensor.allocator()->init(acp.dst_tensor_info);
+
+    src_tensor.allocator()->import_memory(const_cast<src_data_t *>(src_base));
+    wei_tensor.allocator()->import_memory(const_cast<wei_data_t *>(wei_base));
+
+    const auto scratchpad = ctx.get_scratchpad_grantor();
+
+    // If we have an unfused sum post op, put the result in a scratchpad tensor.
+    // Result will be summed to the dst during acl_post_ops.execute
+    auto dst_base = acp.use_dst_acc_for_sum
+            ? scratchpad.get<void>(memory_tracking::names::key_generic_acc)
+            : CTX_OUT_MEM(dst_data_t *, DNNL_ARG_DST);
+    dst_tensor.allocator()->import_memory(dst_base);
+
+    if (acp.with_bias) {
+        auto bia_base = CTX_IN_MEM(const bia_data_t *, DNNL_ARG_BIAS);
+        bia_tensor.allocator()->init(acp.bia_tensor_info);
+        bia_tensor.allocator()->import_memory(
+                const_cast<bia_data_t *>(bia_base));
+    }
+
+    arm_compute::ITensorPack pack
+            = {{arm_compute::TensorType::ACL_SRC_0, &src_tensor},
+                    {arm_compute::TensorType::ACL_SRC_1, &wei_tensor},
+                    {arm_compute::TensorType::ACL_SRC_2, &bia_tensor},
+                    {arm_compute::TensorType::ACL_DST, &dst_tensor}};
+
+    // Get temp workspaces.
+    const auto aux_mem = acl_conv_obj->aux_mem_req;
+
+    // Hold onto tmp tensors while we need pack.
+    std::vector<arm_compute::Tensor> tmp_tensors(aux_mem.size());
+    for (const auto &key : conv_keys) {
+        const auto id = key.first;
+        if (aux_mem[id].size > 0) {
+            const auto info = arm_compute::TensorInfo(
+                    arm_compute::TensorShape(aux_mem[id].size), 1,
+                    arm_compute::DataType::U8);
+            auto buffer = scratchpad.get<void>(key.second);
+            tmp_tensors[id].allocator()->init(info, aux_mem[id].alignment);
+            tmp_tensors[id].allocator()->import_memory(buffer);
+            pack.add_tensor(aux_mem[id].slot, &tmp_tensors[id]);
+        }
+    }
+
+    acl_conv_obj->conv.run(pack);
+
+    void *dst = dst_tensor.buffer();
+    pd->post_ops.execute(ctx, dst);
+
+    return status::success;
+}
 
 template <typename conv_obj_t, typename conv_pd_t, typename src_data_t,
         typename wei_data_t = src_data_t, typename dst_data_t = src_data_t,
--- src/cpu/aarch64/acl_indirect_gemm_convolution.cpp
+++ src/cpu/aarch64/acl_indirect_gemm_convolution.cpp
@@ -48,79 +48,8 @@ status_t acl_indirect_gemm_convolution_fwd_t::init(engine_t *engine) {
 
 status_t acl_indirect_gemm_convolution_fwd_t::execute_forward(
         const exec_ctx_t &ctx) const {
-
-    using src_data_t = data_t;
-    using wei_data_t = data_t;
-    using dst_data_t = data_t;
-    using bia_data_t = data_t;
-
-    auto src_base = CTX_IN_MEM(const src_data_t *, DNNL_ARG_SRC);
-    auto wei_base = CTX_IN_MEM(const wei_data_t *, DNNL_ARG_WEIGHTS);
-
-    // import_memory() and free() methods do not allocate/free any additional
-    // memory, only acquire/release pointers.
-    arm_compute::Tensor src_tensor;
-    arm_compute::Tensor wei_tensor;
-    arm_compute::Tensor bia_tensor = nullptr;
-    arm_compute::Tensor dst_tensor;
-
-    auto const pd = this->pd();
-    auto const acp = pd->acp_;
-
-    src_tensor.allocator()->init(acp.src_tensor_info);
-    wei_tensor.allocator()->init(acp.wei_tensor_info);
-    dst_tensor.allocator()->init(acp.dst_tensor_info);
-
-    src_tensor.allocator()->import_memory(const_cast<src_data_t *>(src_base));
-    wei_tensor.allocator()->import_memory(const_cast<wei_data_t *>(wei_base));
-
-    const auto scratchpad = ctx.get_scratchpad_grantor();
-
-    // If we have an unfused sum post op, put the result in a scratchpad tensor.
-    // Result will be summed to the dst during acl_post_ops.execute
-    auto dst_base = acp.use_dst_acc_for_sum
-            ? scratchpad.get<void>(memory_tracking::names::key_generic_acc)
-            : CTX_OUT_MEM(dst_data_t *, DNNL_ARG_DST);
-    dst_tensor.allocator()->import_memory(dst_base);
-
-    if (acp.with_bias) {
-        auto bia_base = CTX_IN_MEM(const bia_data_t *, DNNL_ARG_BIAS);
-        bia_tensor.allocator()->init(acp.bia_tensor_info);
-        bia_tensor.allocator()->import_memory(
-                const_cast<bia_data_t *>(bia_base));
-    }
-
-    arm_compute::ITensorPack pack
-            = {{arm_compute::TensorType::ACL_SRC_0, &src_tensor},
-                    {arm_compute::TensorType::ACL_SRC_1, &wei_tensor},
-                    {arm_compute::TensorType::ACL_SRC_2, &bia_tensor},
-                    {arm_compute::TensorType::ACL_DST, &dst_tensor}};
-
-    // Get temp workspaces.
-    const auto aux_mem = acl_obj_->aux_mem_req;
-
-    // Hold onto tmp tensors while we need pack.
-    std::vector<arm_compute::Tensor> tmp_tensors(aux_mem.size());
-    for (const auto &key : indirect_conv_keys) {
-        const auto id = key.first;
-        if (aux_mem[id].size > 0) {
-            const auto info = arm_compute::TensorInfo(
-                    arm_compute::TensorShape(aux_mem[id].size), 1,
-                    arm_compute::DataType::U8);
-            auto buffer = scratchpad.get<void>(key.second);
-            tmp_tensors[id].allocator()->init(info, aux_mem[id].alignment);
-            tmp_tensors[id].allocator()->import_memory(buffer);
-            pack.add_tensor(aux_mem[id].slot, &tmp_tensors[id]);
-        }
-    }
-
-    acl_obj_->conv.run(pack);
-
-    void *dst = dst_tensor.buffer();
-
-    pd->post_ops.execute(ctx, dst);
-
-    return status::success;
+    return execute_forward_conv_acl<acl_obj_t<Op>, pd_t, data_t>(
+            ctx, acl_obj_.get(), pd(), indirect_conv_keys);
 }
 
 status_t acl_indirect_gemm_convolution_fwd_t::create_resource(
@@ -189,28 +118,11 @@ status_t acl_indirect_gemm_convolution_fwd_t::pd_t::init(engine_t *engine) {
             &acp_.dst_tensor_info,
             arm_compute::Conv2dInfo(acp_.padstride_info, acp_.dilation_info,
                     acp_.act_info, acp_.fast_math, 1, acp_.weights_info));
-    const auto aux_mem_req = conv.workspace();
+
     auto scratchpad = scratchpad_registry().registrar();
-
-    // Book temp mem.
-    for (const auto &key : indirect_conv_keys) {
-        const auto id = key.first;
-        if (aux_mem_req[id].size > 0) {
-            scratchpad.book(key.second, aux_mem_req[id].size, 1,
-                    aux_mem_req[id].alignment, aux_mem_req[id].alignment);
-        }
-    }
-
-    CHECK(post_ops.init(engine, attr_.post_ops_, dst_md_, acp_.act_info));
-    acp_.use_dst_acc_for_sum = post_ops.has_sum();
-
-    if (acp_.use_dst_acc_for_sum) {
-        const memory_desc_wrapper dst_d(&dst_md_);
-        scratchpad.book(memory_tracking::names::key_generic_acc, dst_d.nelems(),
-                dst_d.data_type_size());
-    }
-
-    return status::success;
+    return init_scratchpad(conv, scratchpad, indirect_conv_keys, engine,
+            post_ops, attr_.post_ops_, acp_.act_info, acp_.use_dst_acc_for_sum,
+            dst_md_);
 }
 
 } // namespace aarch64
