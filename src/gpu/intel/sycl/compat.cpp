6c8c5095d6b476c3313ad13a85c04a1793a66548
Roy Oursler <roy.j.oursler@intel.com>
2024-08-07 09:59:32 -0700

    xe: sycl: fix cl_kernel resource leak

--- src/gpu/intel/sycl/compat.cpp
+++ src/gpu/intel/sycl/compat.cpp
@@ -59,7 +59,8 @@ status_t make_kernel(std::unique_ptr<::sycl::kernel> &sycl_kernel,
         CHECK(xpu::ocl::create_program(ocl_program, sycl_engine->ocl_device(),
                 sycl_engine->ocl_context(), binary));
         cl_int err;
-        cl_kernel ocl_kernel = clCreateKernel(ocl_program, kernel_name, &err);
+        xpu::ocl::wrapper_t<cl_kernel> ocl_kernel
+                = clCreateKernel(ocl_program, kernel_name, &err);
         OCL_CHECK(err);
         sycl_kernel = utils::make_unique<::sycl::kernel>(
                 ::sycl::make_kernel<::sycl::backend::opencl>(
