71f56550e5efefdc48b014a20e05f749b7be948f
EC2 Default User <ec2-user@ip-10-1-35-49.us-east-2.compute.internal>
2024-08-07 16:39:01 +0000

    cpu: x64: improve memory management safety for jit_avx512_core_x8s8s32x_deconvolution.hpp
    on-behalf-of: @permanence-ai <github-ai@permanence.ai>

--- src/cpu/x64/jit_avx512_core_x8s8s32x_deconvolution.hpp
+++ src/cpu/x64/jit_avx512_core_x8s8s32x_deconvolution.hpp
@@ -18,6 +18,7 @@
 #define CPU_X64_JIT_AVX512_CORE_X8S8S32X_DECONVOLUTION_HPP
 
 #include <functional>
+#include <memory>
 #include <vector>
 
 #include "common/c_types_map.hpp"
@@ -198,16 +199,19 @@ struct _jit_avx512_core_x8s8s32x_deconv_fwd_kernel {
         int ch_block = ajcp.is_depthwise ? ajcp.ch_block : ajcp.ic_block;
         switch (ch_block) {
             case 16:
-                kernel_ = new jit_avx512_core_x8s8s32x_deconv_fwd_kernel<
-                        Xbyak::Zmm>(ajcp, attr, dst_md);
+                kernel_ = utils::make_unique<
+                        jit_avx512_core_x8s8s32x_deconv_fwd_kernel<Xbyak::Zmm>>(
+                        ajcp, attr, dst_md);
                 return;
             case 8:
-                kernel_ = new jit_avx512_core_x8s8s32x_deconv_fwd_kernel<
-                        Xbyak::Ymm>(ajcp, attr, dst_md);
+                kernel_ = utils::make_unique<
+                        jit_avx512_core_x8s8s32x_deconv_fwd_kernel<Xbyak::Ymm>>(
+                        ajcp, attr, dst_md);
                 return;
             case 4:
-                kernel_ = new jit_avx512_core_x8s8s32x_deconv_fwd_kernel<
-                        Xbyak::Xmm>(ajcp, attr, dst_md);
+                kernel_ = utils::make_unique<
+                        jit_avx512_core_x8s8s32x_deconv_fwd_kernel<Xbyak::Xmm>>(
+                        ajcp, attr, dst_md);
                 return;
             default: assert(!"invalid channel blocking");
         }
@@ -218,7 +222,7 @@ struct _jit_avx512_core_x8s8s32x_deconv_fwd_kernel {
         return status::out_of_memory;
     }
 
-    ~_jit_avx512_core_x8s8s32x_deconv_fwd_kernel() { delete kernel_; }
+    ~_jit_avx512_core_x8s8s32x_deconv_fwd_kernel() = default;
 
     void operator()(const jit_deconv_call_s *p) const { (*kernel_)(p); }
 
@@ -235,8 +239,7 @@ struct _jit_avx512_core_x8s8s32x_deconv_fwd_kernel {
             const jit_conv_conf_t &jcp, const primitive_attr_t &attr);
 
 private:
-    DNNL_DISALLOW_COPY_AND_ASSIGN(_jit_avx512_core_x8s8s32x_deconv_fwd_kernel);
-    jit_generator *kernel_;
+    std::unique_ptr<jit_generator> kernel_;
 };
 
 struct jit_avx512_core_x8s8s32x_deconvolution_fwd_t : public primitive_t {
