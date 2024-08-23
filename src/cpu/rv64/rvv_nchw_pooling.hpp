06c77b1022324d762ec725155ba49da9ff128543
Umar Arshad <umar.arshad@intel.com>
2024-08-23 13:30:49 -0700

    common: Update copyright to pass CI checks

--- src/cpu/rv64/rvv_nchw_pooling.hpp
+++ src/cpu/rv64/rvv_nchw_pooling.hpp
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright 2023 Intel Corporation
+* Copyright 2024 Intel Corporation
 * Copyright 2023 KNS Group LLC (YADRO)
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
