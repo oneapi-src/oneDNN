c5d191ebafc6554255805dc5a1dc56a5be1658a0
syurkevi <stephen.yurkevitch@intel.com>
2024-08-28 12:41:44 -0700

    gpu: update max_wg_size calculation

--- src/gpu/intel/compute/device_info.cpp
+++ src/gpu/intel/compute/device_info.cpp
@@ -165,11 +165,14 @@ int device_info_t::max_subgroup_size(data_type_t type) const {
 
 size_t device_info_t::max_wg_size(
         bool large_grf_mode, size_t subgroup_size) const {
+    size_t device_max_wg_size
+            = large_grf_mode ? max_wg_size_ / 2 : max_wg_size_;
     if (subgroup_size > 0) {
-        return threads_per_eu(gpu_arch_, large_grf_mode) * max_eus_per_wg_
-                * subgroup_size;
+        size_t sg_max_wg_size = threads_per_eu(gpu_arch_, large_grf_mode)
+                * max_eus_per_wg_ * subgroup_size;
+        return std::min(device_max_wg_size, sg_max_wg_size);
     }
-    return large_grf_mode ? max_wg_size_ / 2 : max_wg_size_;
+    return device_max_wg_size;
 }
 
 int device_info_t::threads_per_eu(gpu_arch_t gpu_arch, bool large_grf_mode) {
