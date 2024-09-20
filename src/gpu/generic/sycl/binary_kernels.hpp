878a35ea840590f4e7abd104cd3c63f0c09a9f3a
Tadej Ciglarič <tadej.ciglaric@codeplay.com>
2024-09-20 13:19:06 +0200

    generic: sycl: bugfix post ops with blocked formats

--- src/gpu/generic/sycl/binary_kernels.hpp
+++ src/gpu/generic/sycl/binary_kernels.hpp
@@ -141,9 +141,10 @@ struct binary_kernel_vec_t {
 
                     auto acc = compute_alg_n(src0, src1, conf_.alg_kind);
 
+                    int dst_idx = dst_mem.md().off_v(off_dst);
                     acc = conf_.post_ops.apply(
-                            acc, dst_, idx, po_args_, off_dst);
-                    dst_mem.store_md(acc, off_dst);
+                            acc, dst_, dst_idx, po_args_, off_dst);
+                    dst_mem.store(acc, dst_idx);
                 }
             }
         }
