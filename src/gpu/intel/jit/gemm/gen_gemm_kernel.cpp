c7895dbbb58a7f4b9bdd37391d5358663a006429
Daniel Youssif <daniel.youssif@intel.com>
2024-10-02 13:23:38 -0700

    xe: jit: gemm: set wgK properly for kInterleave

--- src/gpu/intel/jit/gemm/gen_gemm_kernel.cpp
+++ src/gpu/intel/jit/gemm/gen_gemm_kernel.cpp
@@ -243,6 +243,7 @@ status_t gen_gemm_kernel_desc_t::finalize(const char *tags) {
 
     strategy_.kInterleaveChunk
             = std::min(strategy_.kInterleaveChunk, (int)aux_params_.k0);
+    if (strategy_.kInterleave) aux_params_.wgK = strategy_.wg[LoopK];
     update_driver_info();
 
 #ifdef DNNL_DEV_MODE
