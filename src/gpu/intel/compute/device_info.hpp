7a1712fee28a86f61fce09f986b3dc107899ba03
Simon Ewing <simon.ewing@intel.com>
2024-10-18 13:58:06 -0700

    xe: add integrated query to device_info

--- src/gpu/intel/compute/device_info.hpp
+++ src/gpu/intel/compute/device_info.hpp
@@ -199,6 +199,7 @@ public:
     gpu_arch_t gpu_arch() const { return gpu_arch_; }
     int gpu_product_family() const { return gpu_product_family_; }
     int stepping_id() const { return stepping_id_; }
+    bool is_integrated() const;
     uint32_t ip_version() const { return ip_version_; }
     int max_eus_per_wg() const { return max_eus_per_wg_; }
     static int max_eus_per_wg(gpu_arch_t gpu_arch);
