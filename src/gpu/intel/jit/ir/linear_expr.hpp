8ec60356698b7c921058cb0ef5fb440f74dbfb83
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2024-12-09 15:47:09 -0800

    xe: ir: support initial offsets in split_to_linear()

--- src/gpu/intel/jit/ir/linear_expr.hpp
+++ src/gpu/intel/jit/ir/linear_expr.hpp
@@ -57,7 +57,8 @@ expr_t simplify_linear_mod(const expr_t &e, int64_t factor);
 //     init = expr(idxs[i] = 0)
 //     incs[i] = expr(idxs[i] + 1) - expr(idx[i]).
 void split_to_linear(const expr_t &expr, const std::vector<expr_t> &idxs,
-        expr_t &init, std::vector<expr_t> &incs);
+        const std::vector<expr_t> &start, expr_t &init,
+        std::vector<expr_t> &incs);
 
 } // namespace jit
 } // namespace intel
