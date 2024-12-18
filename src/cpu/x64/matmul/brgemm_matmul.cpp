38ff3a7da8899b6ab984ee2940a936bc4d984ec2
Alexey Makarevich <alexey.makarevich@intel.com>
2024-12-18 20:43:11 +0000

    [rls-v3.6 backport] cpu: x64: matmul/brgemm: enable f16 dst dt for int8 matmul  (#2282)

--- src/cpu/x64/matmul/brgemm_matmul.cpp
+++ src/cpu/x64/matmul/brgemm_matmul.cpp
@@ -52,7 +52,7 @@ status_t brgemm_matmul_t<isa>::pd_t::init(engine_t *engine) {
 
     const bool is_f32 = everyone_is(f32, src_dt, wei_dt, dst_dt);
     const bool is_int8 = one_of(src_dt, u8, s8) && wei_dt == s8
-            && one_of(dst_dt, u8, s8, s32, f32, bf16);
+            && one_of(dst_dt, u8, s8, s32, f32, f16, bf16);
     const bool is_f8 = one_of(src_dt, f8_e5m2, f8_e4m3)
             && one_of(wei_dt, f8_e5m2, f8_e4m3)
             && one_of(dst_dt, f32, f16, bf16, f8_e5m2, f8_e4m3);
