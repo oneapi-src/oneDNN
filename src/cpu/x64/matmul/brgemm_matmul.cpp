cf0c2bd9c3e165ffb54566974d815b88db79e1d7
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2024-12-19 09:48:16 -0800

    cpu: x64: matmul: add f32:bf16 support on avx512_core and avx2

--- src/cpu/x64/matmul/brgemm_matmul.cpp
+++ src/cpu/x64/matmul/brgemm_matmul.cpp
@@ -62,6 +62,8 @@ status_t brgemm_matmul_t<isa>::pd_t::init(engine_t *engine) {
             = everyone_is(f16, src_dt, wei_dt) && one_of(dst_dt, f16, f32);
     const bool is_f32_f16
             = src_dt == f32 && wei_dt == f16 && one_of(dst_dt, f16, f32);
+    const bool is_f32_bf16
+            = src_dt == f32 && wei_dt == bf16 && one_of(dst_dt, bf16, f32);
     const bool is_bf16_with_int_wei = src_dt == bf16
             && one_of(wei_dt, s8, u8, s4, u4) && one_of(dst_dt, bf16, f32);
     const bool is_f16_with_int_wei = src_dt == f16
@@ -121,7 +123,7 @@ status_t brgemm_matmul_t<isa>::pd_t::init(engine_t *engine) {
             = [&]() -> bool { return attr()->zero_points_.common(); };
     const bool problem_dt_correct
             = one_of(true, is_int8, is_f8, is_bf16, is_f32, is_f16, is_f32_f16,
-                    is_bf16_with_int_wei, is_f16_with_int_wei);
+                    is_f32_bf16, is_bf16_with_int_wei, is_f16_with_int_wei);
 
     auto src_d = memory_desc_wrapper(src_md_);
     auto weights_d = memory_desc_wrapper(weights_md_);
@@ -161,7 +163,8 @@ status_t brgemm_matmul_t<isa>::pd_t::init(engine_t *engine) {
 
     // f32:f16 configuration on AVX2 doesn't support tails with proper
     // instruction sequence in copy routines. Anchor: F32_F16_AVX2_NO_TAIL.
-    VDISPATCH_MATMUL(IMPLICATION(is_f32_f16 && isa == avx2, bgmmc_.N % 8 == 0),
+    VDISPATCH_MATMUL(IMPLICATION((is_f32_f16 || is_f32_bf16) && isa == avx2,
+                             bgmmc_.N % 8 == 0),
             "unsupported configuration");
 
     const float alpha = 1.0;
@@ -181,7 +184,7 @@ status_t brgemm_matmul_t<isa>::pd_t::init(engine_t *engine) {
     const auto backup_isa = is_amx && bgmmc_.is_runtime_M && !is_s8s8
             ? (is_f16 || is_f32_f16 || is_f16_with_int_wei
                             ? avx512_core_fp16
-                            : (is_bf16 || is_bf16_with_int_wei
+                            : (is_bf16 || is_f32_bf16 || is_bf16_with_int_wei
                                             ? avx512_core_bf16
                                             : (is_int8 ? avx512_core_vnni
                                                        : avx512_core)))
