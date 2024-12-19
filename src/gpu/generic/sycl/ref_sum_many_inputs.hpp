9fed40d30e1f85f781a96d867182caa5bf439d20
Stefan Palicki <stefan.palicki@intel.com>
2024-12-19 03:31:22 -0800

    gpu: generic: sycl: fix typos in VDISPATCH_SUM calls (#2287)

--- src/gpu/generic/sycl/ref_sum_many_inputs.hpp
+++ src/gpu/generic/sycl/ref_sum_many_inputs.hpp
@@ -46,11 +46,11 @@ struct ref_sum_many_inputs_t : public gpu::generic::sycl::primitive_t {
 
             const int n = n_inputs();
             VDISPATCH_SUM_SC(set_default_params(), VERBOSE_UNSUPPORTED_TAG);
-            VDISPATCH_SUM_SC(
+            VDISPATCH_SUM(
                     attr()->has_default_values(), VERBOSE_UNSUPPORTED_ATTR);
             // prevent inf recursion
-            VDISPATCH_SUM_SC(n > DNNL_REF_SUM_MAX_NUM_TENSORS,
-                    VERBOSE_BAD_PARAM, "n_inputs");
+            VDISPATCH_SUM(n > DNNL_REF_SUM_MAX_NUM_TENSORS, VERBOSE_BAD_PARAM,
+                    "n_inputs");
 
             // the first kernel handles up to 8 inputs and remaining ones up to 7
             const int n_kernels = n == 1
