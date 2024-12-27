36151d916c795fb668cc445c981b8cb9706338f2
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2024-12-27 13:06:57 -0800

    xe: conv_v2: update planner logic

--- src/gpu/intel/jit/v2/conv/planner/search.hpp
+++ src/gpu/intel/jit/v2/conv/planner/search.hpp
@@ -18,6 +18,7 @@
 #define GPU_INTEL_JIT_V2_CONV_PLANNER_SEARCH_HPP
 
 #include "gpu/intel/jit/v2/conv/planner/bench.hpp"
+#include "gpu/intel/jit/v2/conv/planner/planner.hpp"
 
 namespace dnnl {
 namespace impl {
@@ -31,8 +32,7 @@ class kernel_desc_t;
 
 namespace planner {
 
-void search(const bench_manager_t &bench_mger, const kernel_desc_t &desc);
-void auto_search(const bench_manager_t &bench_mger);
+void search(const bench_manager_t &bench_mger, const planner_params_t &params);
 
 } // namespace planner
 } // namespace conv
