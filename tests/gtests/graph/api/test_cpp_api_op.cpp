28f56e69aecfe25798d2b221533b011b7bd1c0a9
Gu, Yonghao <yonghao.gu@intel.com>
2025-01-02 09:16:36 +0000

    gtests: graph: api: add  gtest for GenIndex and GreaterEqual

--- tests/gtests/graph/api/test_cpp_api_op.cpp
+++ tests/gtests/graph/api/test_cpp_api_op.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2020-2024 Intel Corporation
+* Copyright 2020-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -114,6 +114,8 @@ TEST(APIOp, CreateAllOps) {
             op::kind::Select,
             op::kind::Pow,
             op::kind::GroupNorm,
+            op::kind::GenIndex,
+            op::kind::GreaterEqual
     };
     // clang-format on
 
