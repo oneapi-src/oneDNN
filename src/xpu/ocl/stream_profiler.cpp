93fe2cf863ea4a6fbf0e96d5f1f65f0f53e3d81d
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2025-01-06 14:40:11 -0800

    xpu: ocl, sycl: profiler: add per-kernel time query

--- src/xpu/ocl/stream_profiler.cpp
+++ src/xpu/ocl/stream_profiler.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2023-2024 Intel Corporation
+* Copyright 2023-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -35,7 +35,12 @@ namespace ocl {
 status_t stream_profiler_t::get_info(profiling_data_kind_t data_kind,
         int *num_entries, uint64_t *data) const {
     if (!num_entries) return status::invalid_arguments;
+    bool is_per_kernel = (data_kind == profiling_data_kind::time_per_kernel);
     if (!data) {
+        if (is_per_kernel) {
+            *num_entries = (int)events_.size();
+            return status::success;
+        }
         std::unordered_set<uint64_t> seen;
         for (auto &ev : events_)
             seen.insert(ev.stamp);
@@ -44,8 +49,8 @@ status_t stream_profiler_t::get_info(profiling_data_kind_t data_kind,
     }
 
     std::map<uint64_t, xpu::stream_profiler_t::entry_t> stamp2entry;
+    int idx = 0;
     for (auto &ev : events_) {
-        auto &entry = stamp2entry[ev.stamp];
         const xpu::ocl::event_t &ocl_event
                 = *utils::downcast<xpu::ocl::event_t *>(ev.event.get());
         cl_ulong beg, end;
@@ -54,6 +59,11 @@ status_t stream_profiler_t::get_info(profiling_data_kind_t data_kind,
                 CL_PROFILING_COMMAND_START, sizeof(beg), &beg, nullptr));
         OCL_CHECK(clGetEventProfilingInfo(ocl_event[0].get(),
                 CL_PROFILING_COMMAND_END, sizeof(end), &end, nullptr));
+        if (is_per_kernel) {
+            data[idx++] = static_cast<uint64_t>(end - beg);
+            continue;
+        }
+        auto &entry = stamp2entry[ev.stamp];
         entry.min_nsec = std::min(entry.min_nsec, beg);
         entry.max_nsec = std::max(entry.max_nsec, end);
         const auto *gpu_stream
@@ -61,6 +71,7 @@ status_t stream_profiler_t::get_info(profiling_data_kind_t data_kind,
         entry.freq += gpu_stream->get_freq(*ev.event);
         entry.kernel_count++;
     }
+    if (is_per_kernel) return status::success;
     return xpu::stream_profiler_t::get_info_impl(stamp2entry, data_kind, data);
 }
 
--- src/xpu/sycl/stream_profiler.cpp
+++ src/xpu/sycl/stream_profiler.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2023-2024 Intel Corporation
+* Copyright 2023-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -33,7 +33,12 @@ status_t stream_profiler_t::get_info(profiling_data_kind_t data_kind,
         int *num_entries, uint64_t *data) const {
     using namespace ::sycl::info;
     if (!num_entries) return status::invalid_arguments;
+    bool is_per_kernel = (data_kind == profiling_data_kind::time_per_kernel);
     if (!data) {
+        if (is_per_kernel) {
+            *num_entries = (int)events_.size();
+            return status::success;
+        }
         std::unordered_set<uint64_t> seen;
         for (auto &ev : events_)
             seen.insert(ev.stamp);
@@ -42,6 +47,7 @@ status_t stream_profiler_t::get_info(profiling_data_kind_t data_kind,
     }
 
     std::map<uint64_t, stream_profiler_t::entry_t> stamp2entry;
+    int idx = 0;
     for (auto &ev : events_) {
         const xpu::sycl::event_t &sycl_event
                 = *utils::downcast<xpu::sycl::event_t *>(ev.event.get());
@@ -51,11 +57,16 @@ status_t stream_profiler_t::get_info(profiling_data_kind_t data_kind,
                           .get_profiling_info<event_profiling::command_start>();
         auto end = sycl_event[0]
                            .get_profiling_info<event_profiling::command_end>();
+        if (is_per_kernel) {
+            data[idx++] = static_cast<uint64_t>(end - beg);
+            continue;
+        }
         auto &entry = stamp2entry[ev.stamp];
         entry.min_nsec = std::min(entry.min_nsec, beg);
         entry.max_nsec = std::max(entry.max_nsec, end);
         entry.kernel_count++;
     }
+    if (is_per_kernel) return status::success;
     return xpu::stream_profiler_t::get_info_impl(stamp2entry, data_kind, data);
 }
 
