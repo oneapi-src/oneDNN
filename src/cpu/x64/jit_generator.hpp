74643e46da6adfc94abe8877b3af5965dd29a9d4
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2025-01-06 09:36:36 -0800

    cpu: x64: jit_generator: assert on valid avx512 isa for evex compression

--- src/cpu/x64/jit_generator.hpp
+++ src/cpu/x64/jit_generator.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2016-2024 Intel Corporation
+* Copyright 2016-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -289,6 +289,8 @@ public:
     template <typename T>
     Xbyak::Address EVEX_compress_addr(
             Xbyak::Reg64 base, T raw_offt, bool bcast = false) {
+        assert(is_valid_isa(avx512_core));
+
         using Xbyak::Address;
         using Xbyak::Reg64;
         using Xbyak::RegExp;
