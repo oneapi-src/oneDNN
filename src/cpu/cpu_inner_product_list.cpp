e9af4614acfd5934db7ef51e038516f44c83d21e
Denis Samoilov <denis.samoylov@intel.com>
2025-01-08 14:46:02 -0800

    cpu: x64: enable matmul-based IP for bwd_d

--- src/cpu/cpu_inner_product_list.cpp
+++ src/cpu/cpu_inner_product_list.cpp
@@ -126,6 +126,7 @@ const std::map<pk_dt_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map()
         BRGEMM_FP8_FWD_IP(f8_e4m3, f8_e4m3, f8_e4m3),
 
         {{backward_data, f32, f32, f32}, REG_BWD_PK({
+            CPU_INSTANCE_X64(matmul_inner_product_bwd_data_t)
             CPU_INSTANCE_AMX(brgemm_inner_product_bwd_data_t<avx512_core_amx>) // bf32
             CPU_INSTANCE_AVX512(brgemm_inner_product_bwd_data_t<avx512_core>)
             CPU_INSTANCE_AVX2(brgemm_inner_product_bwd_data_t<avx2>)
@@ -134,6 +135,7 @@ const std::map<pk_dt_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map()
             nullptr,
         })},
         {{backward_data, f32, bf16, bf16}, REG_BWD_PK({
+            CPU_INSTANCE_X64(matmul_inner_product_bwd_data_t)
             CPU_INSTANCE_AMX(brgemm_inner_product_bwd_data_t<avx512_core_amx>)
             CPU_INSTANCE_AVX512(brgemm_inner_product_bwd_data_t<avx512_core_bf16>)
             CPU_INSTANCE_AVX512(gemm_bf16_inner_product_bwd_data_t<f32>)
@@ -141,6 +143,7 @@ const std::map<pk_dt_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map()
             nullptr,
         })},
         {{backward_data, bf16, bf16, bf16}, REG_BWD_PK({
+            CPU_INSTANCE_X64(matmul_inner_product_bwd_data_t)
             CPU_INSTANCE_AMX(brgemm_inner_product_bwd_data_t<avx512_core_amx>)
             CPU_INSTANCE_AVX512(brgemm_inner_product_bwd_data_t<avx512_core_bf16>)
             CPU_INSTANCE_AVX512(gemm_bf16_inner_product_bwd_data_t<bf16>)
@@ -148,12 +151,14 @@ const std::map<pk_dt_impl_key_t, std::vector<impl_list_item_t>> &impl_list_map()
             nullptr,
         })},
         {{backward_data, f32, f16, f16}, REG_BWD_PK({
+            CPU_INSTANCE_X64(matmul_inner_product_bwd_data_t)
             CPU_INSTANCE_AMX(brgemm_inner_product_bwd_data_t<avx512_core_amx_fp16>)
             CPU_INSTANCE_AVX512(brgemm_inner_product_bwd_data_t<avx512_core_fp16>)
             CPU_INSTANCE(ref_inner_product_bwd_data_t)
             nullptr,
         })},
         {{backward_data, f16, f16, f16}, REG_BWD_PK({
+            CPU_INSTANCE_X64(matmul_inner_product_bwd_data_t)
             CPU_INSTANCE_AMX(brgemm_inner_product_bwd_data_t<avx512_core_amx_fp16>)
             CPU_INSTANCE_AVX512(brgemm_inner_product_bwd_data_t<avx512_core_fp16>)
             CPU_INSTANCE(ref_inner_product_bwd_data_t)
