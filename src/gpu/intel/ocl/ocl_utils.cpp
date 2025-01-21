cd31998f50779e6a20d238c20512430de006ac74
Kassen, Andrew <andrew.kassen@intel.com>
2025-01-21 10:59:37 -0800

    xe: ocl: strengthen sycl header guard

--- src/gpu/intel/ocl/ocl_utils.cpp
+++ src/gpu/intel/ocl/ocl_utils.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2024 Intel Corporation
+* Copyright 2019-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@
 #include "gpu/intel/ocl/ocl_utils.hpp"
 #include "xpu/ocl/utils.hpp"
 
-#if __has_include(<sycl/sycl.hpp>)
+#ifdef DNNL_WITH_SYCL
 #include "gpu/intel/sycl/engine.hpp"
 #endif
 
