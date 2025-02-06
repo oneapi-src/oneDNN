5dc2e69f319ab59520c9096436488c11ab86fd93
Roy Oursler <roy.j.oursler@intel.com>
2025-02-06 13:51:41 -0800

    xe: jit: gemm: move catalog generation to its own compilation unit
    GCC 11 can take upwards of 15 minutes to compile the gemm catalog. This patch
    moves kcatalog generation to its own compilation unit and removes -fvar-tracking
    to limit compile times.

---
+++ src/gpu/intel/jit/gemm/gen_gemm_kernel_db.cpp
@@ -0,0 +1,38 @@
+/*******************************************************************************
+* Copyright 2025 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include "gpu/intel/jit/gemm/gen_gemm_kernel_db.hpp"
+
+namespace dnnl {
+namespace impl {
+namespace gpu {
+namespace intel {
+namespace jit {
+
+#define _CATALOG_ gemm_catalog
+#include "selector/db/kernel.db"
+#undef _CATALOG_
+
+const kcatalog::Catalog &catalog() {
+    static const kcatalog::Catalog c = gemm_catalog;
+    return c;
+};
+
+} // namespace jit
+} // namespace intel
+} // namespace gpu
+} // namespace impl
+} // namespace dnnl
