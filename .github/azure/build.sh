a038a718f4feb979510b6ba1b29aee97de15470b
Pirogov, Vadim <vadim.o.pirogov@intel.com>
2025-02-10 08:56:07 -0800

    ci: test azure trigger

--- .github/azure/build.sh
+++ .github/azure/build.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #===============================================================================
-
+# Test change for Azure trigger
 
 while [[ $# -gt 0 ]]; do
     key="$1"
