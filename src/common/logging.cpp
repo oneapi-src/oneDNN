e8901c56a3358883ac34fb15f1c5adc7695ee987
Kassen, Andrew <andrew.kassen@intel.com>
2025-02-10 10:49:14 -0800

    common: initialize members in initializer list

--- src/common/logging.cpp
+++ src/common/logging.cpp
@@ -23,15 +23,14 @@
 namespace dnnl {
 namespace impl {
 
-log_manager_t::log_manager_t() {
-
+log_manager_t::log_manager_t()
+    : logfile_path_(getenv_string_user("VERBOSE_LOGFILE"))
     // enables logging as well as printing to stdout
-    console_flag_ = getenv_int_user("VERBOSE_LOG_WITH_CONSOLE", 0);
+    , console_flag_(getenv_int_user("VERBOSE_LOG_WITH_CONSOLE", 0)) {
 
     // logging is automatically disabled when no filepath is provided by
     // DNNL_VERBOSE_LOGFILE
     // in this case, we fall back to printing to stdout
-    logfile_path_ = getenv_string_user("VERBOSE_LOGFILE");
     if (logfile_path_.empty()) {
         console_flag_ = true;
         return;
