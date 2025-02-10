76a41226bb8f18e2a9f174dbe3170d89dec2248e
Renato Arantes <renato.arantes@arm.com>
2025-02-10 17:27:33 +0000

    src: cpu: aarch64: Enable JIT fp32 -> fp16 reorder

--- src/cpu/reorder/cpu_reorder_regular_f32_f16.cpp
+++ src/cpu/reorder/cpu_reorder_regular_f32_f16.cpp
@@ -1,5 +1,6 @@
 /*******************************************************************************
 * Copyright 2020-2024 Intel Corporation
+* Copyright 2025 Arm Ltd. and affiliates
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -29,6 +30,8 @@ const impl_list_map_t &regular_f32_f16_impl_list_map() {
             DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_uni_reorder_direct_copy_t))
             DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_blk_reorder_t))
             DNNL_X64_ONLY(CPU_REORDER_INSTANCE(x64::jit_uni_reorder_t))
+
+            DNNL_AARCH64_ONLY(CPU_REORDER_INSTANCE(aarch64::jit_uni_reorder_t))
 
             REG_SR(f32, any, f16, any, fmt_order::any, spec::reference)
 
