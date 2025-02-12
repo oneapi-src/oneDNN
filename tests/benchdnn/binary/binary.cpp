829fc5ebd7a706d1fc4b0c0035e05eab75cd7058
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2025-02-12 10:54:49 -0800

    benchdnn: move memory check into a dedicated check func

--- tests/benchdnn/binary/binary.cpp
+++ tests/benchdnn/binary/binary.cpp
@@ -297,6 +297,9 @@ int createit(std::vector<benchdnn_dnnl_wrapper_t<dnnl_primitive_t>> &v_prim,
 
 int checkit(std::vector<benchdnn_dnnl_wrapper_t<dnnl_primitive_t>> &v_prim,
         const prb_t *prb, res_t *res) {
+    if (has_bench_mode_bit(mode_bit_t::exec)) {
+        SAFE(check_total_size(res), WARN);
+    }
     if (has_bench_mode_bit(mode_bit_t::corr)) {
         SAFE(check_caches(v_prim[0], prb, res), WARN);
     }
