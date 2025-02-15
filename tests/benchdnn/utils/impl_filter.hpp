78fa67a465d343f628cefa77a6652a996a6dd4b3
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2025-02-14 21:27:13 -0800

    benchdnn: prim_ref impl filter ignores values from global-(skip-)impl

--- tests/benchdnn/utils/impl_filter.hpp
+++ tests/benchdnn/utils/impl_filter.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2024 Intel Corporation
+* Copyright 2024-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -21,18 +21,29 @@
 #include <vector>
 
 struct impl_filter_t {
-    impl_filter_t() {};
-    impl_filter_t(const std::vector<std::string> &impl_names, bool use_impl)
-        : impl_names_(impl_names), use_impl_(use_impl) {}
+    impl_filter_t() = default;
+    impl_filter_t(const std::vector<std::string> &impl_names, bool use_impl,
+            bool respect_global_filter)
+        : impl_names_(impl_names)
+        , use_impl_(use_impl)
+        , respect_global_filter_(respect_global_filter) {}
 
     bool is_def() const { return impl_names_.empty(); }
 
     const std::vector<std::string> &get_names() const { return impl_names_; }
     const bool use_impl() const { return use_impl_; }
+    const bool respect_global_filter() const { return respect_global_filter_; }
 
 private:
     std::vector<std::string> impl_names_;
-    bool use_impl_; // `true` to `--impl`, `false` to `--skip-impl`.
+    bool use_impl_ = false; // `true` to `--impl`, `false` to `--skip-impl`.
+    // Test objects should respect the global filter. CPU prim_ref objects
+    // shouldn't as it affects correctness validation speed.
+    // Default is set to `true` for the cases when global is initialized. In
+    // such cases local is not initialized but always passed to `fetch_impl`,
+    // thus, to pick up values from global it should indicate the global is
+    // respected.
+    bool respect_global_filter_ = true;
 };
 
 extern impl_filter_t global_impl_filter;
