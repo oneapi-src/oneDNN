6f1a913b346c4d6c7e67104dcc9c3df4ea5ae5f9
Ryo Suzuki <ryo.suzuki@arm.com>
2025-02-18 15:42:53 +0000

    ci: aarch64: Initial nightly performance test
    Adding a comprehensive performance test to the nightly pipeline.
    For now, matmul, conv, eltwise and reorders are tested but more tests
    are to be added in the future.
    
    Other minor changes:
    - Fix benchdnn comparison script
    - Update precommit performance tests list
    - Add fast math tests

---
+++ .github/automation/performance/bench_fast_math_performance.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+# *******************************************************************************
+# Copyright 2025 Arm Limited and affiliates.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# *******************************************************************************
+
+# Usage: bash bench_pr_performance.sh {baseline_benchdnn_executable} {benchdnn_executable} {baseline_results_file} {new_results_file}
+
+IFS=$'\n' # Prevents shuffling from using spaces as delimiters
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+TESTS=(
+        "ONEDNN_DEFAULT_FPMATH_MODE=BF16 $1 --matmul --mode=P --perf-template=%prb%,%-time% --batch=${SCRIPT_DIR}/inputs/matmul >> $3"
+        "ONEDNN_DEFAULT_FPMATH_MODE=BF16 $2 --matmul --mode=P --perf-template=%prb%,%-time% --batch=${SCRIPT_DIR}/inputs/matmul >> $4"
+        "ONEDNN_DEFAULT_FPMATH_MODE=BF16 $1 --conv --mode=P --perf-template=%prb%,%-time% --batch=${SCRIPT_DIR}/inputs/conv >> $3"
+        "ONEDNN_DEFAULT_FPMATH_MODE=BF16 $2 --conv --mode=P --perf-template=%prb%,%-time% --batch=${SCRIPT_DIR}/inputs/conv >> $4"
+    )
+
+for i in {1..5}
+do
+    echo "Testing loop ${i} / 5..."
+
+    TESTS=( $(shuf -e "${TESTS[@]}") )
+
+    for test in "${TESTS[@]}"
+    do
+        echo "Starting ${test}"
+        SECONDS=0
+        eval $test
+        duration=$SECONDS
+        echo "Completed in $((duration / 60)):$((duration % 60))"
+    done
+done
