b2335e73ad1c0507ec3b144310081267f3bf5713
syurkevi <stephen.yurkevitch@intel.com>
2025-02-18 17:29:09 -0800

    xe: concat: refactor gen9 kernel to use ocl_io without punning

--- src/gpu/intel/ocl/ocl_io.h
+++ src/gpu/intel/ocl/ocl_io.h
@@ -29,6 +29,11 @@
 #define BLOCK_READ_FUNC_4 intel_sub_group_block_read
 #define BLOCK_READ_FUNC_8 intel_sub_group_block_read_ul
 
+#define BLOCK_WRITE_FUNC_1 intel_sub_group_block_write_uc
+#define BLOCK_WRITE_FUNC_2 intel_sub_group_block_write_us
+#define BLOCK_WRITE_FUNC_4 intel_sub_group_block_write
+#define BLOCK_WRITE_FUNC_8 intel_sub_group_block_write_ul
+
 #define BLOCK_DT_0 uchar
 #define BLOCK_DT_1 uchar
 #define BLOCK_DT_2 ushort
@@ -52,11 +57,94 @@
 #define SIZE(dt) CONCAT2(SIZE_, dt)
 #define BLOCK_DT(dt) CONCAT2(BLOCK_DT_, SIZE(dt))
 #define BLOCK_READ_FUNC(dt) CONCAT2(BLOCK_READ_FUNC_, SIZE(dt))
+#define BLOCK_WRITE_FUNC(dt) CONCAT2(BLOCK_WRITE_FUNC_, SIZE(dt))
+
+#define BLOCK_DT_N(dt, n) CONCAT2(BLOCK_DT(dt), n)
+#define BLOCK_READ_FUNC_N(dt, n) CONCAT2(BLOCK_READ_FUNC(dt), n)
+#define BLOCK_WRITE_FUNC_N(dt, n) CONCAT2(BLOCK_WRITE_FUNC(dt), n)
+
+#define DECLARE_AS_BLOCK(t) \
+    BLOCK_DT(t) __attribute__((overloadable)) as_block_data(t a) { \
+        return CONCAT2(as_, BLOCK_DT(t))(a); \
+    }
+
+#define DECLARE_AS_STRUCT_BLOCK(t) \
+    BLOCK_DT(t) __attribute__((overloadable)) as_block_data(t a) { \
+        return CONCAT2(as_, BLOCK_DT(t))(a.data); \
+    }
+
+DECLARE_AS_BLOCK(char)
+DECLARE_AS_BLOCK(uchar)
+DECLARE_AS_STRUCT_BLOCK(f8_e5m2)
+DECLARE_AS_STRUCT_BLOCK(f8_e4m3)
+DECLARE_AS_STRUCT_BLOCK(e8m0)
+DECLARE_AS_STRUCT_BLOCK(bf16)
+DECLARE_AS_BLOCK(half)
+DECLARE_AS_BLOCK(int)
+DECLARE_AS_BLOCK(float)
+DECLARE_AS_BLOCK(double)
+
+#undef DECLARE_AS_BLOCK
+#undef DECLARE_AS_STRUCT_BLOCK
 
 #define DEF_load(dst_dt, src_dt) \
     void __attribute__((overloadable)) \
-            load(__private dst_dt *dst, __global src_dt *val) { \
+            load(__private dst_dt *dst, __global const src_dt *val) { \
         *dst = CONCAT2(into_, dst_dt)(*val); \
+    } \
+    dst_dt __attribute__((overloadable, warn_unused_result)) \
+            load(dst_dt dst, __global const src_dt *val) { \
+        return CONCAT2(into_, dst_dt)(*val); \
+    } \
+    __attribute__((overloadable)) void block_load( \
+            __private dst_dt *dst, __global const src_dt *src, int n) { \
+        __attribute__((opencl_unroll_hint)) while (n >= 8) { \
+            __global BLOCK_DT(src_dt) *data \
+                    = (__global BLOCK_DT(src_dt) *)(src); \
+            BLOCK_DT_N(src_dt, 8) \
+            block_val = BLOCK_READ_FUNC_N(src_dt, 8)(data); \
+            for (int i = 0; i < 8; i++) { \
+                src_dt src_val = CONCAT2(as_, src_dt)(block_val[i]); \
+                dst[i] = CONCAT2(into_, dst_dt)(src_val); \
+            } \
+            dst += 8; \
+            src += 8 * get_max_sub_group_size(); \
+            n -= 8; \
+        } \
+        if (n >= 4) { \
+            __global BLOCK_DT(src_dt) *data \
+                    = (__global BLOCK_DT(src_dt) *)(src); \
+            BLOCK_DT_N(src_dt, 4) \
+            block_val = BLOCK_READ_FUNC_N(src_dt, 4)(data); \
+            for (int i = 0; i < 4; i++) { \
+                src_dt src_val = CONCAT2(as_, src_dt)(block_val[i]); \
+                dst[i] = CONCAT2(into_, dst_dt)(src_val); \
+            } \
+            dst += 4; \
+            src += 4 * get_max_sub_group_size(); \
+            n -= 4; \
+        } \
+        if (n >= 2) { \
+            __global BLOCK_DT(src_dt) *data \
+                    = (__global BLOCK_DT(src_dt) *)(src); \
+            BLOCK_DT_N(src_dt, 2) \
+            block_val = BLOCK_READ_FUNC_N(src_dt, 2)(data); \
+            for (int i = 0; i < 2; i++) { \
+                src_dt src_val = CONCAT2(as_, src_dt)(block_val[i]); \
+                dst[i] = CONCAT2(into_, dst_dt)(src_val); \
+            } \
+            dst += 2; \
+            src += 2 * get_max_sub_group_size(); \
+            n -= 2; \
+        } \
+        if (n >= 1) { \
+            __global BLOCK_DT(src_dt) *data \
+                    = (__global BLOCK_DT(src_dt) *)(src); \
+            BLOCK_DT(src_dt) \
+            block_val = BLOCK_READ_FUNC(src_dt)(data); \
+            src_dt src_val = CONCAT2(as_, src_dt)(block_val); \
+            *dst = CONCAT2(into_, dst_dt)(src_val); \
+        } \
     } \
     __attribute__((overloadable)) void block_load( \
             __private dst_dt *dst, __global src_dt *src) { \
@@ -82,6 +170,56 @@
     void __attribute__((overloadable)) \
             write(__global dst_dt *dst, __private src_dt val) { \
         *dst = CONCAT2(into_, dst_dt)(val); \
+    } \
+    __attribute__((overloadable)) void block_write( \
+            __global dst_dt *dst, __private const src_dt *src, int n) { \
+        __attribute__((opencl_unroll_hint)) while (n >= 8) { \
+            BLOCK_DT_N(dst_dt, 8) block_val; \
+            for (int i = 0; i < 8; i++) { \
+                dst_dt val = CONCAT2(into_, dst_dt)(src[i]); \
+                block_val[i] = as_block_data(val); \
+            } \
+            __global BLOCK_DT(dst_dt) *data \
+                    = (__global BLOCK_DT(dst_dt) *)(dst); \
+            BLOCK_WRITE_FUNC_N(dst_dt, 8)(data, block_val); \
+            dst += 8 * get_max_sub_group_size(); \
+            src += 8; \
+            n -= 8; \
+        } \
+        if (n >= 4) { \
+            BLOCK_DT_N(dst_dt, 4) block_val; \
+            for (int i = 0; i < 4; i++) { \
+                dst_dt val = CONCAT2(into_, dst_dt)(src[i]); \
+                block_val[i] = as_block_data(val); \
+            } \
+            __global BLOCK_DT(dst_dt) *data \
+                    = (__global BLOCK_DT(dst_dt) *)(dst); \
+            BLOCK_WRITE_FUNC_N(dst_dt, 4)(data, block_val); \
+            dst += 4 * get_max_sub_group_size(); \
+            src += 4; \
+            n -= 4; \
+        } \
+        if (n >= 2) { \
+            BLOCK_DT_N(dst_dt, 2) block_val; \
+            for (int i = 0; i < 2; i++) { \
+                dst_dt val = CONCAT2(into_, dst_dt)(src[i]); \
+                block_val[i] = as_block_data(val); \
+            } \
+            __global BLOCK_DT(dst_dt) *data \
+                    = (__global BLOCK_DT(dst_dt) *)(dst); \
+            BLOCK_WRITE_FUNC_N(dst_dt, 2)(data, block_val); \
+            dst += 2 * get_max_sub_group_size(); \
+            src += 2; \
+            n -= 2; \
+        } \
+        if (n >= 1) { \
+            BLOCK_DT(dst_dt) block_val; \
+            dst_dt val = CONCAT2(into_, dst_dt)(*src); \
+            block_val = as_block_data(val); \
+            __global BLOCK_DT(dst_dt) *data \
+                    = (__global BLOCK_DT(dst_dt) *)(dst); \
+            BLOCK_WRITE_FUNC(dst_dt)(data, block_val); \
+        } \
     }
 
 // Loads
