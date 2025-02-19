bf105741429782492273f58cdce43d92150ab4ef
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2025-02-19 15:07:57 -0800

    doc: fix doxygen file identification

--- examples/example_utils.hpp
+++ examples/example_utils.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2019-2023 Intel Corporation
+* Copyright 2019-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -13,6 +13,9 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *******************************************************************************/
+
+/// @file
+/// Examples C++ Utility Functions
 
 #ifndef EXAMPLE_UTILS_HPP
 #define EXAMPLE_UTILS_HPP
