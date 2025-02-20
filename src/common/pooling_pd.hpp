9cbdb38495c1f7a226f7225acb42bf2cae91f07f
Dmitrii Zarukin <dmitry.zarukin@intel.com>
2025-02-20 13:34:28 -0800

    common: arg_usage: don't do fallback on known arguments

--- src/common/pooling_pd.hpp
+++ src/common/pooling_pd.hpp
@@ -184,8 +184,9 @@ struct pooling_fwd_pd_t : public pooling_pd_t {
 
         if (arg == DNNL_ARG_DST) return arg_usage_t::output;
 
-        if (arg == DNNL_ARG_WORKSPACE && (!types::is_zero_md(workspace_md())))
-            return arg_usage_t::output;
+        if (arg == DNNL_ARG_WORKSPACE)
+            return !types::is_zero_md(workspace_md()) ? arg_usage_t::output
+                                                      : arg_usage_t::unused;
 
         return primitive_desc_t::arg_usage(arg);
     }
@@ -254,8 +255,9 @@ struct pooling_bwd_pd_t : public pooling_pd_t {
 
         if (arg == DNNL_ARG_DIFF_SRC) return arg_usage_t::output;
 
-        if (arg == DNNL_ARG_WORKSPACE && (!types::is_zero_md(workspace_md())))
-            return arg_usage_t::input;
+        if (arg == DNNL_ARG_WORKSPACE)
+            return !types::is_zero_md(workspace_md()) ? arg_usage_t::input
+                                                      : arg_usage_t::unused;
 
         return primitive_desc_t::arg_usage(arg);
     }
