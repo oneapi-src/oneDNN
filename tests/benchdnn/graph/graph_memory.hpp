a9ef36c9db7a61d4bbfb306abb1359d758cd2de3
Lv, Tao A <tao.a.lv@intel.com>
2025-02-24 02:14:05 -0800

    benchdnn: graph: use = default to define a trivial destructor

--- tests/benchdnn/graph/graph_memory.hpp
+++ tests/benchdnn/graph/graph_memory.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2023-2024 Intel Corporation
+* Copyright 2023-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -112,7 +112,7 @@ private:
     graph_memory_req_args_t() {
         req_ = std::vector<std::vector<size_t>>(2, std::vector<size_t>(3, 0));
     }
-    ~graph_memory_req_args_t() {};
+    ~graph_memory_req_args_t() = default;
     BENCHDNN_DISALLOW_COPY_AND_ASSIGN(graph_memory_req_args_t);
 
     // The detailed memory size requrest for specific path and devices.
