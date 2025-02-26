oneapi-src/oneDNN

commit 9c37e1325e8d7ea4910450f502aca43c8cc60d1d

xe: jit: gemm: remove common FlatCatalog merge conflicts

src/gpu/intel/jit/gemm/include/kernel_catalog.hpp
@@ -202,10 +202,6 @@ struct Entry {
 };
 
 struct Catalog {
-    static constexpr int currentVersion() { return 1; }
-
-    int version             DEFAULT(currentVersion());
-    uint64_t revision       DEFAULT(0);
     int entryCount          DEFAULT(0);
 
     const Entry *entries;
@@ -213,16 +209,24 @@ struct Catalog {
 
 template <size_t n>
 struct FlatCatalog {
-    int version;
-    uint64_t revision;
-    int entryCount;
+    FlatCatalog(Entry (&&a)[n]) {
+        for(size_t i = 0; i < n; i++) {
+            entries[i] = std::move(a[i]);
+        }
+    }
+
     Entry entries[n];
 
     /* implicit */ operator Catalog() const {
-        Catalog catalog = {version, revision, entryCount, &entries[0]};
+        Catalog catalog = {n, &entries[0]};
         return catalog;
     }
 };
+
+template <std::size_t n>
+constexpr FlatCatalog<n> toFlatCatalog(Entry (&&a)[n]) {
+    return FlatCatalog<n>(std::move(a));
+}
 
 } /* namespace kcatalog */
 

src/gpu/intel/jit/gemm/selector/db/kernel.db
@@ -15,8 +15,7 @@
 *******************************************************************************/
 
 /*@kcatalog@*/
-kcatalog::FlatCatalog<1148> _CATALOG_
-{1, 8381, 1148, {
+auto _CATALOG_ = kcatalog::toFlatCatalog({
 {{'9', "gemm", {"B", "B", "S"}, {"N", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "ab2x2 as8x2 ab l4 acb", {8, (LoopType) 255, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {32, 16, 16}, {2, 8, 1}, 1, (WGType) 0, 1, 0, 0, {2, 2, 4}, {true, true, true}}, {'W', 1, {512}}},
 {{'9', "gemm", {"B", "B", "S"}, {"N", "T", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "ab4 ab4x2 ab l4 acb nmk", {8, (LoopType) 255, 128, {(LoopType) 1, (LoopType) 0, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {32, 16, 8}, {8, 2, 1}, 1, (WGType) 0, 1, 0, 0, {2, 2, 4}, {true, true, true}}, {'W', 1, {512}}},
 {{'9', "gemm", {"B", "B", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "as8 as16 ab l4 acb", {8, (LoopType) 255, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {4096, 4096, 1024}, {4096, 4096, 1024}, {16, 16, 16}, {2, 8, 1}, 1, (WGType) 0, 1, 0, 0, {2, 2, 4}, {true, true, true}}, {'W', 1, {256}}},
@@ -1165,4 +1164,4 @@ kcatalog::FlatCatalog<1148> _CATALOG_
 {{'G', "gemm", {"[FO]", "O", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {16, 16, 1}, "IAB"}, "at32+m32@64 am32+m64@64 aB wg 4x8 ca3 ks32 xaf st vav hi pt sr br bk0 sm sn nb 4x0 grf256 sys acb cr16", {16, (LoopType) 255, 256, {(LoopType) 208, (LoopType) 255, (LoopType) 255}, {1048576, 524288, 16777216}, {1048576, 524288, 16777216}, {64, 32, 32}, {4, 8, 1}, 1, (WGType) 1, 257, 24576, 0, {16, 16, 4}, {true, true, true}}, {'W', 1, {1e+06}}},
 {{'G', "gemm", {"[FO]", "O", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, 8, -1}, {-1, -1, -1}, {-1, 8, -1}, {16, 16, 1}, "ABIpq"}, "at32 am128 aB wg 2x1x8 ikr xaf st acb hi pt ar sb128 bk0 bm0 nmk sys", {16, (LoopType) 255, 128, {(LoopType) 209, (LoopType) 255, (LoopType) 2}, {16777216, 131072, 16777216}, {262144, 131072, 16777216}, {16, 8, 128}, {2, 1, 8}, 1, (WGType) 0, 4357, 0, 1024, {16, 16, 4}, {true, true, true}}, {'W', 1, {128}}},
 {{'G', "gemm", {"[FO]", "O", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, 9, -1}, {-1, 32, -1}, {-1, 9, -1}, {-1, 32, -1}, {16, 16, 1}, "ABIpq"}, "at32+m128@80 am32+m128@80 aB wg 8x1x4 ikr wx2 xaf acb hi pt ar sb128 bk0 sm sn bm0 nmk sys", {16, (LoopType) 255, 128, {(LoopType) 209, (LoopType) 255, (LoopType) 2}, {16777216, 262144, 16777216}, {262144, 262144, 16777216}, {16, 16, 128}, {8, 1, 4}, 2, (WGType) 1, 4357, 0, 8192, {16, 16, 4}, {true, true, true}}, {'W', 1, {256}}}
-}}
+});

src/gpu/intel/jit/gemm/selector/db/ukernel_lmr.db
@@ -15,8 +15,7 @@
 *******************************************************************************/
 
 /*@kcatalog@*/
-kcatalog::FlatCatalog<2> _CATALOG_
-{1, 4, 2, {
+auto  _CATALOG_ = kcatalog::toFlatCatalog({
 {{'F', "ugemm", {"H", "H", "S"}, {"A2#16,64", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 8, 1}, "xzIB"}, "lB16x2 am32+m32@64 rB sys wg 2x4 vav grf256", {16, (LoopType) 255, 256, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {32, 32, 32}, {2, 4, 1}, 1, (WGType) 1, 257, 0, 0, {128, 8, 4}, {false, true, false}}, {'W', 1, {1024}}},
 {{'F', "ugemm", {"H", "H", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {4, 8, 1}, "xzIB"}, "lS16x2 am32+m32@64 rB sys wg 2x4 vav grf256", {16, (LoopType) 255, 256, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 1024}, {8192, 8192, 1024}, {32, 32, 32}, {2, 4, 1}, 1, (WGType) 1, 257, 0, 0, {4, 8, 4}, {false, true, false}}, {'W', 1, {1024}}}
-}}
+});

src/gpu/intel/jit/gemm/selector/db/ukernel_mlr.db
@@ -15,8 +15,7 @@
 *******************************************************************************/
 
 /*@kcatalog@*/
-kcatalog::FlatCatalog<11> _CATALOG_
-{1, 18, 11, {
+auto _CATALOG_ = kcatalog::toFlatCatalog({
 {{'E', "ugemm", {"H", "H", "S"}, {"N", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {4, 4, 1}, "yI"}, "aB16x2 lB32 rB sys dw wg 4x8 vav", {8, (LoopType) 0, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {16, 4, 32}, {4, 8, 1}, 1, (WGType) 0, 257, 0, 0, {4, 4, 4}, {true, false, true}}, {'W', 1, {64}}},
 {{'F', "ugemm", {"H", "H", "S"}, {"N", "B16", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {8, 1, 1}, "yzIA"}, "av16x2+m16@32 lB16x2 rB sys wg 2x4 vav", {16, (LoopType) 255, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {32, 16, 16}, {2, 4, 1}, 1, (WGType) 1, 257, 0, 0, {8, 128, 4}, {true, false, false}}, {'W', 1, {512}}},
 {{'F', "ugemm", {"H", "H", "S"}, {"N", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {8, 4, 1}, "yIA"}, "av16x2+m16@32 lB32x2 rB sys wg 4x8 vav", {16, (LoopType) 255, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {16, 8, 32}, {4, 8, 1}, 1, (WGType) 1, 257, 0, 0, {8, 4, 4}, {true, false, true}}, {'W', 1, {128}}},
@@ -28,4 +27,4 @@ kcatalog::FlatCatalog<11> _CATALOG_
 {{'F', "ugemm", {"H", "H", "S"}, {"N", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 4, 1}, "#AIy"}, "aB16x2+m32@32 lB32 rB sys wg 2x16 vav l4 grf256", {16, (LoopType) 255, 256, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {32, 8, 32}, {2, 16, 1}, 1, (WGType) 1, 257, 0, 0, {2, 4, 4}, {true, false, true}}, {'W', 1, {256}}},
 {{'F', "ugemm", {"H", "H", "S"}, {"N", "T", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {8, 4, 1}, "yzIA"}, "av16x2+m16@32 lS16x2 rB sys wg 2x4 vav", {16, (LoopType) 255, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {32, 16, 16}, {2, 4, 1}, 1, (WGType) 1, 257, 0, 0, {8, 4, 4}, {true, false, false}}, {'W', 1, {512}}},
 {{'F', "ugemm", {"H", "H", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {8, 4, 1}, "yzIA"}, "at16x2+m16@32 lB32 rB sys wg 2x4 vav grf256", {16, (LoopType) 255, 256, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 1024}, {8192, 8192, 1024}, {32, 32, 32}, {2, 4, 1}, 1, (WGType) 1, 257, 0, 0, {8, 4, 4}, {true, false, false}}, {'W', 1, {1024}}}
-}}
+});

src/gpu/intel/jit/gemm/selector/db/ukernel_mmr.db
@@ -15,8 +15,7 @@
 *******************************************************************************/
 
 /*@kcatalog@*/
-kcatalog::FlatCatalog<598> _CATALOG_
-{1, 10556, 598, {
+auto _CATALOG_ = kcatalog::toFlatCatalog({
 {{'C', "ugemm", {"B", "B", "S"}, {"N", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "ab4 as8 rb l4 ca1 wg 2x8 int", {8, (LoopType) 0, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {32, 16, 8}, {2, 8, 1}, 1, (WGType) 1, 1, 2048, 0, {2, 2, 4}, {true, true, false}}, {'W', 1, {512}}},
 {{'C', "ugemm", {"B", "B", "S"}, {"N", "T", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "ab2 ab8 rb l4 cab1 wg 4x4 int", {8, (LoopType) 0, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {32, 16, 8}, {4, 4, 1}, 1, (WGType) 1, 1, 6144, 0, {2, 2, 4}, {true, true, false}}, {'W', 1, {512}}},
 {{'C', "ugemm", {"B", "B", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "as8x2 ab16/8 rb l4 cb1 wg 8x2 vnc nmk", {8, (LoopType) 1, 128, {(LoopType) 1, (LoopType) 0, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {16, 16, 16}, {8, 2, 1}, 1, (WGType) 1, 1, 2048, 0, {2, 2, 4}, {true, true, false}}, {'W', 1, {256}}},
@@ -615,4 +614,4 @@ kcatalog::FlatCatalog<598> _CATALOG_
 {{'F', "ugemm", {"T", "T", "S"}, {"T", "N", "N"}}, {5, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, "#AI"}, "at16+m16@16 aB8x2 rB wg 4x2 cb4x2 ks16 xaf st vav di sm sn dm grf256 sys", {16, (LoopType) 255, 256, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 1024}, {8192, 8192, 1024}, {16, 32, 16}, {4, 2, 1}, 1, (WGType) 1, 257, 16384, 0, {4, 4, 4}, {true, true, false}}, {'E', 17, {1.8509e+06, -175907, -189196, 392631, 2.74432e+06, 2.18726e+06, 1.12474, 1.04753, 0.43698, 1.42448, 0.0254728, 0.0206988, 0.0107976, 0.953996, 1.38293, 0.912126, 8.89018e-12}}},
 {{'F', "ugemm", {"T", "T", "S"}, {"T", "N", "N"}}, {5, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, "#AI"}, "at8x2+m16@24 aB16 rB wg 4x2 cb4x2 ks32 af vav di sm sn dm grf256 sys", {16, (LoopType) 255, 256, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 1024}, {8192, 8192, 1024}, {16, 16, 32}, {4, 2, 1}, 1, (WGType) 1, 257, 16384, 0, {4, 4, 4}, {true, true, false}}, {'E', 17, {1.64426e+06, -145120, -138637, 309031, 2.31014e+06, 2.00704e+06, 0.918257, 1.44548, 0.262092, 1.02772, 0.0377766, 0.0386483, 0.015333, 0.989041, 1.28006, -1.30164, 1.0788e-10}}},
 {{'F', "ugemm", {"T", "T", "S"}, {"T", "N", "N"}}, {5, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, "#AI"}, "at16+m16@24 aB16x2 rB wg 2x2 cb4x2 ks16 xaf vav di sm sn dm grf256 sys", {16, (LoopType) 255, 256, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 1024}, {8192, 8192, 1024}, {16, 16, 16}, {2, 2, 1}, 1, (WGType) 1, 257, 8192, 0, {4, 4, 4}, {true, true, false}}, {'E', 17, {1.65746e+06, -77290.3, -69015.6, 169032, 2.75251e+06, 1.85958e+06, 0.958255, 0.997806, 0.100098, 0.957911, 0.0468295, 0.0427724, 0.0158824, 1, 1.31247, 0.973533, 8.29974e-12}}}
-}}
+});

commit 6624db92ff14178b610c253646a309ac10f72bb1

xe: jit: gemm: improve catalog initialization
this PR enables better generation of static catalog data. For some reason the
change in 9c37e1325e8d7ea4910450f502aca43c8cc60d1d caused flickering in an
application. While this flickering is unlikely to be related to oneDNN, this PR
enables better code generation as a workaround for the issue.

src/gpu/intel/jit/gemm/gen_gemm_kernel_db.cpp
@@ -26,9 +26,8 @@ namespace jit {
 #include "selector/db/kernel.db"
 #undef _CATALOG_
 
-const kcatalog::Catalog &catalog() {
-    static const kcatalog::Catalog c = gemm_catalog;
-    return c;
+kcatalog::Catalog catalog() {
+    return gemm_catalog;
 };
 
 } // namespace jit

src/gpu/intel/jit/gemm/gen_gemm_kernel_db.hpp
@@ -25,7 +25,7 @@ namespace gpu {
 namespace intel {
 namespace jit {
 
-const kcatalog::Catalog &catalog();
+kcatalog::Catalog catalog();
 
 }
 } // namespace intel

src/gpu/intel/jit/gemm/generator/microkernel_provider.cpp
@@ -50,8 +50,6 @@ Package selectGEMMMicrokernel(GEMMProtocol protocol, HWInformation hwInfo, SizeP
                               const GEMMProblem &problem_, const std::vector<StrategyRequirement> &reqs_,
                               void (*strategyAdjuster)(GEMMStrategy &strategy))
 {
-    kcatalog::Catalog catalog;
-
     bool localA = protocol.options().localA;
     bool localB = protocol.options().localB;
     bool beta1 = protocol.options().addToC;
@@ -118,12 +116,14 @@ Package selectGEMMMicrokernel(GEMMProtocol protocol, HWInformation hwInfo, SizeP
     if (localA && localB)
         stub("Unsupported protocol");
 
-    if (localA)
-        catalog = CatalogLMR;
-    else if (localB)
-        catalog = CatalogMLR;
-    else
-        catalog = CatalogMMR;
+    kcatalog::Catalog catalog = [&] () {
+        if (localA)
+            return kcatalog::Catalog(CatalogLMR);
+        else if (localB)
+            return kcatalog::Catalog(CatalogMLR);
+        else
+            return kcatalog::Catalog(CatalogMMR);
+    }();
 
     /* Call kernel selector */
     EvaluateAuxOutput auxParams;

src/gpu/intel/jit/gemm/include/kernel_catalog.hpp
@@ -19,7 +19,8 @@
 
 #include <string>
 #include <tuple>
-#include <vector>
+#include <array>
+#include <utility>
 
 #include "config.hpp"
 #include "driver_info.hpp"
@@ -203,31 +204,38 @@ struct Entry {
 };
 
 struct Catalog {
+    template <std::size_t N>
+    Catalog(const std::array<Entry, N> &a): entryCount(N), entries(a.data()) {}
     int entryCount          DEFAULT(0);
 
     const Entry *entries;
 };
 
-template <size_t n>
-struct FlatCatalog {
-    FlatCatalog(Entry (&&a)[n]) {
-        for(size_t i = 0; i < n; i++) {
-            entries[i] = std::move(a[i]);
-        }
-    }
-
-    Entry entries[n];
-
-    /* implicit */ operator Catalog() const {
-        Catalog catalog = {n, &entries[0]};
-        return catalog;
-    }
-};
-
-template <std::size_t n>
-constexpr FlatCatalog<n> toFlatCatalog(Entry (&&a)[n]) {
-    return FlatCatalog<n>(std::move(a));
+#if __cplusplus >= 202002L && __cpp_lib_to_array >= 201907L
+template <std::size_t N>
+constexpr std::array<Entry, N> toArray(Entry (&&a)[N]) {
+    return std::to_array(std::move(a));
 }
+#elif __cplusplus >= 201402L && __cpp_lib_integer_sequence >= 201304L
+template <std::size_t N, std::size_t... I>
+constexpr std::array<Entry, N>
+toArrayImpl(Entry (&&a)[N], std::index_sequence<I...>) {
+    return {{std::move(a[I])...}};
+}
+template <std::size_t N>
+constexpr std::array<Entry, N> toArray(Entry (&&a)[N]) {
+    return toArrayImpl(std::move(a), std::make_index_sequence<N>{});
+}
+#else
+template <std::size_t N>
+std::array<Entry, N> toArray(Entry (&&a)[N]) {
+    std::array<Entry, N> ret;
+    for(size_t i = 0; i < N; i++) {
+        ret[i] = std::move(a[i]);
+    }
+    return ret;
+}
+#endif
 
 } /* namespace kcatalog */
 

src/gpu/intel/jit/gemm/selector/db/kernel.db
@@ -15,7 +15,7 @@
 *******************************************************************************/
 
 /*@kcatalog@*/
-auto _CATALOG_ = kcatalog::toFlatCatalog({
+auto _CATALOG_ = kcatalog::toArray({
 {{'C', "gemm", {"B", "B", "S"}, {"N", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "ab4 as8 ab l4 ca1 wg 2x8 int sr", {8, (LoopType) 0, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {32, 16, 8}, {2, 8, 1}, 1, (WGType) 1, 1, 2048, 0, {2, 2, 4}, {true, true, true}}, {'W', 1, {512}}},
 {{'C', "gemm", {"B", "B", "S"}, {"N", "T", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "ab2 ab8 ab l4 cab1 wg 4x4 int sr", {8, (LoopType) 0, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {32, 16, 8}, {4, 4, 1}, 1, (WGType) 1, 1, 6144, 0, {2, 2, 4}, {true, true, true}}, {'W', 1, {512}}},
 {{'C', "gemm", {"B", "B", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "as8x2 ab16 ab l4 cb1 wg 8x2 vnc nmk sr", {8, (LoopType) 1, 128, {(LoopType) 1, (LoopType) 0, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {16, 16, 16}, {8, 2, 1}, 1, (WGType) 1, 1, 2048, 0, {2, 2, 4}, {true, true, true}}, {'W', 1, {256}}},

src/gpu/intel/jit/gemm/selector/db/ukernel_lmr.db
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2024 Intel Corporation
+* Copyright 2024-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
 *******************************************************************************/
 
 /*@kcatalog@*/
-auto _CATALOG_ = kcatalog::toFlatCatalog({
+auto _CATALOG_ = kcatalog::toArray({
 {{'F', "ugemm", {"H", "H", "S"}, {"A2#16,64", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 8, 1}, "xzIB"}, "lB16x2 am32+m32@64 rB sys wg 2x4 vav grf256", {16, (LoopType) 255, 256, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {32, 32, 32}, {2, 4, 1}, 1, (WGType) 1, 257, 0, 0, {128, 8, 4}, {false, true, false}}, {'W', 1, {1024}}},
 {{'F', "ugemm", {"H", "H", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {4, 8, 1}, "xzIB"}, "lS16x2 am32+m32@64 rB sys wg 2x4 vav grf256", {16, (LoopType) 255, 256, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 1024}, {8192, 8192, 1024}, {32, 32, 32}, {2, 4, 1}, 1, (WGType) 1, 257, 0, 0, {4, 8, 4}, {false, true, false}}, {'W', 1, {1024}}}
 });

src/gpu/intel/jit/gemm/selector/db/ukernel_mlr.db
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2024 Intel Corporation
+* Copyright 2024-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
 *******************************************************************************/
 
 /*@kcatalog@*/
-auto _CATALOG_ = kcatalog::toFlatCatalog({
+auto _CATALOG_ = kcatalog::toArray({
 {{'E', "ugemm", {"H", "H", "S"}, {"N", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {4, 4, 1}, "yI"}, "aB16x2 lB32 rB sys dw wg 4x8 vav", {8, (LoopType) 0, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {16, 4, 32}, {4, 8, 1}, 1, (WGType) 0, 257, 0, 0, {4, 4, 4}, {true, false, true}}, {'W', 1, {64}}},
 {{'F', "ugemm", {"H", "H", "S"}, {"N", "B16", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {8, 1, 1}, "yzIA"}, "av16x2+m16@32 lB16x2 rB sys wg 2x4 vav", {16, (LoopType) 255, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {32, 16, 16}, {2, 4, 1}, 1, (WGType) 1, 257, 0, 0, {8, 128, 4}, {true, false, false}}, {'W', 1, {512}}},
 {{'F', "ugemm", {"H", "H", "S"}, {"N", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {8, 4, 1}, "yIA"}, "av16x2+m16@32 lB32x2 rB sys wg 4x8 vav", {16, (LoopType) 255, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {8192, 8192, 2048}, {8192, 8192, 2048}, {16, 8, 32}, {4, 8, 1}, 1, (WGType) 1, 257, 0, 0, {8, 4, 4}, {true, false, true}}, {'W', 1, {128}}},

src/gpu/intel/jit/gemm/selector/db/ukernel_mmr.db
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright 2022-2024 Intel Corporation
+* Copyright 2022-2025 Intel Corporation
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
 *******************************************************************************/
 
 /*@kcatalog@*/
-auto _CATALOG_ = kcatalog::toFlatCatalog({
+auto _CATALOG_ = kcatalog::toArray({
 {{'C', "ugemm", {"B", "B", "S"}, {"N", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "ab4 as8 rb l4 ca1 wg 2x8 int", {8, (LoopType) 0, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {32, 16, 8}, {2, 8, 1}, 1, (WGType) 1, 1, 2048, 0, {2, 2, 4}, {true, true, false}}, {'W', 1, {512}}},
 {{'C', "ugemm", {"B", "B", "S"}, {"N", "T", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "ab2 ab8 rb l4 cab1 wg 4x4 int", {8, (LoopType) 0, 128, {(LoopType) 0, (LoopType) 1, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {32, 16, 8}, {4, 4, 1}, 1, (WGType) 1, 1, 6144, 0, {2, 2, 4}, {true, true, false}}, {'W', 1, {512}}},
 {{'C', "ugemm", {"B", "B", "S"}, {"T", "N", "N"}}, {-1, -1, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}, {1, 1, 1}, ""}, "as8x2 ab16/8 rb l4 cb1 wg 8x2 vnc nmk", {8, (LoopType) 1, 128, {(LoopType) 1, (LoopType) 0, (LoopType) 255}, {4096, 4096, 2048}, {4096, 4096, 2048}, {16, 16, 16}, {8, 2, 1}, 1, (WGType) 1, 1, 2048, 0, {2, 2, 4}, {true, true, false}}, {'W', 1, {256}}},