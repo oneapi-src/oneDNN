9132b4a1fae2da73f746a7e241e2f8f55473b7d4
Chereshnev, Eugene <eugene.chereshnev@intel.com>
2025-02-28 12:45:20 -0800

    xe, common: conv: exclude grouped scales from check as not supported

--- src/gpu/intel/jit/conv/config.cpp
+++ src/gpu/intel/jit/conv/config.cpp
@@ -978,7 +978,7 @@ bool post_ops_ok(const conv_problem_t &prb, const hw_t &hw) {
     auto attr_skip_mask = sm::fpmath_mode | sm::accumulation_mode;
     if (prb.is_fwd || prb.is_bwd_d) {
         attr_skip_mask |= sm::post_ops | sm::sum_dt | sm::zero_points_data_type
-                | sm::rounding_mode | sm::scales_groups | sm::scales_data_type;
+                | sm::rounding_mode | sm::scales_data_type;
         if (!attr->has_default_values(attr_skip_mask)) return false;
     } else {
         if (!attr->has_default_values(attr_skip_mask)) return false;
