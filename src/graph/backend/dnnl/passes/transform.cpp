58d528e9e38a55fd033135aef43445b8a518e357
Gu, Yonghao <yonghao.gu@intel.com>
2025-03-10 14:19:19 +0000

    graph: backend: dnnl:  fix the fuse_src_transpose_to_matmul pass

--- src/graph/backend/dnnl/passes/transform.cpp
+++ src/graph/backend/dnnl/passes/transform.cpp
@@ -3796,8 +3796,22 @@ impl::status_t fuse_src_transpose_to_matmul(std::shared_ptr<subgraph_t> &sg) {
             break;
         }
 
-        std::vector<int> axes = dnnl_impl::utils::fmap(order,
-                [](int64_t index) { return static_cast<int32_t>(index); });
+        /// The order in spec op is used as:
+        /// for (i = 0; i < ndims(); i++)
+        ///     new_shape[i] = org_shape[order[i]];
+        ///
+        /// The axes for permute_axes function is used as:
+        /// for (i = 0; i < ndims(); i++)
+        ///     new_shape[axes[i]] = org_shape[i];
+        ///
+        /// So, we need to convert the order to axes
+        std::vector<int> axes(order.size(), -1);
+        for (size_t i = 0; i < order.size(); i++) {
+            size_t new_shape_idx = i;
+            size_t org_shape_idx = order[i];
+            axes[org_shape_idx] = static_cast<int>(new_shape_idx);
+        }
+
         // calculate the expected transposed layout by permuting the md
         auto expected_stride = get_dense_strides(ltw(in_lt).vdims());
         auto &consumer = transpose_op->get_output_value(0)
